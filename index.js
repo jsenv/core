c8a30216f5ac2f878e95105f68499e39e7169f38
Damien Maillard <damien.maillard@dailymotion.com>
2018-07-20 17:33:15 +0200

    prepare exporting executeOnNode

--- index.js
+++ index.js
@@ -4,3 +4,22 @@
 // pour le coverage
 // https://github.com/jsenv/core/blob/master/more/test/playground/coverage/run.js
 // https://github.com/jsenv/core/blob/master/more/to-externalize/module-cover/index.js
+
+import { openCompileServer } from "./src/openCompileServer/openCompileServer.js"
+import { openNodeClient } from "./src/openNodeClient/openNodeClient.js"
+
+export const createExecuteOnNode = ({ location }) => {
+  return openCompileServer({
+    rootLocation: location,
+  }).then((server) => {
+    const execute = ({ file }) => {
+      return openNodeClient({ server }).then((nodeClient) => {
+        nodeClient.execute({
+          file: `${server.compileURL}/${file}`,
+        })
+      })
+    }
+
+    return { execute }
+  })
+}
