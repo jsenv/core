ba70fbb80633f053ed1592bef2f7d524290b5fb3
Damien Maillard <damien.maillard@dailymotion.com>
2018-07-20 17:57:01 +0200

    introduce createModuleRunner

--- index.js
+++ index.js
@@ -7,19 +7,40 @@
 
 import { openCompileServer } from "./src/openCompileServer/openCompileServer.js"
 import { openNodeClient } from "./src/openNodeClient/openNodeClient.js"
+import { openChromiumClient } from "./src/openChromiumClient/openChromiumClient.js"
 
-export const createExecuteOnNode = ({ location }) => {
+export const createModuleRunner = ({ location }) => {
+  // if there is already a compileServer running for that location
+  // they will work as long as the code which created them run in the same terminal
+  // if two terminal spawns a server trying to compile a given project they will concurrently
+  // read/write filesystem.
+  // To fix that we could:
+  // - update enqueueCallByArgs so that, somehow, it can queue calls from different terminals
+  // - save somewhere the port used for that specific project and reuse when existing
+  // save used port is the easiest solution but we'll ignore this issue for now
+  // and assume noone will try to open two server for the same location
   return openCompileServer({
     rootLocation: location,
   }).then((server) => {
-    const execute = ({ file }) => {
+    const runInsideNode = ({ file }) => {
       return openNodeClient({ server }).then((nodeClient) => {
-        nodeClient.execute({
+        return nodeClient.execute({
           file: `${server.compileURL}/${file}`,
         })
       })
     }
 
-    return { execute }
+    const runInsideChromium = ({ file, headless = true }) => {
+      return openChromiumClient({
+        server,
+        headless,
+      }).then((chromiumClient) => {
+        return chromiumClient.execute({
+          file: `${server.compileURL}/${file}`,
+        })
+      })
+    }
+
+    return { runInsideNode, runInsideChromium }
   })
 }
