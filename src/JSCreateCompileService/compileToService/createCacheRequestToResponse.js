f8cc0fea7e112f500f60123f51e798216c0e5e7c
Damien Maillard <damien.maillard@dailymotion.com>
2018-10-18 11:17:18 +0200

    cache request does not vary per user-agent

--- src/JSCreateCompileService/compileToService/createCacheRequestToResponse.js
+++ src/JSCreateCompileService/compileToService/createCacheRequestToResponse.js
@@ -1,16 +1,1 @@
-import { isCacheRequest } from "../../compileToService/index.js"
-import { createRequestToFileResponse } from "../../createRequestToFileResponse/index.js"
-import { responseCompose } from "../../openServer/index.js"
-
-export const createCacheRequestToResponse = ({ root, cacheFolder, cacheIgnore, cacheStrategy }) => {
-  const cacheService = createRequestToFileResponse({ root, cacheIgnore, cacheStrategy })
-
-  return (request) => {
-    if (isCacheRequest(request, cacheFolder)) {
-      return cacheService(request).then((response) => {
-        return responseCompose({ headers: { vary: "User-Agent" } }, response)
-      })
-    }
-    return null
-  }
-}
+export { createRequestToFileResponse } from "../../createRequestToFileResponse/index.js"
