e8217d6353764401441faf0c16919a930518f8f5
Damien Maillard <damien.maillard@dailymotion.com>
2018-11-08 15:25:12 +0100

    update package json and exports

--- index.js
+++ index.js
@@ -5,9 +5,9 @@
 // https://github.com/jsenv/core/blob/master/more/test/playground/coverage/run.js
 // https://github.com/jsenv/core/blob/master/more/to-externalize/module-cover/index.js
 
-export { envDescriptionToCompileMap } from "./src/envDescriptionToCompileMap/index.js"
+export { pluginOptionMapToPluginMap } from "@dmail/project-structure-compile-babel"
 
-export { projectConfigToJsCompileService } from "./src/createJsCompileService.js"
+export { envDescriptionToCompileMap } from "./src/envDescriptionToCompileMap/index.js"
 
 export { open as serverCompileOpen } from "./src/server-compile/index.js"
 export { open as serverBrowserOpen } from "./src/server-browser/index.js"
