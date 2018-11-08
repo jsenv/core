db6f282583c70adc22ff3c1cf042b8eb8d59b27b
Damien Maillard <damien.maillard@dailymotion.com>
2018-11-08 19:24:21 +0100

    add commit to prepare fix for child hotreload/restart and all thoose things

--- src/createExecuteOnNode/client.js
+++ src/createExecuteOnNode/client.js
@@ -1,6 +1,9 @@
 import { createNodePlatform } from "../platform/index.js"
 import { uneval } from "@dmail/uneval"
 import { getCompileMapLocalURL } from "../compilePlatformAndSystem.js"
+import { createCancel } from "../cancel/index.js"
+
+const { cancellation, cancel } = createCancel()
 
 const sendToParent = (type, data) => {
   // process.send algorithm does not send non enumerable values
@@ -13,11 +16,9 @@ const sendToParent = (type, data) => {
   })
 }
 
-const listenParentOnce = (type, callback) => {
+const listenParent = (type, callback) => {
   const listener = (event) => {
     if (event.type === type) {
-      // why do we want to let child process die ?
-      // process.removeListener("message", listener)
       callback(eval(`(${event.data})`))
     }
   }
@@ -33,33 +34,47 @@ process.on("unhandledRejection", (error) => {
   throw error
 })
 
+// keep checking how https://github.com/dmail/dev-server/commit/a971e5dde3dd275ffa0c5a90220b3d6b514a7461
+// impacted hotreload and stufff
+// we must move again hotreloading here instead of parent
 process.on("SIGINT", () => {
-  if (process.listeners("SIGINT").length === 1) {
-    process.exit(0)
-  }
-  // otherwise let any custom "SIGINT" listener do exit
+  // cancel will remove listener to process.on('message')
+  // which is sufficient to let child process die
+  // assuming nothing else keeps it alive
+  cancel("process interrupt").then(() => {
+    if (process.listeners("SIGINT").length === 1) {
+      // ensure child exits in case some code forgot
+      // to register 'SIGINT' and keeps process alive
+      process.exit(0)
+    }
+  })
 })
 
-listenParentOnce("exit-please", () => {
-  process.emit("SIGINT")
-})
+cancellation.register(
+  listenParent("exit-please", () => {
+    process.emit("SIGINT")
+  }),
+)
 
-listenParentOnce(
-  "execute",
-  ({ localRoot, remoteRoot, compileInto, file, instrument, setup, teardown }) => {
-    const compileMapLocalURL = getCompileMapLocalURL({ localRoot, compileInto })
-    // eslint-disable-next-line import/no-dynamic-require
-    const compileMap = require(compileMapLocalURL)
+cancellation.register(
+  listenParent(
+    "execute",
+    ({ localRoot, remoteRoot, compileInto, file, instrument, setup, teardown }) => {
+      const compileMapLocalURL = getCompileMapLocalURL({ localRoot, compileInto })
+      // eslint-disable-next-line import/no-dynamic-require
+      const compileMap = require(compileMapLocalURL)
 
-    const { executeFile } = createNodePlatform({
-      localRoot,
-      remoteRoot,
-      compileInto,
-      compileMap,
-    })
+      const { executeFile } = createNodePlatform({
+        cancellation,
+        localRoot,
+        remoteRoot,
+        compileInto,
+        compileMap,
+      })
 
-    executeFile({ file, instrument, setup, teardown }).then((value) => {
-      sendToParent("execute", value)
-    })
-  },
+      executeFile({ file, instrument, setup, teardown }).then((value) => {
+        sendToParent("execute", value)
+      })
+    },
+  ),
 )
