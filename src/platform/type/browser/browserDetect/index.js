2d05392bfbf978f65838bec04119d61f1713f765
Damien Maillard <damien.maillard@dailymotion.com>
2018-11-08 10:35:42 +0100

    add android & ios dectectors and improve version normalisation

--- src/platform/type/browser/browserDetect/index.js
+++ src/platform/type/browser/browserDetect/index.js
@@ -1,3 +1,7 @@
+// https://github.com/Ahmdrza/detect-browser/blob/26254f85cf92795655a983bfd759d85f3de850c6/detect-browser.js#L1
+// https://github.com/lancedikson/bowser/blob/master/src/parser-browsers.js#L1
+
+import { detect as androidDetect } from "./android.js"
 import { detect as ieDetect } from "./ie.js"
 import { detect as operaDetect } from "./opera.js"
 import { detect as edgeDetect } from "./edge.js"
@@ -29,6 +33,7 @@ const detector = detectorCompose([
   safariDetect,
   electronDetect,
   iosDetect,
+  androidDetect,
 ])
 
 const normalizeName = (name) => {
@@ -36,9 +41,19 @@ const normalizeName = (name) => {
 }
 
 const normalizeVersion = (version) => {
-  const parts = version.split(".")
-  // remove extraneous .
-  return parts.slice(0, 3).join(".")
+  if (version.indexOf(".") > -1) {
+    const parts = version.split(".")
+    // remove extraneous .
+    return parts.slice(0, 3).join(".")
+  }
+
+  if (version.indexOf("_") > -1) {
+    const parts = version.split("_")
+    // remove extraneous _
+    return parts.slice(0, 3).join("_")
+  }
+
+  return version
 }
 
 export const detect = () => {
--- src/platform/type/browser/browserDetect/ios.js
+++ src/platform/type/browser/browserDetect/ios.js
@@ -1,1 +1,19 @@
-export const detect = () => null // TODO
+import { firstMatch } from "./util.js"
+
+const navigatorToBrowser = ({ userAgent, appVersion }) => {
+  if (/iPhone;/.test(userAgent)) {
+    return {
+      name: "ios",
+      version: firstMatch(/OS (\d+(\.?_?\d+)+)/i, appVersion),
+    }
+  }
+  if (/iPad;/.test(userAgent)) {
+    return {
+      name: "ios",
+      version: firstMatch(/OS (\d+(\.?_?\d+)+)/i, appVersion),
+    }
+  }
+  return null
+}
+
+export const detect = () => navigatorToBrowser(window.navigator)
