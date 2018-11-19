2b003147220c7f82fdf45279b05ade54b1b5e1e9
Damien Maillard <damien.maillard@dailymotion.com>
2018-11-19 12:07:44 +0100

    hotreload same signature on browser

--- src/platform/type/browser/hotreload.js
+++ src/platform/type/browser/hotreload.js
@@ -1,18 +1,28 @@
 export const open = (url, callback) => {
   if (typeof window.EventSource !== "function") {
-    return
+    return () => {}
   }
 
   const eventSource = new window.EventSource(url, { withCredentials: true })
+
+  const close = () => {
+    eventSource.close()
+  }
+
   eventSource.onerror = () => {
     // we could try to reconnect several times before giving up
     // but dont keep it open as it would try to reconnect forever
-    eventSource.close()
+    // maybe, it depends what error occurs, or we could
+    // retry less frequently
+    close()
   }
   eventSource.addEventListener("file-changed", (e) => {
     if (e.origin !== url) {
       return
     }
-    callback(e.data)
+    const fileChanged = e.data
+    callback(fileChanged)
   })
+
+  return close
 }
