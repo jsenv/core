6b78ae39de00f447babd4dab73121f33ccdf6757
Damien Maillard <damien.maillard@dailymotion.com>
2018-11-21 12:08:59 +0100

    fix jscompile instrument test

--- src/jsCompile/jsCompile.test.instrument.js
+++ src/jsCompile/jsCompile.test.instrument.js
@@ -1,65 +1,64 @@
-import { jsCompile } from "./jsCompile.js"
-import istanbul from "istanbul"
 import fs from "fs"
 import path from "path"
-import { pluginNameToPlugin } from "@dmail/project-structure-compile-babel"
-import { objectMap } from "../objectHelper.js"
 import assert from "assert"
+import istanbul from "istanbul"
+import { pluginOptionMapToPluginMap } from "@dmail/project-structure-compile-babel"
+import { objectMap } from "../objectHelper.js"
 import { createInstrumentPlugin } from "./createInstrumentPlugin.js"
+import { jsCompile } from "./jsCompile.js"
 
-const pluginNames = [
-  "proposal-async-generator-functions",
-  "proposal-json-strings",
-  "proposal-object-rest-spread",
-  "proposal-optional-catch-binding",
-  "proposal-unicode-property-regex",
-  "transform-arrow-functions",
-  "transform-async-to-generator",
-  "transform-block-scoped-functions",
-  "transform-block-scoping",
-  "transform-classes",
-  "transform-computed-properties",
-  "transform-destructuring",
-  "transform-dotall-regex",
-  "transform-duplicate-keys",
-  "transform-exponentiation-operator",
-  "transform-for-of",
-  "transform-function-name",
-  "transform-literals",
-  // "transform-modules-systemjs",
-  "transform-new-target",
-  "transform-object-super",
-  "transform-parameters",
-  "transform-regenerator",
-  "transform-shorthand-properties",
-  "transform-spread",
-  "transform-sticky-regex",
-  "transform-template-literals",
-  "transform-typeof-symbol",
-  "transform-unicode-regex",
-]
-const babelPlugins = pluginNames.map(pluginNameToPlugin)
-babelPlugins.push(createInstrumentPlugin())
-const root = path.resolve(__dirname, "../../../")
+const pluginMap = pluginOptionMapToPluginMap({
+  // "transform-modules-systemjs": {},
+
+  "proposal-async-generator-functions": {},
+  "proposal-json-strings": {},
+  "proposal-object-rest-spread": {},
+  "proposal-optional-catch-binding": {},
+  "proposal-unicode-property-regex": {},
+  "transform-arrow-functions": {},
+  "transform-async-to-generator": {},
+  "transform-block-scoped-functions": {},
+  "transform-block-scoping": {},
+  "transform-classes": {},
+  "transform-computed-properties": {},
+  "transform-destructuring": {},
+  "transform-dotall-regex": {},
+  "transform-duplicate-keys": {},
+  "transform-exponentiation-operator": {},
+  "transform-for-of": {},
+  "transform-function-name": {},
+  "transform-literals": {},
+  "transform-new-target": {},
+  "transform-object-super": {},
+  "transform-parameters": {},
+  "transform-regenerator": {},
+  "transform-shorthand-properties": {},
+  "transform-spread": {},
+  "transform-sticky-regex": {},
+  "transform-template-literals": {},
+  "transform-typeof-symbol": {},
+  "transform-unicode-regex": {},
+})
+pluginMap["transform-instrument"] = createInstrumentPlugin()
+const localRoot = path.resolve(__dirname, "../../../")
 const file = "src/jsCompile/fixtures/file.js"
-const filename = `${root}/${file}`
+const fileAbsolute = `${localRoot}/${file}`
 
 jsCompile({
-  root,
-  plugins: babelPlugins,
-  inputName: file,
+  localRoot,
+  file,
+  fileAbsolute,
+  input: fs.readFileSync(fileAbsolute).toString(),
+  pluginMap,
   instrument: true,
-  inputSource: fs.readFileSync(filename).toString(),
-  outputName: "file.compiled.js",
-}).then(({ outputSource, assetMap }) => {
-  eval(outputSource)
-  const coverage = global.__coverage__
-  assert.equal(typeof assetMap["file.js.map"], "string")
-  assert.equal(typeof assetMap["coverage.json"], "string")
+}).then(({ assets, output }) => {
+  assert.deepEqual(assets, ["file.js.map", "coverage.json"])
+  eval(output)
 
+  const coverage = global.__coverage__
   const absoluteCoverage = objectMap(coverage, (file, coverage) => {
     return {
-      [`${root}/${file}`]: { ...coverage, path: `${root}/${file}` },
+      [`${localRoot}/${file}`]: { ...coverage, path: `${localRoot}/${file}` },
     }
   })
   const collector = new istanbul.Collector()
