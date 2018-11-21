eecfb47862a1465ee97c58a1a493db7204fffb12
Damien Maillard <damien.maillard@dailymotion.com>
2018-11-21 12:59:04 +0100

    test server

--- src/server-compile/serverCompile.test.js
+++ src/server-compile/serverCompile.test.js
@@ -1,14 +1,14 @@
 import { open } from "./serverCompile.js"
 import path from "path"
 
-const root = path.resolve(__dirname, "../../../")
+const localRoot = path.resolve(__dirname, "../../../")
 
 open({
-  root,
+  localRoot,
   into: "build",
   protocol: "http",
   ip: "127.0.0.1",
   port: 8998,
 }).then(({ origin }) => {
-  console.log(`compiling ${root} at ${origin}`)
+  console.log(`compiling ${localRoot} at ${origin}`)
 })
--- src/server/server.test.js
+++ src/server/server.test.js
@@ -3,47 +3,44 @@ import fetch from "node-fetch"
 import { createCancellationSource } from "@dmail/cancellation"
 import { open } from "./server.js"
 
-const { cancel, token } = createCancellationSource()
+const test = async () => {
+  const { cancel, token } = createCancellationSource()
 
-open({
-  cancellationToken: token,
-  protocol: "http",
-  port: 8998,
-  verbose: true,
-  requestToResponse: () => {
-    return {
-      status: 200,
-      headers: {
-        "Content-Type": "text/plain",
-      },
-      body: "ok",
-    }
-  },
-})
-  .then(({ origin, agent, close }) => {
-    assert.deepEqual(origin, "http://127.0.0.1:8998")
-
-    return fetch(origin, { agent })
-      .then((response) => response.text())
-      .then((text) => {
-        assert.equal(text, "ok")
-        return close()
-      })
-  })
-  .then(() => {
-    console.log("passed")
+  const timer = setTimeout(() => {}, 100000000)
+  token.register(() => {
+    clearTimeout(timer)
   })
 
-const timer = setTimeout(() => {}, 100000000)
-token.register(() => {
-  clearTimeout(timer)
-})
-
-process.on("SIGINT", () => {
-  cancel("process interrupt").then(() => {
-    process.exit(0)
+  process.on("SIGINT", () => {
+    cancel("process interrupt").then(() => {
+      process.exit(0)
+    })
   })
-})
+
+  const { origin, agent, close } = await open({
+    cancellationToken: token,
+    protocol: "http",
+    port: 8998,
+    verbose: true,
+    requestToResponse: () => {
+      return {
+        status: 200,
+        headers: {
+          "Content-Type": "text/plain",
+        },
+        body: "ok",
+      }
+    },
+  })
+  assert.deepEqual(origin, "http://127.0.0.1:8998")
+
+  const response = await fetch(origin, { agent })
+  const text = await response.text()
+  assert.equal(text, "ok")
+  await close()
+  console.log("passed")
+}
+test()
 
 // we should test than close/cancel server gives expected response client side
 // test(() => {
