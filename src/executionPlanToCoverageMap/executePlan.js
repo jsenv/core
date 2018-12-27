ca13541651a7d35c458f2626b8e1a8ea4c6539f3
Damien Maillard <damien.maillard@dailymotion.com>
2018-12-27 17:40:32 +0100

    working on executionPlanToCoverageMap

---
+++ src/executionPlanToCoverageMap/executePlan.js
@@ -0,0 +1,66 @@
+import { createCancellationToken } from "@dmail/cancellation"
+import { promiseConcurrent } from "../promiseHelper.js"
+import { executeFileOnPlatform } from "../executeFileOnPlatform/index.js"
+
+export const executePlan = async (
+  executionPlan,
+  {
+    cancellationToken = createCancellationToken(),
+    cover = false,
+    maxParallelExecution = 5,
+    beforeEach = () => {},
+    afterEach = () => {},
+  } = {},
+) => {
+  const plannedExecutionArray = []
+  Object.keys(executionPlan).forEach((file) => {
+    const fileExecutionPlan = executionPlan[file]
+    Object.keys(fileExecutionPlan).forEach((name) => {
+      // TODO: add allocatedMs { launch, allocatedMs }
+      // and pass it
+      const { launch } = fileExecutionPlan[name]
+      plannedExecutionArray.push({
+        file,
+        name,
+        launch,
+      })
+    })
+  })
+
+  const planResult = {}
+  await promiseConcurrent(
+    plannedExecutionArray,
+    async ({ file, name, launch }) => {
+      beforeEach({ file, name })
+      // TODO: if the test fails to execute it should not prevent
+      // subsequent execution (or an option should control that)
+      // in other words I think I have to catch
+      // and wrap result into passed: true/false, value: {output, coverageMap}
+      const result = await executeFileOnPlatform(file, launch, {
+        cancellationToken,
+        instrument: cover,
+        collectCoverage: cover,
+      })
+      afterEach({ file, name, result })
+
+      if (file in planResult === false) {
+        planResult[file] = {}
+      }
+      planResult[file][name] = result
+
+      if (cover && result.coverageMap === null) {
+        // coverageMap can be null for 2 reason:
+        // - test file import a source file which is not instrumented
+        // here we should throw
+        // - test file import nothing so global__coverage__ is not set
+        // here it's totally normal
+        // throw new Error(`missing coverageMap after ${file} execution, it was not instrumented`)
+      }
+    },
+    {
+      cancellationToken,
+      maxParallelExecution,
+    },
+  )
+  return planResult
+}
--- src/executionPlanToCoverageMap/executionPlanToCoverageMap.js
+++ src/executionPlanToCoverageMap/executionPlanToCoverageMap.js
@@ -1,35 +1,46 @@
-import { executionPlanToPlatformResultMap } from "./executionPlanToPlatformResultMap.js"
-import { platformCoverageMapToCoverageMap } from "./platformCoverageMapToCoverageMap.js"
-import { platformResultMapToCoverageMap } from "./platformResultMapToCoverageMap.js"
+import { executePlan } from "./executePlan.js"
+import { coverageMapCompose } from "./coverageMapCompose.js"
+import { fileToEmptyCoverage } from "./fileToEmptyCoverage.js"
 
 export const executionPlanToCoverageMap = async (
   executionPlan,
-  { cancellationToken, localRoot, listFilesToCover },
+  { cancellationToken, localRoot, filesToCover = [] },
 ) => {
-  const filesToCover = await listFilesToCover()
+  const fileToExecuteAndCover = filesToCover.find((file) => file in executionPlan)
+  if (fileToExecuteAndCover)
+    throw new Error(`${fileToExecuteAndCover} must be covered but is also part of execution plan`)
 
-  filesToCover.forEach((file) => {
-    Object.keys(executionPlan).some((platformName) => {
-      if (executionPlan[platformName].files.includes(file)) {
-        throw new Error(
-          `${file} must be covered but is also part of ${platformName} execution plan`,
-        )
+  const result = await executePlan(executionPlan, {
+    cancellationToken,
+    cover: true,
+  })
+
+  const coverageMapList = []
+  Object.keys(result).forEach((file) => {
+    Object.keys(result[file]).forEach((name) => {
+      const { coverageMap } = result[file][name]
+      if (coverageMap) {
+        coverageMapList.push(coverageMap)
       }
     })
   })
+  const executionCoverageMap = coverageMapCompose(...coverageMapList)
 
-  const platformResultMap = await executionPlanToPlatformResultMap(executionPlan, {
-    cancellationToken,
-  })
+  const filesMissed = filesToCover.filter((file) => file in executionCoverageMap === false)
 
-  const platformCoverageMap = platformResultMapToCoverageMap(platformResultMap)
+  const missedCoverageMap = {}
+  await Promise.all(
+    filesMissed.map(async (file) => {
+      const emptyCoverage = await fileToEmptyCoverage(file, { cancellationToken, localRoot })
+      missedCoverageMap[file] = emptyCoverage
+      return emptyCoverage
+    }),
+  )
 
-  const coverageMap = await platformCoverageMapToCoverageMap({
-    cancellationToken,
-    platformCoverageMap,
-    localRoot,
-    filesToCover,
-  })
+  const coverageMap = {
+    ...executionCoverageMap,
+    ...missedCoverageMap,
+  }
 
   return coverageMap
 }
