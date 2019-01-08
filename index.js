a48a3f1b21afd865ce656ab3c606e8a874457cb5
Damien Maillard <damien.maillard@dailymotion.com>
2019-01-08 14:04:18 +0100

    update exports

--- index.js
+++ index.js
@@ -1,11 +1,10 @@
 // https://github.com/jsenv/core/blob/master/src/api/api.js
 // https://github.com/ModuleLoader/system-register-loader/blob/master/src/system-register-loader.js
 
-// pour le coverage
-// https://github.com/jsenv/core/blob/master/more/test/playground/coverage/run.js
-// https://github.com/jsenv/core/blob/master/more/to-externalize/module-cover/index.js
-
-export { open as serverCompileOpen } from "./src/server-compile/index.js"
-export { open as serverBrowserOpen } from "./src/server-browser/index.js"
+export { open as openCompileServer } from "./src/server-compile/index.js"
+export { open as openBrowserServer } from "./src/server-browser/index.js"
+export { launchChromium } from "./src/launchChromium/index.js"
+export { launchNode } from "./src/launchNode/index.js"
 export { createJsCompileService } from "./src/createJsCompileService.js"
-export { fileWriteFromString, readFile } from "./src/fileHelper.js"
+export { executionPlanToCoverageMap } from "./src/executionPlanToCoverageMap/index.js"
+export { executeFileOnPlatform } from "./src/executeFileOnPlatform/index.js"
