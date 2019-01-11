bd3dd80fecfb802485aa526b4a0b9b0a5d371fd1
Damien Maillard <damien.maillard@dailymotion.com>
2019-01-11 11:56:18 +0100

    remove signalHelper now unused

--- src/signalHelper.js
+++
@@ -1,9 +0,0 @@
-import { race } from "@dmail/signal"
-
-export const cancellableAction = (callback, triggerSignal, ...cancelSignals) => {
-  return race([triggerSignal, ...cancelSignals], ({ index, args }) => {
-    if (index === 0) {
-      callback(...args)
-    }
-  })
-}
