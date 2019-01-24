b7f72c9d5b2c51ac14ca357a06f80ae36cd70fb6
Damien Maillard <damien.maillard@dailymotion.com>
2019-01-24 15:38:17 +0100

    disable jsenv module resolution
    it should be enabled for module using this one but not for this one

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,12 +1,12 @@
 const config = require("@dmail/project-eslint-config").config
 
-config.settings["import/resolver"] = {
-  "@jsenv/eslint-import-resolver/dist/src/resolver.js": {
-    localRoot: __dirname,
-    useNodeModuleResolutionOnRelative: true,
-    verbose: true,
-  },
-}
-config.rules["import/no-absolute-path"] = ["off"]
+// config.settings["import/resolver"] = {
+//   "@jsenv/eslint-import-resolver/dist/src/resolver.js": {
+//     localRoot: __dirname,
+//     useNodeModuleResolutionOnRelative: true,
+//     verbose: true,
+//   },
+// }
+// config.rules["import/no-absolute-path"] = ["off"]
 
 module.exports = config
