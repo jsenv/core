bbd35868989ae22dffb6c7880f1cb99cbb9078df
Damien Maillard <damien.maillard@dailymotion.com>
2019-01-29 17:30:07 +0100

    dynamic import looks good too

---
+++ src/launchNode/test/execution/dynamic-import.execution.test.js
@@ -0,0 +1,34 @@
+import { assert } from "@dmail/assert"
+import transformAsyncToPromises from "babel-plugin-transform-async-to-promises"
+import transformModulesSystemJs from "../../../babel-plugin-transform-modules-systemjs/index.js"
+import { localRoot } from "../../../localRoot.js"
+import { launchNode } from "../../launchNode.js"
+import { executeFile } from "../../../executeFile.js"
+
+const file = `src/launchNode/test/fixtures/dynamic-import.js`
+const compileInto = "build"
+const pluginMap = {
+  "transform-modules-systemjs": [transformModulesSystemJs, { topLevelAwait: true }],
+  "transform-async-to-promises": [transformAsyncToPromises],
+}
+
+;(async () => {
+  const actual = await executeFile(file, {
+    localRoot,
+    compileInto,
+    pluginMap,
+    launchPlatform: launchNode,
+    platformTypeForLog: "node process",
+    verbose: true,
+    collectNamespace: true,
+  })
+  const expected = {
+    status: "completed",
+    value: {
+      namespace: {
+        default: 42,
+      },
+    },
+  }
+  assert({ actual, expected })
+})()
---
+++ src/launchNode/test/fixtures/dynamic-import.js
@@ -0,0 +1,3 @@
+const { answer } = await import("./modules/dependency.js")
+
+export default answer
