a20df19115c6cd9290cc1e03496c74aa839a8402
Damien Maillard <damien.maillard@dailymotion.com>
2019-02-01 17:33:05 +0100

    move import meta url test in own folder

--- src/launchNode/test/import-meta-url/import-meta-url.execution.test.js
+++ src/launchNode/test/import-meta-url/import-meta-url.execution.test.js
@@ -1,10 +1,10 @@
 import { assert } from "@dmail/assert"
 import transformModulesSystemJs from "../../../babel-plugin-transform-modules-systemjs/index.js"
 import { localRoot } from "../../../localRoot.js"
-import { launchNode } from "../../launchNode.js"
 import { executeFile } from "../../../executeFile.js"
+import { launchNode } from "../../launchNode.js"
 
-const file = `src/launchNode/test/fixtures/import-meta-url.js`
+const file = `src/launchNode/test/import-meta-url/import-meta-url.js`
 const compileInto = "build"
 const pluginMap = {
   "transform-modules-systemjs": [transformModulesSystemJs, { topLevelAwait: true }],
@@ -18,15 +18,16 @@ const pluginMap = {
     launchPlatform: launchNode,
     platformTypeForLog: "node process",
     verbose: true,
+    mirrorConsole: true,
     collectNamespace: true,
     port: 4500,
   })
   const expected = {
     status: "completed",
-    value: {
-      namespace: {
-        default: "http://127.0.0.1:4500/build/best/src/launchNode/test/fixtures/import-meta-url.js",
-      },
+    coverageMap: undefined,
+    namespace: {
+      default:
+        "http://127.0.0.1:4500/build/best/src/launchNode/test/import-meta-url/import-meta-url.js",
     },
   }
   assert({ actual, expected })
