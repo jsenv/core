7ab7c5eb7449465e8845ea01519d637e3f5cc6a1
Damien Maillard <damien.maillard@dailymotion.com>
2019-02-01 16:30:32 +0100

    move alive test in own folder

---
+++ src/launchNode/test/alive/alive.execution.test.js
@@ -0,0 +1,29 @@
+import { assert } from "@dmail/assert"
+import { pluginOptionMapToPluginMap } from "@dmail/project-structure-compile-babel"
+import { localRoot } from "../../../localRoot.js"
+import { launchNode } from "../../launchNode.js"
+import { executeFile } from "../../../executeFile.js"
+
+const file = `src/launchNode/test/alive/alive.js`
+const compileInto = "build"
+const pluginMap = pluginOptionMapToPluginMap({
+  "transform-modules-systemjs": {},
+})
+
+;(async () => {
+  const actual = await executeFile(file, {
+    launchPlatform: launchNode,
+    localRoot,
+    compileInto,
+    pluginMap,
+    platformTypeForLog: "node process",
+    verbose: true,
+    mirrorConsole: true,
+  })
+  const expected = {
+    status: "completed",
+    coverageMap: undefined,
+    namespace: undefined,
+  }
+  assert({ actual, expected })
+})()
