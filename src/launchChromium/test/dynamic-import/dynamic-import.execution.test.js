61f6d8c1eaa06a0a8a8b2beb158433b29e5ed0fe
Damien Maillard <damien.maillard@dailymotion.com>
2019-02-04 18:04:20 +0100

    test dynamic import & import meta url on chromium

---
+++ src/launchChromium/test/dynamic-import/dynamic-import.execution.test.js
@@ -0,0 +1,40 @@
+import { assert } from "@dmail/assert"
+import transformAsyncToPromises from "babel-plugin-transform-async-to-promises"
+import transformModulesSystemJs from "../../../babel-plugin-transform-modules-systemjs/index.js"
+import { localRoot } from "../../../localRoot.js"
+import { launchAndExecute } from "../../../launchAndExecute/index.js"
+import { startCompileServer } from "../../../server-compile/index.js"
+import { launchChromium } from "../../launchChromium.js"
+
+const file = `src/launchChromium/test/dynamic-import/dynamic-import.js`
+const compileInto = "build"
+const pluginMap = {
+  "transform-modules-systemjs": [transformModulesSystemJs, { topLevelAwait: true }],
+  "transform-async-to-promises": [transformAsyncToPromises],
+}
+
+;(async () => {
+  const { origin: remoteRoot } = await startCompileServer({
+    localRoot,
+    compileInto,
+    pluginMap,
+  })
+
+  const actual = await launchAndExecute(
+    (options) => launchChromium({ ...options, localRoot, remoteRoot, compileInto }),
+    file,
+    {
+      platformTypeForLog: "node process",
+      verbose: true,
+      collectNamespace: true,
+      stopOnceExecuted: true,
+    },
+  )
+  const expected = {
+    status: "completed",
+    namespace: {
+      default: 42,
+    },
+  }
+  assert({ actual, expected })
+})()
---
+++ src/launchChromium/test/import-meta-url/import-meta-url.execution.test.js
@@ -0,0 +1,39 @@
+import { assert } from "@dmail/assert"
+import transformModulesSystemJs from "../../../babel-plugin-transform-modules-systemjs/index.js"
+import { localRoot } from "../../../localRoot.js"
+import { launchAndExecute } from "../../../launchAndExecute/index.js"
+import { startCompileServer } from "../../../server-compile/index.js"
+import { launchChromium } from "../../launchChromium.js"
+
+const file = `src/launchChromium/test/import-meta-url/import-meta-url.js`
+const compileInto = "build"
+const pluginMap = {
+  "transform-modules-systemjs": [transformModulesSystemJs, { topLevelAwait: true }],
+}
+
+;(async () => {
+  const { origin: remoteRoot } = await startCompileServer({
+    localRoot,
+    compileInto,
+    pluginMap,
+  })
+
+  const actual = await launchAndExecute(
+    (options) => launchChromium({ ...options, localRoot, remoteRoot, compileInto }),
+    file,
+    {
+      platformTypeForLog: "chromium process",
+      verbose: true,
+      mirrorConsole: true,
+      collectNamespace: true,
+      stopOnceExecuted: true,
+    },
+  )
+  const expected = {
+    status: "completed",
+    namespace: {
+      default: `${remoteRoot}/${compileInto}/best/${file}`,
+    },
+  }
+  assert({ actual, expected })
+})()
