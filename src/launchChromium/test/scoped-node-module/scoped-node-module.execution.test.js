339ee3a9028b60a72ebb62d908d30c4e6194f473
Damien Maillard <damien.maillard@dailymotion.com>
2019-02-04 16:43:55 +0100

    scoped node module ok

---
+++ src/launchChromium/test/scoped-node-module/scoped-node-module.execution.test.js
@@ -0,0 +1,39 @@
+import { assert } from "@dmail/assert"
+import { pluginOptionMapToPluginMap } from "@dmail/project-structure-compile-babel"
+import { localRoot } from "../../../localRoot.js"
+import { startCompileServer } from "../../../server-compile/index.js"
+import { launchAndExecute } from "../../../launchAndExecute/index.js"
+import { launchChromium } from "../../launchChromium.js"
+
+const file = `src/launchNode/test/scoped-node-module/scoped-node-module.js`
+const compileInto = "build"
+const pluginMap = pluginOptionMapToPluginMap({
+  "transform-modules-systemjs": {},
+})
+
+;(async () => {
+  const { origin: remoteRoot } = await startCompileServer({
+    localRoot,
+    compileInto,
+    pluginMap,
+  })
+
+  const actual = await launchAndExecute(
+    () => launchChromium({ localRoot, remoteRoot, compileInto, headless: false }),
+    file,
+    {
+      platformTypeForLog: "chromium browser",
+      verbose: true,
+      stopOnceExecuted: true,
+      mirrorConsole: true,
+      collectNamespace: true,
+    },
+  )
+  const expected = {
+    status: "completed",
+    namespace: {
+      foo: "scoped-foo",
+    },
+  }
+  assert({ actual, expected })
+})()
