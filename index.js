051e91b8c1e5052e8c60df4e9c5838703dc874e7
Damien Maillard <damien.maillard@dailymotion.com>
2019-02-05 16:09:16 +0100

    prepare cleaning exported functions

--- index.js
+++ index.js
@@ -1,14 +1,23 @@
+// belong to core
 export { startCompileServer } from "./src/server-compile/index.js"
+
+// belong to core
 export { launchAndExecute } from "./src/launchAndExecute/index.js"
 
+// I would say methods (used inside dependent scripts) belong to core
+export { execute, format, test, cover } from "./src/script/index.js"
+
+// to be removed because used internally by test and cover
 export { executePlan } from "./src/executePlan/index.js"
-// may be moved in own repo ?
+
+// to be removed because used internally only by cover
 export { executionPlanResultToCoverageMap } from "./src/executionPlanResultToCoverageMap/index.js"
 
-// browser served need own repo?
+// may be moved to jsenv/browser-server
 export { startBrowserServer } from "./src/server-browser/index.js"
 
-// must be moved in own repo
+// must be moved later to jsenv/chromium-launcher
 export { launchChromium } from "./src/launchChromium/index.js"
-// must be moved in own repo
+
+// must be moved later to jsenv/node-launcher
 export { launchNode } from "./src/launchNode/index.js"
---
+++ src/script/index.js
@@ -0,0 +1,4 @@
+export { execute } from "./execute/execute.js"
+export { format } from "./format/format.js"
+export { test } from "./test/test.js"
+export { cover } from "./cover/cover.js"
