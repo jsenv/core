79e99385f27db50791a2434ea4307abfb1c8cb1a
Damien Maillard <damien.maillard@dailymotion.com>
2019-02-08 11:32:48 +0100

    add cleanCompileInto

---
+++ src/clean-compile-into/cleanCompileInto.js
@@ -0,0 +1,23 @@
+// because compileInto mirror files
+// it can contain files that no longer exists.
+// this function is responsible to clean these files.
+// for now it just removes completely the compileInfo folder.
+// next iteration will check if a file inside compileInto still exists
+// and remove only useless files
+import rimraf from "rimraf"
+
+export const cleanCompileInto = ({
+  // cancellationToken,
+  localRoot,
+  compileInto,
+}) => {
+  return new Promise((resolve, reject) => {
+    rimraf(`${localRoot}/${compileInto}`, { glob: false }, (error) => {
+      if (error) {
+        reject(error)
+      } else {
+        resolve()
+      }
+    })
+  })
+}
