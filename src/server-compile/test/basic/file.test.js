49ae85eb34d175e2fa678e0b67d487dea96f6a73
Damien Maillard <damien.maillard@dailymotion.com>
2019-02-12 19:11:35 +0100

    test starCompileServer

--- src/server-compile/test/basic/file.test.js
+++ src/server-compile/test/basic/file.test.js
@@ -1,7 +1,7 @@
 import fetch from "node-fetch"
 import { assert } from "@dmail/assert"
-import { rootname } from "../rootname.js"
-import { startCompileServer } from "./startCompileServer.js"
+import { rootname } from "../../../rootname.js"
+import { startCompileServer } from "../../startCompileServer.js"
 
 const test = async () => {
   const compileInto = "build"
@@ -16,27 +16,28 @@ const test = async () => {
     port: 8998,
   })
 
-  const response = await fetch(`${compileServer.origin}/build/best/src/__test__/file.js`)
+  const response = await fetch(
+    `${compileServer.origin}/build/best/src/server-compile/test/basic/file.js`,
+  )
+  // { ... } because response.headers.raw() an object create with Object.create(null)
+  const actualHeaders = { ...response.headers.raw() }
 
   assert({
     actual: {
       status: response.status,
-      // { ... } because response.headers.raw() an object create with Object.create(null)
-      headers: { ...response.headers.raw() },
+      headers: actualHeaders,
     },
     expected: {
       status: 200,
       headers: {
+        ...actualHeaders,
         "access-control-allow-credentials": ["true"],
         "access-control-allow-headers": ["x-requested-with, content-type, accept"],
         "access-control-allow-methods": ["GET, POST, PUT, DELETE, OPTIONS"],
         "access-control-allow-origin": ["*"],
         "access-control-max-age": ["1"],
         connection: ["close"],
-        "content-length": ["310"],
         "content-type": ["application/javascript"],
-        date: [response.headers.get("date")],
-        etag: [`"136-GIFV9GrTzr7XuulsY3J7DuWtAT4"`],
       },
     },
   })
