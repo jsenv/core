2d5201e26e14150286931c6ba8620ee2a5f0f5a0
Damien Maillard <damien.maillard@dailymotion.com>
2019-02-13 18:42:20 +0100

    test shared node module on chromium

--- src/launchChromium/test/shared-node-module/shared-node-module.execution.test.js
+++ src/launchChromium/test/shared-node-module/shared-node-module.execution.test.js
@@ -1,26 +1,36 @@
 import { assert } from "@dmail/assert"
-import { root } from "../../../root.js"
+import { filenameToFileHref } from "@jsenv/module-resolution"
+import { projectFolder } from "../../../projectFolder.js"
 import { startCompileServer } from "../../../server-compile/index.js"
 import { launchAndExecute } from "../../../launchAndExecute/index.js"
 import { launchChromium } from "../../launchChromium.js"
 
-const file = `src/launchChromium/test/shared-node-module/shared-node-module.js`
+const filenameRelative = `src/launchChromium/test/shared-node-module/shared-node-module.js`
 const compileInto = "build"
 const babelPluginDescription = {}
 
 ;(async () => {
-  const { origin: remoteRoot } = await startCompileServer({
-    root,
+  const sourceOrigin = filenameToFileHref(projectFolder)
+
+  const { origin: compileServerOrigin } = await startCompileServer({
+    projectFolder,
     compileInto,
     babelPluginDescription,
   })
 
   const actual = await launchAndExecute({
-    launch: () => launchChromium({ root, compileInto, remoteRoot, headless: false }),
+    launch: (options) =>
+      launchChromium({
+        ...options,
+        compileInto,
+        sourceOrigin,
+        compileServerOrigin,
+        headless: false,
+      }),
     stopOnceExecuted: true,
     mirrorConsole: true,
     collectNamespace: true,
-    file,
+    filenameRelative,
     verbose: true,
     platformTypeForLog: "chromium browser",
   })
