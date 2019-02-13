4b9164bb81baf984e3d21568cfa9cf1e3682e48a
Damien Maillard <damien.maillard@dailymotion.com>
2019-02-13 18:08:30 +0100

    test shared and scope node module resolution node

--- src/launchNode/test/shared-node-module/shared-node-module.execution.test.js
+++ src/launchNode/test/shared-node-module/shared-node-module.execution.test.js
@@ -1,24 +1,27 @@
 import { assert } from "@dmail/assert"
-import { root } from "../../../root.js"
+import { filenameToFileHref } from "@jsenv/module-resolution"
+import { projectFolder } from "../../../projectFolder.js"
 import { launchAndExecute } from "../../../launchAndExecute/index.js"
 import { startCompileServer } from "../../../server-compile/index.js"
 import { launchNode } from "../../launchNode.js"
 
-const file = `src/launchNode/test/shared-node-module/shared-node-module.js`
+const filenameRelative = `src/launchNode/test/shared-node-module/shared-node-module.js`
 const compileInto = "build"
 const babelPluginDescription = {}
 
 ;(async () => {
-  const { origin: remoteRoot } = await startCompileServer({
-    root,
+  const sourceOrigin = filenameToFileHref(projectFolder)
+
+  const { origin: compileServerOrigin } = await startCompileServer({
+    projectFolder,
     compileInto,
     babelPluginDescription,
   })
 
   const actual = await launchAndExecute({
-    launch: (options) => launchNode({ ...options, root, compileInto, remoteRoot }),
+    launch: (options) => launchNode({ ...options, compileInto, sourceOrigin, compileServerOrigin }),
     collectNamespace: true,
-    file,
+    filenameRelative,
     verbose: true,
     platformTypeForLog: "node process",
   })
