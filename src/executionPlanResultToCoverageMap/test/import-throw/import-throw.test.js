ceb8a0eaf021db11379552ce94742dcf0d4dc278
Damien Maillard <damien.maillard@dailymotion.com>
2019-03-13 17:10:49 +0100

    test coverage when imported file throw

---
+++ src/executionPlanResultToCoverageMap/test/import-throw/import-throw.test.js
@@ -0,0 +1,58 @@
+import { assert } from "@dmail/assert"
+import { projectFolder } from "../../../../projectFolder.js"
+import { launchNode } from "../../../launchNode/index.js"
+import { launchChromium } from "../../../launchChromium/index.js"
+import { executePlan } from "../../../executePlan/index.js"
+import { startCompileServer } from "../../../server-compile/index.js"
+import { executionPlanResultToCoverageMap } from "../../executionPlanResultToCoverageMap.js"
+
+const testFolder = `${projectFolder}/src/executionPlanResultToCoverageMap/test/import-throw`
+const compileInto = ".dist"
+const babelPluginDescription = {}
+
+;(async () => {
+  const sourceOrigin = `file://${testFolder}`
+
+  const { origin: compileServerOrigin } = await startCompileServer({
+    projectFolder: testFolder,
+    compileInto,
+    babelPluginDescription,
+    verbose: false,
+  })
+
+  const nodeLaunch = (options) =>
+    launchNode({ ...options, sourceOrigin, compileServerOrigin, compileInto })
+
+  const chromiumLaunch = (options) =>
+    launchChromium({ ...options, sourceOrigin, compileServerOrigin, compileInto })
+
+  const executionPlan = {
+    "import-throw.js": {
+      node: {
+        launch: nodeLaunch,
+      },
+      chromium: {
+        launch: chromiumLaunch,
+      },
+    },
+  }
+
+  const executionPlanResult = await executePlan(executionPlan, {
+    cover: true,
+  })
+
+  const coverageMap = await executionPlanResultToCoverageMap(executionPlanResult, {
+    projectFolder,
+    arrayOfFilenameRelativeToCover: [],
+  })
+
+  assert({
+    actual: coverageMap,
+    expected: {
+      "throw.js": {
+        ...coverageMap["throw.js"],
+        s: { 0: 2, 1: 2 },
+      },
+    },
+  })
+})()
---
+++ src/executionPlanResultToCoverageMap/test/import-throw/throw.js
@@ -0,0 +1,3 @@
+console.log("here")
+
+throw new Error("here")
