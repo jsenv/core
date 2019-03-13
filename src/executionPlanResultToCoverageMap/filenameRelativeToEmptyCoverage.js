64e20fd3304e4d5d785c804409415aa5de5b23e9
Damien Maillard <damien.maillard@dailymotion.com>
2019-03-13 16:21:33 +0100

    return empty coverage on syntaxError

--- src/executionPlanResultToCoverageMap/filenameRelativeToEmptyCoverage.js
+++ src/executionPlanResultToCoverageMap/filenameRelativeToEmptyCoverage.js
@@ -1,3 +1,4 @@
+import { createFileCoverage } from "istanbul-lib-coverage"
 import { fileRead } from "@dmail/helper"
 import { createOperation } from "@dmail/cancellation"
 import { jsCompile, createInstrumentPlugin } from "../jsCompile/index.js"
@@ -16,27 +17,40 @@ export const filenameRelativeToEmptyCoverage = async ({
   // we must compile to get the coverage object
   // without evaluating the file because it would increment coverage
   // and execute code that can be doing anything
-  const { assetMap } = await createOperation({
-    cancellationToken,
-    start: () =>
-      jsCompile({
-        input,
-        filename,
-        filenameRelative,
-        projectFolder,
-        babelPluginDescription: {
-          instrument: [createInstrumentPlugin({ predicate: () => true })],
-        },
-        remap: false,
-      }),
-  })
 
-  const coverageAsset = assetMap["coverage.json"]
-  const coverage = JSON.parse(coverageAsset)
-  // https://github.com/gotwarlost/istanbul/blob/bc84c315271a5dd4d39bcefc5925cfb61a3d174a/lib/command/common/run-with-cover.js#L229
-  Object.keys(coverage.s).forEach(function(key) {
-    coverage.s[key] = 0
-  })
+  try {
+    const { assets, assetsContent } = await createOperation({
+      cancellationToken,
+      start: () =>
+        jsCompile({
+          input,
+          filename,
+          filenameRelative,
+          projectFolder,
+          babelPluginDescription: {
+            instrument: [createInstrumentPlugin({ predicate: () => true })],
+          },
+          remap: false,
+        }),
+    })
 
-  return coverage
+    const coverageIndex = assets.indexOf("coverage.json")
+    const coverageContent = assetsContent[coverageIndex]
+    const coverage = JSON.parse(coverageContent)
+
+    // https://github.com/gotwarlost/istanbul/blob/bc84c315271a5dd4d39bcefc5925cfb61a3d174a/lib/command/common/run-with-cover.js#L229
+    Object.keys(coverage.s).forEach(function(key) {
+      coverage.s[key] = 0
+    })
+
+    return coverage
+  } catch (e) {
+    if (e && e.name === "PARSE_ERROR") {
+      // return an empty coverage for that file when
+      // it contains a syntax error
+      const coverage = createFileCoverage(filenameRelative)
+      return coverage
+    }
+    throw e
+  }
 }
