a3a5583041f9aba4b90c19fbdd8a4c52357566b6
Damien Maillard <damien.maillard@dailymotion.com>
2019-03-27 15:23:16 +0100

    add import.meta.require for nodejs

---
+++ src/launchNode/test/import-meta-require/import-meta-require.execution.test.js
@@ -0,0 +1,35 @@
+import { assert } from "@dmail/assert"
+import { projectFolder } from "../../../../projectFolder.js"
+import { launchAndExecute } from "../../../launchAndExecute/index.js"
+import { startCompileServer } from "../../../server-compile/index.js"
+import { launchNode } from "../../launchNode.js"
+
+const testFolder = `${projectFolder}/src/launchNode/test/import-meta-require`
+const filenameRelative = `import-meta-require.js`
+const compileInto = ".dist"
+const babelPluginDescription = {}
+
+;(async () => {
+  const sourceOrigin = `file://${testFolder}`
+
+  const { origin: compileServerOrigin } = await startCompileServer({
+    projectFolder: testFolder,
+    compileInto,
+    babelPluginDescription,
+  })
+
+  const actual = await launchAndExecute({
+    launch: (options) => launchNode({ ...options, compileInto, sourceOrigin, compileServerOrigin }),
+    mirrorConsole: true,
+    filenameRelative,
+    verbose: true,
+    collectNamespace: true,
+  })
+  const expected = {
+    status: "completed",
+    namespace: {
+      default: 42,
+    },
+  }
+  assert({ actual, expected })
+})()
--- src/platform/node/system/createNodeSystem.js
+++ src/platform/node/system/createNodeSystem.js
@@ -1,6 +1,13 @@
+import { Module } from "module"
 import "systemjs/dist/system.js"
-import { resolveImport, remapResolvedImport } from "@jsenv/module-resolution"
+import {
+  resolveImport,
+  remapResolvedImport,
+  pathnameToDirname,
+  hrefToPathname,
+} from "@jsenv/module-resolution"
 import { isNativeNodeModuleBareSpecifier } from "@jsenv/module-resolution/dist/src/isNativeNodeModuleBareSpecifier.js"
+import { hrefToFilenameRelative } from "../../hrefToFilenameRelative.js"
 import { fromFunctionReturningNamespace, fromHref } from "../../registerModuleFrom.js"
 import { fetchSource } from "../fetchSource.js"
 import { moduleSourceToSystemRegisteredModule } from "../moduleSourceToSystemRegisteredModule.js"
@@ -60,9 +67,41 @@ export const createNodeSystem = ({
   }
 
   // https://github.com/systemjs/systemjs/blob/master/docs/hooks.md#createcontexturl---object
-  // nodeSystem.createContext = (url) => {
-  //   return { url }
-  // }
+  nodeSystem.createContext = (url) => {
+    const filenameRelative = hrefToFilenameRelative(url, { compileInto, compileServerOrigin })
+    const fileURL = `${sourceOrigin}/${filenameRelative}`
+    const filename = hrefToPathname(fileURL)
+    const require = createRequireFromFilename(filename)
+
+    return {
+      url,
+      require,
+    }
+  }
 
   return nodeSystem
 }
+
+// https://nodejs.org/api/modules.html#modules_module_createrequirefrompath_filename
+const createRequireFromFilename =
+  typeof Module.createRequireFromPath === "function"
+    ? Module.createRequireFromPath
+    : (filename) => {
+        const dirname = pathnameToDirname(filename)
+        const moduleObject = new Module(filename)
+        moduleObject.filename = filename
+        moduleObject.paths = Module._nodeModulePaths(dirname)
+
+        // https://github.com/nodejs/node/blob/f76ce0a75641991bfc235775a4747c978e0e281b/lib/module.js#L506
+        const resolve = (specifier) => Module._resolveFilename(specifier, moduleObject)
+
+        // eslint-disable-next-line import/no-dynamic-require
+        const scopedRequire = (specifier) => require(resolve(specifier))
+
+        scopedRequire.main = require.main
+        scopedRequire.extensions = require.extensions
+        scopedRequire.cache = require.cache
+        scopedRequire.resolve = resolve
+
+        return scopedRequire
+      }
