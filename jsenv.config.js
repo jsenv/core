3874b54c0bb042ee3e4e6fc5b082f34d2f64321e
Damien Maillard <damien.maillard@dailymotion.com>
2019-04-11 18:19:26 +0200

    ensure readImportMap is not memoized

--- jsenv.config.js
+++ jsenv.config.js
@@ -1,6 +1,10 @@
 const { babelConfigMap } = require("./node_modules/@jsenv/babel-config-map/index.js")
 
-exports.readImportMap = () => require("./importMap.json")
+exports.readImportMap = () => {
+  const importMap = require("./importMap.json")
+  delete require.cache[require.resolve("./importMap.json")]
+  return importMap
+}
 
 const projectFolder = __dirname
 exports.projectFolder = projectFolder
