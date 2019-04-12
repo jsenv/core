4551a2ffddb17c10370f14a25a6986ba59b16c7e
Damien Maillard <damien.maillard@dailymotion.com>
2019-04-12 11:23:42 +0200

    enable only one failing test on travis

--- script/test/test.config.js
+++ script/test/test.config.js
@@ -30,7 +30,7 @@ const testDescription = {
   //     launch: launchNode,
   //   },
   // },
-  "/test/launch-node/**/*.test.js": {
+  "/test/launch-node/debug-export-named/*.test.js": {
     node: {
       launch: launchNode,
     },
