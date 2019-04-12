73139503b7987573fd71207432c702dc5181fb83
Damien Maillard <damien.maillard@dailymotion.com>
2019-04-12 12:05:28 +0200

    update to latest eslint import resolver

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,13 +1,11 @@
 const { createConfig } = require("@jsenv/eslint-config")
-const { readImportMap, projectFolder } = require("./jsenv.config.js")
+const { projectFolder } = require("./jsenv.config.js")
 
 const config = createConfig()
 config.rules["import/no-absolute-path"] = ["off"]
 config.settings["import/resolver"] = {
   [`${projectFolder}/node_modules/@jsenv/eslint-import-resolver/dist/node/main.js`]: {
-    importMap: readImportMap(),
     projectFolder,
-    verbose: false,
   },
 }
 
