9a7492268e5b3b77f0acb5e4a489ceac1b3346cf
Damien Maillard <damien.maillard@dailymotion.com>
2019-04-15 10:31:35 +0200

    avoid useless dependencyMap inside generateImportMap

--- src/import-map/generateImportMapForProjectNodeModules.js
+++ src/import-map/generateImportMapForProjectNodeModules.js
@@ -1,6 +1,7 @@
 import { basename } from "path"
 import { normalizePathname, pathnameToDirname } from "@jsenv/module-resolution"
 import { fileWrite } from "@dmail/helper"
+import { catchAsyncFunctionCancellation } from "../cancellationHelper.js"
 import { readPackageData } from "./node-module-resolution/readPackageData.js"
 import { resolveNodeModule } from "./node-module-resolution/resolveNodeModule.js"
 import { packageDataToMain } from "./node-module-resolution/packageDataToMain.js"
@@ -17,198 +18,186 @@ export const generateImportMapForProjectNodeModules = async ({
   writeImportMapFile = true,
   logImportMapFilePath = true,
   throwUnhandled = true,
-}) => {
-  projectFolder = normalizePathname(projectFolder)
-  const topLevelPackageFilename = `${projectFolder}/package.json`
-  const topLevelImporterName = basename(pathnameToDirname(topLevelPackageFilename))
+}) =>
+  catchAsyncFunctionCancellation(async () => {
+    projectFolder = normalizePathname(projectFolder)
+    const topLevelPackageFilename = `${projectFolder}/package.json`
+    const topLevelImporterName = basename(pathnameToDirname(topLevelPackageFilename))
 
-  const dependenciesCache = {}
-  const findDependency = ({ importerFilename, nodeModuleName }) => {
-    if (importerFilename in dependenciesCache === false) {
-      dependenciesCache[importerFilename] = {}
-    }
-    if (nodeModuleName in dependenciesCache[importerFilename]) {
-      return dependenciesCache[importerFilename][nodeModuleName]
-    }
+    const dependenciesCache = {}
+    const findDependency = ({ importerFilename, nodeModuleName }) => {
+      if (importerFilename in dependenciesCache === false) {
+        dependenciesCache[importerFilename] = {}
+      }
+      if (nodeModuleName in dependenciesCache[importerFilename]) {
+        return dependenciesCache[importerFilename][nodeModuleName]
+      }
 
-    const dependencyPromise = resolveNodeModule({
-      rootFolder: projectFolder,
-      importerFilename,
-      nodeModuleName,
-    })
-    dependenciesCache[importerFilename][nodeModuleName] = dependencyPromise
-    return dependencyPromise
-  }
-
-  const imports = {}
-  const scopes = {}
-
-  const addImportMapping = ({ from, to }) => {
-    imports[from] = to
-  }
-
-  const addScopedImportMapping = ({ scope, from, to }) => {
-    scopes[scope] = {
-      ...(scopes[scope] || {}),
-      [from]: to,
-    }
-  }
-
-  const addMapping = ({ importerName, from, to }) => {
-    if (importerName === topLevelImporterName) {
-      addImportMapping({ from, to })
-    } else {
-      addScopedImportMapping({ scope: `/${importerName}/`, from, to })
-    }
-  }
-
-  const visit = async ({ packageFilename, packageData }) => {
-    const isTopLevel = packageFilename === topLevelPackageFilename
-
-    if (!isTopLevel && !packageMayNeedRemapping(packageData)) return
-
-    const importerName = isTopLevel
-      ? topLevelImporterName
-      : pathnameToDirname(packageFilename.slice(`${projectFolder}/`.length))
-    const { dependencies = {}, devDependencies = {} } = packageData
-
-    const arrayOfDependencyToRemap = Object.keys({
-      ...dependencies,
-      ...(remapDevDependencies && isTopLevel ? devDependencies : {}),
-    }).filter((dependencyName) => {
-      return remapPredicate({
-        importerName,
-        isTopLevel,
-        dependencyName,
-        isDev: dependencyName in devDependencies,
+      const dependencyPromise = resolveNodeModule({
+        rootFolder: projectFolder,
+        importerFilename,
+        nodeModuleName,
       })
-    })
+      dependenciesCache[importerFilename][nodeModuleName] = dependencyPromise
+      return dependencyPromise
+    }
 
-    const dependencyMap = {}
-    await Promise.all(
-      arrayOfDependencyToRemap.map(async (dependencyName) => {
-        const dependency = await findDependency({
-          importerFilename: packageFilename,
-          nodeModuleName: dependencyName,
-        })
-        if (!dependency) {
-          throw new Error(
-            createNodeModuleNotFoundMessage({
-              projectFolder,
-              importerFilename: packageFilename,
-              nodeModuleName: dependencyName,
-            }),
-          )
-        }
+    const imports = {}
+    const scopes = {}
 
-        const {
-          packageData: dependencyPackageData,
-          packageFilename: dependencyPackageFilename,
-        } = dependency
+    const addImportMapping = ({ from, to }) => {
+      imports[from] = to
+    }
 
-        const dependencyActualFolder = pathnameToDirname(dependencyPackageFilename)
-        const dependencyActualPathname = dependencyActualFolder.slice(projectFolder.length)
-        const dependencyExpectedFolder = `${pathnameToDirname(
-          packageFilename,
-        )}/node_modules/${dependencyName}`
-        const dependencyExpectedPathname = dependencyExpectedFolder.slice(projectFolder.length)
-
-        dependencyMap[dependencyName] = {
-          packageFilename: dependencyPackageFilename,
-          packageData: dependencyPackageData,
-          actualPathname: dependencyActualPathname,
-          expectedPathname: dependencyExpectedPathname,
-        }
-      }),
-    )
-
-    await Promise.all(
-      Object.keys(dependencyMap).map((dependencyName) => {
-        const { packageData, packageFilename, actualPathname, expectedPathname } = dependencyMap[
-          dependencyName
-        ]
-        const moved = actualPathname !== expectedPathname
-
-        if (remapFolder) {
-          const from = `${dependencyName}/`
-          const to = `${actualPathname}/`
-
-          addMapping({ importerName, from, to })
-          if (moved) {
-            addScopedImportMapping({ scope: `/${importerName}/`, from, to })
-          }
-        }
-
-        if (remapMain) {
-          const dependencyMain = packageDataToMain(packageData, packageFilename)
-          const from = dependencyName
-          const to = `${actualPathname}/${dependencyMain}`
-
-          addMapping({ importerName, from, to })
-          if (moved) {
-            addScopedImportMapping({ scope: `/${importerName}/`, from, to })
-          }
-        }
-
-        if (scopeOriginRelativePerModule) {
-          addScopedImportMapping({
-            scope: `${expectedPathname}/`,
-            from: `${expectedPathname}/`,
-            to: `${actualPathname}/`,
-          })
-          addScopedImportMapping({
-            scope: `${expectedPathname}/`,
-            from: `/`,
-            to: `${actualPathname}/`,
-          })
-
-          if (moved) {
-            addScopedImportMapping({
-              scope: `/${importerName}/`,
-              from: `${expectedPathname}/`,
-              to: `${actualPathname}/`,
-            })
-            addScopedImportMapping({
-              scope: `/${importerName}/`,
-              from: `${actualPathname}/`,
-              to: `${actualPathname}/`,
-            })
-          }
-        }
-
-        return visit({
-          packageFilename,
-          packageData,
-        })
-      }),
-    )
-  }
-
-  const start = async () => {
-    const topLevelPackageData = await readPackageData({ filename: topLevelPackageFilename })
-    await visit({
-      packageFilename: topLevelPackageFilename,
-      packageData: topLevelPackageData,
-    })
-    const importMap = sortImportMap({ imports, scopes })
-    if (writeImportMapFile) {
-      const importMapFilename = `${projectFolder}/${importMapFilenameRelative}`
-      await fileWrite(importMapFilename, JSON.stringify(importMap, null, "  "))
-      if (logImportMapFilePath) {
-        console.log(`-> ${importMapFilename}`)
+    const addScopedImportMapping = ({ scope, from, to }) => {
+      scopes[scope] = {
+        ...(scopes[scope] || {}),
+        [from]: to,
       }
     }
-    return importMap
-  }
 
-  const promise = start()
-  if (!throwUnhandled) return promise
-  return promise.catch((e) => {
-    setTimeout(() => {
-      throw e
+    const addMapping = ({ importerName, from, to }) => {
+      if (importerName === topLevelImporterName) {
+        addImportMapping({ from, to })
+      } else {
+        addScopedImportMapping({ scope: `/${importerName}/`, from, to })
+      }
+    }
+
+    const visit = async ({ packageFilename, packageData }) => {
+      const isTopLevel = packageFilename === topLevelPackageFilename
+
+      if (!isTopLevel && !packageMayNeedRemapping(packageData)) return
+
+      const importerName = isTopLevel
+        ? topLevelImporterName
+        : pathnameToDirname(packageFilename.slice(`${projectFolder}/`.length))
+      const { dependencies = {}, devDependencies = {} } = packageData
+
+      const arrayOfDependencyToRemap = Object.keys({
+        ...dependencies,
+        ...(remapDevDependencies && isTopLevel ? devDependencies : {}),
+      }).filter((dependencyName) =>
+        remapPredicate({
+          importerName,
+          isTopLevel,
+          dependencyName,
+          isDev: dependencyName in devDependencies,
+        }),
+      )
+
+      await Promise.all(
+        arrayOfDependencyToRemap.map(async (dependencyName) => {
+          const dependency = await findDependency({
+            importerFilename: packageFilename,
+            nodeModuleName: dependencyName,
+          })
+          if (!dependency) {
+            throw new Error(
+              createNodeModuleNotFoundMessage({
+                projectFolder,
+                importerFilename: packageFilename,
+                nodeModuleName: dependencyName,
+              }),
+            )
+          }
+
+          const {
+            packageData: dependencyPackageData,
+            packageFilename: dependencyPackageFilename,
+          } = dependency
+
+          const dependencyActualFolder = pathnameToDirname(dependencyPackageFilename)
+          const dependencyActualPathname = dependencyActualFolder.slice(projectFolder.length)
+          const dependencyExpectedFolder = `${pathnameToDirname(
+            packageFilename,
+          )}/node_modules/${dependencyName}`
+          const dependencyExpectedPathname = dependencyExpectedFolder.slice(projectFolder.length)
+          const moved = dependencyActualPathname !== dependencyExpectedPathname
+
+          if (remapFolder) {
+            const from = `${dependencyName}/`
+            const to = `${dependencyActualPathname}/`
+
+            addMapping({ importerName, from, to })
+            if (moved) {
+              addScopedImportMapping({ scope: `/${importerName}/`, from, to })
+            }
+          }
+
+          if (remapMain) {
+            const dependencyMain = packageDataToMain(
+              dependencyPackageData,
+              dependencyPackageFilename,
+            )
+            const from = dependencyName
+            const to = `${dependencyActualPathname}/${dependencyMain}`
+
+            addMapping({ importerName, from, to })
+            if (moved) {
+              addScopedImportMapping({ scope: `/${importerName}/`, from, to })
+            }
+          }
+
+          if (scopeOriginRelativePerModule) {
+            addScopedImportMapping({
+              scope: `${dependencyExpectedPathname}/`,
+              from: `${dependencyExpectedPathname}/`,
+              to: `${dependencyActualPathname}/`,
+            })
+            addScopedImportMapping({
+              scope: `${dependencyExpectedPathname}/`,
+              from: `/`,
+              to: `${dependencyActualPathname}/`,
+            })
+
+            if (moved) {
+              addScopedImportMapping({
+                scope: `/${importerName}/`,
+                from: `${dependencyExpectedPathname}/`,
+                to: `${dependencyActualPathname}/`,
+              })
+              addScopedImportMapping({
+                scope: `/${importerName}/`,
+                from: `${dependencyActualPathname}/`,
+                to: `${dependencyActualPathname}/`,
+              })
+            }
+          }
+
+          return visit({
+            packageFilename: dependencyPackageFilename,
+            packageData: dependencyPackageData,
+          })
+        }),
+      )
+    }
+
+    const start = async () => {
+      const topLevelPackageData = await readPackageData({ filename: topLevelPackageFilename })
+      await visit({
+        packageFilename: topLevelPackageFilename,
+        packageData: topLevelPackageData,
+      })
+      const importMap = sortImportMap({ imports, scopes })
+      if (writeImportMapFile) {
+        const importMapFilename = `${projectFolder}/${importMapFilenameRelative}`
+        await fileWrite(importMapFilename, JSON.stringify(importMap, null, "  "))
+        if (logImportMapFilePath) {
+          console.log(`-> ${importMapFilename}`)
+        }
+      }
+      return importMap
+    }
+
+    const promise = start()
+    if (!throwUnhandled) return promise
+    return promise.catch((e) => {
+      setTimeout(() => {
+        throw e
+      })
     })
   })
-}
 
 const sortImportMap = (importMap) => {
   const orderedImportMap = {
