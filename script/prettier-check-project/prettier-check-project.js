e8291c82d9e61af7b864365dcd667ac09d9ec686
Damien Maillard <damien.maillard@dailymotion.com>
2019-04-15 16:52:10 +0200

    update prettier-check-project to latest

--- script/prettier-check-project/prettier-check-project.js
+++ script/prettier-check-project/prettier-check-project.js
@@ -1,26 +1,14 @@
-const { prettierCheckProject } = require("@jsenv/prettier-check-project")
+const {
+  prettierCheckProject,
+  extendDefaultPrettifyMetaMap,
+} = require("@jsenv/prettier-check-project")
 const { projectFolder } = require("../../jsenv.config.js")
 
 prettierCheckProject({
   projectFolder,
-  prettifyDescription: {
-    // js
-    "/index.js": true,
-    "/src/**/*.js": true,
-    "/test/**/*.js": true,
-    "/script/**/*.js": true,
-    "/**/**syntax-error**.js": false,
+  prettifyMetaMap: extendDefaultPrettifyMetaMap({
     "/**/.dist/": false,
     "/**/dist/": false,
-    // json
-    "/src/**/*.json": true,
-    "/test/**/*.json": true,
-    "/script/**/*.json": true,
-    // md
-    "/readme.md": true,
-    "/doc/**.md": true,
-    "/src/**/*.md": true,
-    "/test/**/*.md": true,
-    "/script/**/*.md": true,
-  },
+    "/**/**syntax-error**.js": false,
+  }),
 })
