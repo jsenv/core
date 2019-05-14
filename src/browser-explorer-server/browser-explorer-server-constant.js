34c1c98163d970027c4404152d28007ff14cb314
Damien Maillard <damien.maillard@dailymotion.com>
2019-05-14 16:43:10 +0200

    fix browser explorer server 404

--- src/browser-explorer-server/browser-explorer-server-constant.js
+++ src/browser-explorer-server/browser-explorer-server-constant.js
@@ -7,7 +7,7 @@ export const DEFAULT_IMPORT_MAP_FILENAME_RELATIVE = "importMap.json"
 export const DEFAULT_BROWSER_GROUP_RESOLVER_FILENAME_RELATIVE =
   "src/browser-group-resolver/index.js"
 
-export const DEFAULT_BROWSER_CLIENT_FOLDER_RELATIVE = `browser-client`
+export const DEFAULT_BROWSER_CLIENT_FOLDER_RELATIVE = `src/browser-client`
 
 export const DEFAULT_BROWSABLE_DESCRIPTION = {
   "/index.js": true,
