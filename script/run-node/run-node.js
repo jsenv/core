eef410bd2155177587faacdbc6f2aa1a79fc626f
Damien Maillard <dmaillard06@gmail.com>
2019-05-18 16:59:57 +0200

    prepare to fix run node on windows

--- script/run-node/run-node.js
+++ script/run-node/run-node.js
@@ -2,7 +2,7 @@ const { execute, launchNode } = require("@jsenv/core")
 const { projectFolder } = require("../../jsenv.config.js")
 const { getFromProcessArguments } = require("./getFromProcessArguments.js")
 
-const filenameRelative = getFromProcessArguments("file")
+const filenameRelative = getFromProcessArguments("file").replace(/\\/g, "/")
 
 execute({
   projectFolder,
