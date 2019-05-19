cce532ca694144dc0bf58c38c41f90d80aaabcca
Damien Maillard <dmaillard06@gmail.com>
2019-05-19 16:02:08 +0200

    fix jsenv path on windows

--- src/JSENV_PATH-2.js
+++ src/JSENV_PATH-2.js
@@ -1,12 +1,17 @@
 import { resolve } from "path"
 import { hrefToPathname } from "@jsenv/module-resolution"
-import { operatingSystemPathToPathname } from "./operating-system-path.js"
+import {
+  pathnameToOperatingSystemPath,
+  operatingSystemPathToPathname,
+} from "./operating-system-path.js"
 
 let jsenvPath
 if (typeof __filename === "string") {
   jsenvPath = resolve(__filename, "../../../") // get ride of dist/node/main.js
 } else {
-  jsenvPath = resolve(hrefToPathname(import.meta.url), "../../") // get ride of src/ROOT_FOLDER.js
+  const selfPathname = hrefToPathname(import.meta.url)
+  const selfPath = pathnameToOperatingSystemPath(selfPathname)
+  jsenvPath = resolve(selfPath, "../../") // get ride of src/JSENV_PATH.js
 }
 
 export const JSENV_PATH = jsenvPath
