cce0bf9f5dfd365bc0e3b12950b1f057af2e2c03
Damien Maillard <damien.maillard@dailymotion.com>
2019-05-24 18:26:00 +0200

    fix how jsenv file self coverage works

--- script/generate-coverage/generate-coverage.js
+++ script/generate-coverage/generate-coverage.js
@@ -1,11 +1,6 @@
-const { cover, jsenvCoverDescription } = require("@jsenv/core")
+const { cover } = require("@jsenv/core")
 const { projectPath } = require("../../jsenv.config.js")
 
 cover({
   projectPath,
-  coverDescription: {
-    ...jsenvCoverDescription,
-    "/node_modules/@jsenv/core/": true, // also cover jsenv internal files
-  },
-  logCoverageTable: true,
 })
