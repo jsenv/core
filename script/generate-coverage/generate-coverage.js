6afe9b4383d20f000b5cb4f4eaa09edcf8e60d0d
Damien Maillard <damien.maillard@dailymotion.com>
2019-05-24 17:41:29 +0200

    jsenvception

--- script/generate-coverage/generate-coverage.js
+++ script/generate-coverage/generate-coverage.js
@@ -1,6 +1,10 @@
-const { cover } = require("@jsenv/core")
+const { cover, jsenvCoverDescription } = require("@jsenv/core")
 const { projectPath } = require("../../jsenv.config.js")
 
 cover({
   projectPath,
+  coverDescription: {
+    ...jsenvCoverDescription,
+    "/.jsenv/": true,
+  },
 })
