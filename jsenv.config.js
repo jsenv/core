7b3087a83c8b70765ca3805db780a9304e2db3fa
Damien Maillard <damien.maillard@dailymotion.com>
2019-05-24 17:26:53 +0200

    remove useless jsenv config

--- jsenv.config.js
+++ jsenv.config.js
@@ -1,24 +1,2 @@
-const { launchNode } = require("@jsenv/core")
-const { jsenvBabelPluginMap } = require("./node_modules/@jsenv/babel-plugin-map/index.js")
-
 const projectPath = __dirname
 exports.projectPath = projectPath
-
-const testDescription = {
-  "/test/**/*.test.js": {
-    node: {
-      launch: launchNode,
-    },
-  },
-}
-exports.testDescription = testDescription
-
-const coverDescription = {
-  "/index.js": true,
-  "/src/**/*.js": true,
-  "/**/*.test.*": false, // contains .test. -> nope
-  "/**/test/": false, // inside a test folder -> nope
-}
-exports.coverDescription = coverDescription
-
-exports.babelPluginMap = jsenvBabelPluginMap
