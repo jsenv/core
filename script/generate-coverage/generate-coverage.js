8285409e784010ae180a565245053ca6ed8a9d06
Damien Maillard <damien.maillard@dailymotion.com>
2019-05-27 15:00:17 +0200

    restore generate coverage

--- script/generate-coverage/generate-coverage.js
+++ script/generate-coverage/generate-coverage.js
@@ -1,13 +1,6 @@
-const { cover, launchNode } = require("@jsenv/core")
+const { cover } = require("@jsenv/core")
 const { projectPath } = require("../../jsenv.config.js")
 
 cover({
   projectPath,
-  executeDescription: {
-    "/test/node-launcher/log/*.test.js": {
-      node: {
-        launch: launchNode,
-      },
-    },
-  },
 })
