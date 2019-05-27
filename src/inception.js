0e0643093bfddcc5c76b6896f9a4c652b015b2fc
Damien Maillard <damien.maillard@dailymotion.com>
2019-05-27 14:59:46 +0200

    fix inception

--- src/inception.js
+++ src/inception.js
@@ -1,13 +1,14 @@
+import { readFileSync } from "fs"
 import { pathnameToOperatingSystemPath } from "@jsenv/operating-system-path"
-import { JSENV_PATHNAME } from "./JSENV_PATH.js"
+import { JSENV_PATH, JSENV_PATHNAME } from "./JSENV_PATH.js"
 
 const JSENV_RELATIVE_PATH = "/node_modules/@jsenv/core"
 
 export const relativePathInception = ({ projectPathname, relativePath }) => {
-  const projectIsJsenv = projectPathname.endsWith("/@jsenv/core")
+  const projectIsJsenv = projectPathname === JSENV_PATH || pathnameIsJsenvCore(projectPathname)
 
   if (!projectIsJsenv) {
-    const projectIsInsideJsenv = projectPathname.includes("/@jsenv/core/")
+    const projectIsInsideJsenv = projectPathname.startWith(`${JSENV_PATH}/`)
     if (projectIsInsideJsenv) {
       throw new Error(`relativePathInception work only inside jsenv.
 project path: ${pathnameToOperatingSystemPath(projectPathname)}
@@ -19,4 +20,26 @@ relative path: ${relativePath}`)
   }
 
   return relativePath
+}
+
+const pathMap = {}
+const pathnameIsJsenvCore = (pathname) => {
+  if (pathname in pathMap) return pathMap[pathname]
+
+  const value = (() => {
+    const packagePath = pathnameToOperatingSystemPath(`${pathname}/package.json`)
+    try {
+      const buffer = readFileSync(packagePath)
+      const content = String(buffer)
+      const packageData = JSON.parse(content)
+      return packageData.name === "@jsenv/core"
+    } catch (e) {
+      if (e.code === "ENOENT") return false
+      if (e.name === "SyntaxError") return false
+      throw e
+    }
+  })()
+
+  pathMap[pathname] = value
+  return value
 }
