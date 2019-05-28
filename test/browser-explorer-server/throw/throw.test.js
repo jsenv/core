c5e85a388ae0905dad026fbc4f3544b41fb6edcb
Damien Maillard <damien.maillard@dailymotion.com>
2019-05-28 16:31:21 +0200

    add two test for browser explorer server

---
+++ test/browser-explorer-server/throw/throw.test.js
@@ -0,0 +1,18 @@
+import { assert } from "@dmail/assert"
+import { importMetaURLToFolderJsenvRelativePath } from "../../../src/import-meta-url-to-folder-jsenv-relative-path.js"
+import { startBrowserExplorerServer } from "../../../src/browser-explorer-server/index.js"
+import { openBrowserPage } from "../open-browser-page.js"
+import { BROWSER_EXPLORER_SERVER_TEST_PARAM } from "../browser-explorer-server-test-param.js"
+
+const folderJsenvRelativePath = importMetaURLToFolderJsenvRelativePath(import.meta.url)
+const compileIntoRelativePath = `${folderJsenvRelativePath}/.dist`
+const fileRelativePath = `${folderJsenvRelativePath}/throw.main.js`
+
+const { origin: browserExplorerServerOrigin } = await startBrowserExplorerServer({
+  ...BROWSER_EXPLORER_SERVER_TEST_PARAM,
+  compileIntoRelativePath,
+})
+const { browser, page } = await openBrowserPage(`${browserExplorerServerOrigin}${fileRelativePath}`)
+const actual = await page.title()
+assert({ actual, expected: `browser client index` })
+browser.close()
