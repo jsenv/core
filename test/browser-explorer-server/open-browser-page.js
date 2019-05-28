57a0122ca4cc8e6a9a5561552b993e5f7cd27234
Damien Maillard <damien.maillard@dailymotion.com>
2019-05-28 11:32:53 +0200

    ensure openBrowserPage inherit coverage too

--- test/browser-explorer-server/open-browser-page.js
+++ test/browser-explorer-server/open-browser-page.js
@@ -1,8 +1,18 @@
+import { coverageMapCompose } from "../../src/coverage/executionPlanResultToCoverageMap/coverageMapCompose.js"
+
 const puppeteer = import.meta.require("puppeteer")
 
 export const openBrowserPage = async (url) => {
   const browser = await puppeteer.launch()
   const page = await browser.newPage()
   await page.goto(url)
+
+  if (process.env.COVERAGE_ENABLED === "true") {
+    const coverageMap = await page.evaluate(`(() => {
+  return window.__coverage__
+})()`)
+    global.__coverage__ = coverageMapCompose(global.__coverage__ || {}, coverageMap)
+  }
+
   return { browser, page }
 }
