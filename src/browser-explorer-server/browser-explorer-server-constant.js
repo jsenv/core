cbd5451de35c15bac6901f788808bfd6b73dbf79
Damien Maillard <damien.maillard@dailymotion.com>
2019-06-12 14:50:47 +0200

    update browser explorer server default compile into

--- src/browser-explorer-server/browser-explorer-server-constant.js
+++ src/browser-explorer-server/browser-explorer-server-constant.js
@@ -1,6 +1,6 @@
 const { jsenvBabelPluginMap } = import.meta.require("@jsenv/babel-plugin-map")
 
-export const DEFAULT_COMPILE_INTO_RELATIVE_PATH = "/dist"
+export const DEFAULT_COMPILE_INTO_RELATIVE_PATH = "/.dist"
 
 export const DEFAULT_IMPORT_MAP_RELATIVE_PATH = "/importMap.json"
 
