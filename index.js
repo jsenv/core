a5a9f587fbc961a5e5fefdbd372f9338521e63d3
Damien Maillard <damien.maillard@dailymotion.com>
2019-07-01 18:26:49 +0200

    update exports and associated comments

--- index.js
+++ index.js
@@ -1,33 +1,32 @@
 // belong to core
 export { startCompileServer } from "./src/compile-server/index.js"
-
-// belong to core
 export { launchAndExecute } from "./src/launchAndExecute/index.js"
 
-// belong to core
+// could be moved to jsenv/jsenv-execution
 export { execute } from "./src/execution/execute.js"
 
-// belong to core
+// could be moved to jsenv/jsenv-testing
 export { test } from "./src/testing/test.js"
 
-// belong to core
+// could be moved inside jsenv/jsenv-testing
 export { cover } from "./src/coverage/cover.js"
 export {
   DEFAULT_COVER_DESCRIPTION as jsenvCoverDescription,
 } from "./src/coverage/cover-constant.js"
 
-// belong to core (let's keep it in core because way easier to manage)
+// could be moved to jsenv/jsenv-bundling
 export {
+  serveBrowserGlobalBundle,
   generateGlobalBundle,
   generateCommonJsBundle,
   generateSystemJsBundle,
 } from "./src/bundling/index.js"
 
-// may be moved to jsenv/browsing-server
+// should be moved to jsenv/jsenv-exploring-server
 export { startExploringServer } from "./src/exploring-server/index.js"
 
-// must be moved later to jsenv/chromium-launcher
+// should be moved to jsenv/jsenv-chromium-launcher
 export { launchChromium } from "./src/chromium-launcher/index.js"
 
-// must be moved later to jsenv/node-launcher
+// should be moved to jsenv/jsenv-node-launcher
 export { launchNode } from "./src/node-launcher/index.js"
