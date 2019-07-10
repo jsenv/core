1168da56de98cece21cfecaff3a709bba6f7dccc
Damien Maillard <damien.maillard@dailymotion.com>
2019-07-10 15:42:25 +0200

    more exports to prepare jsenv/bundling

--- index.js
+++ index.js
@@ -2,6 +2,17 @@
 export { startCompileServer } from "./src/compile-server/index.js"
 export { launchAndExecute } from "./src/launchAndExecute/index.js"
 export { readProjectImportMap } from "./src/import-map/readProjectImportMap.js"
+export { generateGroupMap, browserScoreMap, nodeVersionScoreMap } from "./src/group-map/index.js"
+export { resolveCompileId } from "./src/platform/compile-id-resolution.js"
+export { resolveBrowserGroup } from "./src/browser-group-resolver/index.js"
+export { resolveNodeGroup } from "./src/node-group-resolver/index.js"
+export {
+  transpiler,
+  findAsyncPluginNameInbabelPluginMap,
+} from "./src/compiled-js-service/transpiler.js"
+export {
+  getOrGenerateCompiledFile,
+} from "./src/compiled-file-service/get-or-generate-compiled-file.js"
 
 // could be moved to jsenv/jsenv-execution
 export { execute } from "./src/execution/execute.js"
@@ -15,15 +26,6 @@ export {
   DEFAULT_COVER_DESCRIPTION as jsenvCoverDescription,
 } from "./src/coverage/cover-constant.js"
 export { createInstrumentPlugin } from "./src/coverage/createInstrumentPlugin.js"
-
-// could be moved to jsenv/jsenv-bundling
-export {
-  serveBrowserGlobalBundle,
-  generateNodeCommonJsBundle,
-  generateGlobalBundle,
-  generateCommonJsBundle,
-  generateSystemJsBundle,
-} from "./src/bundling/index.js"
 
 // should be moved to jsenv/jsenv-exploring-server
 export { startExploringServer } from "./src/exploring-server/index.js"
