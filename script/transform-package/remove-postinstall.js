0afba79c4453d32169ff0e4600e17069022ef262
Damien Maillard <dmaillard06@gmail.com>
2019-08-16 12:21:21 +0200

    improve postinstall script removal

--- script/transform-package/remove-postinstall.js
+++ script/transform-package/remove-postinstall.js
@@ -5,8 +5,14 @@ const PACKAGE_PATH = resolve(__dirname, "../../package.json")
 const PACKAGE_CACHED_PATH = resolve(__dirname, "../../cached-package.json")
 
 const packageString = String(readFileSync(PACKAGE_PATH))
-writeFileSync(PACKAGE_CACHED_PATH, packageString)
-
 const publishedPackageObject = JSON.parse(packageString)
-delete publishedPackageObject.scripts.postinstall
-writeFileSync(PACKAGE_PATH, JSON.stringify(publishedPackageObject, null, "  "))
+
+// if package.json has no posinstall script we either
+// 1) have nothing to do
+// 2) already have deleted it during prepublish but npm publish failed
+// so we should just keep package.json and cached verison untouched
+if (publishedPackageObject.scripts.postinstall) {
+  writeFileSync(PACKAGE_CACHED_PATH, packageString)
+  delete publishedPackageObject.scripts.postinstall
+  writeFileSync(PACKAGE_PATH, JSON.stringify(publishedPackageObject, null, "  "))
+}
--- script/transform-package/restore-postinstall.js
+++ script/transform-package/restore-postinstall.js
@@ -1,8 +1,10 @@
-const { readFileSync, writeFileSync, unlinkSync } = require("fs")
+const { existsSync, readFileSync, writeFileSync, unlinkSync } = require("fs")
 const { resolve } = require("path")
 
 const PACKAGE_PATH = resolve(__dirname, "../../package.json")
 const PACKAGE_CACHED_PATH = resolve(__dirname, "../../cached-package.json")
 
-writeFileSync(PACKAGE_PATH, readFileSync(PACKAGE_CACHED_PATH))
-unlinkSync(PACKAGE_CACHED_PATH)
+if (existsSync(PACKAGE_CACHED_PATH)) {
+  writeFileSync(PACKAGE_PATH, readFileSync(PACKAGE_CACHED_PATH))
+  unlinkSync(PACKAGE_CACHED_PATH)
+}
