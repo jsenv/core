fde54afdcf3e44f75fa04b8aea65834a7474e8c2
Damien Maillard <damien.maillard@dailymotion.com>
2019-08-23 11:45:21 +0200

    remove execute from jsenv core

--- index.js
+++ index.js
@@ -2,7 +2,4 @@ export { transformSource, findAsyncPluginNameInbabelPluginMap } from "./src/tran
 export { compileJs } from "./src/compileJs.js"
 export { convertCommonJsWithBabel } from "./src/convert-commonjs/convertCommonJsWithBabel.js"
 export { convertCommonJsWithRollup } from "./src/convert-commonjs/convertCommonJsWithRollup.js"
-export { launchAndExecute } from "./src/launchAndExecute/index.js"
-
-// could be moved to jsenv/jsenv-execution
-export { execute } from "./src/execution/execute.js"
+export { launchAndExecute } from "./src/launchAndExecute/launchAndExecute.js"
--- src/launchAndExecute/index.js
+++
@@ -1,1 +0,0 @@
-export { launchAndExecute } from "./launchAndExecute.js"
