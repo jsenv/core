a7e1aaebb51b32144fcaa6b279b753a5b5293e8b
Damien Maillard <damien.maillard@dailymotion.com>
2019-08-23 10:49:21 +0200

    file org

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
-export { launchAndExecute } from "./src/launchAndExecute/index.js"
 export { transformSource, findAsyncPluginNameInbabelPluginMap } from "./src/transformSource.js"
+export { compileJs } from "./src/compileJs.js"
 export { convertCommonJsWithBabel } from "./src/convert-commonjs/convertCommonJsWithBabel.js"
 export { convertCommonJsWithRollup } from "./src/convert-commonjs/convertCommonJsWithRollup.js"
+export { launchAndExecute } from "./src/launchAndExecute/index.js"
 
 // could be moved to jsenv/jsenv-execution
 export { execute } from "./src/execution/execute.js"
---
+++ src/source-mapping-url.js
@@ -0,0 +1,53 @@
+export const writeSourceMappingURL = (source, location) => `${source}
+${"//#"} sourceMappingURL=${location}`
+
+export const updateSourceMappingURL = (source, callback) => {
+  const sourceMappingUrlRegExp = /\/\/# ?sourceMappingURL=([^\s'"]+)/g
+  let lastSourceMappingUrl
+  let matchSourceMappingUrl
+  while ((matchSourceMappingUrl = sourceMappingUrlRegExp.exec(source))) {
+    lastSourceMappingUrl = matchSourceMappingUrl
+  }
+  if (lastSourceMappingUrl) {
+    const index = lastSourceMappingUrl.index
+    const before = source.slice(0, index)
+    const after = source.slice(index)
+    const mappedAfter = after.replace(sourceMappingUrlRegExp, (match, firstGroup) => {
+      return `${"//#"} sourceMappingURL=${callback(firstGroup)}`
+    })
+    return `${before}${mappedAfter}`
+  }
+  return source
+}
+
+export const readSourceMappingURL = (source) => {
+  let sourceMappingURL
+  updateSourceMappingURL(source, (value) => {
+    sourceMappingURL = value
+  })
+  return sourceMappingURL
+}
+
+export const parseSourceMappingURL = (source) => {
+  const sourceMappingURL = readSourceMappingURL(source)
+
+  if (!sourceMappingURL) return null
+
+  const base64Prefix = "data:application/json;charset=utf-8;base64,"
+  if (sourceMappingURL.startsWith(base64Prefix)) {
+    const mapBase64Source = sourceMappingURL.slice(base64Prefix.length)
+    const sourcemapString = new Buffer(mapBase64Source, "base64").toString("utf8")
+    return { sourcemapString }
+  }
+
+  return {
+    sourcemapURL: sourceMappingURL,
+  }
+}
+
+export const writeOrUpdateSourceMappingURL = (source, location) => {
+  if (readSourceMappingURL(source)) {
+    return updateSourceMappingURL(source, location)
+  }
+  return writeSourceMappingURL(source, location)
+}
