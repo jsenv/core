a72b0ea35aafbe4ad378c5e9d03f017f34272797
Damien Maillard <damien.maillard@dailymotion.com>
2019-10-31 18:19:35 +0100

    update convert commonjs test

--- test/startCompileServer/convert-commonjs/convert-commonjs.xtest.js
+++ test/startCompileServer/convert-commonjs/convert-commonjs.xtest.js
@@ -1,3 +1,17 @@
+/**
+ * cannot really be tested for now because:
+ *
+ * convertCommonJsWithBabel exported by @jsenv/commonjs-converter
+ * uses transformJs which but @jsenv/commonjs-converter package.json
+ * does not contain @jsenv/core
+ *
+ * @jsenv/commonjs-converter is something we will also absorb back here
+ * especially because it introduces only a fwe new dependencies
+ * and is part of jsenv overall features
+ *
+ *
+ */
+
 import { assert } from "@dmail/assert"
 import { resolveDirectoryUrl, resolveFileUrl, fileUrlToRelativePath } from "src/private/urlUtils.js"
 import { startCompileServer } from "../../../index.js"
@@ -11,6 +25,10 @@ const fileRelativePath = fileUrlToRelativePath(
   fileUrl,
   COMPILE_SERVER_TEST_PARAMS.projectDirectoryUrl,
 )
+const compileDirectoryRelativePath = fileUrlToRelativePath(
+  compileDirectoryUrl,
+  COMPILE_SERVER_TEST_PARAMS.projectDirectoryUrl,
+)
 const compileServer = await startCompileServer({
   ...COMPILE_SERVER_TEST_PARAMS,
   compileDirectoryUrl,
@@ -19,7 +37,7 @@ const compileServer = await startCompileServer({
       convertCommonJsWithBabel({ ...options, processEnvNodeEnv: "production" }),
   },
 })
-const fileServerUrl = `${compileServer.origin}/.dist/best/${fileRelativePath}`
+const fileServerUrl = `${compileServer.origin}/${compileDirectoryRelativePath}best/${fileRelativePath}`
 
 const fileResponse = await fetch(fileServerUrl)
 const actual = {
