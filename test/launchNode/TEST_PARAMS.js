71ad48b73b6a093679b17ad5002760528edf876a
Damien Maillard <damien.maillard@dailymotion.com>
2019-11-06 11:44:16 +0100

    prepare testing class.test.js for launchNode

---
+++ test/launchNode/TEST_PARAMS.js
@@ -0,0 +1,19 @@
+import { jsenvCoreDirectoryUrl } from "../../src/private/jsenvCoreDirectoryUrl.js"
+import { testBabelPluginMap } from "../testBabelPluginMap.js"
+
+export const START_COMPILE_SERVER_TEST_PARAMS = {
+  logLevel: "off",
+  projectDirectoryUrl: jsenvCoreDirectoryUrl,
+  babelPluginMap: testBabelPluginMap,
+  compileDirectoryClean: true,
+  compileGroupCount: 2,
+}
+
+export const EXECUTE_TEST_PARAMS = {
+  collectNamespace: true,
+  inheritCoverage: process.env.COVERAGE_ENABLED === "true",
+}
+
+export const LAUNCH_TEST_PARAMS = {
+  projectDirectoryUrl: jsenvCoreDirectoryUrl,
+}
--- test/launchNode/removeFolder.js
+++
@@ -1,9 +0,0 @@
-const rimraf = import.meta.require("rimraf")
-
-export const removeFolder = (foldername) =>
-  new Promise((resolve, reject) =>
-    rimraf(foldername, (error) => {
-      if (error) reject(error)
-      else resolve()
-    }),
-  )
