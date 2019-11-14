217f52e8ef1b92413be2683e5d936e49df3ef84b
Damien Maillard <damien.maillard@dailymotion.com>
2019-11-14 17:14:53 +0100

    update public exports

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
+// will certainly move to src/internal
+// and not be exported anymore
 export { computeBabelPluginMapForPlatform } from "./src/computeBabelPluginMapForPlatform.js"
 export { computeJsenvPluginMapForPlatform } from "./src/computeJsenvPluginMapForPlatform.js"
+export { launchAndExecute } from "./src/launchAndExecute.js"
+export { startCompileServer } from "./src/startCompileServer.js"
+
 export { convertCommonJsWithBabel } from "./src/convertCommonJsWithBabel.js"
 export { convertCommonJsWithRollup } from "./src/convertCommonJsWithRollup.js"
 export { execute } from "./src/execute.js"
@@ -13,6 +18,5 @@ export { jsenvBabelPluginMap } from "./src/jsenvBabelPluginMap.js"
 export { jsenvBrowserScoreMap } from "./src/jsenvBrowserScoreMap.js"
 export { jsenvNodeVersionScoreMap } from "./src/jsenvNodeVersionScoreMap.js"
 export { jsenvPluginCompatMap } from "./src/jsenvPluginCompatMap.js"
-export { launchAndExecute } from "./src/launchAndExecute.js"
 export { launchNode } from "./src/launchNode.js"
-export { startCompileServer } from "./src/startCompileServer.js"
+export { startExploring } from "./src/startExploring.js"
