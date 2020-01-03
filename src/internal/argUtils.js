41e511921fa050902af21671821403638b6b16b8
Damien Maillard <damien.maillard@dailymotion.com>
2020-01-03 15:21:54 +0100

    use @jsenv/util to validate args

--- src/internal/argUtils.js
+++ src/internal/argUtils.js
@@ -1,28 +1,7 @@
-import { filePathToUrl, hasScheme, ensureUrlTrailingSlash } from "./urlUtils.js"
-import { assertDirectoryExists } from "./filesystemUtils.js"
+import { assertAndNormalizeDirectoryUrl, assertDirectoryExists, urlIsInsideOf } from "@jsenv/util"
 
 export const assertProjectDirectoryUrl = ({ projectDirectoryUrl }) => {
-  if (projectDirectoryUrl instanceof URL) {
-    projectDirectoryUrl = projectDirectoryUrl.href
-  }
-
-  if (typeof projectDirectoryUrl === "string") {
-    const url = hasScheme(projectDirectoryUrl)
-      ? projectDirectoryUrl
-      : filePathToUrl(projectDirectoryUrl)
-
-    if (!url.startsWith("file://")) {
-      throw new Error(
-        `projectDirectoryUrl must starts with file://, received ${projectDirectoryUrl}`,
-      )
-    }
-
-    return ensureUrlTrailingSlash(url)
-  }
-
-  throw new TypeError(
-    `projectDirectoryUrl must be a string or an url, received ${projectDirectoryUrl}`,
-  )
+  return assertAndNormalizeDirectoryUrl(projectDirectoryUrl)
 }
 
 export const assertProjectDirectoryExists = ({ projectDirectoryUrl }) => {
@@ -38,7 +17,7 @@ export const assertImportMapFileRelativeUrl = ({ importMapFileRelativeUrl }) => 
 }
 
 export const assertImportMapFileInsideProject = ({ importMapFileUrl, projectDirectoryUrl }) => {
-  if (!importMapFileUrl.startsWith(projectDirectoryUrl)) {
+  if (!urlIsInsideOf(importMapFileUrl, projectDirectoryUrl)) {
     throw new Error(`importmap file must be inside project directory
 --- import map file url ---
 ${importMapFileUrl}
