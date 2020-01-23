eb5ebcbaea30d04f95ce9848d13b91fa0ffa48d4
Damien Maillard <damien.maillard@dailymotion.com>
2020-01-23 15:50:49 +0100

    update browser platform test to node13

--- test/startCompileServer/browser-platform/browser-platform.test.js
+++ test/startCompileServer/browser-platform/browser-platform.test.js
@@ -1,14 +1,15 @@
 import { assert } from "@jsenv/assert"
-import { resolveDirectoryUrl, urlToRelativeUrl } from "@jsenv/util"
+import { resolveUrl, urlToRelativeUrl } from "@jsenv/util"
 import { fetchUrl } from "@jsenv/server"
-import { jsenvCoreDirectoryUrl } from "internal/jsenvCoreDirectoryUrl.js"
-import { startCompileServer } from "internal/compiling/startCompileServer.js"
+import { jsenvCoreDirectoryUrl } from "../../../src/internal/jsenvCoreDirectoryUrl.js"
+import { startCompileServer } from "../../../src/internal/compiling/startCompileServer.js"
 import { COMPILE_SERVER_TEST_PARAMS } from "../TEST_PARAMS.js"
 
-const testDirectoryUrl = resolveDirectoryUrl("./", import.meta.url)
+const testDirectoryUrl = resolveUrl("./", import.meta.url)
 const testDirectoryRelativeUrl = urlToRelativeUrl(testDirectoryUrl, jsenvCoreDirectoryUrl)
 const projectDirectoryUrl = jsenvCoreDirectoryUrl
 const jsenvDirectoryRelativeUrl = `${testDirectoryRelativeUrl}.jsenv/`
+
 const { origin: compileServerOrigin, outDirectoryRelativeUrl } = await startCompileServer({
   ...COMPILE_SERVER_TEST_PARAMS,
   compileServerLogLevel: "warn",
@@ -16,18 +17,15 @@ const { origin: compileServerOrigin, outDirectoryRelativeUrl } = await startComp
   jsenvDirectoryRelativeUrl,
 })
 const fileServerUrl = `${compileServerOrigin}/${outDirectoryRelativeUrl}otherwise-global-bundle/src/browserPlatform.js`
-const response = await fetchUrl(fileServerUrl)
-const actual = {
-  status: response.status,
-  statusText: response.statusText,
-  headers: response.headers,
-}
+const actual = await fetchUrl(fileServerUrl, { simplified: true })
 const expected = {
+  url: fileServerUrl,
   status: 200,
   statusText: "OK",
   headers: {
     ...actual.headers,
-    "content-type": ["application/javascript"],
+    "content-type": "application/javascript",
   },
+  body: actual.body,
 }
 assert({ actual, expected })
