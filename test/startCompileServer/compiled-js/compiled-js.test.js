939f7be6e140ac5848abac50d2ac017ef3665e3e
Damien Maillard <damien.maillard@dailymotion.com>
2020-01-23 15:57:51 +0100

    update compiled js test

--- test/startCompileServer/compiled-js/compiled-js.test.js
+++ test/startCompileServer/compiled-js/compiled-js.test.js
@@ -1,13 +1,13 @@
 import { basename } from "path"
 import { assert } from "@jsenv/assert"
-import { resolveDirectoryUrl, urlToRelativeUrl } from "@jsenv/util"
+import { resolveUrl, urlToRelativeUrl } from "@jsenv/util"
 import { fetchUrl } from "@jsenv/server"
-import { COMPILE_ID_OTHERWISE } from "internal/CONSTANTS.js"
-import { jsenvCoreDirectoryUrl } from "internal/jsenvCoreDirectoryUrl.js"
-import { startCompileServer } from "internal/compiling/startCompileServer.js"
+import { COMPILE_ID_OTHERWISE } from "../../../src/internal/CONSTANTS.js"
+import { jsenvCoreDirectoryUrl } from "../../../src/internal/jsenvCoreDirectoryUrl.js"
+import { startCompileServer } from "../../../src/internal/compiling/startCompileServer.js"
 import { COMPILE_SERVER_TEST_PARAMS } from "../TEST_PARAMS.js"
 
-const testDirectoryUrl = resolveDirectoryUrl("./", import.meta.url)
+const testDirectoryUrl = resolveUrl("./", import.meta.url)
 const testDirectoryRelativePath = urlToRelativeUrl(testDirectoryUrl, jsenvCoreDirectoryUrl)
 const testDirectoryname = basename(testDirectoryRelativePath)
 const filename = `${testDirectoryname}.js`
@@ -17,19 +17,37 @@ const { origin: compileServerOrigin, outDirectoryRelativeUrl } = await startComp
   ...COMPILE_SERVER_TEST_PARAMS,
   jsenvDirectoryRelativeUrl,
 })
+
 const fileServerUrl = `${compileServerOrigin}/${outDirectoryRelativeUrl}${COMPILE_ID_OTHERWISE}/${fileRelativeUrl}`
 const response = await fetchUrl(fileServerUrl)
-const actual = {
-  status: response.status,
-  statusText: response.statusText,
-  headers: response.headers,
+{
+  const { status, statusText, headers } = response
+  const actual = {
+    status,
+    statusText,
+    contentType: headers.get("content-type"),
+  }
+  const expected = {
+    status: 200,
+    statusText: "OK",
+    contentType: "application/javascript",
+  }
+  assert({ actual, expected })
 }
-const expected = {
-  status: 200,
-  statusText: "OK",
-  headers: {
-    ...actual.headers,
-    "content-type": ["application/javascript"],
-  },
+// test the cache now
+{
+  const { status, statusText } = await fetchUrl(fileServerUrl, {
+    headers: {
+      "if-none-match": response.headers.get("etag"),
+    },
+  })
+  const actual = {
+    status,
+    statusText,
+  }
+  const expected = {
+    status: 304,
+    statusText: "Not Modified",
+  }
+  assert({ actual, expected })
 }
-assert({ actual, expected })
