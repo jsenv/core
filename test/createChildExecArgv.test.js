ee48745e081258f5fe4a1dc8114d3be641204fa7
Damien Maillard <damien.maillard@dailymotion.com>
2020-01-24 12:22:24 +0100

    update execargv tests

--- test/createChildExecArgv.test.js
+++ test/createChildExecArgv.test.js
@@ -1,7 +1,7 @@
 import { assert } from "@jsenv/assert"
 import { createChildExecArgv } from "../src/internal/node-launcher/createChildExecArgv.js"
 
-// inherit without debug mode
+// debug mode inherited from nothing
 {
   const actual = await createChildExecArgv({
     processExecArgv: ["--test"],
@@ -12,7 +12,7 @@ import { createChildExecArgv } from "../src/internal/node-launcher/createChildEx
   assert({ actual, expected })
 }
 
-// inherit with --inspect
+// debug mode inherited from inspect
 {
   const actual = await createChildExecArgv({
     processExecArgv: ["--before", "--inspect", "--after"],
@@ -23,7 +23,30 @@ import { createChildExecArgv } from "../src/internal/node-launcher/createChildEx
   assert({ actual, expected })
 }
 
-// inspect with nothing
+// debug mode inherited from inspect + port
+{
+  const actual = await createChildExecArgv({
+    processExecArgv: ["--before", "--inspect", "--inspect-port=10", "--after"],
+    processDebugPort: 10,
+    debugMode: "inherit",
+    debugPort: 11,
+  })
+  const expected = ["--before", "--inspect", "--inspect-port=11", "--after"]
+  assert({ actual, expected })
+}
+
+// debug mode becomes null from inspect
+{
+  const actual = await createChildExecArgv({
+    processExecArgv: ["--before", "--inspect", "--inspect-port=10", "--after"],
+    processDebugPort: 10,
+    debugMode: "none",
+  })
+  const expected = ["--before", "--after"]
+  assert({ actual, expected })
+}
+
+// debug mode becomes inspect from nothing
 {
   const actual = await createChildExecArgv({
     processExecArgv: ["--before", "--after"],
@@ -34,7 +57,7 @@ import { createChildExecArgv } from "../src/internal/node-launcher/createChildEx
   assert({ actual, expected })
 }
 
-// inspect with inspect-brk
+// debug mode becomes inspect from inspect-brk
 {
   const actual = await createChildExecArgv({
     processExecArgv: ["--before", "--inspect-brk=100", "--after"],
@@ -42,5 +65,34 @@ import { createChildExecArgv } from "../src/internal/node-launcher/createChildEx
     debugPort: 10,
   })
   const expected = ["--before", "--inspect=10", "--after"]
+  assert({ actual, expected })
+}
+
+// unhandledRejection inherited by default
+{
+  const actual = await createChildExecArgv({
+    processExecArgv: ["--before", "--unhandled-rejections=warn", "--after"],
+  })
+  const expected = ["--before", "--unhandled-rejections=warn", "--after"]
+  assert({ actual, expected })
+}
+
+// unhandledRejection becomes strict form warn
+{
+  const actual = await createChildExecArgv({
+    processExecArgv: ["--before", "--unhandled-rejections=warn", "--after"],
+    unhandledRejection: "strict",
+  })
+  const expected = ["--before", "--unhandled-rejections=strict", "--after"]
+  assert({ actual, expected })
+}
+
+// unhandledRejection becomes strict from nothing
+{
+  const actual = await createChildExecArgv({
+    processExecArgv: ["--before", "--after"],
+    unhandledRejection: "strict",
+  })
+  const expected = ["--before", "--after", "--unhandled-rejections=strict"]
   assert({ actual, expected })
 }
