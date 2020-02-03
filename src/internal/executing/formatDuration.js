30901626c3f9d6c2f07032515cb27b941c7a140b
Damien Maillard <damien.maillard@dailymotion.com>
2020-02-03 16:17:52 +0100

    make formatDuration even more readable

--- src/internal/executing/formatDuration.js
+++ src/internal/executing/formatDuration.js
@@ -3,5 +3,5 @@ import { require } from "../require.js"
 const humanizeDuration = require("humanize-duration")
 
 export const formatDuration = (duration) => {
-  return humanizeDuration(duration)
+  return humanizeDuration(duration, { largest: 2, maxDecimalPoints: 2 })
 }
