70d6c78a4951746cfb5020fa2998585d9b938256
Damien Maillard <damien.maillard@dailymotion.com>
2020-02-05 11:57:53 +0100

    debug net

--- script/test/test.js
+++ script/test/test.js
@@ -1,6 +1,8 @@
 import { executeTestPlan, launchNode } from "@jsenv/core"
 import * as jsenvConfig from "../../jsenv.config.js"
 
+process.env.NODE_DEBUG = "net"
+
 executeTestPlan({
   ...jsenvConfig,
   testPlan: {
