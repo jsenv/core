84ef2e57127d5b12213bbfeb83924877a170ccc8
Damien Maillard <damien.maillard@dailymotion.com>
2020-04-01 17:24:38 +0200

    add numeric separator to the default babel plugins

--- src/internal/minimalBabelPluginArray.js
+++ src/internal/minimalBabelPluginArray.js
@@ -9,5 +9,10 @@ import { require } from "./require.js"
 
 const syntaxDynamicImport = require("@babel/plugin-syntax-dynamic-import")
 const syntaxImportMeta = require("@babel/plugin-syntax-import-meta")
+const syntaxNumericSeparator = require("@babel/plugin-syntax-numeric-separator")
 
-export const minimalBabelPluginArray = [syntaxDynamicImport, syntaxImportMeta]
+export const minimalBabelPluginArray = [
+  syntaxDynamicImport,
+  syntaxImportMeta,
+  syntaxNumericSeparator,
+]
