5bb41de195eb9cf2f871fd8ea956d70f2afe205a
Damien Maillard <damien.maillard@dailymotion.com>
2020-04-02 08:15:26 +0200

    add numeric separator and optional chaining test for bundling

---
+++ test/generateCommonJsBundle/optional-chaining/optional-chaining.test.js
@@ -0,0 +1,30 @@
+import { basename } from "path"
+import { assert } from "@jsenv/assert"
+import { resolveUrl, urlToRelativeUrl } from "@jsenv/util"
+import { generateCommonJsBundle } from "../../../index.js"
+import { jsenvCoreDirectoryUrl } from "../../../src/internal/jsenvCoreDirectoryUrl.js"
+import { requireCommonJsBundle } from "../requireCommonJsBundle.js"
+import {
+  GENERATE_COMMONJS_BUNDLE_TEST_PARAMS,
+  REQUIRE_COMMONJS_BUNDLE_TEST_PARAMS,
+} from "../TEST_PARAMS.js"
+
+const testDirectoryUrl = resolveUrl("./", import.meta.url)
+const testDirectoryRelativeUrl = urlToRelativeUrl(testDirectoryUrl, jsenvCoreDirectoryUrl)
+const testDirectoryname = basename(testDirectoryRelativeUrl)
+const jsenvDirectoryRelativeUrl = `${testDirectoryRelativeUrl}.jsenv`
+const bundleDirectoryRelativeUrl = `${testDirectoryRelativeUrl}dist/commonjs`
+const mainFileBasename = `${testDirectoryname}.js`
+
+await generateCommonJsBundle({
+  ...GENERATE_COMMONJS_BUNDLE_TEST_PARAMS,
+  jsenvDirectoryRelativeUrl,
+  bundleDirectoryRelativeUrl,
+  entryPointMap: {
+    main: `./${testDirectoryRelativeUrl}${mainFileBasename}`,
+  },
+})
+await requireCommonJsBundle({
+  ...REQUIRE_COMMONJS_BUNDLE_TEST_PARAMS,
+  bundleDirectoryRelativeUrl,
+})
