3bf9a0136861acca98c4317a7b69e983c5dc5d38
Damien Maillard <damien.maillard@dailymotion.com>
2020-05-04 13:49:14 +0200

    fix some stuff

--- src/internal/exploring/serveExploring.js
+++ src/internal/exploring/serveExploring.js
@@ -8,10 +8,6 @@ const EXPLORING_CSS_RELATIVE_URL = "src/internal/exploring/exploring.css"
 const EXPLORING_JS_RELATIVE_URL = "src/internal/exploring/exploring.js"
 const SYSTEMJS_RELATIVE_URL = "src/internal/exploring/system.js"
 
-const exploringHtmlFileUrl = resolveUrl(EXPLORING_HTML_RELATIVE_URL, jsenvCoreDirectoryUrl)
-const exploringFileUrl = resolveUrl(EXPLORING_JS_RELATIVE_URL, jsenvCoreDirectoryUrl)
-const exploringCssFileUrl = resolveUrl(EXPLORING_CSS_RELATIVE_URL, jsenvCoreDirectoryUrl)
-
 export const serveExploring = async (
   request,
   {
@@ -22,26 +18,39 @@ export const serveExploring = async (
     importMapFileRelativeUrl,
   },
 ) => {
+  const exploringHtmlFileUrl = resolveUrl(EXPLORING_HTML_RELATIVE_URL, jsenvCoreDirectoryUrl)
   const html = await readFile(exploringHtmlFileUrl)
 
-  const exploringFileRelativeUrl = urlToRelativeUrl(exploringFileUrl, projectDirectoryUrl)
   // use worst compileId to be sure it's compatible
   const compileId =
     COMPILE_ID_OTHERWISE in compileServerGroupMap
       ? COMPILE_ID_OTHERWISE
       : getLastKey(compileServerGroupMap)
   const compileDirectoryUrl = `${compileServerOrigin}/${outDirectoryRelativeUrl}${compileId}/`
-  const exploringFileCompiledUrl = resolveUrl(exploringFileRelativeUrl, compileDirectoryUrl)
 
-  const exploringCssRelativeUrl = urlToRelativeUrl(exploringCssFileUrl, projectDirectoryUrl)
+  const jsenvDirectoryRelativeUrl = urlToRelativeUrl(projectDirectoryUrl, jsenvCoreDirectoryUrl)
+  const exploringCssProjectRelativeUrl = jsenvRelativeUrlToProjectRelativeUrl(
+    EXPLORING_CSS_RELATIVE_URL,
+    projectDirectoryUrl,
+  )
+  const systemJsProjectRelativeUrl = jsenvRelativeUrlToProjectRelativeUrl(
+    SYSTEMJS_RELATIVE_URL,
+    projectDirectoryUrl,
+  )
+  const exploringJsProjectRelativeUrl = jsenvRelativeUrlToProjectRelativeUrl(
+    EXPLORING_JS_RELATIVE_URL,
+    projectDirectoryUrl,
+  )
 
   const replacements = {
     $COMPILE_SERVER_ORIGIN: compileServerOrigin,
-    $JSENV_DIRECTORY_RELATIVE_URL: urlToRelativeUrl(projectDirectoryUrl, jsenvCoreDirectoryUrl),
-    $STYLE_HREF: resolveUrl(exploringCssRelativeUrl, compileDirectoryUrl),
+    $JSENV_DIRECTORY_RELATIVE_URL: jsenvDirectoryRelativeUrl,
+    $STYLE_HREF: resolveUrl(exploringCssProjectRelativeUrl, compileDirectoryUrl),
     $COMPILE_SERVER_IMPORT_MAP_SRC: resolveUrl(importMapFileRelativeUrl, compileDirectoryUrl),
-    $SYSTEMJS_SCRIPT_SRC: resolveUrl(SYSTEMJS_RELATIVE_URL, compileServerOrigin),
-    $JSENV_EXPLORING_FILE: JSON.stringify(exploringFileCompiledUrl),
+    $SYSTEMJS_SCRIPT_SRC: resolveUrl(systemJsProjectRelativeUrl, compileServerOrigin),
+    $JSENV_EXPLORING_FILE: JSON.stringify(
+      resolveUrl(exploringJsProjectRelativeUrl, compileDirectoryUrl),
+    ),
   }
   const body = Object.keys(replacements).reduce((previous, key) => {
     const regex = new RegExp(escapeRegexpSpecialCharacters(key), "g")
@@ -63,4 +72,10 @@ export const serveExploring = async (
 const getLastKey = (object) => {
   const keys = Object.keys(object)
   return keys[keys.length - 1]
+}
+
+const jsenvRelativeUrlToProjectRelativeUrl = (jsenvRelativeUrl, projectDirectoryUrl) => {
+  const fileUrl = resolveUrl(jsenvRelativeUrl, jsenvCoreDirectoryUrl)
+  const projectRelativeUrl = urlToRelativeUrl(fileUrl, projectDirectoryUrl)
+  return projectRelativeUrl
 }
