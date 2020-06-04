e1d67a22f7052b7bc613ec8e7d9ef96e61ba018c
Damien Maillard <damien.maillard@dailymotion.com>
2020-06-04 10:03:45 +0200

    fix notification warnings failing tests

--- test/startExploring/TEST_PARAMS.js
+++ test/startExploring/TEST_PARAMS.js
@@ -3,6 +3,7 @@ import { testBabelPluginMap } from "../testBabelPluginMap.js"
 
 export const START_EXPLORING_TEST_PARAMS = {
   logLevel: "warn",
+  protocol: "https",
   compileServerLogLevel: "warn",
   projectDirectoryUrl: jsenvCoreDirectoryUrl,
   jsenvDirectoryClean: true,
