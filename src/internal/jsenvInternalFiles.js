aea2c7337e8949fb3cf1fbe5aaafc2a2b9266951
Damien Maillard <damien.maillard@dailymotion.com>
2020-07-02 17:56:59 +0200

    rename jsenv internal files maybe for later

---
+++ src/internal/jsenvInternalFiles.js
@@ -0,0 +1,17 @@
+import { resolveUrl } from "@jsenv/util"
+import { jsenvCoreDirectoryUrl } from "./jsenvCoreDirectoryUrl.js"
+
+export const nodeJsFileUrl = resolveUrl(
+  "./src/internal/node-launcher/node-js-file.js",
+  jsenvCoreDirectoryUrl,
+)
+
+export const browserJsFileUrl = resolveUrl(
+  "./src/internal/browser-launcher/jsenv-browser-system.js",
+  jsenvCoreDirectoryUrl,
+)
+
+export const jsenvHtmlFileUrl = resolveUrl(
+  "./src/internal/jsenv-html-file.html",
+  jsenvCoreDirectoryUrl,
+)
