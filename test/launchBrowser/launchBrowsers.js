65213b271849c9425ddd445be1ffab3a90c75d21
Damien Maillard <damien.maillard@dailymotion.com>
2020-07-27 11:39:53 +0200

    restore launch browser in sequence

--- test/launchBrowser/launchBrowsers.js
+++ test/launchBrowser/launchBrowsers.js
@@ -1,19 +1,24 @@
+/*
+on windows and for some reason if we launch 3 browsers in parallel
+all requesting jsenv-browser-system.js concurrently
+it sometimes (~20% of the time) makes compile server await forever and not respond
+to browser request to get the compiled file.
+
+It never happens on mac suggesting it's related to the filesystem.
+But I don't see where or what would make server unresponsive like this.
+I don't get what server is waiting for.
+
+The unpredictible nature of this bug associated to being windows only makes it hard to fix.
+
+For now let's launch browser in sequence on windows.
+
+UPDATE: let's try again now that compile server
+pre-generates internal files
+*/
+const launchInSequence = false // process.platform === "win32"
+
 export const launchBrowsers = (launchers, fn) => {
-  /*
-  on windows and for some reason if we launch 3 browsers in parallel
-  all requesting jsenv-browser-system.js concurrently
-  it sometimes (~20% of the time) makes compile server await forever and not respond
-  to browser request to get the compiled file.
-
-  It never happens on mac suggesting it's related to the filesystem.
-  But I don't see where or what would make server unresponsive like this.
-  I don't get what server is waiting for.
-
-  The unpredictible nature of this bug associated to being windows only makes it hard to fix.
-
-  For now let's launch browser in sequence on windows.
-  */
-  if (process.platform === "win32") {
+  if (launchInSequence) {
     return launchers.reduce(async (previous, current) => {
       await previous
       return fn(current)
