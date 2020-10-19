8f5972b7ec113fad491b4fb658ff67c72385ed4c
Damien Maillard <damien.maillard@dailymotion.com>
2020-10-19 09:49:39 +0200

    hanlde very long line in show source location

--- src/internal/bundling/showSourceLocation.js
+++ src/internal/bundling/showSourceLocation.js
@@ -6,7 +6,7 @@ import { grey, red, ansiResetSequence } from "../executing/ansi.js"
 
 export const showSourceLocation = (
   source,
-  { line, column, numberOfSurroundingLinesToShow = 1, color = false },
+  { line, column, numberOfSurroundingLinesToShow = 1, lineMaxLength = 100, color = false },
 ) => {
   let mark = (string) => string
   let aside = (string) => string
@@ -27,22 +27,93 @@ export const showSourceLocation = (
   const endLineNumber = lineRange.end
   const lineNumberMaxWidth = String(endLineNumber).length
 
+  const columnRange = {}
+  if (column === undefined) {
+    columnRange.start = 0
+    columnRange.end = lineMaxLength
+  } else if (column > lineMaxLength) {
+    columnRange.start = column - Math.floor(lineMaxLength / 2)
+    columnRange.end = column + Math.ceil(lineMaxLength / 2)
+  } else {
+    columnRange.start = 0
+    columnRange.end = lineMaxLength
+  }
+
   return linesToShow.map((lineSource, index) => {
     const lineNumber = lineRange.start + index + 1
     const isMainLine = lineNumber === line
+    const lineSourceTruncated = applyColumnRange(columnRange, lineSource)
     const lineNumberWidth = String(lineNumber).length
     // ensure if line moves from 7,8,9 to 10 the display is still great
     const lineNumberRightSpacing = " ".repeat(lineNumberMaxWidth - lineNumberWidth)
     const asideSource = `${lineNumber}${lineNumberRightSpacing} |`
-    const lineFormatted = `${aside(asideSource)} ${lineSource}`
+    const lineFormatted = `${aside(asideSource)} ${lineSourceTruncated}`
     if (isMainLine) {
-      const spacing = stringToSpaces(`${asideSource} ${lineSource.slice(0, column - 1)}`)
+      if (column === undefined) {
+        return `${mark(">")} ${lineFormatted}`
+      }
+      const spacing = stringToSpaces(
+        `${asideSource} ${lineSourceTruncated.slice(0, column - columnRange.start - 1)}`,
+      )
       return `${mark(">")} ${lineFormatted}
   ${spacing}${mark("^")}`
     }
     return `  ${lineFormatted}`
   }).join(`
 `)
+}
+
+const applyColumnRange = ({ start, end }, line) => {
+  if (typeof start !== "number") {
+    throw new TypeError(`start must be a number, received ${start}`)
+  }
+  if (typeof end !== "number") {
+    throw new TypeError(`end must be a number, received ${end}`)
+  }
+  if (end < start) {
+    throw new Error(`end must be greater than start, but ${end} is smaller than ${start}`)
+  }
+
+  const prefix = "…"
+  const suffix = "…"
+  const lastIndex = line.length
+
+  if (line.length === 0) {
+    // don't show any ellipsis if the line is empty
+    // because it's not truncated in that case
+    return ""
+  }
+
+  const startTruncated = start > 0
+  const endTruncated = lastIndex > end
+
+  let from = startTruncated ? start + prefix.length : start
+  let to = endTruncated ? end - suffix.length : end
+  if (to > lastIndex) to = lastIndex
+
+  if (start >= lastIndex || from === to) {
+    return ""
+  }
+
+  let result = ""
+  while (from < to) {
+    result += line[from]
+    from++
+  }
+
+  if (result.length === 0) {
+    return ""
+  }
+  if (startTruncated && endTruncated) {
+    return `${prefix}${result}${suffix}`
+  }
+  if (startTruncated) {
+    return `${prefix}${result}`
+  }
+  if (endTruncated) {
+    return `${result}${suffix}`
+  }
+  return result
 }
 
 const stringToSpaces = (string) => string.replace(/[^\t]/g, " ")
--- src/internal/bundling/showSourceLocation.test.js
+++ src/internal/bundling/showSourceLocation.test.js
@@ -86,3 +86,120 @@ const e = false;
   6 | `.slice(1)
   assert({ actual, expected })
 }
+
+// no column given
+{
+  const actual = showSourceLocation(
+    `const a = false;
+const b = true;
+const c = true;
+const d = true;
+const e = false;
+`,
+    { line: 3 },
+  )
+  const expected = `
+  2 | const b = true;
+> 3 | const c = true;
+  4 | const d = true;`.slice(1)
+  assert({ actual, expected })
+}
+
+// line number goes from 1 digit to 2 digits
+{
+  const actual = showSourceLocation(
+    `const a = false;
+const b = false;
+const c = false;
+const d = false;
+const e = false;
+const f = false;
+const g = false;
+const h = false;
+const i = true;
+const j = true;
+const k = true;
+const l = true;`,
+    { line: 10 },
+  )
+  const expected = `
+  9  | const i = true;
+> 10 | const j = true;
+  11 | const k = true;`.slice(1)
+  assert({ actual, expected })
+}
+
+// line is too long and column is undefined
+{
+  const actual = showSourceLocation(
+    `const a = false;
+const b = true;
+const thisVariableNameisQuiteLong = true;
+const d = true;
+const e = false;
+`,
+    { line: 3, lineMaxLength: 15 },
+  )
+  const expected = `
+  2 | const b = true;
+> 3 | const thisVari…
+  4 | const d = true;`.slice(1)
+  assert({ actual, expected })
+}
+
+// line is too long and column is near beginning
+{
+  const actual = showSourceLocation(
+    `const a = false;
+const b = true;
+const thisVariableNameisQuiteLong = true;
+const d = true;
+const e = false;
+`,
+    { line: 3, column: 4, lineMaxLength: 15 },
+  )
+  const expected = `
+  2 | const b = true;
+> 3 | const thisVari…
+         ^
+  4 | const d = true;`.slice(1)
+  assert({ actual, expected })
+}
+
+// line is too long and column is near middle
+{
+  const actual = showSourceLocation(
+    `const a = false;
+const b = true
+const thisVariableNameisQuiteLong = true;
+const d = tru
+const e = false;
+`,
+    { line: 3, column: 20, lineMaxLength: 15 },
+  )
+  const expected = `
+  2 |${" "}
+> 3 | …ableNameisQui…
+            ^
+  4 |${" "}`.slice(1)
+  assert({ actual, expected })
+}
+
+// line is too long and column is near end
+{
+  const actual = showSourceLocation(
+    `const a = false;
+
+const thisVariableNameisQuiteLong = true;
+const d = tru
+const e = false;
+`,
+    { line: 3, column: 35, lineMaxLength: 15 },
+  )
+  const expected = `
+  2 |${" "}
+> 3 | …Long = true;
+            ^
+  4 |${" "}`.slice(1)
+  assert({ actual, expected })
+}
