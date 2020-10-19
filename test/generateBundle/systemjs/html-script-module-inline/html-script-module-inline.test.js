4488badb31f2b4bc4751845ff23e0559641a92af
Damien Maillard <damien.maillard@dailymotion.com>
2020-10-19 11:22:50 +0200

    update inline module test

--- test/generateBundle/systemjs/html-script-module-inline/html-script-module-inline.test.js
+++ test/generateBundle/systemjs/html-script-module-inline/html-script-module-inline.test.js
@@ -1,8 +1,13 @@
 import { basename } from "path"
+import { assert } from "@jsenv/assert"
 import { resolveDirectoryUrl, urlToRelativeUrl } from "@jsenv/util"
 import { generateBundle } from "@jsenv/core/index.js"
 import { jsenvCoreDirectoryUrl } from "@jsenv/core/src/internal/jsenvCoreDirectoryUrl.js"
-import { GENERATE_SYSTEMJS_BUNDLE_TEST_PARAMS } from "../TEST_PARAMS.js"
+import { browserImportSystemJsBundle } from "../browserImportSystemJsBundle.js"
+import {
+  GENERATE_SYSTEMJS_BUNDLE_TEST_PARAMS,
+  IMPORT_SYSTEM_JS_BUNDLE_TEST_PARAMS,
+} from "../TEST_PARAMS.js"
 
 const testDirectoryUrl = resolveDirectoryUrl("./", import.meta.url)
 const testDirectoryRelativeUrl = urlToRelativeUrl(testDirectoryUrl, jsenvCoreDirectoryUrl)
@@ -14,23 +19,30 @@ const entryPointMap = {
   [`./${testDirectoryRelativeUrl}${mainFilename}`]: "./main.html",
 }
 
-await generateBundle({
+const { bundleManifest } = await generateBundle({
   ...GENERATE_SYSTEMJS_BUNDLE_TEST_PARAMS,
+  logLevel: "info",
   jsenvDirectoryRelativeUrl,
   bundleDirectoryRelativeUrl,
   entryPointMap,
   // minify: true,
-  // minifyHtmlOptions: {
-  //   collapseWhitespace: true,
-  // },
 })
 
-// const { namespace: actual } = await browserImportSystemJsBundle({
-//   ...IMPORT_SYSTEM_JS_BUNDLE_TEST_PARAMS,
-//   testDirectoryRelativeUrl,
-// })
-// const expected = {
-//   htmlText: `<button>Hello world</button>`,
-//   innerText: "Hello world",
-// }
-// assert({ actual, expected })
+const getBundleRelativeUrl = (urlRelativeToTestDirectory) => {
+  const relativeUrl = `${testDirectoryRelativeUrl}${urlRelativeToTestDirectory}`
+  const bundleRelativeUrl = bundleManifest[relativeUrl]
+  return bundleRelativeUrl
+}
+
+const inlineScriptBundleRelativeUrl = getBundleRelativeUrl("html-script-module-inline.10.js")
+
+const { namespace: actual } = await browserImportSystemJsBundle({
+  ...IMPORT_SYSTEM_JS_BUNDLE_TEST_PARAMS,
+  testDirectoryRelativeUrl,
+  mainRelativeUrl: `./${inlineScriptBundleRelativeUrl}`,
+  // debug: true,
+})
+const expected = {
+  value: 42,
+}
+assert({ actual, expected })
