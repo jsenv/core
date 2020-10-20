7e44c3e305adc499292cd07381b19363bc840c95
Damien Maillard <damien.maillard@dailymotion.com>
2020-10-20 10:03:47 +0200

    fix issue on windows with postcss

--- src/internal/bundling/css/applyPostCss.js
+++ src/internal/bundling/css/applyPostCss.js
@@ -29,11 +29,17 @@ export const applyPostCss = async (
   return result
 }
 
+// the goal of this function is to take an url that is likely an http url
+// info a file:// url
+// for instance http://example.com/dir/file.js
+// must becomes file:///dir/file.js
+// but in windows it must be file://C:/dir/file.js
+const filesystemRootUrl = new URL("/", import.meta.url)
 const urlToFileUrl = (url) => {
   if (url.startsWith("file://")) {
     return url
   }
   const origin = new URL(url).origin
   const afterOrigin = url.slice(origin.length)
-  return `file://${afterOrigin}`
+  return new URL(afterOrigin, filesystemRootUrl).href
 }
