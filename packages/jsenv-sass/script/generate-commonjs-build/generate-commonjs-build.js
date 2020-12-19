96f9acc03db5d17663c2906d518434d413bbc118
Damien Maillard <damien.maillard@dailymotion.com>
2020-12-19 08:48:24 +0100

    generate jsenv-sass build

--- packages/jsenv-sass/script/generate-commonjs-build/generate-commonjs-build.js
+++ packages/jsenv-sass/script/generate-commonjs-build/generate-commonjs-build.js
@@ -4,9 +4,10 @@ import * as jsenvConfig from "../../../../jsenv.config.js"
 buildProject({
   ...jsenvConfig,
   format: "commonjs",
-  entryPointMap: {
-    "./packages/jsenv-sass/main.js": "./packages/jsenv-sass/dist/commonjs/main.cjs",
-  },
   babelPluginMap: getBabelPluginMapForNode(),
+  buildDirectoryRelativeUrl: "packages/jsenv-sass/dist/commonjs",
+  entryPointMap: {
+    "./packages/jsenv-sass/main.js": "./main.cjs",
+  },
   buildDirectoryClean: true,
 })
