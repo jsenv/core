555b611479732a12278ddaa95c8cb0a52be5cc29
Damien Maillard <damien.maillard@dailymotion.com>
2020-12-19 08:07:04 +0100

    sass test is working, jsenv can use sass, still have to document how

--- test-manual/sass/sass.test.js
+++ test-manual/sass/sass.test.js
@@ -5,6 +5,7 @@ import { COMPILE_ID_OTHERWISE } from "@jsenv/core/src/internal/CONSTANTS.js"
 import { jsenvCoreDirectoryUrl } from "@jsenv/core/src/internal/jsenvCoreDirectoryUrl.js"
 import { startCompileServer } from "@jsenv/core/src/internal/compiling/startCompileServer.js"
 import { COMPILE_SERVER_TEST_PARAMS } from "@jsenv/core/test/startCompileServer/TEST_PARAMS.js"
+import { getCssSourceMappingUrl } from "@jsenv/core/src/internal/sourceMappingURLUtils.js"
 import { jsenvCompilerForSass } from "./jsenvCompilerForSass.js"
 
 const testDirectoryUrl = resolveUrl("./", import.meta.url)
@@ -17,25 +18,58 @@ const jsenvDirectoryRelativeUrl = `${testDirectoryRelativeUrl}.jsenv/`
 const compiledFileRelativeUrl = `${jsenvDirectoryRelativeUrl}out/${COMPILE_ID_OTHERWISE}/${fileRelativeUrl}`
 // const compiledFileUrl = `${jsenvCoreDirectoryUrl}${compiledFileRelativeUrl}`
 
+const fetchSourceAsText = async (urlRelativeToSourcemap, sourceMapUrl) => {
+  const sourceUrl = resolveUrl(urlRelativeToSourcemap, sourceMapUrl)
+  const response = await fetchUrl(sourceUrl)
+  const text = await response.text()
+  return text
+}
+
 {
   const { origin: compileServerOrigin } = await startCompileServer({
     ...COMPILE_SERVER_TEST_PARAMS,
     jsenvDirectoryRelativeUrl,
     customCompilers: [jsenvCompilerForSass],
   })
-  const fileServerUrl = `${compileServerOrigin}/${compiledFileRelativeUrl}`
-  const response = await fetchUrl(fileServerUrl, { ignoreHttpsError: true })
+  const cssServerUrl = `${compileServerOrigin}/${compiledFileRelativeUrl}`
+  const response = await fetchUrl(cssServerUrl, { ignoreHttpsError: true })
   {
     const actual = {
       status: response.status,
       contentType: response.headers.get("content-type"),
-      text: await response.text(),
     }
     const expected = {
       status: 200,
       contentType: "text/css",
-      text: "",
     }
+    assert({ actual, expected })
+  }
+  const css = await response.text()
+  // css is the concatenation of sass files
+  {
+    const actual = css.includes("background-color: yellow")
+    const expected = true
+    assert({ actual, expected })
+  }
+  // sourcemap points the sass files
+  const cssMapRelativeUrl = getCssSourceMappingUrl(css)
+  const cssMapUrl = resolveUrl(cssMapRelativeUrl, cssServerUrl)
+  const cssMapResponse = await fetchUrl(cssMapUrl)
+  const cssMap = await cssMapResponse.json()
+  {
+    const actual = {
+      file: cssMap.file,
+      sources: ["../../../../../dep.scss", "../../../../../sass.scss"],
+    }
+    const expected = actual
+    assert({ actual, expected })
+  }
+  {
+    const actual = cssMap.sourcesContent
+    const expected = [
+      await fetchSourceAsText(cssMap.sources[0], cssMapUrl),
+      await fetchSourceAsText(cssMap.sources[1], cssMapUrl),
+    ]
     assert({ actual, expected })
   }
 }
