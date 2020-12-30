0f6ea4ea59f59d1c9887ca8961ee84213ea65114
Damien Maillard <damien.maillard@dailymotion.com>
2020-12-30 20:30:44 +0100

    ensure import.meta.url pattern works with systemjs

---
+++ test/buildProject/systemjs/js-import-meta-js/js-import-meta-js.test.js
@@ -0,0 +1,59 @@
+import { basename } from "path"
+import { assert } from "@jsenv/assert"
+import { resolveUrl, urlToRelativeUrl } from "@jsenv/util"
+import { jsenvCoreDirectoryUrl } from "@jsenv/core/src/internal/jsenvCoreDirectoryUrl.js"
+import { buildProject } from "@jsenv/core"
+import {
+  GENERATE_SYSTEMJS_BUILD_TEST_PARAMS,
+  IMPORT_SYSTEM_JS_BUILD_TEST_PARAMS,
+} from "../TEST_PARAMS.js"
+import { browserImportSystemJsBuild } from "../browserImportSystemJsBuild.js"
+
+const testDirectoryUrl = resolveUrl("./", import.meta.url)
+const testDirectoryRelativeUrl = urlToRelativeUrl(testDirectoryUrl, jsenvCoreDirectoryUrl)
+const testDirectoryname = basename(testDirectoryRelativeUrl)
+const jsenvDirectoryRelativeUrl = `${testDirectoryRelativeUrl}.jsenv/`
+const buildDirectoryRelativeUrl = `${testDirectoryRelativeUrl}dist/systemjs/`
+const mainFilename = `${testDirectoryname}.html`
+const entryPointMap = {
+  [`./${testDirectoryRelativeUrl}${mainFilename}`]: "./main.html",
+}
+
+const { buildMappings } = await buildProject({
+  ...GENERATE_SYSTEMJS_BUILD_TEST_PARAMS,
+  jsenvDirectoryRelativeUrl,
+  buildDirectoryRelativeUrl,
+  entryPointMap,
+})
+
+const getBuildRelativeUrl = (urlRelativeToTestDirectory) => {
+  const relativeUrl = `${testDirectoryRelativeUrl}${urlRelativeToTestDirectory}`
+  const buildRelativeUrl = buildMappings[relativeUrl]
+  return buildRelativeUrl
+}
+
+// assert build mappings does not contains dep.js
+// -> js was handled like an asset (no parsing)
+{
+  const depRelativeUrl = getBuildRelativeUrl("dep.js")
+  const actual = Object.keys(buildMappings).includes(depRelativeUrl)
+  const expected = false
+  assert({ actual, expected })
+}
+
+const indexRelativeUrl = getBuildRelativeUrl("index.js")
+
+{
+  const { namespace, serverOrigin } = await browserImportSystemJsBuild({
+    ...IMPORT_SYSTEM_JS_BUILD_TEST_PARAMS,
+    testDirectoryRelativeUrl,
+    mainRelativeUrl: `./${indexRelativeUrl}`,
+    // debug: true,
+  })
+  const fileBuildRelativeUrl = buildMappings[`${testDirectoryRelativeUrl}file.js`]
+  const actual = namespace
+  const expected = {
+    jsUrl: String(new URL(`./dist/systemjs/${fileBuildRelativeUrl}`, serverOrigin)),
+  }
+  assert({ actual, expected })
+}
