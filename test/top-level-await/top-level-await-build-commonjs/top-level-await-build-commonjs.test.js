8d93f22f190c8c32535dc73f7661f87f87841f8e
Damien Maillard <damien.maillard@dailymotion.com>
2020-12-31 10:51:12 +0100

    file structure for top level await tests

--- test/top-level-await/top-level-await-build-commonjs/top-level-await-build-commonjs.test.js
+++ test/top-level-await/top-level-await-build-commonjs/top-level-await-build-commonjs.test.js
@@ -1,9 +1,9 @@
 import { basename } from "path"
 import { assert } from "@jsenv/assert"
 import { resolveDirectoryUrl, urlToRelativeUrl } from "@jsenv/util"
-import { buildProject } from "@jsenv/core"
 import { jsenvCoreDirectoryUrl } from "@jsenv/core/src/internal/jsenvCoreDirectoryUrl.js"
-import { GENERATE_COMMONJS_BUILD_TEST_PARAMS } from "../TEST_PARAMS.js"
+import { GENERATE_COMMONJS_BUILD_TEST_PARAMS } from "@jsenv/core/test/TEST_PARAMS_BUILD_COMMONJS.js"
+import { buildProject } from "@jsenv/core"
 
 const testDirectoryUrl = resolveDirectoryUrl("./", import.meta.url)
 const testDirectoryRelativeUrl = urlToRelativeUrl(testDirectoryUrl, jsenvCoreDirectoryUrl)
@@ -11,15 +11,16 @@ const testDirectoryname = basename(testDirectoryRelativeUrl)
 const jsenvDirectoryRelativeUrl = `${testDirectoryRelativeUrl}.jsenv`
 const buildDirectoryRelativeUrl = `${testDirectoryRelativeUrl}dist/commonjs/`
 const mainFilename = `${testDirectoryname}.js`
+const entryPointMap = {
+  [`./${testDirectoryRelativeUrl}${mainFilename}`]: "./main.cjs",
+}
 
 try {
   await buildProject({
     ...GENERATE_COMMONJS_BUILD_TEST_PARAMS,
     jsenvDirectoryRelativeUrl,
     buildDirectoryRelativeUrl,
-    entryPointMap: {
-      [`./${testDirectoryRelativeUrl}${mainFilename}`]: "./main.cjs",
-    },
+    entryPointMap,
   })
 } catch (actual) {
   process.exitCode = undefined // restore process exitCode set by error in buildProject
--- test/top-level-await/top-level-await-build-esmodule/top-level-await-build-esmodule.test.js
+++ test/top-level-await/top-level-await-build-esmodule/top-level-await-build-esmodule.test.js
@@ -3,14 +3,14 @@ import { basename } from "path"
 import { assert } from "@jsenv/assert"
 import { resolveUrl, urlToRelativeUrl } from "@jsenv/util"
 import { jsenvCoreDirectoryUrl } from "@jsenv/core/src/internal/jsenvCoreDirectoryUrl.js"
-import { buildProject } from "@jsenv/core"
 import {
   GENERATE_ESMODULE_BUILD_TEST_PARAMS,
   BROWSER_IMPORT_BUILD_TEST_PARAMS,
   NODE_IMPORT_BUILD_TEST_PARAMS,
-} from "../TEST_PARAMS.js"
-import { browserImportEsModuleBuild } from "../browserImportEsModuleBuild.js"
-import { nodeImportEsModuleBuild } from "../nodeImportEsModuleBuild.js"
+} from "@jsenv/core/test/TEST_PARAMS_BUILD_ESMODULE.js"
+import { browserImportEsModuleBuild } from "@jsenv/core/test/browserImportEsModuleBuild.js"
+import { nodeImportEsModuleBuild } from "@jsenv/core/test/nodeImportEsModuleBuild.js"
+import { buildProject } from "@jsenv/core"
 
 const testDirectoryUrl = resolveUrl("./", import.meta.url)
 const testDirectoryRelativeUrl = urlToRelativeUrl(testDirectoryUrl, jsenvCoreDirectoryUrl)
