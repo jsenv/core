6649ffd18d4cfbdb3cecd8acd8b280d3778648c3
Damien Maillard <damien.maillard@dailymotion.com>
2020-12-31 17:17:37 +0100

    prepare testing no import map

---
+++ test/building/no-importmap-build-esmodule/no-importmap-build-esmodule.test.js
@@ -0,0 +1,27 @@
+import { basename } from "path"
+// import { assert } from "@jsenv/assert"
+import { resolveUrl, urlToRelativeUrl } from "@jsenv/util"
+import { jsenvCoreDirectoryUrl } from "@jsenv/core/src/internal/jsenvCoreDirectoryUrl.js"
+import { buildProject } from "@jsenv/core"
+import {
+  GENERATE_ESMODULE_BUILD_TEST_PARAMS,
+  // BROWSER_IMPORT_BUILD_TEST_PARAMS,
+} from "@jsenv/core/test/TEST_PARAMS_BUILD_ESMODULE.js"
+// import { browserImportEsModuleBuild } from "@jsenv/core/test/browserImportEsModuleBuild.js"
+
+const testDirectoryUrl = resolveUrl("./", import.meta.url)
+const testDirectoryRelativeUrl = urlToRelativeUrl(testDirectoryUrl, jsenvCoreDirectoryUrl)
+const testDirectoryname = basename(testDirectoryRelativeUrl)
+const jsenvDirectoryRelativeUrl = `${testDirectoryRelativeUrl}.jsenv/`
+const buildDirectoryRelativeUrl = `${testDirectoryRelativeUrl}dist/esmodule/`
+const mainFilename = `${testDirectoryname}.js`
+const entryPointMap = {
+  [`./${testDirectoryRelativeUrl}${mainFilename}`]: "./main.js",
+}
+
+await buildProject({
+  ...GENERATE_ESMODULE_BUILD_TEST_PARAMS,
+  jsenvDirectoryRelativeUrl,
+  buildDirectoryRelativeUrl,
+  entryPointMap,
+})
