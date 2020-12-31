ce80b46d546085bf49bf27b905ab1ced4abfb10b
Damien Maillard <damien.maillard@dailymotion.com>
2020-12-31 17:38:21 +0100

    Finalize import map simplification. Fixes #138

--- src/internal/compiling/transformImportmap.js
+++ src/internal/compiling/transformImportmap.js
@@ -1,44 +1,17 @@
-import { urlToRelativeUrl, urlIsInsideOf } from "@jsenv/util"
-import { composeTwoImportMaps } from "@jsenv/import-map"
-import { jsenvCoreDirectoryUrl } from "../jsenvCoreDirectoryUrl.js"
-
 /**
  * allows the following:
  *
  * import "@jsenv/core/helpers/regenerator-runtime/regenerator-runtime.js"
  * -> searches a file inside @jsenv/core/*
  *
- * import importMap from "/jsenv.importmap"
- * -> searches project importMap at importMapFileRelativeUrl
- * (if importMap file does not exists an empty object is returned)
- * (if project uses a custom importMapFileRelativeUrl jsenv that file is returned)
- *
- * An other idea: instead we should create a @jsenv/helpers package with the source code
- * that might end up in the project files. Then you will have to add this to your package.json
- * in "dependencies" instead of "devDependencies" so that it ends in the importmap
- * compile server would almost no touch the importmap as it's the case today.
- *
  */
+
+import { urlToRelativeUrl, urlIsInsideOf } from "@jsenv/util"
+import { composeTwoImportMaps } from "@jsenv/import-map"
+import { jsenvCoreDirectoryUrl } from "../jsenvCoreDirectoryUrl.js"
 
 export const transformImportmap = async (importmapBeforeTransformation, { originalFileUrl }) => {
   const importMapForProject = JSON.parse(importmapBeforeTransformation)
-
-  // we do this "just" so that import.meta.resolve dependencies to
-  // @jsenv/import-map and @jsenv/cancellation are found
-  // in theory we don't need to do this because user should have @jsenv/core in its devDependencies
-  // which should pull what I just said. However
-  // when building for production one could ignore dev dependencies
-  // and end up without these important remappings
-  // so remapping for jsenv should always be there, that's
-  // what we are doing here.
-  // ideally we should do this also on 404 (no importmap file created in the project for now)
-  // const importMapForJsenvCore = await getImportMapFromNodeModules({
-  //   logLevel: loggerToLogLevel(logger),
-  //   projectDirectoryUrl: jsenvCoreDirectoryUrl,
-  //   rootProjectDirectoryUrl: projectDirectoryUrl,
-  //   importMapFileRelativeUrl: originalFileRelativeUrl,
-  //   projectPackageDevDependenciesIncluded: false,
-  // })
 
   const topLevelRemappingForJsenvCore = {
     "@jsenv/core/": urlToRelativeUrlRemapping(jsenvCoreDirectoryUrl, originalFileUrl),
