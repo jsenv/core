fff5e638018938f564f65eecdc3002a267a4536c
Damien Maillard <damien.maillard@dailymotion.com>
2021-04-30 13:54:38 +0200

    Improve eslint config

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,4 +1,3 @@
-/* global require, __dirname */
 const { createEslintConfig } = require("@jsenv/eslint-config")
 
 const config = createEslintConfig({
@@ -9,9 +8,24 @@ const config = createEslintConfig({
   // },
 })
 
-// simulate node 13
-config.globals.__filename = "off"
-config.globals.__dirname = "off"
-config.globals.require = "off"
+// disable commonjs globals by default
+// (package is "type": "module")
+Object.assign(config.globals, {
+  __filename: "off",
+  __dirname: "off",
+  require: "off",
+})
+
+config.overrides = [
+  // inside *.cjs files. restore commonJS "globals"
+  {
+    files: ["**/*.cjs"],
+    globals: {
+      __filename: true,
+      __dirname: true,
+      require: true,
+    },
+  },
+]
 
 module.exports = config
