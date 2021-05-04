5851c8472622d7f26aa20e63718e63288258d07a
Damien Maillard <damien.maillard@dailymotion.com>
2021-05-04 14:14:13 +0200

    fix executeTestplan color test

--- src/internal/runtime/node-feature-detect/nodeSupportsDynamicImport.js
+++ src/internal/runtime/node-feature-detect/nodeSupportsDynamicImport.js
@@ -1,10 +1,5 @@
 import { memoize } from "@jsenv/util"
 
 export const nodeSupportsDynamicImport = memoize(async () => {
-  try {
-    await import("./feature-detect-dynamic-import.js")
-    return true
-  } catch (e) {
-    return false
-  }
+  return true
 })
--- src/internal/runtime/node-feature-detect/nodeSupportsTopLevelAwait.js
+++ src/internal/runtime/node-feature-detect/nodeSupportsTopLevelAwait.js
@@ -1,9 +1,17 @@
-import { memoize } from "@jsenv/util"
+import { memoize, resolveUrl } from "@jsenv/util"
+
+import { jsenvCoreDirectoryUrl } from "@jsenv/core/src/internal/jsenvCoreDirectoryUrl.js"
+
+const featureDetetcTopLevelAwaitFileUrl = resolveUrl(
+  "./src/internal/runtime/node-feature-detect/feature-detect-top-level-await.mjs",
+  jsenvCoreDirectoryUrl,
+)
 
 export const nodeSupportsTopLevelAwait = memoize(async () => {
   try {
-    await import("./feature-detect-top-level-await.js")
-    return true
+    const namespace = await import(featureDetetcTopLevelAwaitFileUrl)
+    const supported = namespace.default === 42
+    return supported
   } catch (e) {
     return false
   }
