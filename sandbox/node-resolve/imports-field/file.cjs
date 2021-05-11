a5ba6918211b309c41bfc4b30fbaa6c31fbea3cd
Damien Maillard <damien.maillard@dailymotion.com>
2021-05-11 14:11:21 +0200

    fix eslint issue

--- sandbox/node-resolve/imports-field/file.cjs
+++ sandbox/node-resolve/imports-field/file.cjs
@@ -1,3 +1,6 @@
+// unfortunately https://www.npmjs.com/package/eslint-import-resolver-node
+// does not seems to be compatible with package imports field
+// eslint-disable-next-line import/no-unresolved
 const { dev } = require("#env")
 
 console.log(dev)
