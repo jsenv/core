b572e296ca6500cefdb74fa8c15c18f1e5d50891
Damien Maillard <damien.maillard@dailymotion.com>
2021-07-17 18:33:56 +0200

    update a failing test

--- test/importmap/bare-specifier-not-mapped-launch-browser/bare-specifier-not-mapped-launch-browser.test.js
+++ test/importmap/bare-specifier-not-mapped-launch-browser/bare-specifier-not-mapped-launch-browser.test.js
@@ -1,34 +1,40 @@
-import { basename } from "path"
 import { assert } from "@jsenv/assert"
-import { resolveDirectoryUrl, urlToRelativeUrl } from "@jsenv/util"
+import { resolveDirectoryUrl, urlToRelativeUrl, urlToBasename } from "@jsenv/util"
+
+import { launchChromium, launchFirefox, execute } from "@jsenv/core"
 import { jsenvCoreDirectoryUrl } from "@jsenv/core/src/internal/jsenvCoreDirectoryUrl.js"
 import { EXECUTE_TEST_PARAMS } from "@jsenv/core/test/TEST_PARAMS_LAUNCH_NODE.js"
-import { launchChromium, execute } from "@jsenv/core"
 
 const testDirectoryUrl = resolveDirectoryUrl("./", import.meta.url)
 const testDirectoryRelativeUrl = urlToRelativeUrl(testDirectoryUrl, jsenvCoreDirectoryUrl)
-const testDirectoryname = basename(testDirectoryRelativeUrl)
+const testDirectoryname = urlToBasename(testDirectoryRelativeUrl)
 const jsenvDirectoryRelativeUrl = `${testDirectoryRelativeUrl}.jsenv/`
 const htmlFilename = `${testDirectoryname}.html`
 const importerRelativeUrl = `${testDirectoryRelativeUrl}${testDirectoryname}.js`
 const fileRelativeUrl = `${testDirectoryRelativeUrl}${htmlFilename}`
 const importMapFileRelativeUrl = `${testDirectoryRelativeUrl}import-map.importmap`
 
-const executionResult = await execute({
-  ...EXECUTE_TEST_PARAMS,
-  ignoreError: true,
-  jsenvDirectoryRelativeUrl,
-  fileRelativeUrl,
-  launch: launchChromium,
-  stopAfterExecute: true,
-})
-const actual = {
-  executionResultStatus: executionResult.status,
-  executionResultErrorMessage: executionResult.error.message,
+const test = async (params) => {
+  const executionResult = await execute({
+    ...EXECUTE_TEST_PARAMS,
+    ignoreError: true,
+    jsenvDirectoryRelativeUrl,
+    fileRelativeUrl,
+    stopAfterExecute: true,
+    ...params,
+  })
+  const actual = {
+    executionResultStatus: executionResult.status,
+    executionResultErrorMessage: executionResult.error.message,
+  }
+  return actual
 }
-const expected = {
-  executionResultStatus: "errored",
-  executionResultErrorMessage: `Unmapped bare specifier.
+
+{
+  const actual = await test({ launch: launchFirefox })
+  const expected = {
+    executionResultStatus: "errored",
+    executionResultErrorMessage: `Unmapped bare specifier.
 --- specifier ---
 foo
 --- importer ---
@@ -37,5 +43,15 @@ ${importerRelativeUrl}
 Add a mapping for "foo" into the importmap file at ${importMapFileRelativeUrl}
 --- suggestion ---
 Generate importmap using https://github.com/jsenv/jsenv-node-module-import-map`,
+  }
+  assert({ actual, expected })
 }
-assert({ actual, expected })
+
+{
+  const actual = await test({ launch: launchChromium })
+  const expected = {
+    executionResultStatus: "errored",
+    executionResultErrorMessage: `Failed to resolve module specifier "foo". Relative references must start with either "/", "./", or "../".`,
+  }
+  assert({ actual, expected })
+}
