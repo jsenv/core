1a9fc753b98258b34c9d45811025119a0b0930e4
Damien Maillard <damien.maillard@dailymotion.com>
2021-07-23 17:05:20 +0200

    set 10% as significant perf impact

--- .github/workflows/perf_impact/report_perf_impact.js
+++ .github/workflows/perf_impact/report_perf_impact.js
@@ -6,4 +6,21 @@ reportPerformanceImpact({
   installCommand: "npm install",
   generatePerformanceReportFileRelativeUrl:
     "./.github/workflows/perf_impact/generate_perf_report.js",
+  isPerformanceImpactSignificant: ({ metricDurationBeforeMerge, metricDurationDelta }) => {
+    const absoluteDelta = Math.abs(metricDurationDelta)
+
+    // the absolute diff is below 10ms -> not important
+    if (absoluteDelta < 10) {
+      return false
+    }
+
+    // the absolute diff as percentage is below 10% -> not important
+    const absoluteDeltaAsRatio = absoluteDelta / metricDurationBeforeMerge
+    const absoluteDeltaAsPercentage = absoluteDeltaAsRatio * 100
+    if (absoluteDeltaAsPercentage <= 10) {
+      return false
+    }
+
+    return true
+  },
 })
