71638aa3ae233e33c609b8c14e8685303a195c0e
Damien Maillard <damien.maillard@dailymotion.com>
2021-08-07 17:06:32 +0200

    prepare test for preload css

---
+++ test/build_preload_css/build_preload_css.test.js
@@ -0,0 +1,52 @@
+import { assert } from "@jsenv/assert"
+import { resolveUrl, urlToRelativeUrl, urlToBasename, readFile } from "@jsenv/util"
+
+import { buildProject } from "@jsenv/core"
+import { jsenvCoreDirectoryUrl } from "@jsenv/core/src/internal/jsenvCoreDirectoryUrl.js"
+import { GENERATE_ESMODULE_BUILD_TEST_PARAMS } from "@jsenv/core/test/TEST_PARAMS_BUILD_ESMODULE.js"
+import {
+  findNode,
+  getHtmlNodeAttributeByName,
+} from "@jsenv/core/src/internal/compiling/compileHtml.js"
+
+const testDirectoryUrl = resolveUrl("./", import.meta.url)
+const testDirectoryRelativeUrl = urlToRelativeUrl(testDirectoryUrl, jsenvCoreDirectoryUrl)
+const testDirectoryname = urlToBasename(testDirectoryRelativeUrl)
+const jsenvDirectoryRelativeUrl = `${testDirectoryRelativeUrl}.jsenv/`
+const buildDirectoryRelativeUrl = `${testDirectoryRelativeUrl}dist/esmodule/`
+
+const { buildMappings } = await buildProject({
+  ...GENERATE_ESMODULE_BUILD_TEST_PARAMS,
+  jsenvDirectoryRelativeUrl,
+  buildDirectoryRelativeUrl,
+  entryPointMap: {
+    [`./${testDirectoryRelativeUrl}${testDirectoryname}.html`]: "./main.html",
+  },
+  logLevel: "debug",
+})
+
+{
+  const actual = buildMappings
+  const expected = {
+    [`${testDirectoryRelativeUrl}src/main.css`]: "assets/main-a3f2aec7.css",
+    [`${testDirectoryRelativeUrl}src/main.js`]: "smain-d56393bd.js",
+    [`${testDirectoryRelativeUrl}${testDirectoryname}.html`]: "main.html",
+  }
+  assert({ actual, expected })
+}
+
+// ensure link.href is correct
+{
+  const buildDirectoryUrl = resolveUrl(buildDirectoryRelativeUrl, jsenvCoreDirectoryUrl)
+  const htmlBuildUrl = resolveUrl("main.html", buildDirectoryUrl)
+  const htmlString = await readFile(htmlBuildUrl)
+  const linkPreload = findNode(
+    htmlString,
+    (node) => node.nodeName === "link" && Boolean(getHtmlNodeAttributeByName(node, "crossorigin")),
+  )
+  const href = getHtmlNodeAttributeByName(linkPreload, "href").value
+
+  const actual = href
+  const expected = "https://fonts.googleapis.com/css2?family=Roboto"
+  assert({ actual, expected })
+}
