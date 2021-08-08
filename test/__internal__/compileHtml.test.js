1d1a80de438a7c8dfd1043d5f36a26dcebf8f97c
Damien Maillard <damien.maillard@dailymotion.com>
2021-08-08 16:55:55 +0200

    fix html compilation test

--- test/__internal__/compileHtml.test.js
+++ test/__internal__/compileHtml.test.js
@@ -72,52 +72,6 @@ const compileHtml = (
   assert({ actual, expected })
 }
 
-// transform external module script inside body
-{
-  const htmlBeforeCompilation = `
-<html>
-  <head></head>
-  <body>
-    <script src="foo.js"></script>
-    <script type="module" src="bar.js" async></script>
-  </body>
-</html>`
-  const { htmlAfterCompilation } = compileHtml(htmlBeforeCompilation)
-  const actual = htmlAfterCompilation
-  const expected = `<html><head></head>
-  <body>
-    <script src="foo.js"></script>
-    <script async="">
-      window.__jsenv__.executeFileUsingSystemJs("bar.js")
-    </script>
-${`  ` /* this is for prettier */}
-</body></html>`
-  assert({ actual, expected })
-}
-
-// transform inline module script in head
-{
-  const htmlBeforeCompilation = `
-<html>
-  <head>
-    <script type="module">
-      console.log(42)
-    </script>
-  </head>
-  <body></body>
-</html>`
-  const { htmlAfterCompilation } = compileHtml(htmlBeforeCompilation)
-  const actual = htmlAfterCompilation
-  const expected = `<html><head>
-    <script>
-      window.__jsenv__.executeFileUsingSystemJs("./8d473bbc.js")
-    </script>
-  </head>
-  <body>
-</body></html>`
-  assert({ actual, expected })
-}
-
 // ''
 {
   const htmlBeforeCompilation = ""
