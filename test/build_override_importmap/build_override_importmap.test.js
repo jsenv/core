91f25929ad1357b836bfa03405e5b923fbe426f6
Damien Maillard <damien.maillard@dailymotion.com>
2021-09-09 09:12:28 +0200

    prepare test for url override

---
+++ test/build_override_importmap/build_override_importmap.test.js
@@ -0,0 +1,59 @@
+import { assert } from "@jsenv/assert"
+import {
+  resolveDirectoryUrl,
+  urlToRelativeUrl,
+  urlToBasename,
+  resolveUrl,
+  readFile,
+} from "@jsenv/filesystem"
+
+import { buildProject } from "@jsenv/core"
+import { jsenvCoreDirectoryUrl } from "@jsenv/core/src/internal/jsenvCoreDirectoryUrl.js"
+import { GENERATE_ESMODULE_BUILD_TEST_PARAMS } from "@jsenv/core/test/TEST_PARAMS_BUILD_ESMODULE.js"
+
+const testDirectoryUrl = resolveDirectoryUrl("./", import.meta.url)
+const testDirectoryRelativeUrl = urlToRelativeUrl(testDirectoryUrl, jsenvCoreDirectoryUrl)
+const testDirectoryname = urlToBasename(testDirectoryRelativeUrl)
+const jsenvDirectoryRelativeUrl = `${testDirectoryRelativeUrl}.jsenv/`
+const buildDirectoryRelativeUrl = `${testDirectoryRelativeUrl}dist/esmodule/`
+const entryPointMap = {
+  [`./${testDirectoryRelativeUrl}${testDirectoryname}.html`]: "./main.html",
+}
+
+const { buildMappings } = await buildProject({
+  ...GENERATE_ESMODULE_BUILD_TEST_PARAMS,
+  jsenvDirectoryRelativeUrl,
+  buildDirectoryRelativeUrl,
+  entryPointMap,
+  urlOverrides: {
+    "./dev.importmap": "./prod.importmap",
+  },
+  // minify: true,
+  // logLevel: "debug",
+})
+
+const buildDirectoryUrl = resolveUrl(buildDirectoryRelativeUrl, jsenvCoreDirectoryUrl)
+// const jsBuildRelativeUrl = buildMappings[`${testDirectoryRelativeUrl}main.js`]
+
+{
+  const actual = buildMappings
+  const expected = {}
+  assert({ actual, expected })
+}
+
+// check importmap content
+{
+  const importmapBuildRelativeUrl = buildMappings[`${testDirectoryRelativeUrl}dev.importmap`]
+  const importmapBuildUrl = resolveUrl(importmapBuildRelativeUrl, buildDirectoryUrl)
+  const importmapString = await readFile(importmapBuildUrl)
+  const importmap = JSON.parse(importmapString)
+
+  const actual = importmap
+  // importmap is the same because non js files are remapped
+  const expected = {
+    imports: {
+      "./img.png": "./img-remap.png",
+    },
+  }
+  assert({ actual, expected })
+}
---
+++ test/build_override_importmap/main.js
@@ -0,0 +1,4 @@
+// eslint-disable-next-line import/no-unresolved
+import { DEV } from "#env"
+
+console.log(DEV)
