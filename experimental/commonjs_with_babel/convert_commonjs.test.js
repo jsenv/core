6a14911faf0c2a1c4c46bc40f7b8597ad1b9aa09
Damien Maillard <damien.maillard@dailymotion.com>
2021-09-17 19:12:37 +0200

    move commonjs convert to experimental

--- experimental/commonjs_with_babel/convert_commonjs.test.js
+++ experimental/commonjs_with_babel/convert_commonjs.test.js
@@ -6,8 +6,8 @@ import { fetchUrl } from "@jsenv/server"
 
 import { jsenvCoreDirectoryUrl } from "@jsenv/core/src/internal/jsenvCoreDirectoryUrl.js"
 import { startCompileServer } from "@jsenv/core/src/internal/compiling/startCompileServer.js"
-import { convertCommonJsWithBabel } from "@jsenv/core"
-import { COMPILE_SERVER_TEST_PARAMS } from "../TEST_PARAMS_COMPILE_SERVER.js"
+import { COMPILE_SERVER_TEST_PARAMS } from "@jsenv/core/test/__internal__/startCompileServer/TEST_PARAMS_COMPILE_SERVER.js"
+import { convertCommonJsWithBabel } from "./convertCommonJsWithBabel.js"
 
 const testDirectoryUrl = resolveUrl("./", import.meta.url)
 const testDirectoryRelativeUrl = urlToRelativeUrl(
