aaa3f6394cae5116d04dfdab705bf72aa9aeec78
Damien Maillard <damien.maillard@dailymotion.com>
2021-09-27 09:00:42 +0200

    update import meta url tests

--- test/import_meta_url/import_meta_url_build_commonjs.test.js
+++ test/import_meta_url/import_meta_url_build_commonjs.test.js
@@ -1,44 +1,46 @@
-import { basename } from "path"
 import { assert } from "@jsenv/assert"
 import {
   resolveDirectoryUrl,
   resolveUrl,
   urlToRelativeUrl,
+  urlToBasename,
 } from "@jsenv/filesystem"
+
+import { buildProject } from "@jsenv/core"
 import { jsenvCoreDirectoryUrl } from "@jsenv/core/src/internal/jsenvCoreDirectoryUrl.js"
 import {
   GENERATE_COMMONJS_BUILD_TEST_PARAMS,
   REQUIRE_COMMONJS_BUILD_TEST_PARAMS,
 } from "@jsenv/core/test/TEST_PARAMS_BUILD_COMMONJS.js"
 import { requireCommonJsBuild } from "@jsenv/core/test/requireCommonJsBuild.js"
-import { buildProject } from "@jsenv/core"
 
 const testDirectoryUrl = resolveDirectoryUrl("./", import.meta.url)
 const testDirectoryRelativeUrl = urlToRelativeUrl(
   testDirectoryUrl,
   jsenvCoreDirectoryUrl,
 )
-const testDirectoryname = basename(testDirectoryRelativeUrl)
+const testDirectoryname = urlToBasename(testDirectoryRelativeUrl)
 const jsenvDirectoryRelativeUrl = `${testDirectoryRelativeUrl}.jsenv`
 const buildDirectoryRelativeUrl = `${testDirectoryRelativeUrl}dist/commonjs`
 const mainFileBasename = `${testDirectoryname}.js`
 const entryPointMap = {
   [`./${testDirectoryRelativeUrl}${mainFileBasename}`]: "./main.cjs",
 }
-
 await buildProject({
   ...GENERATE_COMMONJS_BUILD_TEST_PARAMS,
   jsenvDirectoryRelativeUrl,
   buildDirectoryRelativeUrl,
   entryPointMap,
 })
-
-const { namespace: actual } = await requireCommonJsBuild({
+const { namespace } = await requireCommonJsBuild({
   ...REQUIRE_COMMONJS_BUILD_TEST_PARAMS,
   buildDirectoryRelativeUrl,
 })
+
+const actual = namespace
 const expected = {
-  url: resolveUrl(
+  isInstanceOfUrl: false,
+  urlString: resolveUrl(
     `${buildDirectoryRelativeUrl}/main.cjs`,
     jsenvCoreDirectoryUrl,
   ),
--- test/import_meta_url/import_meta_url_build_global.browser.test.js
+++ test/import_meta_url/import_meta_url_build_global.browser.test.js
@@ -16,22 +16,25 @@ const testDirectoryRelativeUrl = urlToRelativeUrl(
 )
 const testDirectoryname = basename(testDirectoryRelativeUrl)
 const jsenvDirectoryRelativeUrl = `${testDirectoryRelativeUrl}.jsenv/`
-const buildDirectoryRelativeUrl = `${testDirectoryRelativeUrl}dist/commonjs/`
+const buildDirectoryRelativeUrl = `${testDirectoryRelativeUrl}dist/global/`
 const mainFilename = `${testDirectoryname}.js`
 const entryPointMap = {
   [`./${testDirectoryRelativeUrl}${mainFilename}`]: "./main.js",
 }
-
 await buildProject({
   ...GENERATE_GLOBAL_BUILD_TEST_PARAMS,
   jsenvDirectoryRelativeUrl,
   buildDirectoryRelativeUrl,
-  importMapFileRelativeUrl: `${testDirectoryRelativeUrl}test.importmap`,
   entryPointMap,
 })
-const { globalValue: actual, serverOrigin } = await scriptLoadGlobalBuild({
+const { globalValue, serverOrigin } = await scriptLoadGlobalBuild({
   ...SCRIPT_LOAD_GLOBAL_BUILD_TEST_PARAMS,
   buildDirectoryRelativeUrl,
 })
-const expected = `${serverOrigin}/main.js`
+
+const actual = globalValue
+const expected = {
+  isInstanceOfUrl: false,
+  urlString: `${serverOrigin}/main.js`,
+}
 assert({ actual, expected })
