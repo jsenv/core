e837c0d69b3db882dec4fd0fcd86ecf0b0a7c5f9
Damien Maillard <damien.maillard@dailymotion.com>
2021-09-29 09:41:57 +0200

    testing import of jquery and flight

---
+++ test/module_conversion/importing_jquery_and_flight/importing_jquery_and_flight_build_systemjs.test.js
@@ -0,0 +1,42 @@
+import { assert } from "@jsenv/assert"
+import { resolveDirectoryUrl, urlToRelativeUrl } from "@jsenv/filesystem"
+
+import { buildProject } from "@jsenv/core"
+import { jsenvCoreDirectoryUrl } from "@jsenv/core/src/internal/jsenvCoreDirectoryUrl.js"
+import {
+  GENERATE_SYSTEMJS_BUILD_TEST_PARAMS,
+  IMPORT_SYSTEM_JS_BUILD_TEST_PARAMS,
+} from "@jsenv/core/test/TEST_PARAMS_BUILD_SYSTEMJS.js"
+import { browserImportSystemJsBuild } from "@jsenv/core/test/browserImportSystemJsBuild.js"
+
+const testDirectoryUrl = resolveDirectoryUrl("./", import.meta.url)
+const testDirectoryRelativeUrl = urlToRelativeUrl(
+  testDirectoryUrl,
+  jsenvCoreDirectoryUrl,
+)
+const jsenvDirectoryRelativeUrl = `${testDirectoryRelativeUrl}.jsenv/`
+const buildDirectoryRelativeUrl = `${testDirectoryRelativeUrl}dist/systemjs/`
+const mainFilename = `importing_jquery_and_flight.html`
+const entryPointMap = {
+  [`./${testDirectoryRelativeUrl}${mainFilename}`]: "./main.html",
+}
+const { buildMappings } = await buildProject({
+  ...GENERATE_SYSTEMJS_BUILD_TEST_PARAMS,
+  jsenvDirectoryRelativeUrl,
+  buildDirectoryRelativeUrl,
+  entryPointMap,
+})
+const mainJsBuildRelativeUrl =
+  buildMappings[`${testDirectoryRelativeUrl}importing_jquery_and_flight.js`]
+const { namespace } = await browserImportSystemJsBuild({
+  ...IMPORT_SYSTEM_JS_BUILD_TEST_PARAMS,
+  testDirectoryRelativeUrl,
+  mainRelativeUrl: `./${mainJsBuildRelativeUrl}`,
+})
+
+const actual = namespace
+const expected = {
+  jqueryType: "function",
+  flightComponentType: "function",
+}
+assert({ actual, expected })
