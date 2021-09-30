56f4c731ee1fae7bdffd08f94922764b82b640ba
Damien Maillard <damien.maillard@dailymotion.com>
2021-09-30 14:46:41 +0200

    fix textToJavaScriptModule

--- src/textToJavaScriptModule.js
+++ src/textToJavaScriptModule.js
@@ -1,6 +1,10 @@
 export const textToJavaScriptModule = async ({ code, url }) => {
+  const codeAsJson = JSON.stringify(code)
+    .replace(/\u2028/g, "\\u2028")
+    .replace(/\u2029/g, "\\u2029")
+
   return {
-    compiledSource: `export default "${JSON.stringify(code)}"`,
+    compiledSource: `export default ${codeAsJson}`,
     contentType: "application/javascript",
     sources: [url],
     sourcesContent: [code],
