f66a116cc1898acd4bc651adb3f27b96ddf40039
Damien Maillard <damien.maillard@dailymotion.com>
2021-10-01 05:51:48 +0200

    test export not found on node

---
+++ test/export_not_found/export_not_found_launch_node.test.js
@@ -0,0 +1,53 @@
+import { assert } from "@jsenv/assert"
+import {
+  resolveUrl,
+  resolveDirectoryUrl,
+  urlToRelativeUrl,
+} from "@jsenv/filesystem"
+
+import { launchNode } from "@jsenv/core"
+import { jsenvCoreDirectoryUrl } from "@jsenv/core/src/internal/jsenvCoreDirectoryUrl.js"
+import { startCompileServer } from "@jsenv/core/src/internal/compiling/startCompileServer.js"
+import { launchAndExecute } from "@jsenv/core/src/internal/executing/launchAndExecute.js"
+import {
+  START_COMPILE_SERVER_TEST_PARAMS,
+  LAUNCH_AND_EXECUTE_TEST_PARAMS,
+  LAUNCH_TEST_PARAMS,
+} from "@jsenv/core/test/TEST_PARAMS_LAUNCH_NODE.js"
+
+const testDirectoryUrl = resolveDirectoryUrl("./", import.meta.url)
+const testDirectoryRelativeUrl = urlToRelativeUrl(
+  testDirectoryUrl,
+  jsenvCoreDirectoryUrl,
+)
+const jsenvDirectoryRelativeUrl = `${testDirectoryRelativeUrl}.jsenv/`
+const mainFilename = `export_not_found.js`
+const fileRelativeUrl = `${testDirectoryRelativeUrl}${mainFilename}`
+const { origin: compileServerOrigin, outDirectoryRelativeUrl } =
+  await startCompileServer({
+    ...START_COMPILE_SERVER_TEST_PARAMS,
+    jsenvDirectoryRelativeUrl,
+  })
+const importerFileUrl = resolveUrl(mainFilename, testDirectoryUrl)
+
+const result = await launchAndExecute({
+  ...LAUNCH_AND_EXECUTE_TEST_PARAMS,
+  launchAndExecuteLogLevel: "off",
+  launch: (options) =>
+    launchNode({
+      ...LAUNCH_TEST_PARAMS,
+      ...options,
+      outDirectoryRelativeUrl,
+      compileServerOrigin,
+    }),
+  executeParams: {
+    fileRelativeUrl,
+  },
+})
+const stack = result.error.stack
+const expected = `${importerFileUrl}:1
+import { answer } from "./file.js"
+         ^^^^^^
+SyntaxError: The requested module './file.js' does not provide an export named 'answer'`
+const actual = stack.slice(0, expected.length)
+assert({ actual, expected })
