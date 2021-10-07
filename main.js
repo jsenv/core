e6e3fbcbd2e04e3089c5d7fa7165dfa75d3457df
Damien Maillard <damien.maillard@dailymotion.com>
2021-10-07 19:47:17 +0200

    add json to javascript module

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 /* eslint-disable import/max-dependencies */
 export { buildProject } from "./src/buildProject.js"
 export { commonJsToJavaScriptModule } from "./src/commonJsToJavaScriptModule.js"
+export { jsonToJavaScriptModule } from "./src/jsonToJavaScriptModule.js"
 export { textToJavaScriptModule } from "./src/textToJavaScriptModule.js"
 export { execute } from "./src/execute.js"
 export { executeTestPlan } from "./src/executeTestPlan.js"
