442813dad0d2f3582f8eebe92dad23d7eb82973c
Damien Maillard <damien.maillard@dailymotion.com>
2021-10-11 19:45:39 +0200

    prepare testing new brower system where capabel to handle import assertions

--- src/internal/runtime/createBrowserRuntime/createBrowserSystem.js
+++ src/internal/runtime/createBrowserRuntime/createBrowserSystem.js
@@ -23,6 +23,24 @@ export const createBrowserSystem = ({
   const instantiate = browserSystem.instantiate
   browserSystem.instantiate = async function (url, importerUrl) {
     try {
+      const { importType, urlWithoutImportType } = extractImportTypeFromUrl(url)
+
+      if (importType === "json") {
+        const jsonModule = await instantiateAsJsonModule(urlWithoutImportType, {
+          loader: this,
+          fetchSource,
+        })
+        return jsonModule
+      }
+
+      if (importType === "css") {
+        const cssModule = await instantiateAsCssModule(urlWithoutImportType, {
+          loader: this,
+          fetchSource,
+        })
+        return cssModule
+      }
+
       const returnValue = await instantiate.call(this, url, importerUrl)
       return returnValue
     } catch (e) {
@@ -47,6 +65,67 @@ export const createBrowserSystem = ({
 
   return browserSystem
 }
+
+const extractImportTypeFromUrl = (url) => {
+  const urlObject = new URL(url)
+  const { search } = urlObject
+  const searchParams = new URLSearchParams(search)
+
+  const importType = searchParams.get("import_type")
+  if (!importType) {
+    return {}
+  }
+
+  searchParams.delete("import_type")
+  urlObject.search = String(searchParams)
+  return {
+    importType,
+    urlWithoutImportType: urlObject.href,
+  }
+}
+
+const instantiateAsJsonModule = async (url, { loader, fetchSource }) => {
+  const response = await fetchSource(url)
+  const jsonAsText = await response.text()
+  const jsonAsJsModule = `System.register([], function (export) {
+  return{
+    execute: function () {
+     export("default", '${jsonAsText}')
+    }
+  }
+})`
+
+  // eslint-disable-next-line no-eval
+  ;(0, window.eval)(jsonAsJsModule)
+  return loader.getRegister(url)
+}
+
+const instantiateAsCssModule = async (url, { loader, fetchSource }) => {
+  const response = await fetchSource(url)
+  const cssTextOriginal = await response.text()
+  // https://github.com/systemjs/systemjs/blob/98609dbeef01ec62447e4b21449ce47e55f818bd/src/extras/module-types.js#L37
+  const cssTextRelocated = cssTextOriginal.replace(
+    /url\(\s*(?:(["'])((?:\\.|[^\n\\"'])+)\1|((?:\\.|[^\s,"'()\\])+))\s*\)/g,
+    (match, quotes, relUrl1, relUrl2) => {
+      return `url(" ${quotes}${resolveUrl(relUrl1 || relUrl2, url)}${quotes}")`
+    },
+  )
+  const cssAsJsModule = `System.register([], function (export) {
+  return {
+    execute: function () {
+      var sheet = new CSSStyleSheet()
+      sheet.replaceSync(${JSON.stringify(cssTextRelocated)})
+      export('default', sheet)
+    }
+  }
+})`
+
+  // eslint-disable-next-line no-eval
+  ;(0, window.eval)(cssAsJsModule)
+  return loader.getRegister(url)
+}
+
+const resolveUrl = (specifier) => new URL(specifier).href
 
 const createDetailedInstantiateError = async ({
   instantiateError,
---
+++ test/import_assertions/import_type_css/import_type_css_launch_browser.test.js
@@ -0,0 +1,73 @@
+import { assert } from "@jsenv/assert"
+import { resolveUrl, urlToRelativeUrl } from "@jsenv/filesystem"
+
+import { launchChromium, launchFirefox, launchWebkit } from "@jsenv/core"
+import { jsenvCoreDirectoryUrl } from "@jsenv/core/src/internal/jsenvCoreDirectoryUrl.js"
+import { startCompileServer } from "@jsenv/core/src/internal/compiling/startCompileServer.js"
+import { launchAndExecute } from "@jsenv/core/src/internal/executing/launchAndExecute.js"
+import {
+  START_COMPILE_SERVER_TEST_PARAMS,
+  EXECUTION_TEST_PARAMS,
+  LAUNCH_TEST_PARAMS,
+} from "@jsenv/core/test/TEST_PARAMS_LAUNCH_BROWSER.js"
+import { launchBrowsers } from "@jsenv/core/test/launchBrowsers.js"
+
+const testDirectoryUrl = resolveUrl("./", import.meta.url)
+const testDirectoryRelativeUrl = urlToRelativeUrl(
+  testDirectoryUrl,
+  jsenvCoreDirectoryUrl,
+)
+const jsenvDirectoryRelativeUrl = `${testDirectoryRelativeUrl}.jsenv/`
+const htmlFilename = `main.html`
+const htmlFileRelativeUrl = `${testDirectoryRelativeUrl}${htmlFilename}`
+const { origin: compileServerOrigin, outDirectoryRelativeUrl } =
+  await startCompileServer({
+    ...START_COMPILE_SERVER_TEST_PARAMS,
+    jsenvDirectoryRelativeUrl,
+  })
+
+await launchBrowsers(
+  [
+    // comment force multiline
+    // launchChromium,
+    launchFirefox,
+    // launchWebkit,
+  ],
+  async (launchBrowser) => {
+    const result = await launchAndExecute({
+      ...EXECUTION_TEST_PARAMS,
+      launchAndExecuteLogLevel: "off",
+      launch: (options) =>
+        launchBrowser({
+          ...LAUNCH_TEST_PARAMS,
+          ...options,
+          outDirectoryRelativeUrl,
+          compileServerOrigin,
+        }),
+      executeParams: {
+        fileRelativeUrl: htmlFileRelativeUrl,
+      },
+      // launchParams: {
+      //   headless: false,
+      // },
+      // stopAfterExecute: false,
+    })
+
+    const actual = {
+      status: result.status,
+      namespace: result.namespace,
+    }
+    const expected = {
+      status: "completed",
+      namespace: {
+        "./main_script_inline_10.js": {
+          status: "completed",
+          namespace: {
+            backgroundBodyColor: "rgb(255, 0, 0)",
+          },
+        },
+      },
+    }
+    assert({ actual, expected })
+  },
+)
