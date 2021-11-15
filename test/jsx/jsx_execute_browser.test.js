4055f1adbbb18c92bd2922b5f02b73b2c1c61b31
Damien Maillard <damien.maillard@dailymotion.com>
2021-11-15 13:27:20 +0100

    update jsx tests

--- test/jsx/jsx_execute_browser.test.js
+++ test/jsx/jsx_execute_browser.test.js
@@ -1,12 +1,19 @@
 import { assert } from "@jsenv/assert"
 import { resolveDirectoryUrl, urlToRelativeUrl } from "@jsenv/filesystem"
 
-import { execute, chromiumRuntime } from "@jsenv/core"
+import {
+  execute,
+  chromiumRuntime,
+  commonJsToJavaScriptModule,
+} from "@jsenv/core"
+import { require } from "@jsenv/core/src/internal/require.js"
 import { jsenvCoreDirectoryUrl } from "@jsenv/core/src/internal/jsenvCoreDirectoryUrl.js"
 import {
   EXECUTE_TEST_PARAMS,
   LAUNCH_TEST_PARAMS,
 } from "@jsenv/core/test/TEST_PARAMS_LAUNCH_BROWSER.js"
+
+const transformReactJSX = require("@babel/plugin-transform-react-jsx")
 
 const testDirectoryUrl = resolveDirectoryUrl("./", import.meta.url)
 const testDirectoryRelativeUrl = urlToRelativeUrl(
@@ -14,7 +21,7 @@ const testDirectoryRelativeUrl = urlToRelativeUrl(
   jsenvCoreDirectoryUrl,
 )
 const jsenvDirectoryRelativeUrl = `${testDirectoryRelativeUrl}.jsenv`
-const filename = `jsx-launch-browser.html`
+const filename = `jsx.html`
 const fileRelativeUrl = `${testDirectoryRelativeUrl}${filename}`
 
 const { status, namespace } = await execute({
@@ -23,6 +30,15 @@ const { status, namespace } = await execute({
   runtime: chromiumRuntime,
   runtimeParams: {
     ...LAUNCH_TEST_PARAMS,
+  },
+  babelPluginMap: {
+    "transform-react-jsx": [
+      transformReactJSX,
+      { pragma: "React.createElement" },
+    ],
+  },
+  customCompilers: {
+    "**/node_modules/react/index.js": commonJsToJavaScriptModule,
   },
   fileRelativeUrl,
 })
@@ -33,7 +49,7 @@ const actual = {
 const expected = {
   status: "completed",
   namespace: {
-    [`./jsx-launch-browser.js`]: {
+    [`./jsx.js`]: {
       status: "completed",
       namespace: {
         default: 42,
