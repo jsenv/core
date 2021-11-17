c2023d63e5a7f010a563db45d9ca508157371b2e
Damien Maillard <damien.maillard@dailymotion.com>
2021-11-17 11:48:42 +0100

    fix file not generated during build for import pattern + global format

--- test/import_meta_url_pattern/import_meta_url_pattern_build_global.test.js
+++ test/import_meta_url_pattern/import_meta_url_pattern_build_global.test.js
@@ -1,5 +1,10 @@
+import { existsSync } from "node:fs"
 import { assert } from "@jsenv/assert"
-import { resolveDirectoryUrl, urlToRelativeUrl } from "@jsenv/filesystem"
+import {
+  resolveDirectoryUrl,
+  urlToRelativeUrl,
+  resolveUrl,
+} from "@jsenv/filesystem"
 
 import { buildProject } from "@jsenv/core"
 import { jsenvCoreDirectoryUrl } from "@jsenv/core/src/internal/jsenvCoreDirectoryUrl.js"
@@ -31,13 +36,31 @@ const { globalValue, serverOrigin } = await scriptLoadGlobalBuild({
   buildDirectoryRelativeUrl,
 })
 
+const jsUrlInstanceOfUrl = globalValue.jsUrlInstanceOfUrl
+const jsUrl = globalValue.jsUrlString
+
 // it would be great to have an error or at least a warning.
+// ! The file must be hashed
+// ! The file must be on filesystem
 const actual = {
-  jsUrlInstanceOfUrl: globalValue.jsUrlInstanceOfUrl,
-  jsUrlString: globalValue.jsUrlString,
+  jsUrlInstanceOfUrl,
+  jsUrl,
 }
 const expected = {
   jsUrlInstanceOfUrl: true,
-  jsUrlString: String(new URL(`./file.js`, serverOrigin)),
+  jsUrl: String(new URL(`./assets/file-b514b4fb.js`, serverOrigin)),
 }
 assert({ actual, expected })
+
+{
+  const buildDirectoryUrl = resolveUrl(
+    buildDirectoryRelativeUrl,
+    jsenvCoreDirectoryUrl,
+  )
+  const jsRelativeUrl = urlToRelativeUrl(jsUrl, serverOrigin)
+  const jsBuildUrl = resolveUrl(jsRelativeUrl, buildDirectoryUrl)
+  const jsBuildFileExists = existsSync(new URL(jsBuildUrl))
+  const actual = jsBuildFileExists
+  const expected = true
+  assert({ actual, expected })
+}
