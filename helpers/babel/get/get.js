30ebfc0fa5f4fa6ef574fedef44bc8939b6240d4
Damien Maillard <damien.maillard@dailymotion.com>
2021-11-21 09:48:05 +0100

    update babel helpers

--- helpers/babel/get/get.js
+++ helpers/babel/get/get.js
@@ -1,13 +1,19 @@
 import superPropBase from "../superPropBase/superPropBase.js"
 
-function reflectGet(target, property, receiver) {
-  var base = superPropBase(target, property)
-  if (!base) return undefined
-  var desc = Object.getOwnPropertyDescriptor(base, property)
-  if (desc.get) {
-    return desc.get.call(receiver)
+export default function _get() {
+  if (typeof Reflect !== "undefined" && Reflect.get) {
+    _get = Reflect.get;
+  } else {
+    _get = function _get(target, property, receiver) {
+      var base = superPropBase(target, property);
+      if (!base) return;
+      var desc = Object.getOwnPropertyDescriptor(base, property);
+      if (desc.get) {
+        // STEP 3. If receiver is not present, then set receiver to target.
+        return desc.get.call(arguments.length < 3 ? target : receiver);
+      }
+      return desc.value;
+    };
   }
-  return desc.value
+  return _get.apply(this, arguments);
 }
-
-export default typeof Reflect !== "undefined" && Reflect.get ? Reflect.get : reflectGet
--- helpers/babel/readme.md
+++ helpers/babel/readme.md
@@ -2,8 +2,12 @@
 
 Babel helpers are copied in there to properly appear in sourcemap.
 
-- All thoose file are inlines versions of babel helpers declared in: https://github.com/babel/babel/blob/main/packages/babel-helpers/src/helpers.js
+- Last sync date: 21 November 2021
 
 - History of the file: https://github.com/babel/babel/commits/main/packages/babel-helpers/src/helpers.js
 
+
 - History of individual helpers: https://github.com/babel/babel/commits/main/packages/babel-helpers/src/helpers
+
+
+- All thoose file are inlines versions of babel helpers declared in: https://github.com/babel/babel/blob/main/packages/babel-helpers/src/helpers.js
