07536ac218b4ad521ba99a392224910b92fd35c2
Damien Maillard <damien.maillard@dailymotion.com>
2021-12-06 17:28:22 +0100

    update dist

--- dist/build_manifest.js
+++ dist/build_manifest.js
@@ -7,6 +7,6 @@ export const COMPILE_PROXY_BUILD_URL = new URL("compile_proxy/compile_proxy-1dfc
 
 export const EVENT_SOURCE_CLIENT_BUILD_URL = new URL("event_source_client/event_source_client-9f14c8b9.js", import.meta.url).href
 
-export const TOOLBAR_BUILD_URL = new URL("toolbar/toolbar-201c4093.html", import.meta.url).href
+export const TOOLBAR_BUILD_URL = new URL("toolbar/toolbar-afb71355.html", import.meta.url).href
 
-export const TOOLBAR_INJECTOR_BUILD_URL = new URL("toolbar_injector/toolbar_injector-84042210.js", import.meta.url).href
+export const TOOLBAR_INJECTOR_BUILD_URL = new URL("toolbar_injector/toolbar_injector-445d3ea0.js", import.meta.url).href
