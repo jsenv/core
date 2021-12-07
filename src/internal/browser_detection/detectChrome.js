5d582cd878a6e2ce7339121fd2fa364863386a03
dmail <dmaillard06@gmail.com>
2021-12-07 15:49:48 +0100

    Prefer navigator.userAgentData when possible

--- src/internal/browser_detection/detectChrome.js
+++ src/internal/browser_detection/detectChrome.js
@@ -1,6 +1,20 @@
 import { userAgentToVersion, firstMatch } from "./util.js"
 
-export const detectChrome = () => userAgentToBrowser(window.navigator.userAgent)
+export const detectChrome = () => {
+  const { userAgentData } = window.navigator
+  if (userAgentData) {
+    const brand = userAgentData.brands.some((brand) => {
+      return brand.brand === "chromium" || brand.brand === "Google Chrome"
+    })
+    if (brand) {
+      return {
+        name: "chrome",
+        version: brand.version,
+      }
+    }
+  }
+  return userAgentToBrowser(window.navigator.userAgent)
+}
 
 const userAgentToBrowser = (userAgent) => {
   if (/chromium/i.test(userAgent)) {
