0d6dec9281a302489f7ab0e055bb4d96171aee82
dmail <dmaillard06@gmail.com>
2022-01-02 13:03:10 +0100

    update a test

--- test/entry_many/entry_many_build_commonjs.test.js
+++ test/entry_many/entry_many_build_commonjs.test.js
@@ -52,9 +52,12 @@ const { buildManifest, buildMappings } = await buildProject({
   const manifestFileObject = JSON.parse(manifestFileContent)
   const actual = manifestFileObject
   const expected = {
-    [`a.cjs`]: "a.cjs",
-    [`b.cjs`]: "b.cjs",
-    [`used-by-both.cjs`]: actual[`used-by-both.cjs`],
+    "a.cjs": "a.cjs",
+    "a.cjs.map": "a.cjs.map",
+    "b.cjs": "b.cjs",
+    "b.cjs.map": "b.cjs.map",
+    "used-by-both.cjs": "used-by-both_2f9b92e5.cjs",
+    "used-by-both.cjs.map": "used-by-both_2f9b92e5.cjs.map",
   }
   assert({ actual, expected })
 
