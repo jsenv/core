c78747bf2f791fcedcdbe3bbf734a39c4b9a9256
dmail <dmaillard06@gmail.com>
2022-01-12 16:26:52 +0100

    remove needs for https in a test

--- test/external_url/main.js
+++ test/external_url/main.js
@@ -1,4 +1,4 @@
-import { url } from "https://localhost:9999/file.js"
+import { url } from "http://localhost:9999/file.js"
 
 console.log(url)
 
--- test/external_url/script/serve.js
+++ test/external_url/script/serve.js
@@ -1,15 +1,9 @@
 import { startServer, fetchFileSystem, pluginCORS } from "@jsenv/server"
-import { requestCertificateForLocalhost } from "@jsenv/https-local"
 
-const { serverCertificate, serverCertificatePrivateKey } =
-  await requestCertificateForLocalhost()
 const serverDirectoryUrl = new URL("../server/", import.meta.url)
 
 export const server = await startServer({
-  protocol: "https",
   logLevel: "warn",
-  certificate: serverCertificate,
-  privateKey: serverCertificatePrivateKey,
   port: 9999,
   requestToResponse: async (request) => {
     const fileUrl = new URL(request.ressource.slice(1), serverDirectoryUrl)
