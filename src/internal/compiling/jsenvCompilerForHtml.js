4f6f3c660a783d004263d1fd800adcaa6ac9a6cd
dmail <dmaillard06@gmail.com>
2022-01-17 18:32:24 +0100

    remove integrity attribute from compiled html

--- src/internal/compiling/jsenvCompilerForHtml.js
+++ src/internal/compiling/jsenvCompilerForHtml.js
@@ -34,7 +34,7 @@ import {
   removeHtmlNodeAttribute,
   setHtmlNodeText,
   visitHtmlAst,
-  replaceHtmlNode,
+  addHtmlNodeAttribute,
 } from "./compileHtml.js"
 import { generateCompilationAssetUrl } from "./compile-directory/compile-asset.js"
 
@@ -77,11 +77,6 @@ export const compileHtml = async ({
 
   // ideally we should try/catch html syntax error
   const htmlAst = parseHtmlString(code)
-
-  if (moduleOutFormat !== "esmodule") {
-    await mutateRessourceHints(htmlAst)
-  }
-
   manipulateHtmlAst(htmlAst, {
     scriptInjections: [
       ...(jsenvScriptInjection
@@ -138,52 +133,26 @@ export const compileHtml = async ({
       specifier,
     })
   }
-
-  const importmapInfo = await visitImportmapScript({
+  if (moduleOutFormat !== "esmodule") {
+    const ressourceHints = collectRessourceHints(htmlAst)
+    await visitRessourceHints({
+      ressourceHints,
+      addHtmlMutation,
+    })
+  }
+  await visitImportmapScript({
+    htmlAst,
     logger,
     url,
     compiledUrl,
     projectDirectoryUrl,
     compileDirectoryUrl,
+    moduleOutFormat,
     scripts,
+    addHtmlMutation,
     addHtmlSourceFile,
+    onHtmlImportmapInfo,
   })
-  const importmap = (await importmapInfo.load()) || {}
-  const importmapAsText = JSON.stringify(importmap, null, "  ")
-  importmapInfo.inlinedFrom = importmapInfo.url
-  importmapInfo.text = importmapAsText
-  addHtmlMutation(() => {
-    if (importmapInfo.needsInjection) {
-      manipulateHtmlAst(htmlAst, {
-        scriptInjections: [
-          {
-            type:
-              moduleOutFormat === "systemjs"
-                ? "systemjs-importmap"
-                : "importmap",
-            // in case there is no importmap, force the presence
-            // so that '@jsenv/core/' are still remapped
-            text: importmapAsText,
-          },
-        ],
-      })
-    } else {
-      replaceHtmlNode(
-        importmapInfo.script,
-        `<script type="${
-          moduleOutFormat === "systemjs" ? "systemjs-importmap" : "importmap"
-        }">${importmapAsText}</script>`,
-        {
-          attributesToIgnore: ["src"],
-        },
-      )
-    }
-  })
-  onHtmlImportmapInfo({
-    htmlUrl: url,
-    importmapInfo,
-  })
-
   await visitScripts({
     logger,
     projectDirectoryUrl,
@@ -214,13 +183,11 @@ export const compileHtml = async ({
     }),
   )
   htmlAssetGenerators.length = 0
-
   htmlMutations.forEach((htmlMutation) => {
     htmlMutation()
   })
   htmlMutations.length = 0
   const htmlAfterTransformation = stringifyHtmlAst(htmlAst)
-
   return {
     contentType: "text/html",
     compiledSource: htmlAfterTransformation,
@@ -234,32 +201,94 @@ export const compileHtml = async ({
   }
 }
 
+// transform <link type="modulepreload"> into <link type="preload">
+// also remove integrity attributes because we don't know in advance
+// the result of the file compilation
+const visitRessourceHints = async ({ ressourceHints, addHtmlMutation }) => {
+  await Promise.all(
+    ressourceHints.map(async (ressourceHint) => {
+      const hrefAttribute = getHtmlNodeAttributeByName(ressourceHint, "href")
+      const href = hrefAttribute ? hrefAttribute.value : ""
+      if (!href) {
+        return
+      }
+      const integrityAttribute = getHtmlNodeAttributeByName(
+        ressourceHint,
+        "integrity",
+      )
+      if (integrityAttribute) {
+        addHtmlMutation(() => {
+          removeHtmlNodeAttribute(ressourceHint, integrityAttribute)
+        })
+      }
+      const relAttribute = getHtmlNodeAttributeByName(ressourceHint, "rel")
+      const asAttribute = getHtmlNodeAttributeByName(ressourceHint, "as")
+      // - "modulepreload" -> "preload" because it's now regular js script
+      if (ressourceHint.rel === "modulepreload") {
+        addHtmlMutation(() => {
+          relAttribute.value = "preload"
+          if (asAttribute) {
+            asAttribute.value = "script"
+          } else {
+            addHtmlNodeAttribute(ressourceHint, { name: "as", value: "script" })
+          }
+        })
+        return
+      }
+      // if (asAttribute && asAttribute.value === "script") {
+      //   addHtmlMutation(() => {
+      //     replaceHtmlNode(htmlNode, `<link as="script" />`)
+      //   })
+      //   return
+      // }
+    }),
+  )
+}
+
 const visitImportmapScript = async ({
+  htmlAst,
   logger,
   url,
   compiledUrl,
   projectDirectoryUrl,
   compileDirectoryUrl,
+  moduleOutFormat,
   scripts,
+  addHtmlMutation,
   addHtmlSourceFile,
+  onHtmlImportmapInfo,
 }) => {
   const importmapScripts = scripts.filter((script) => {
     const typeAttribute = getHtmlNodeAttributeByName(script, "type")
     const type = typeAttribute ? typeAttribute.value : "application/javascript"
     return type === "importmap"
   })
+  // in case there is no importmap, force the presence
+  // so that '@jsenv/core/' are still remapped
   if (importmapScripts.length === 0) {
-    return {
-      needsInjection: true,
+    const defaultImportMap = getDefaultImportmap(compiledUrl, {
+      projectDirectoryUrl,
+      compileDirectoryUrl,
+    })
+    const defaultImportMapAsText = JSON.stringify(defaultImportMap, null, "  ")
+    onHtmlImportmapInfo({
       url: compiledUrl,
-      load: () => {
-        const defaultImportMap = getDefaultImportmap(compiledUrl, {
-          projectDirectoryUrl,
-          compileDirectoryUrl,
-        })
-        return defaultImportMap
-      },
-    }
+      text: defaultImportMapAsText,
+    })
+    addHtmlMutation(() => {
+      manipulateHtmlAst(htmlAst, {
+        scriptInjections: [
+          {
+            type:
+              moduleOutFormat === "systemjs"
+                ? "systemjs-importmap"
+                : "importmap",
+            text: defaultImportMapAsText,
+          },
+        ],
+      })
+    })
+    return
   }
 
   if (importmapScripts.length > 1) {
@@ -270,53 +299,72 @@ const visitImportmapScript = async ({
   const src = srcAttribute ? srcAttribute.value : ""
   if (src) {
     const importmapUrl = resolveUrl(src, url)
-    const importmapInfo = {
-      script: firstImportmapScript,
-      url: importmapUrl,
-      load: async () => {
-        const importMapResponse = await fetchUrl(importmapUrl)
-        if (importMapResponse.status === 200) {
-          const importmapAsText = await importMapResponse.text()
-          addHtmlSourceFile({
-            url: importmapUrl,
-            content: importmapAsText,
-          })
-          let htmlImportmap = JSON.parse(importmapAsText)
-          htmlImportmap = moveImportMap(htmlImportmap, importmapUrl, url)
-          return htmlImportmap
-        }
-        logger.warn(
-          createDetailedMessage(
-            importMapResponse.status === 404
-              ? `importmap script file cannot be found.`
-              : `importmap script file unexpected response status (${importMapResponse.status}).`,
-            {
-              "importmap url": importmapInfo.url,
-              "html url": url,
-            },
-          ),
-        )
-        return null
-      },
-    }
-    return importmapInfo
-  }
-  const importmapInfo = {
-    script: firstImportmapScript,
-    url: compiledUrl,
-    load: () => {
-      const jsenvImportmap = getDefaultImportmap(compiledUrl, {
-        projectDirectoryUrl,
-        compileDirectoryUrl,
+    const importMapResponse = await fetchUrl(importmapUrl)
+    let importmap
+    if (importMapResponse.status === 200) {
+      const importmapAsText = await importMapResponse.text()
+      addHtmlSourceFile({
+        url: importmapUrl,
+        content: importmapAsText,
       })
-      const htmlImportmap = JSON.parse(
-        getHtmlNodeTextNode(firstImportmapScript).value,
+      let htmlImportmap = JSON.parse(importmapAsText)
+      importmap = moveImportMap(htmlImportmap, importmapUrl, url)
+    } else {
+      logger.warn(
+        createDetailedMessage(
+          importMapResponse.status === 404
+            ? `importmap script file cannot be found.`
+            : `importmap script file unexpected response status (${importMapResponse.status}).`,
+          {
+            "importmap url": importmapUrl,
+            "html url": url,
+          },
+        ),
       )
-      const importmap = composeTwoImportMaps(jsenvImportmap, htmlImportmap)
-      return importmap
-    },
+      importmap = {}
+    }
+    const importmapAsText = JSON.stringify(importmap, null, "  ")
+    onHtmlImportmapInfo({
+      url: importmapUrl,
+      text: importmapAsText,
+    })
+    addHtmlMutation(() => {
+      removeHtmlNodeAttribute(firstImportmapScript, srcAttribute)
+      if (moduleOutFormat === "systemjs") {
+        const typeAttribute = getHtmlNodeAttributeByName(
+          firstImportmapScript,
+          "type",
+        )
+        typeAttribute.value = "systemjs-importmap"
+      }
+    })
+    return
   }
-  return importmapInfo
+
+  const jsenvImportmap = getDefaultImportmap(compiledUrl, {
+    projectDirectoryUrl,
+    compileDirectoryUrl,
+  })
+  const htmlImportmap = JSON.parse(
+    getHtmlNodeTextNode(firstImportmapScript).value,
+  )
+  const importmap = composeTwoImportMaps(jsenvImportmap, htmlImportmap)
+  const importmapAsText = JSON.stringify(importmap, null, "  ")
+  onHtmlImportmapInfo({
+    url: compiledUrl,
+    text: importmapAsText,
+  })
+  addHtmlMutation(() => {
+    removeHtmlNodeAttribute(firstImportmapScript, srcAttribute)
+    if (moduleOutFormat === "systemjs") {
+      const typeAttribute = getHtmlNodeAttributeByName(
+        firstImportmapScript,
+        "type",
+      )
+      typeAttribute.value = "systemjs-importmap"
+    }
+  })
+  return
 }
 
 const visitScripts = async ({
@@ -344,6 +392,7 @@ const visitScripts = async ({
     const type = typeAttribute ? typeAttribute.value : "application/javascript"
     const srcAttribute = getHtmlNodeAttributeByName(script, "src")
     const src = srcAttribute ? srcAttribute.value : ""
+    const integrityAttribute = getHtmlNodeAttributeByName(script, "integrity")
     const textNode = getHtmlNodeTextNode(script)
 
     if (type === "module") {
@@ -351,6 +400,9 @@ const visitScripts = async ({
         addHtmlMutation(() => {
           if (moduleOutFormat === "systemjs") {
             removeHtmlNodeAttribute(script, typeAttribute)
+          }
+          if (integrityAttribute) {
+            removeHtmlNodeAttribute(script, integrityAttribute)
           }
           removeHtmlNodeAttribute(script, srcAttribute)
           const jsenvMethod =
@@ -487,6 +539,9 @@ const visitScripts = async ({
           })
         })
         addHtmlMutation(() => {
+          if (integrityAttribute) {
+            removeHtmlNodeAttribute(script, integrityAttribute)
+          }
           srcAttribute.value = `./${urlToRelativeUrl(
             scriptCompiledUrl,
             compiledUrl,
@@ -598,11 +653,12 @@ const transformHtmlScript = async ({
   ]
 }
 
-// transform <link type="modulepreload"> into <link type="preload">
-const mutateRessourceHints = async (htmlAst) => {
+const collectRessourceHints = (htmlAst) => {
   const ressourceHints = []
   visitHtmlAst(htmlAst, (htmlNode) => {
-    if (htmlNode.nodeName !== "link") return
+    if (htmlNode.nodeName !== "link") {
+      return
+    }
     const relAttribute = getHtmlNodeAttributeByName(htmlNode, "rel")
     const rel = relAttribute ? relAttribute.value : ""
     const isRessourceHint = [
@@ -612,44 +668,10 @@ const mutateRessourceHints = async (htmlAst) => {
       "preload",
       "modulepreload",
     ].includes(rel)
-    if (!isRessourceHint) return
-
-    ressourceHints.push({ rel, htmlNode })
+    if (!isRessourceHint) {
+      return
+    }
+    ressourceHints.push(htmlNode)
   })
-
-  const mutations = []
-  await Promise.all(
-    ressourceHints.map(async (ressourceHint) => {
-      const hrefAttribute = getHtmlNodeAttributeByName(
-        ressourceHint.htmlNode,
-        "href",
-      )
-      const href = hrefAttribute ? hrefAttribute.value : ""
-      if (!href) return
-
-      // - "modulepreload" -> "preload" because it's now regular js script
-      const asAttribute = getHtmlNodeAttributeByName(
-        ressourceHint.htmlNode,
-        "as",
-      )
-
-      if (ressourceHint.rel === "modulepreload") {
-        mutations.push(() => {
-          replaceHtmlNode(
-            ressourceHint.htmlNode,
-            `<link rel="preload" as="script" />`,
-          )
-        })
-        return
-      }
-
-      if (asAttribute && asAttribute.value === "script") {
-        mutations.push(() => {
-          replaceHtmlNode(ressourceHint.htmlNode, `<link as="script" />`)
-        })
-        return
-      }
-    }),
-  )
-  mutations.forEach((mutation) => mutation())
+  return ressourceHints
 }
