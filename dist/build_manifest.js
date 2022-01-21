96cccaf01b941bb21daad291de03e31b5318bebb
dmail <dmaillard06@gmail.com>
2022-01-21 18:32:21 +0100

    update build files

--- dist/build_manifest.js
+++ dist/build_manifest.js
@@ -1,12 +1,12 @@
 
-export const REDIRECTOR_BUILD_URL = new URL("redirector/redirector_368fcabe.html", import.meta.url).href
+export const REDIRECTOR_BUILD_URL = new URL("redirector/redirector_5bf1ccb1.html", import.meta.url).href
 
 export const BROWSER_RUNTIME_BUILD_URL = new URL("browser_runtime/browser_runtime_93a975f9.js", import.meta.url).href
 
-export const COMPILE_PROXY_BUILD_URL = new URL("compile_proxy/compile_proxy_aa7e3f70.html", import.meta.url).href
+export const COMPILE_PROXY_BUILD_URL = new URL("compile_proxy/compile_proxy_7338f879.html", import.meta.url).href
 
 export const EVENT_SOURCE_CLIENT_BUILD_URL = new URL("event_source_client/event_source_client_80644aee.js", import.meta.url).href
 
-export const TOOLBAR_BUILD_URL = new URL("toolbar/toolbar_3e8b9312.html", import.meta.url).href
+export const TOOLBAR_BUILD_URL = new URL("toolbar/toolbar_5cf5601b.html", import.meta.url).href
 
-export const TOOLBAR_INJECTOR_BUILD_URL = new URL("toolbar_injector/toolbar_injector_133a4728.js", import.meta.url).href
+export const TOOLBAR_INJECTOR_BUILD_URL = new URL("toolbar_injector/toolbar_injector_b027d3f2.js", import.meta.url).href
