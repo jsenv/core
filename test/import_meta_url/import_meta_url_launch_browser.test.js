0a2790ddf45504a5b3a4a0721fff718d7a77aa6d
dmail <dmaillard06@gmail.com>
2022-01-24 11:04:11 +0100

    update import.meta.url test on browser

--- test/import_meta_url/import_meta_url_launch_browser.test.js
+++ test/import_meta_url/import_meta_url_launch_browser.test.js
@@ -1,5 +1,5 @@
-import { assert } from "@jsenv/assert"
 import { resolveDirectoryUrl, urlToRelativeUrl } from "@jsenv/filesystem"
+import { assert } from "@jsenv/assert"
 
 import {
   execute,
@@ -8,7 +8,6 @@ import {
   webkitRuntime,
 } from "@jsenv/core"
 import { jsenvCoreDirectoryUrl } from "@jsenv/core/src/internal/jsenvCoreDirectoryUrl.js"
-import { COMPILE_ID_BEST } from "@jsenv/core/src/internal/CONSTANTS.js"
 import {
   EXECUTE_TEST_PARAMS,
   LAUNCH_TEST_PARAMS,
@@ -20,11 +19,9 @@ const testDirectoryRelativeUrl = urlToRelativeUrl(
   testDirectoryUrl,
   jsenvCoreDirectoryUrl,
 )
-const jsenvDirectoryRelativeUrl = `${testDirectoryRelativeUrl}.jsenv`
-const htmlFilename = `import_meta_url.html`
-const htmlFileRelativeUrl = `${testDirectoryRelativeUrl}${htmlFilename}`
+const jsenvDirectoryRelativeUrl = `${testDirectoryRelativeUrl}.jsenv/`
+const htmlFileRelativeUrl = `${testDirectoryRelativeUrl}import_meta_url.html`
 const fileRelativeUrl = `${testDirectoryRelativeUrl}import_meta_url.js`
-const compileId = COMPILE_ID_BEST
 
 await launchBrowsers(
   [
@@ -34,17 +31,16 @@ await launchBrowsers(
     webkitRuntime,
   ],
   async (browserRuntime) => {
-    const { status, namespace, compileServerOrigin, outDirectoryRelativeUrl } =
-      await execute({
-        ...EXECUTE_TEST_PARAMS,
-        jsenvDirectoryRelativeUrl,
-        runtime: browserRuntime,
-        runtimeParams: {
-          ...LAUNCH_TEST_PARAMS,
-        },
-        fileRelativeUrl: htmlFileRelativeUrl,
-        collectCompileServerInfo: true,
-      })
+    const { status, namespace, compileServerOrigin } = await execute({
+      ...EXECUTE_TEST_PARAMS,
+      jsenvDirectoryRelativeUrl,
+      runtime: browserRuntime,
+      runtimeParams: {
+        ...LAUNCH_TEST_PARAMS,
+      },
+      fileRelativeUrl: htmlFileRelativeUrl,
+      collectCompileServerInfo: true,
+    })
     const actual = {
       status,
       namespace,
@@ -59,7 +55,7 @@ await launchBrowsers(
             urlString:
               browserRuntime === chromiumRuntime
                 ? `${compileServerOrigin}/${fileRelativeUrl}`
-                : `${compileServerOrigin}/${outDirectoryRelativeUrl}${compileId}/${fileRelativeUrl}`,
+                : `${compileServerOrigin}/${jsenvDirectoryRelativeUrl}out/${fileRelativeUrl}`,
           },
         },
       },
