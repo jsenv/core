67ae7472a494b202fca7d5fde0b39e2608956906
dmail <dmaillard06@gmail.com>
2022-02-22 05:32:55 +0100

    clean file service

--- omega/server/file_service.js
+++ omega/server/file_service.js
@@ -30,13 +30,12 @@ export const createFileService = ({
     const { runtimeName, runtimeVersion } = parseUserAgentHeader(
       request.headers["user-agent"],
     )
+    context = {
+      ...context,
+      runtimeName,
+      runtimeVersion,
+    }
     try {
-      context = {
-        ...context,
-        runtimeName,
-        runtimeVersion,
-      }
-
       const url = await callHook("resolve", {
         baseUrl: projectDirectoryUrl,
         urlSpecifier: request.ressource.slice(1),
@@ -46,6 +45,7 @@ export const createFileService = ({
           status: 404,
         }
       }
+
       const loadResult = await callHook("load", {
         baseUrl: projectDirectoryUrl,
         url,
@@ -66,116 +66,8 @@ export const createFileService = ({
         contentType,
         content,
       })
-      context.references = referencesimport {
-        urlToRelativeUrl,
-        resolveUrl,
-        urlToParentUrl,
-        ensureWindowsDriveLetter,
-      } from "@jsenv/filesystem"
-      
-      import { memoizeAsyncFunctionByUrl } from "../memoizeAsyncFunction.js"
-      import { findAsync } from "../find_async.js"
-      import {
-        readPackageFile,
-        PACKAGE_NOT_FOUND,
-        PACKAGE_WITH_SYNTAX_ERROR,
-      } from "./readPackageFile.js"
-      import { applyPackageManualOverride } from "./applyPackageManualOverride.js"
-      
-      export const createFindNodeModulePackage = () => {
-        const readPackageFileMemoized = memoizeAsyncFunctionByUrl(
-          (packageFileUrl) => {
-            return readPackageFile(packageFileUrl)
-          },
-        )
-        return ({
-          projectDirectoryUrl,
-          nodeModulesOutsideProjectAllowed,
-          packagesManualOverrides = {},
-          packageFileUrl,
-          dependencyName,
-        }) => {
-          const nodeModuleCandidates = [
-            ...getNodeModuleCandidatesInsideProject({
-              projectDirectoryUrl,
-              packageFileUrl,
-            }),
-            ...(nodeModulesOutsideProjectAllowed
-              ? getNodeModuleCandidatesOutsideProject({
-                  projectDirectoryUrl,
-                })
-              : []),
-          ]
-          return findAsync({
-            array: nodeModuleCandidates,
-            start: async (nodeModuleCandidate) => {
-              const packageFileUrlCandidate = `${nodeModuleCandidate}${dependencyName}/package.json`
-              const packageObjectCandidate = await readPackageFileMemoized(
-                packageFileUrlCandidate,
-              )
-              return {
-                packageFileUrl: packageFileUrlCandidate,
-                packageJsonObject: applyPackageManualOverride(
-                  packageObjectCandidate,
-                  packagesManualOverrides,
-                ),
-                syntaxError: packageObjectCandidate === PACKAGE_WITH_SYNTAX_ERROR,
-              }
-            },
-            predicate: ({ packageJsonObject }) => {
-              return packageJsonObject !== PACKAGE_NOT_FOUND
-            },
-          })
-        }
-      }
-      
-      const getNodeModuleCandidatesInsideProject = ({
-        projectDirectoryUrl,
-        packageFileUrl,
-      }) => {
-        const packageDirectoryUrl = resolveUrl("./", packageFileUrl)
-        if (packageDirectoryUrl === projectDirectoryUrl) {
-          return [`${projectDirectoryUrl}node_modules/`]
-        }
-        const packageDirectoryRelativeUrl = urlToRelativeUrl(
-          packageDirectoryUrl,
-          projectDirectoryUrl,
-        )
-        const candidates = []
-        const relativeNodeModuleDirectoryArray =
-          packageDirectoryRelativeUrl.split("node_modules/")
-        // remove the first empty string
-        relativeNodeModuleDirectoryArray.shift()
-        let i = relativeNodeModuleDirectoryArray.length
-        while (i--) {
-          candidates.push(
-            `${projectDirectoryUrl}node_modules/${relativeNodeModuleDirectoryArray
-              .slice(0, i + 1)
-              .join("node_modules/")}node_modules/`,
-          )
-        }
-        return [...candidates, `${projectDirectoryUrl}node_modules/`]
-      }
-      
-      const getNodeModuleCandidatesOutsideProject = ({ projectDirectoryUrl }) => {
-        const candidates = []
-        const parentDirectoryUrl = urlToParentUrl(projectDirectoryUrl)
-        const { pathname } = new URL(parentDirectoryUrl)
-        const directories = pathname.slice(1, -1).split("/")
-        let i = directories.length
-        while (i--) {
-          const nodeModulesDirectoryUrl = ensureWindowsDriveLetter(
-            `file:///${directories.slice(0, i + 1).join("/")}/node_modules/`,
-            projectDirectoryUrl,
-          )
-          candidates.push(nodeModulesDirectoryUrl)
-        }
-        return [
-          ...candidates,
-          ensureWindowsDriveLetter(`file:///node_modules`, projectDirectoryUrl),
-        ]
-      }
-      
+      context.references = references
+
       const transformResult = await callHook("transform", {
         url,
         contentType,
