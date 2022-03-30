4a658d0784133ad2c873a9c7bec8b6c8164da52c
dmail <dmaillard06@gmail.com>
2022-03-30 14:03:00 +0200

    node coverage is good

---
+++ test/test_plan/coverage_node/coverage_node.test.js
@@ -0,0 +1,44 @@
+import { assert } from "@jsenv/assert"
+
+import { executeTestPlan, nodeProcess } from "@jsenv/core"
+
+const { testPlanCoverage } = await executeTestPlan({
+  logLevel: "warn",
+  rootDirectoryUrl: new URL("./", import.meta.url),
+  testPlan: {
+    "./main.js": {
+      node: {
+        runtime: nodeProcess,
+        runtimeParams: {
+          // headful: true,
+        },
+        collectConsole: false,
+      },
+    },
+  },
+  // keepRunning: true,
+  coverage: true,
+  coverageConfig: {
+    "./file.js": true,
+  },
+  coverageTextLog: false,
+  coverageHtmlDirectory: false,
+})
+const actual = testPlanCoverage
+const expected = {
+  "./file.js": {
+    ...actual["./file.js"],
+    path: "./file.js",
+    s: {
+      0: 1,
+      1: 1,
+      2: 0,
+      3: 1,
+      4: 1,
+      5: 1,
+      6: 0,
+      7: 0,
+    },
+  },
+}
+assert({ actual, expected })
