109befa88f6da866f2afe8465af67ec7df9c1488
dmail <dmaillard06@gmail.com>
2022-03-30 07:49:41 +0200

    notes on inline workers

--- src/omega/plugins/inline/core_plugin_inline_query_param.js
+++ src/omega/plugins/inline/core_plugin_inline_query_param.js
@@ -10,6 +10,15 @@ export const corePluginInlineQueryParam = () => {
       // so that the bundled file gets inlined and not the entry point
       "link_href": () => null,
       "script_src": () => null,
+      // if the referenced url is a worker we could use
+      // https://www.oreilly.com/library/view/web-workers/9781449322120/ch04.html
+      // but maybe we should rather use ?object_url
+      // or people could do this:
+      // import workerText from './worker.js?text'
+      // const blob = new Blob(workerText, { type: 'application/javascript' })
+      // window.URL.createObjectURL(blob)
+      // in any case the recommended way is to use an url
+      // to benefit from shared worker and reuse worker between tabs
       "*": (reference, { urlGraph, cook }) => {
         if (!new URL(reference.url).searchParams.has("inline")) {
           return null
