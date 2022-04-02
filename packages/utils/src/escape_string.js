5e5670921e1167ed818413874d9fae3b19d580c9
dmail <dmaillard06@gmail.com>
2022-04-02 16:14:40 +0200

    add escape string

--- packages/utils/src/escape_string.js
+++ packages/utils/src/escape_string.js
@@ -1,1 +1,81 @@
-export const escapeString = () => {}
+const DOUBLE_QUOTE = `"`
+const SINGLE_QUOTE = `'`
+const BACKTICK = "`"
+
+export const escapeString = (
+  value,
+  { quote = "auto", canUseTemplateString = true, fallback = DOUBLE_QUOTE } = {},
+) => {
+  quote =
+    quote === "auto"
+      ? determineQuote(value, canUseTemplateString) || fallback
+      : quote
+  if (quote === BACKTICK) {
+    return `\`${escapeTemplateString(value)}`
+  }
+  return surroundStringWith(value, quote)
+}
+
+// https://github.com/mgenware/string-to-template-literal/blob/main/src/main.ts#L1
+
+const escapeTemplateString = (string) => {
+  string = String(string)
+  let i = 0
+  let escapedString = ""
+  while (i < string.length) {
+    const char = string[i]
+    i++
+    escapedString += isTemplateStringSpecialChar(char) ? `\\${char}` : char
+  }
+  return `${BACKTICK}${escapedString}${BACKTICK}`
+}
+const isTemplateStringSpecialChar = (char) =>
+  templateStringSpecialChars.indexOf(char) > -1
+const templateStringSpecialChars = ["\\", "`", "$"]
+
+const determineQuote = (string, canUseTemplateString) => {
+  const containsDoubleQuote = string.includes(DOUBLE_QUOTE)
+  if (!containsDoubleQuote) {
+    return DOUBLE_QUOTE
+  }
+  const containsSimpleQuote = string.includes(SINGLE_QUOTE)
+  if (!containsSimpleQuote) {
+    return SINGLE_QUOTE
+  }
+  if (canUseTemplateString) {
+    const containsBackTick = string.includes(BACKTICK)
+    if (!containsBackTick) {
+      return BACKTICK
+    }
+  }
+  return null
+}
+
+// https://github.com/jsenv/jsenv-uneval/blob/6c97ef9d8f2e9425a66f2c88347e0a118d427f3a/src/internal/escapeString.js#L3
+// https://github.com/jsenv/jsenv-inspect/blob/bb11de3adf262b68f71ed82b0a37d4528dd42229/src/internal/string.js#L3
+// https://github.com/joliss/js-string-escape/blob/master/index.js
+// http://javascript.crockford.com/remedial.html
+const surroundStringWith = (string, quote) => {
+  let escapedString = ""
+  let i = 0
+  while (i < string.length) {
+    const char = string[i]
+    i++
+    let escapedChar
+    if (char === quote) {
+      escapedChar = `\\${quote}`
+    } else if (char === "\n") {
+      escapedChar = "\\n"
+    } else if (char === "\r") {
+      escapedChar = "\\r"
+    } else if (char === "\u2028") {
+      escapedChar = "\\u2028"
+    } else if (char === "\u2029") {
+      escapedChar = "\\u2029"
+    } else {
+      escapedChar = char
+    }
+    escapedString += escapedChar
+  }
+  return `${quote}${escapedString}${quote}`
+}
