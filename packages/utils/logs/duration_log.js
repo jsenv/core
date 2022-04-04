2b95b9ccd44fbf2c324974bab3a78704402a47b1
dmail <dmaillard06@gmail.com>
2022-04-04 16:15:05 +0200

    update duration log

--- packages/utils/logs/duration_log.js
+++ packages/utils/logs/duration_log.js
@@ -1,12 +1,33 @@
 import { require } from "../require.js"
 
+// https://github.com/EvanHahn/HumanizeDuration.js
 const humanizeDuration = require("humanize-duration")
 
 export const msAsDuration = (ms) => {
+  // below 100 ms it would display "0 seconds", so use milliseconds
+  if (ms < 100) {
+    return humanizeDuration(ms, {
+      maxDecimalPoints: 0,
+      units: ["ms"],
+    })
+  }
+  // below 1s we want "0.01 seconds" and "0.3 seconds"
+  if (ms < 1000) {
+    return humanizeDuration(ms, {
+      maxDecimalPoints: 2,
+      units: ["s"],
+    })
+  }
+  // below 60s we just display the exact amount of seconds
+  if (ms < 60000) {
+    return humanizeDuration(ms, {
+      largest: 1,
+      maxDecimalPoints: 0,
+    })
+  }
   return humanizeDuration(ms, {
     largest: 2,
-    maxDecimalPoints: ms < 0.1 ? 3 : ms < 1000 ? 2 : ms < 60000 ? 1 : 0,
+    maxDecimalPoints: 0,
     delimiter: " and ",
-    // units: ["s"]
   })
 }
