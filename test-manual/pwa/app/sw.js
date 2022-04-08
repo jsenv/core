30a1e8fc5045de193184778c87052342e49d8d70
dmail <dmaillard06@gmail.com>
2022-04-08 10:01:09 +0200

    rerun prettier + update eslint and prettier ignore

--- test-manual/pwa/app/sw.js
+++ test-manual/pwa/app/sw.js
@@ -1,8 +1,12 @@
 /* globals self */
 
-self.importScripts("../../../node_modules/@jsenv/pwa/src/service-worker.setup.js")
+self.importScripts(
+  "../../../node_modules/@jsenv/pwa/src/service-worker.setup.js",
+)
 
 self.config.cachePrefix = "toto"
 self.config.logLevel = "debug"
 
-self.importScripts("../../../node_modules/@jsenv/pwa/src/service-worker.main.js")
+self.importScripts(
+  "../../../node_modules/@jsenv/pwa/src/service-worker.main.js",
+)
