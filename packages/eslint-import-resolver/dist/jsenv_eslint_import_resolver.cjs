57defe4b96991ee3e25d56d483f94190f47acd75
dmail <dmaillard06@gmail.com>
2022-04-11 08:37:47 +0200

    publish dist

--- packages/eslint-import-resolver/dist/jsenv_eslint_import_resolver.cjs
+++ packages/eslint-import-resolver/dist/jsenv_eslint_import_resolver.cjs
@@ -2251,7 +2251,7 @@ ${urlToFileSystemPath(rootDirectoryUrl)}`);
       !nodeInPackageConditions &&
       specifier[0] === "/"
     ) {
-      return onUrl(new URL(specifier, rootDirectoryUrl).href)
+      return onUrl(new URL(specifier.slice(1), rootDirectoryUrl).href)
     }
 
     // data:*, http://*, https://*, file://*
