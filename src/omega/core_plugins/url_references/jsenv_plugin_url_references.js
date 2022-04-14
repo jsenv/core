f5868b5afc657c50d67dc41ff3c6ee05e477032f
dmail <dmaillard06@gmail.com>
2022-04-14 14:19:58 +0200

    use as_js_classic

--- src/omega/core_plugins/url_references/jsenv_plugin_url_references.js
+++ src/omega/core_plugins/url_references/jsenv_plugin_url_references.js
@@ -2,16 +2,20 @@ import { parseAndTransformHtmlUrls } from "./html_urls.js"
 import { parseAndTransformCssUrls } from "./css_urls.js"
 import { parseAndTransformJsClassicUrls } from "./js_classic_urls.js"
 import { parseAndTransformJsModuleUrls } from "./js_module_urls.js"
+import { jsenvPluginJsModuleFallback } from "./jsenv_plugin_js_module_fallback.js"
 
 export const jsenvPluginUrlReferences = () => {
-  return {
-    name: "jsenv:url_references",
-    appliesDuring: "*",
-    transform: {
-      html: parseAndTransformHtmlUrls,
-      css: parseAndTransformCssUrls,
-      js_classic: parseAndTransformJsClassicUrls,
-      js_module: parseAndTransformJsModuleUrls,
+  return [
+    {
+      name: "jsenv:url_references",
+      appliesDuring: "*",
+      transform: {
+        html: parseAndTransformHtmlUrls,
+        css: parseAndTransformCssUrls,
+        js_classic: parseAndTransformJsClassicUrls,
+        js_module: parseAndTransformJsModuleUrls,
+      },
     },
-  }
+    jsenvPluginJsModuleFallback(),
+  ]
 }
