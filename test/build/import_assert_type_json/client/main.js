c2d9d499bf5f147e315b3f3576530d8d256f6d56
dmail <dmaillard06@gmail.com>
2022-04-20 18:23:57 +0200

    test json import assertion

---
+++ test/build/import_assert_type_json/client/main.js
@@ -0,0 +1,5 @@
+import data from "./data.json" assert { type: "json" }
+
+window.resolveNamespacePromise({
+  data,
+})
---
+++ test/build/import_assert_type_json/import_assert_type_json.test.mjs
@@ -0,0 +1,52 @@
+import { assert } from "@jsenv/assert"
+
+import { build } from "@jsenv/core"
+import { executeInChromium } from "@jsenv/core/test/execute_in_chromium.js"
+
+const test = async (options) => {
+  await build({
+    logLevel: "warn",
+    rootDirectoryUrl: new URL("./client/", import.meta.url),
+    buildDirectoryUrl: new URL("./dist/", import.meta.url),
+    entryPoints: {
+      "./main.html": "main.html",
+    },
+    ...options,
+  })
+  const { returnValue } = await executeInChromium({
+    rootDirectoryUrl: new URL("./dist/", import.meta.url),
+    htmlFileRelativeUrl: "./main.html",
+    /* eslint-disable no-undef */
+    pageFunction: async () => {
+      return window.namespacePromise
+    },
+    /* eslint-enable no-undef */
+  })
+  return { returnValue }
+}
+
+// with bundling (default)
+{
+  const { returnValue } = await test()
+  const actual = returnValue
+  const expected = {
+    data: {
+      answer: 42,
+    },
+  }
+  assert({ actual, expected })
+}
+
+// without bundling
+{
+  const { returnValue } = await test({
+    bundling: false,
+  })
+  const actual = returnValue
+  const expected = {
+    data: {
+      answer: 42,
+    },
+  }
+  assert({ actual, expected })
+}
