f183b550a8f2e82e0fea16ecff400d6e185646ff
dmail <dmaillard06@gmail.com>
2022-04-21 10:01:51 +0200

    update json minification

--- src/build/plugins/minify_json/jsenv_plugin_minify_json.js
+++ src/build/plugins/minify_json/jsenv_plugin_minify_json.js
@@ -1,24 +1,24 @@
 // https://github.com/terser-js/terser#minify-options
 
 export const jsenvPluginMinifyJson = () => {
+  const minifyJson = (urlInfo) => {
+    const { content } = urlInfo
+    if (content.startsWith("{\n")) {
+      const jsonWithoutWhitespaces = JSON.stringify(JSON.parse(content))
+      return jsonWithoutWhitespaces
+    }
+    return null
+  }
+
   return {
     name: "jsenv:minify_json",
     appliesDuring: {
       build: true,
     },
     optimize: {
-      json: (urlInfo) => {
-        const { content } = urlInfo
-        if (
-          content.includes(" ") ||
-          content.includes("\n") ||
-          content.includes("\t")
-        ) {
-          const jsonWithoutWhitespaces = JSON.stringify(JSON.parse(content))
-          return jsonWithoutWhitespaces
-        }
-        return null
-      },
+      importmap: minifyJson,
+      json: minifyJson,
+      webmanifest: minifyJson,
     },
   }
 }
