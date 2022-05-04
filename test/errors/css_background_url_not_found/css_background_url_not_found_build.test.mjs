52a550e2584edb7a646a5867b50b6cf3953b0002
dmail <dmaillard06@gmail.com>
2022-05-04 15:23:15 +0200

    finish test about file not foun

---
+++ test/errors/css_background_url_not_found/css_background_url_not_found_build.test.mjs
@@ -0,0 +1,33 @@
+import { urlToFileSystemPath } from "@jsenv/filesystem"
+import { assert } from "@jsenv/assert"
+
+import { build } from "@jsenv/core"
+
+try {
+  await build({
+    logLevel: "warn",
+    rootDirectoryUrl: new URL("./client/", import.meta.url),
+    buildDirectoryUrl: new URL("./dist/", import.meta.url),
+    entryPoints: {
+      "./main.html": "main.html",
+    },
+    minification: false,
+  })
+  throw new Error("should throw")
+} catch (e) {
+  const actual = e.message
+  const expected = `Failed to fetch url content
+--- reason ---
+no entry on filesystem
+--- url ---
+${new URL("./client/img.png", import.meta.url).href}
+--- url reference trace ---
+${urlToFileSystemPath(new URL("./client/style.css", import.meta.url).href)}:2:25
+  1 | body {
+> 2 |   background-image: url("./img.png");
+                              ^
+  3 | }
+--- plugin name ---
+"jsenv:fetch_file_urls"`
+  assert({ actual, expected })
+}
---
+++ test/errors/js_import_meta_url_not_found/js_import_meta_url_not_found_build.test.mjs
@@ -0,0 +1,32 @@
+import { urlToFileSystemPath } from "@jsenv/filesystem"
+import { assert } from "@jsenv/assert"
+
+import { build } from "@jsenv/core"
+
+try {
+  await build({
+    logLevel: "warn",
+    rootDirectoryUrl: new URL("./client/", import.meta.url),
+    buildDirectoryUrl: new URL("./dist/", import.meta.url),
+    entryPoints: {
+      "./main.html": "main.html",
+    },
+    minification: false,
+  })
+  throw new Error("should throw")
+} catch (e) {
+  const actual = e.message
+  const expected = `Failed to fetch url content
+--- reason ---
+no entry on filesystem
+--- url ---
+${new URL("./client/style.css", import.meta.url).href}
+--- url reference trace ---
+${urlToFileSystemPath(new URL("./client/main.js", import.meta.url).href)}:1:23
+> 1 | const cssUrl = new URL("./style.css", import.meta.url)
+                            ^
+  2 |${" "}
+--- plugin name ---
+"jsenv:fetch_file_urls"`
+  assert({ actual, expected })
+}
