23c5a97c19dc42b6af2c07e747f70c70259eb4e9
dmail <dmaillard06@gmail.com>
2022-05-04 15:16:07 +0200

    write script module not found test

---
+++ test/errors/script_module_not_found/script_module_not_found_build.test.mjs
@@ -0,0 +1,35 @@
+import { assert } from "@jsenv/assert"
+
+import { build } from "@jsenv/core"
+import { urlToFileSystemPath } from "@jsenv/filesystem"
+
+try {
+  await build({
+    logLevel: "warn",
+    rootDirectoryUrl: new URL("./client/", import.meta.url),
+    buildDirectoryUrl: new URL("./dist/", import.meta.url),
+    entryPoints: {
+      "./main.html": "main.html",
+    },
+    minification: false,
+  })
+  throw new Error("should throw")
+} catch (e) {
+  const actual = e.message
+  const expected = `Failed to fetch url content
+--- reason ---
+no entry on filesystem
+--- url ---
+${new URL("./client/404.js", import.meta.url).href}
+--- url reference trace ---
+${urlToFileSystemPath(
+  new URL("./client/main.html", import.meta.url).href,
+)}:10:27
+  9  |   <body>
+> 10 |     <script type="module" src="./404.js"></script>
+                                 ^
+  11 |   </body>
+--- plugin name ---
+"jsenv:fetch_file_urls"`
+  assert({ actual, expected })
+}
