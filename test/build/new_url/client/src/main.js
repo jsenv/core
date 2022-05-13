fdcf816f333b0b4c8a25d183eb0c129f8aa46e08
dmail <dmaillard06@gmail.com>
2022-05-13 17:24:32 +0200

    update new url test

--- test/build/new_url/client/src/main.js
+++ test/build/new_url/client/src/main.js
@@ -7,7 +7,7 @@ const absoluteUrl = new URL("http://example.com/file.txt", "https://jsenv.dev")
   .href
 console.log(absoluteUrl)
 
-const windowOriginRelativeUrl = new URL("./file.txt", window.origin).href
+const windowOriginRelativeUrl = new URL("/src/file.txt", window.origin).href
 
 const absoluteBaseUrl = new URL("./file.txt", "http://jsenv.dev").href
 
