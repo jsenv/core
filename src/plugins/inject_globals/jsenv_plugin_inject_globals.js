1ccc2fcd47a8e6f9eb9ea26bc99da7b7a6b304be
dmail <dmaillard06@gmail.com>
2022-05-17 11:14:03 +0200

    make jsenv globals smarted (inject globals only on entry points)

--- src/plugins/inject_globals/jsenv_plugin_inject_globals.js
+++ src/plugins/inject_globals/jsenv_plugin_inject_globals.js
@@ -1,3 +1,9 @@
+import {
+  parseHtmlString,
+  injectScriptAsEarlyAsPossible,
+  createHtmlNode,
+  stringifyHtmlAst,
+} from "@jsenv/utils/html_ast/html_ast.js"
 import { createMagicSource } from "@jsenv/utils/sourcemap/magic_source.js"
 import { isWebWorkerUrlInfo } from "@jsenv/core/src/omega/web_workers.js"
 
@@ -5,11 +11,41 @@ export const jsenvPluginInjectGlobals = (globals = {}) => {
   if (Object.keys(globals).length === 0) {
     return []
   }
-  const injectGlobals = (urlInfo) => {
+
+  const globalInjectorOnHtmlEntryPoint = (urlInfo) => {
+    if (!urlInfo.data.isEntryPoint) {
+      return null
+    }
+    // ideally we would inject an importmap but browser support is too low
+    // (even worse for worker/service worker)
+    // so for now we inject code into entry points
+    const htmlAst = parseHtmlString(urlInfo.content, {
+      storeOriginalPositions: false,
+    })
+    injectScriptAsEarlyAsPossible(
+      htmlAst,
+      createHtmlNode({
+        "tagName": "script",
+        "textContent": generateClientCodeForGlobals({
+          globals,
+          isWebWorker: false,
+        }),
+        "injected-by": "jsenv:inject_globals",
+      }),
+    )
+    return stringifyHtmlAst(htmlAst)
+  }
+
+  const globalsInjectorOnJsEntryPoints = (urlInfo) => {
+    if (!urlInfo.data.isEntryPoint && !urlInfo.data.isWebWorkerEntryPoint) {
+      return null
+    }
     const magicSource = createMagicSource(urlInfo.content)
-    const globalName = isWebWorkerUrlInfo(urlInfo) ? "self" : "window"
-    magicSource.prepend(
-      `Object.assign(${globalName}, ${JSON.stringify(globals, null, "  ")});`,
+    magicSource.append(
+      generateClientCodeForGlobals({
+        globals,
+        isWebWorker: isWebWorkerUrlInfo(urlInfo),
+      }),
     )
     return magicSource.toContentAndSourcemap()
   }
@@ -18,8 +54,14 @@ export const jsenvPluginInjectGlobals = (globals = {}) => {
     name: "jsenv:inject_globals",
     appliesDuring: "*",
     transformUrlContent: {
-      js_classic: injectGlobals,
-      js_module: injectGlobals,
+      html: globalInjectorOnHtmlEntryPoint,
+      js_classic: globalsInjectorOnJsEntryPoints,
+      js_module: globalsInjectorOnJsEntryPoints,
     },
   }
+}
+
+const generateClientCodeForGlobals = ({ isWebWorker = false, globals }) => {
+  const globalName = isWebWorker ? "self" : "window"
+  return `Object.assign(${globalName}, ${JSON.stringify(globals, null, "  ")});`
 }
