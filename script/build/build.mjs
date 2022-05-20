462b496e8f953c8be24538296e70d5360ec0393c
dmail <dmaillard06@gmail.com>
2022-05-20 09:29:53 +0200

    update dist files location

--- script/build/build.mjs
+++ script/build/build.mjs
@@ -1,17 +1,20 @@
-// check jsenv_file_selector.js too
+import { ensureEmptyDirectory } from "@jsenv/filesystem"
 
 import { build } from "@jsenv/core"
 
 const jsenvRootDirectoryUrl = new URL("../../", import.meta.url)
 const jsenvDistDirectoryUrl = new URL("./dist/", jsenvRootDirectoryUrl)
 
+await ensureEmptyDirectory(jsenvDistDirectoryUrl)
+
 await build({
   rootDirectoryUrl: jsenvRootDirectoryUrl,
-  buildDirectoryUrl: new URL("./event_source_client/", jsenvDistDirectoryUrl),
+  buildDirectoryUrl: jsenvDistDirectoryUrl,
   entryPoints: {
     "./src/plugins/autoreload/dev_sse/client/event_source_client.js":
       "event_source_client.js",
   },
+  buildDirectoryClean: false,
   sourcemaps: "file",
   minification: false,
   versioning: false,
@@ -20,11 +23,12 @@ await build({
 
 await build({
   rootDirectoryUrl: jsenvRootDirectoryUrl,
-  buildDirectoryUrl: new URL("./html_supervisor_setup/", jsenvDistDirectoryUrl),
+  buildDirectoryUrl: jsenvDistDirectoryUrl,
   entryPoints: {
     "./src/plugins/html_supervisor/client/html_supervisor_setup.js":
       "html_supervisor_setup.js",
   },
+  buildDirectoryClean: false,
   sourcemaps: "file",
   minification: false,
   versioning: false,
@@ -33,14 +37,12 @@ await build({
 
 await build({
   rootDirectoryUrl: jsenvRootDirectoryUrl,
-  buildDirectoryUrl: new URL(
-    "./html_supervisor_installer/",
-    jsenvDistDirectoryUrl,
-  ),
+  buildDirectoryUrl: jsenvDistDirectoryUrl,
   entryPoints: {
     "./src/plugins/html_supervisor/client/html_supervisor_installer.js":
       "html_supervisor_installer.js",
   },
+  buildDirectoryClean: false,
   sourcemaps: "file",
   minification: false,
   versioning: false,
