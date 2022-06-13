e6df66faa0687acb51c4f207eaeb5d309142e990
dmail <dmaillard06@gmail.com>
2022-06-13 08:22:20 +0200

    test sort by dependencies

---
+++ packages/utils/graph/sort_by_dependencies.test.mjs
@@ -0,0 +1,38 @@
+import { assert } from "@jsenv/assert"
+
+import { sortByDependencies } from "./sort_by_dependencies.js"
+
+// when both have dependencies, but one depends an an other, the other must come first
+{
+  const actual = sortByDependencies({
+    "a.js": {
+      dependencies: ["b.js"],
+    },
+    "b.js": {
+      dependencies: ["img.png"],
+    },
+    "img.png": {
+      dependencies: [],
+    },
+  })
+  const expected = ["img.png", "b.js", "a.js"]
+  expected.circular = []
+  assert({ actual, expected })
+}
+
+{
+  const actual = sortByDependencies({
+    "b.js": {
+      dependencies: ["img.png"],
+    },
+    "a.js": {
+      dependencies: ["b.js"],
+    },
+    "img.png": {
+      dependencies: [],
+    },
+  })
+  const expected = ["img.png", "b.js", "a.js"]
+  expected.circular = []
+  assert({ actual, expected })
+}
