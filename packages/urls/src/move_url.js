d2849b342ef69fb914577f74ad9b3bad890f4ea4
dmail <dmaillard06@gmail.com>
2022-06-16 15:26:41 +0200

    fix move url

--- packages/urls/src/move_url.js
+++ packages/urls/src/move_url.js
@@ -1,7 +1,11 @@
 import { urlToRelativeUrl } from "./url_to_relative_url.js"
 
 export const moveUrl = ({ url, from, to, preferAbsolute = false }) => {
-  const relativeUrl = urlToRelativeUrl(url, from)
+  let relativeUrl = urlToRelativeUrl(url, from)
+  if (relativeUrl.slice(0, 2) === "//") {
+    // restore the protocol
+    relativeUrl = new URL(relativeUrl, url).href
+  }
   const absoluteUrl = new URL(relativeUrl, to).href
   if (preferAbsolute) {
     return absoluteUrl
