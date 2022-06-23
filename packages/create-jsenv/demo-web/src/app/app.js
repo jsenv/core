e8599db413528200e1bb496a352b16302829045b
dmail <dmaillard06@gmail.com>
2022-06-23 09:59:02 +0200

    rerun prettier

--- packages/create-jsenv/demo-web/src/app/app.js
+++ packages/create-jsenv/demo-web/src/app/app.js
@@ -1,8 +1,8 @@
-import appStyleSheet from "./app.css" assert { type: "css" };
+import appStyleSheet from "./app.css" assert { type: "css" }
 
-document.adoptedStyleSheets = [...document.adoptedStyleSheets, appStyleSheet];
+document.adoptedStyleSheets = [...document.adoptedStyleSheets, appStyleSheet]
 
-const jsenvLogoUrl = new URL("/src/jsenv_logo.svg", import.meta.url);
+const jsenvLogoUrl = new URL("/src/jsenv_logo.svg", import.meta.url)
 
 document.querySelector("#app").innerHTML = `
   <h1>Hello world!</h1>
@@ -11,14 +11,14 @@ document.querySelector("#app").innerHTML = `
     Edit <code>jsenv_logo.svg</code> and save to test HMR updates.
   </p>
   <a href="https://github.com/jsenv/jsenv-core" target="_blank">Documentation</a>
-`;
+`
 
 if (import.meta.hot) {
-  import.meta.hot.accept();
+  import.meta.hot.accept()
   import.meta.hot.dispose(() => {
-    document.querySelector("#app").innerHTML = "";
+    document.querySelector("#app").innerHTML = ""
     document.adoptedStyleSheets = document.adoptedStyleSheets.filter(
       (s) => s !== appStyleSheet,
-    );
-  });
+    )
+  })
 }
