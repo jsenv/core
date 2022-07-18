6bedc574ea8b5c606e509f5f5d11ace7a1132c32
dmail <dmaillard06@gmail.com>
2022-07-18 10:47:45 +0200

    write test to ensure unrelated pages are not affected by server errors

---
+++ tests/dev_server/server_error_scope/server_error_scope.test.mjs
@@ -0,0 +1,75 @@
+/*
+ * Ensure server errors are dispatched to clients only if this page is responsible
+ * for the error; unrelated pages must not display an error.
+ */
+
+import { chromium } from "playwright"
+import { assert } from "@jsenv/assert"
+
+import { startDevServer } from "@jsenv/core"
+
+const devServer = await startDevServer({
+  logLevel: "off",
+  omegaServerLogLevel: "off",
+  rootDirectoryUrl: new URL("./client/", import.meta.url),
+  plugins: [
+    {
+      name: "plugin_throwing",
+      appliesDuring: "*",
+      transformUrlContent: ({ url }) => {
+        if (url.includes("error.js")) {
+          throw new Error("error_during_transform")
+        }
+      },
+    },
+  ],
+})
+
+const browser = await chromium.launch({ headless: true })
+const pageUnrelated = await browser.newPage({ ignoreHTTPSErrors: true })
+await pageUnrelated.goto(`${devServer.origin}/unrelated.html`)
+await pageUnrelated.evaluate(
+  /* eslint-disable no-undef */
+  () => {
+    return window.resultPromise
+  },
+  /* eslint-enable no-undef */
+)
+
+const pageGeneratingError = await browser.newPage({ ignoreHTTPSErrors: true })
+await pageGeneratingError.goto(`${devServer.origin}/error.html`)
+await pageGeneratingError.waitForSelector("jsenv-error-overlay")
+await pageGeneratingError.mouse.click(0, 0) // close jsenv-error-overlay
+{
+  const actual = await pageGeneratingError.evaluate(
+    /* eslint-disable no-undef */
+    () => {
+      return document.querySelector("jsenv-error-overlay")
+    },
+    /* eslint-enable no-undef */
+  )
+  const expected = null
+  assert({ actual, expected })
+}
+
+// regen the server error on an other page
+const pageRelated = await browser.newPage({ ignoreHTTPSErrors: true })
+await pageRelated.goto(`${devServer.origin}/related.html`)
+
+// ensure:
+// - jsenv error overlay is displayed on this page
+// - it is also displayed on the other page using the same ressource (despites being closed)
+// - it is not displayed on page not depending on this file
+await pageGeneratingError.waitForSelector("jsenv-error-overlay")
+await pageRelated.waitForSelector("jsenv-error-overlay")
+{
+  const actual = await pageUnrelated.evaluate(
+    /* eslint-disable no-undef */
+    () => {
+      return document.querySelector("jsenv-error-overlay")
+    },
+    /* eslint-enable no-undef */
+  )
+  const expected = null
+  assert({ actual, expected })
+}
