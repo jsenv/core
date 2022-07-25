303f56fa31382f1aaf6178b545172ea5e769422f
dmail <dmaillard06@gmail.com>
2022-07-25 16:53:11 +0200

    give more time for snapshot tests

--- tests/dev_server/errors/dev_errors_snapshots.test.mjs
+++ tests/dev_server/errors/dev_errors_snapshots.test.mjs
@@ -16,7 +16,9 @@ const readHtmlFiles = () => {
 }
 
 // disable on windows because it would fails due to line endings (CRLF)
+// if (process.platform !== "win32") {
 const expected = readHtmlFiles()
 await import("./generate_snapshot_files.mjs")
 const actual = readHtmlFiles()
 assert({ actual, expected })
+// }
