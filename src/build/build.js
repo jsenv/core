8390ae2f54685195585b81f797a7fbae27541745
dmail <dmaillard06@gmail.com>
2022-07-28 10:01:29 +0200

    runtime compat during dev = runtime compat during build

--- src/build/build.js
+++ src/build/build.js
@@ -47,6 +47,20 @@ import { createVersionGenerator } from "./version_generator.js"
 import { injectServiceWorkerUrls } from "./inject_service_worker_urls.js"
 import { resyncRessourceHints } from "./resync_ressource_hints.js"
 
+// default runtimeCompat corresponds to
+// "we can keep <script type="module"> intact":
+// so script_type_module + dynamic_import + import_meta
+export const defaultRuntimeCompat = {
+  // android: "8",
+  chrome: "64",
+  edge: "79",
+  firefox: "67",
+  ios: "12",
+  opera: "51",
+  safari: "11.3",
+  samsung: "9.2",
+}
+
 /**
  * Generate an optimized version of source files into a directory
  * @param {Object} buildParameters
@@ -85,19 +99,7 @@ export const build = async ({
   entryPoints = {},
   baseUrl = "/",
 
-  // default runtimeCompat corresponds to
-  // "we can keep <script type="module"> intact":
-  // so script_type_module + dynamic_import + import_meta
-  runtimeCompat = {
-    // android: "8",
-    chrome: "64",
-    edge: "79",
-    firefox: "67",
-    ios: "12",
-    opera: "51",
-    safari: "11.3",
-    samsung: "9.2",
-  },
+  runtimeCompat = defaultRuntimeCompat,
   plugins = [],
   sourcemaps = false,
   sourcemapsSourcesContent,
