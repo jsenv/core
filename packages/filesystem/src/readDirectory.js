c258a347e266d479310aac0ec91145e8b28a0c5b
dmail <dmaillard06@gmail.com>
2022-08-01 16:57:45 +0200

    fix collect files when file names contains special chars

--- packages/filesystem/src/readDirectory.js
+++ packages/filesystem/src/readDirectory.js
@@ -1,54 +1,39 @@
 import { readdir } from "node:fs"
-import { urlToFileSystemPath } from "@jsenv/urls"
 import { assertAndNormalizeDirectoryUrl } from "./assertAndNormalizeDirectoryUrl.js"
 
 export const readDirectory = async (url, { emfileMaxWait = 1000 } = {}) => {
   const directoryUrl = assertAndNormalizeDirectoryUrl(url)
-  const directoryPath = urlToFileSystemPath(directoryUrl)
+  const directoryUrlObject = new URL(directoryUrl)
   const startMs = Date.now()
   let attemptCount = 0
 
-  const attempt = () => {
-    return readdirNaive(directoryPath, {
-      handleTooManyFilesOpenedError: async (error) => {
+  const attempt = async () => {
+    try {
+      const names = await new Promise((resolve, reject) => {
+        readdir(directoryUrlObject, (error, names) => {
+          if (error) {
+            reject(error)
+          } else {
+            resolve(names)
+          }
+        })
+      })
+      return names.map(encodeURIComponent)
+    } catch (e) {
+      // https://nodejs.org/dist/latest-v13.x/docs/api/errors.html#errors_common_system_errors
+      if (e.code === "EMFILE" || e.code === "ENFILE") {
         attemptCount++
         const nowMs = Date.now()
         const timeSpentWaiting = nowMs - startMs
         if (timeSpentWaiting > emfileMaxWait) {
-          throw error
+          throw e
         }
-
-        return new Promise((resolve) => {
-          setTimeout(() => {
-            resolve(attempt())
-          }, attemptCount)
-        })
-      },
-    })
+        await new Promise((resolve) => setTimeout(resolve), attemptCount)
+        return await attempt()
+      }
+      throw e
+    }
   }
 
   return attempt()
-}
-
-const readdirNaive = (
-  directoryPath,
-  { handleTooManyFilesOpenedError = null } = {},
-) => {
-  return new Promise((resolve, reject) => {
-    readdir(directoryPath, (error, names) => {
-      if (error) {
-        // https://nodejs.org/dist/latest-v13.x/docs/api/errors.html#errors_common_system_errors
-        if (
-          handleTooManyFilesOpenedError &&
-          (error.code === "EMFILE" || error.code === "ENFILE")
-        ) {
-          resolve(handleTooManyFilesOpenedError(error))
-        } else {
-          reject(error)
-        }
-      } else {
-        resolve(names)
-      }
-    })
-  })
 }
