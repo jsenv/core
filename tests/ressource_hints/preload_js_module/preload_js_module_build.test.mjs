54c90f09f5af1e47365e3f1c5bb4f031fd311b0d
dmail <dmaillard06@gmail.com>
2022-08-29 10:30:08 +0200

    first build passing when script module supported

--- tests/ressource_hints/preload_js_module/preload_js_module_build.test.mjs
+++ tests/ressource_hints/preload_js_module/preload_js_module_build.test.mjs
@@ -41,8 +41,8 @@ const test = async (params) => {
 }
 
 // support for <script type="module">
-await test({ runtimeCompat: { chrome: "64" } })
+// await test({ runtimeCompat: { chrome: "64" } })
 // no support for <script type="module">
-// await test({ runtimeCompat: { chrome: "60" } })
+await test({ runtimeCompat: { chrome: "60" } })
 // no support + no versioning
 // await test({ runtimeCompat: { chrome: "60" }, versioning: false })
