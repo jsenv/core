e533fe47f8de7192f3b1dfff0b2444dadcb2f0d4
dmail <dmaillard06@gmail.com>
2022-09-13 15:14:48 +0200

    fix node module not cached anymore

--- src/plugins/url_version/jsenv_plugin_url_version.js
+++ src/plugins/url_version/jsenv_plugin_url_version.js
@@ -9,9 +9,14 @@ export const jsenvPluginUrlVersion = () => {
       // this goal is achieved when we reach this part of the code
       // We get rid of this params so that urlGraph and other parts of the code
       // recognize the url (it is not considered as a different url)
-      const urlObject = new URL(reference.url)
-      urlObject.searchParams.delete("v")
-      return urlObject.href
+      const version = reference.searchParams.get("v")
+      if (version) {
+        const urlObject = new URL(reference.url)
+        urlObject.searchParams.delete("v")
+        reference.version = version
+        return urlObject.href
+      }
+      return null
     },
     transformUrlSearchParams: (reference) => {
       if (!reference.version) {
--- tests/dev_server/cache_node_package/cache_node_package.manual_test.mjs
+++ tests/dev_server/cache_node_package/cache_node_package.manual_test.mjs
@@ -3,5 +3,6 @@ import { startDevServer } from "@jsenv/core"
 await startDevServer({
   logLevel: "info",
   rootDirectoryUrl: new URL("./client/", import.meta.url),
-  port: 5432,
+  port: 5433,
+  clientMainFileUrl: new URL("./client/main.html", import.meta.url),
 })
