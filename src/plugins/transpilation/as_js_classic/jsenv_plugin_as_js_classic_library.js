abc9da3855e49c15d342be08e1c9aacb135e9698
dmail <dmaillard06@gmail.com>
2022-10-18 15:00:50 +0200

    fix error when building multiple as js classic library scripts

--- src/plugins/transpilation/as_js_classic/jsenv_plugin_as_js_classic_library.js
+++ src/plugins/transpilation/as_js_classic/jsenv_plugin_as_js_classic_library.js
@@ -68,7 +68,7 @@ export const jsenvPluginAsJsClassicLibrary = ({
       } else if (context.scenarios.build) {
         jsModuleBundledUrlInfo.sourceUrls.forEach((sourceUrl) => {
           const sourceUrlInfo = context.urlGraph.getUrlInfo(sourceUrl)
-          if (sourceUrlInfo.dependents.size === 0) {
+          if (sourceUrlInfo && sourceUrlInfo.dependents.size === 0) {
             context.urlGraph.deleteUrlInfo(sourceUrl)
           }
         })
