592c13389f94c87e7fc031b583e65d9c49494dac
dmail <dmaillard06@gmail.com>
2022-11-15 11:24:59 +0100

    write a test for import meta resolve during dev

---
+++ tests/dev_and_build/import_meta_resolve/import_meta_resolve_dev.test.mjs
@@ -0,0 +1,38 @@
+import { chromium, firefox } from "playwright"
+import { assert } from "@jsenv/assert"
+
+import { startDevServer } from "@jsenv/core"
+import { launchBrowserPage } from "@jsenv/core/tests/launch_browser_page.js"
+
+const devServer = await startDevServer({
+  logLevel: "warn",
+  rootDirectoryUrl: new URL("./client/", import.meta.url),
+  keepProcessAlive: false,
+})
+
+const test = async ({ browserLauncher }) => {
+  const browser = await browserLauncher.launch({ headless: true })
+  try {
+    const page = await launchBrowserPage(browser)
+    await page.goto(`${devServer.origin}/main.html`)
+
+    const result = await page.evaluate(
+      /* eslint-disable no-undef */
+      () => {
+        return window.resultPromise
+      },
+      /* eslint-enable no-undef */
+    )
+    const actual = result
+    const expected = {
+      importMetaResolveReturnValue: `${devServer.origin}/node_modules/foo/foo.js?v=0.0.1`,
+      __TEST__: `${devServer.origin}/node_modules/foo/foo.js?v=0.0.1`,
+    }
+    assert({ actual, expected })
+  } finally {
+    browser.close()
+  }
+}
+
+// await test({ browserLauncher: chromium })
+await test({ browserLauncher: firefox })
