817c6793b7c1b111afbd4fb2a70e9479b6dcaeb7
dmail <dmaillard06@gmail.com>
2022-11-16 09:52:08 +0100

    add a test about node esm + query param

---
+++ packages/node-esm-resolution/tests/query_param/query_param.test.mjs
@@ -0,0 +1,17 @@
+import { assert } from "@jsenv/assert"
+
+import { applyNodeEsmResolution } from "@jsenv/node-esm-resolution"
+
+const { type, url } = applyNodeEsmResolution({
+  parentUrl: new URL("./root/src/index.mjs", import.meta.url),
+  specifier: "foo.js?test",
+})
+const actual = {
+  type,
+  url,
+}
+const expected = {
+  type: "field:main",
+  url: new URL("./root/node_modules/foo.js/entry.js", import.meta.url).href,
+}
+assert({ actual, expected })
