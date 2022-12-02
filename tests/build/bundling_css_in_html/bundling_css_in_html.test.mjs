4105ba5d7e935b156f7cc9b0542e38bce1284c66
dmail <dmaillard06@gmail.com>
2022-12-02 14:24:16 +0100

    fix css bundling when referenced by html itself referenced by js

---
+++ tests/build/bundling_css_in_html/bundling_css_in_html.test.mjs
@@ -0,0 +1,23 @@
+import { assert } from "@jsenv/assert"
+
+import { build } from "@jsenv/core"
+
+const { buildFileContents } = await build({
+  logLevel: "warn",
+  rootDirectoryUrl: new URL("./client/", import.meta.url),
+  buildDirectoryUrl: new URL("./dist/", import.meta.url),
+  entryPoints: {
+    "./main.js": "main.js",
+  },
+  bundling: true,
+  minification: false,
+})
+const actual = {
+  numberOfCssFiles: Object.keys(buildFileContents).filter((key) =>
+    key.startsWith("css/"),
+  ).length,
+}
+const expected = {
+  numberOfCssFiles: 1,
+}
+assert({ actual, expected })
