4ce28086885896bb3036a5c8a2ab8521849ce963
dmail <dmaillard06@gmail.com>
2022-12-22 12:12:47 +0100

    update browser versions

--- src/execute/runtimes/browsers/chromium.js
+++ src/execute/runtimes/browsers/chromium.js
@@ -2,7 +2,7 @@ import { createRuntimeFromPlaywright } from "./from_playwright.js"
 
 export const chromium = createRuntimeFromPlaywright({
   browserName: "chromium",
-  browserVersion: "108.0.5359.29", // to update, check https://github.com/microsoft/playwright/releases
+  browserVersion: "109.0.5414.46", // to update, check https://github.com/microsoft/playwright/releases
   coveragePlaywrightAPIAvailable: true,
 })
 export const chromiumIsolatedTab = chromium.isolatedTab
--- src/execute/runtimes/browsers/firefox.js
+++ src/execute/runtimes/browsers/firefox.js
@@ -2,6 +2,6 @@ import { createRuntimeFromPlaywright } from "./from_playwright.js"
 
 export const firefox = createRuntimeFromPlaywright({
   browserName: "firefox",
-  browserVersion: "106.0", // to update, check https://github.com/microsoft/playwright/releases
+  browserVersion: "107.0", // to update, check https://github.com/microsoft/playwright/releases
 })
 export const firefoxIsolatedTab = firefox.isolatedTab
