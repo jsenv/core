f15f51a3f9f761d41c00cc787a7a435ce957fca1
dmail <dmaillard06@gmail.com>
2023-01-25 17:17:42 +0100

    prepare a test for autoreload and css syntax error

---
+++ tests/dev_server/autoreload_inline_style_syntax_error/autoreload_inline_style_syntax_error.test_manual.mjs
@@ -0,0 +1,14 @@
+/*
+ * Ensure server errors are dispatched to clients only if this page is responsible
+ * for the error; unrelated pages must not display an error.
+ */
+
+import { startDevServer } from "@jsenv/core"
+
+await startDevServer({
+  rootDirectoryUrl: new URL("./client/", import.meta.url),
+  clientFiles: {
+    "**/*": true,
+  },
+  clientMainFileUrl: new URL("./client/main.html", import.meta.url),
+})
