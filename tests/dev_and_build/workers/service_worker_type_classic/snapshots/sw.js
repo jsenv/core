ecdf2703070bb47708c5c9de0c49ff49f96e1895
dmail <dmaillard06@gmail.com>
2023-01-26 17:06:58 +0100

    same as prev commit

--- tests/dev_and_build/workers/service_worker_type_classic/snapshots/sw.js
+++ tests/dev_and_build/workers/service_worker_type_classic/snapshots/sw.js
@@ -2,16 +2,45 @@
 self.serviceWorkerUrls = {
   "/main.html": {
     "versioned": false,
-    "version": "37eb36a2"
+    "version": "ceb7c6c8"
   },
-  "/css/style.css?v=65c914e7": {
+  "/css/style.css?v=0e312da1": {
     "versioned": true
   },
-  "/js/a.js?v=07327beb": {
+  "/js/a.js?v=acc03e99": {
     "versioned": true
   },
-  "/js/b.js?v=2cc2d9e4": {
+  "/js/b.js?v=7342c38c": {
     "versioned": true
   }
 };
-;(function(){var m = {"/js/a.js":"/js/a.js?v=07327beb","/js/b.js":"/js/b.js?v=2cc2d9e4"}; self.__v__ = function (s) { return m[s] || s }; })();self.order=[],self.order.push("before-a"),importScripts(__v__("/js/a.js")),self.order.push("after-a"),self.addEventListener("message",(async e=>{"inspect"===e.data&&e.ports[0].postMessage({order:self.order,serviceWorkerUrls:self.serviceWorkerUrls})}));const fn=([e])=>{console.log(e)};fn(["a"]);
+
+;(function() {
+  var __versionMappings__ = {
+  "/js/a.js": "/js/a.js?v=acc03e99",
+  "/js/b.js": "/js/b.js?v=7342c38c"
+};
+  self.__v__ = function (specifier) {
+    return __versionMappings__[specifier] || specifier
+  };
+})();
+/* globals self, importScripts */
+
+self.order = [];
+self.order.push("before-a");
+importScripts(__v__("/js/a.js"));
+self.order.push("after-a");
+self.addEventListener("message", async messageEvent => {
+  if (messageEvent.data === "inspect") {
+    messageEvent.ports[0].postMessage({
+      order: self.order,
+      serviceWorkerUrls: self.serviceWorkerUrls
+    });
+  }
+});
+
+// trigger jsenv dynamic import for slicedToArray
+const fn = ([a]) => {
+  console.log(a);
+};
+fn(["a"]);
