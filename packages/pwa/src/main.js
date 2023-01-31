de03012d361104ed5b61f5a58c07afa81394e105
dmail <dmaillard06@gmail.com>
2023-01-31 15:30:39 +0100

    few fixes before testing pwa

---
+++ packages/pwa/src/main.js
@@ -0,0 +1,7 @@
+export {
+  canUseServiceWorkers,
+  createServiceWorkerScript,
+} from "./service_worker_script.js"
+export { addToHomescreen } from "./add_to_home_screen.js"
+export { displayModeStandalone } from "./display_mode_standalone.js"
+export { listenAppInstalled } from "./listen_app_installed.js"
