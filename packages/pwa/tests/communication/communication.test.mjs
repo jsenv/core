68ed978f938bbe7705f415b0152e68c6fe9ca46f
dmail <dmaillard06@gmail.com>
2023-03-02 16:42:53 +0100

    run come tests only on linux

--- packages/pwa/tests/communication/communication.test.mjs
+++ packages/pwa/tests/communication/communication.test.mjs
@@ -2,22 +2,25 @@ import { assert } from "@jsenv/assert"
 
 import { setupTest } from "@jsenv/pwa/tests/setup_test.mjs"
 
-const debug = false
-const { testServer, page, browser } = await setupTest({
-  rootDirectoryUrl: new URL("./client/", import.meta.url),
-  debug,
-})
-try {
-  await page.goto(`${testServer.origin}/main.html`)
-  const actual = await page.evaluate(
-    /* eslint-disable no-undef */
-    () => window.resultPromise,
-    /* eslint-enable no-undef */
-  )
-  const expected = "pong"
-  assert({ actual, expected })
-} finally {
-  if (!debug) {
-    browser.close()
+// https certificate only generated on linux
+if (process.platform === "linux") {
+  const debug = false
+  const { testServer, page, browser } = await setupTest({
+    rootDirectoryUrl: new URL("./client/", import.meta.url),
+    debug,
+  })
+  try {
+    await page.goto(`${testServer.origin}/main.html`)
+    const actual = await page.evaluate(
+      /* eslint-disable no-undef */
+      () => window.resultPromise,
+      /* eslint-enable no-undef */
+    )
+    const expected = "pong"
+    assert({ actual, expected })
+  } finally {
+    if (!debug) {
+      browser.close()
+    }
   }
 }
--- packages/service-worker/tests/errors/errors.test.mjs
+++ packages/service-worker/tests/errors/errors.test.mjs
@@ -1,9 +1,15 @@
 import { assert } from "@jsenv/assert"
 import { readSnapshotsFromDirectory } from "@jsenv/core/tests/snapshots_directory.js"
 
-const snapshotsHtmlDirectoryUrl = new URL("./snapshots/html/", import.meta.url)
-const expected = readSnapshotsFromDirectory(snapshotsHtmlDirectoryUrl)
-process.env.FROM_TESTS = "true"
-await import("./errors_snapshots.mjs")
-const actual = readSnapshotsFromDirectory(snapshotsHtmlDirectoryUrl)
-assert({ actual, expected })
+// https certificate only generated on linux
+if (process.platform === "linux") {
+  const snapshotsHtmlDirectoryUrl = new URL(
+    "./snapshots/html/",
+    import.meta.url,
+  )
+  const expected = readSnapshotsFromDirectory(snapshotsHtmlDirectoryUrl)
+  process.env.FROM_TESTS = "true"
+  await import("./errors_snapshots.mjs")
+  const actual = readSnapshotsFromDirectory(snapshotsHtmlDirectoryUrl)
+  assert({ actual, expected })
+}
