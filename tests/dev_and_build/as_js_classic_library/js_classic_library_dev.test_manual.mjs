4f55ede5762cd86c3b4cbd9f2dde5dd301f7856c
dmail <dmaillard06@gmail.com>
2023-03-22 17:03:06 +0100

    wroking on a test

--- tests/dev_and_build/as_js_classic_library/js_classic_library_dev.test_manual.mjs
+++ tests/dev_and_build/as_js_classic_library/js_classic_library_dev.test_manual.mjs
@@ -3,7 +3,7 @@ import { startDevServer } from "@jsenv/core"
 startDevServer({
   logLevel: "info",
   sourceDirectoryUrl: new URL("./client/", import.meta.url),
+  sourceMainFilePath: "main.html",
   clientAutoreload: true,
   supervisor: false,
-  clientMainFileUrl: new URL("./client/main.html", import.meta.url),
 })
