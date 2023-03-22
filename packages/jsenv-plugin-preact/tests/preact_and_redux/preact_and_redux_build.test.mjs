a5518259e5a28542c348d96b2a6e435d82167bfe
dmail <dmaillard06@gmail.com>
2023-03-22 17:59:39 +0100

    update tests

--- packages/jsenv-plugin-preact/tests/preact_and_redux/preact_and_redux_build.test.mjs
+++ packages/jsenv-plugin-preact/tests/preact_and_redux/preact_and_redux_build.test.mjs
@@ -13,11 +13,11 @@ const test = async (params) => {
   )
   await build({
     logLevel: "warn",
-    rootDirectoryUrl: new URL("./client/", import.meta.url),
-    buildDirectoryUrl: new URL("./dist/", import.meta.url),
+    sourceDirectoryUrl: new URL("./client/", import.meta.url),
     entryPoints: {
       "./main.html": "main.html",
     },
+    buildDirectoryUrl: new URL("./dist/", import.meta.url),
     plugins: [...plugins, jsenvPluginBundling()],
     writeGeneratedFiles: true,
     ...params,
@@ -28,17 +28,13 @@ const test = async (params) => {
   const { returnValue } = await executeInChromium({
     url: `${server.origin}/main.html`,
     /* eslint-disable no-undef */
-    pageFunction: async () => window.resultPromise,
+    pageFunction: () => window.resultPromise,
     /* eslint-enable no-undef */
   })
-  const actual = {
-    returnValue,
-  }
+  const actual = returnValue
   const expected = {
-    returnValue: {
-      spanContentAfterIncrement: "1",
-      spanContentAfterDecrement: "0",
-    },
+    spanContentAfterIncrement: "1",
+    spanContentAfterDecrement: "0",
   }
   assert({ actual, expected })
 }
--- packages/jsenv-plugin-react/tests/react_build/react_build.test.mjs
+++ packages/jsenv-plugin-react/tests/react_build/react_build.test.mjs
@@ -9,11 +9,11 @@ import { jsenvPluginReact } from "@jsenv/plugin-react"
 const test = async (params) => {
   await build({
     logLevel: "warn",
-    rootDirectoryUrl: new URL("./client/", import.meta.url),
-    buildDirectoryUrl: new URL("./dist/", import.meta.url),
+    sourceDirectoryUrl: new URL("./client/", import.meta.url),
     entryPoints: {
       "./main.html": "main.html",
     },
+    buildDirectoryUrl: new URL("./dist/", import.meta.url),
     ...params,
     plugins: [
       jsenvPluginReact({
@@ -28,19 +28,11 @@ const test = async (params) => {
   const { returnValue } = await executeInChromium({
     url: `${server.origin}/main.html`,
     /* eslint-disable no-undef */
-    pageFunction: async () => {
-      return window.resultPromise
-    },
+    pageFunction: () => window.resultPromise,
     /* eslint-enable no-undef */
   })
-  const actual = {
-    returnValue,
-  }
-  const expected = {
-    returnValue: {
-      spanContent: "Hello world",
-    },
-  }
+  const actual = returnValue
+  const expected = { spanContent: "Hello world" }
   assert({ actual, expected })
 }
 
