6f8d5aa44bd49895557a44655039c7c05411aa39
dmail <dmaillard06@gmail.com>
2023-03-22 17:40:18 +0100

    update preact demo

--- packages/create-jsenv/demo-web/scripts/start_dev_server.mjs
+++ packages/create-jsenv/demo-web/scripts/start_dev_server.mjs
@@ -12,5 +12,5 @@ export const devServer = await startDevServer({
   port: 3400,
 })
 if (process.argv.includes("--open")) {
-  open(`${devServer.origin}/src/main.html`)
+  open(`${devServer.origin}/main.html`)
 }
