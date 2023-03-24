fc04b2afcf12b515dbf7324478fdea04e64452c7
dmail <dmaillard06@gmail.com>
2023-03-24 10:33:02 +0100

    update test on manifest

--- src/plugins/url_analysis/webmanifest/webmanifest_urls.js
+++ src/plugins/url_analysis/webmanifest/webmanifest_urls.js
@@ -2,15 +2,6 @@ export const parseAndTransformWebmanifestUrls = async (urlInfo, context) => {
   const content = urlInfo.content
   const manifest = JSON.parse(content)
   const actions = []
-  const { start_url } = manifest
-  if (start_url) {
-    if (context.build) {
-      manifest.start_url = "/"
-    } else {
-      const parentUrl = context.reference.parentUrl
-      manifest.start_url = `${parentUrl.slice(context.rootDirectoryUrl.length)}`
-    }
-  }
   const { icons = [] } = manifest
   icons.forEach((icon) => {
     const [reference] = context.referenceUtils.found({
