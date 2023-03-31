45d71ef4c1ebc8913c699e9afcd50e34b302b5b7
dmail <dmaillard06@gmail.com>
2023-03-31 16:04:12 +0200

    convert static import into dynamic ones

--- src/plugins/supervisor/js_supervisor_injection.js
+++ src/plugins/supervisor/js_supervisor_injection.js
@@ -55,32 +55,122 @@ const babelPluginJsModuleSupervisor = (babel) => {
         const topLevelNodes = []
         for (const topLevelNodePath of bodyPath) {
           const topLevelNode = topLevelNodePath.node
-          if (
-            t.isImportDeclaration(topLevelNode) ||
-            t.isImportDefaultSpecifier(topLevelNode) ||
-            t.isImportNamespaceSpecifier(topLevelNode) ||
-            t.isImportSpecifier(topLevelNode)
-          ) {
+          if (t.isImportDeclaration(topLevelNode)) {
             importNodes.push(topLevelNode)
           } else {
             topLevelNodes.push(topLevelNode)
           }
         }
+
+        // replace all import nodes with dynamic imports
+        const dynamicImports = []
+        importNodes.forEach((importNode) => {
+          const dynamicImportConversion = convertStaticImportIntoDynamicImport(
+            importNode,
+            t,
+          )
+          if (Array.isArray(dynamicImportConversion)) {
+            dynamicImports.push(...dynamicImportConversion)
+          } else {
+            dynamicImports.push(dynamicImportConversion)
+          }
+        })
+
         const tryCatchNode = t.tryStatement(
-          t.blockStatement([...topLevelNodes, endCallNode]),
+          t.blockStatement([...dynamicImports, ...topLevelNodes, endCallNode]),
           t.catchClause(t.identifier("e"), t.blockStatement([errorCallNode])),
         )
-        programPath.replaceWith(
-          t.program([
-            // prettier-ignore-next
-            startCallNode,
-            ...importNodes,
-            tryCatchNode,
-          ]),
-        )
+        programPath.replaceWith(t.program([startCallNode, tryCatchNode]))
       },
     },
   }
+}
+
+const convertStaticImportIntoDynamicImport = (staticImportNode, t) => {
+  const awaitExpression = t.awaitExpression(
+    t.callExpression(t.import(), [
+      t.stringLiteral(staticImportNode.source.value),
+    ]),
+  )
+
+  // import "./file.js" -> await import("./file.js")
+  if (staticImportNode.specifiers.length === 0) {
+    return t.expressionStatement(awaitExpression)
+  }
+  if (staticImportNode.specifiers.length === 1) {
+    const [firstSpecifier] = staticImportNode.specifiers
+    if (firstSpecifier.type === "ImportNamespaceSpecifier") {
+      return t.variableDeclaration("const", [
+        t.variableDeclarator(
+          t.identifier(firstSpecifier.local.name),
+          awaitExpression,
+        ),
+      ])
+    }
+  }
+  if (staticImportNode.specifiers.length === 2) {
+    const [first, second] = staticImportNode.specifiers
+    if (
+      first.type === "ImportDefaultSpecifier" &&
+      second.type === "ImportNamespaceSpecifier"
+    ) {
+      const namespaceDeclaration = t.variableDeclaration("const", [
+        t.variableDeclarator(t.identifier(second.local.name), awaitExpression),
+      ])
+      const defaultDeclaration = t.variableDeclaration("const", [
+        t.variableDeclarator(
+          t.identifier(first.local.name),
+          t.memberExpression(
+            t.identifier(second.local.name),
+            t.identifier("default"),
+          ),
+        ),
+      ])
+      return [namespaceDeclaration, defaultDeclaration]
+    }
+  }
+
+  // import { name } from "./file.js" -> const { name } = await import("./file.js")
+  // import toto, { name } from "./file.js" -> const { name, default as toto } = await import("./file.js")
+  const objectPattern = t.objectPattern(
+    staticImportNode.specifiers.map((specifier) => {
+      if (specifier.type === "ImportDefaultSpecifier") {
+        return t.objectProperty(
+          t.identifier("default"),
+          t.identifier(specifier.local.name),
+          false, // computed
+          false, // shorthand
+        )
+      }
+      // if (specifier.type === "ImportNamespaceSpecifier") {
+      //   return t.restElement(t.identifier(specifier.local.name))
+      // }
+      const isRenamed = specifier.imported.name !== specifier.local.name
+      if (isRenamed) {
+        return t.objectProperty(
+          t.identifier(specifier.imported.name),
+          t.identifier(specifier.local.name),
+          false, // computed
+          false, // shorthand
+        )
+      }
+      // shorthand must be true
+      return t.objectProperty(
+        t.identifier(specifier.local.name),
+        t.identifier(specifier.local.name),
+        false, // computed
+        true, // shorthand
+      )
+    }),
+  )
+  const variableDeclarator = t.variableDeclarator(
+    objectPattern,
+    awaitExpression,
+  )
+  const variableDeclaration = t.variableDeclaration("const", [
+    variableDeclarator,
+  ])
+  return variableDeclaration
 }
 
 const babelPluginJsClassicSupervisor = (babel) => {
--- tests/__internal__/html_supervisor/fixtures/answer.js
+++ tests/__internal__/html_supervisor/fixtures/answer.js
@@ -2,3 +2,6 @@ export const getAnswer = async () => {
   await new Promise((resolve) => setTimeout(resolve, 500))
   return 43
 }
+
+// eslint-disable-next-line import/no-default-export
+export default 42
