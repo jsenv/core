c98e2e82bbdbe5605cc2b293c712d2947055c442
dmail <dmaillard06@gmail.com>
2023-04-17 09:50:03 +0200

    prepare as js classic plugin package

---
+++ packages/jsenv-plugin-as-js-classic/src/jsenv_plugin_as_js_classic.js
@@ -0,0 +1,91 @@
+import { bundleJsModules } from "@jsenv/plugin-bundling"
+
+import { createUrlGraphLoader } from "@jsenv/core/src/kitchen/url_graph/url_graph_loader.js"
+import { convertJsModuleToJsClassic } from "@jsenv/core/src/plugins/transpilation/js_module_fallback/convert_js_module_to_js_classic.js"
+
+export const jsenvPluginAsJsClassic = ({
+  systemJsInjection,
+  systemJsClientFileUrl,
+  generateJsClassicFilename,
+}) => {
+  const markAsJsClassicLibraryProxy = (reference) => {
+    reference.expectedType = "js_classic"
+    reference.filename = generateJsClassicFilename(reference.url)
+  }
+
+  return {
+    name: "jsenv:as_js_classic",
+    appliesDuring: "*",
+    redirectUrl: (reference) => {
+      if (reference.searchParams.has("as_js_classic_library")) {
+        markAsJsClassicLibraryProxy(reference)
+      }
+    },
+    fetchUrlContent: async (urlInfo, context) => {
+      const [jsModuleReference, jsModuleUrlInfo] =
+        context.getWithoutSearchParam({
+          urlInfo,
+          context,
+          searchParam: "as_js_classic_library",
+          // override the expectedType to "js_module"
+          // because when there is ?as_js_classic_library it means the underlying resource
+          // is a js_module
+          expectedType: "js_module",
+        })
+      if (!jsModuleReference) {
+        return null
+      }
+      // cook it to get content + dependencies
+      await context.cook(jsModuleUrlInfo, { reference: jsModuleReference })
+      const loader = createUrlGraphLoader(context)
+      loader.loadReferencedUrlInfos(jsModuleUrlInfo, {
+        // we ignore dynamic import to cook lazyly (as browser request the server)
+        // these dynamic imports must inherit "?as_js_classic_library"
+        // This is done inside rollup for convenience
+        ignoreDynamicImport: true,
+      })
+      await loader.getAllLoadDonePromise()
+      const bundleUrlInfos = await bundleJsModules({
+        jsModuleUrlInfos: [jsModuleUrlInfo],
+        context: {
+          ...context,
+          buildDirectoryUrl: new URL("./", import.meta.url),
+        },
+        preserveDynamicImport: true,
+      })
+      const jsModuleBundledUrlInfo = bundleUrlInfos[jsModuleUrlInfo.url]
+      if (context.dev) {
+        jsModuleBundledUrlInfo.sourceUrls.forEach((sourceUrl) => {
+          context.referenceUtils.inject({
+            type: "js_url",
+            specifier: sourceUrl,
+            isImplicit: true,
+          })
+        })
+      } else if (context.build) {
+        jsModuleBundledUrlInfo.sourceUrls.forEach((sourceUrl) => {
+          const sourceUrlInfo = context.urlGraph.getUrlInfo(sourceUrl)
+          if (sourceUrlInfo && sourceUrlInfo.dependents.size === 0) {
+            context.urlGraph.deleteUrlInfo(sourceUrl)
+          }
+        })
+      }
+      const { content, sourcemap } = await convertJsModuleToJsClassic({
+        rootDirectoryUrl: context.rootDirectoryUrl,
+        systemJsInjection,
+        systemJsClientFileUrl,
+        urlInfo,
+        jsModuleUrlInfo: jsModuleBundledUrlInfo,
+      })
+      return {
+        content,
+        contentType: "text/javascript",
+        type: "js_classic",
+        originalUrl: urlInfo.originalUrl,
+        originalContent: jsModuleUrlInfo.originalContent,
+        sourcemap,
+        data: jsModuleUrlInfo.data,
+      }
+    },
+  }
+}
