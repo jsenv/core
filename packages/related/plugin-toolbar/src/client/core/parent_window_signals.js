ba09489f3462991862d560d3b562cf42153eabb6
dmail <dmaillard06@gmail.com>
2023-07-18 10:09:04 +0200

    fix jsenv toolbar always appearing as enabled

--- packages/related/plugin-toolbar/src/client/core/parent_window_signals.js
+++ packages/related/plugin-toolbar/src/client/core/parent_window_signals.js
@@ -1,23 +1,13 @@
 import { signal } from "@preact/signals";
 
-import {
-  paramsFromParentWindow,
-  parentWindowReloader,
-} from "./parent_window_context.js";
+import { parentWindowReloader } from "./parent_window_context.js";
 
 export const autoreloadEnabledSignal = signal(false);
 export const reloaderStatusSignal = signal("idle");
 export const changesSignal = signal(0);
 
 if (parentWindowReloader) {
-  if (
-    !parentWindowReloader.autoreload.enabled &&
-    paramsFromParentWindow.autoreload
-  ) {
-    autoreloadEnabledSignal.value = true;
-  } else {
-    autoreloadEnabledSignal.value = parentWindowReloader.autoreload.enabled;
-  }
+  autoreloadEnabledSignal.value = parentWindowReloader.autoreload.enabled;
   parentWindowReloader.autoreload.onchange = () => {
     autoreloadEnabledSignal.value = parentWindowReloader.autoreload.enabled;
   };
