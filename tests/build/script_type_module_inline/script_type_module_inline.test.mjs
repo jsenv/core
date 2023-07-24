a924e6c9804c70f93fb1dbef2f947995498d0651
dmail <dmaillard06@gmail.com>
2023-07-24 14:39:21 +0200

    rerunning some test

--- tests/build/script_type_module_inline/script_type_module_inline.test.mjs
+++ tests/build/script_type_module_inline/script_type_module_inline.test.mjs
@@ -2,21 +2,26 @@ import { assert } from "@jsenv/assert";
 import { jsenvPluginBundling } from "@jsenv/plugin-bundling";
 
 import { build } from "@jsenv/core";
+import { takeDirectorySnapshot } from "@jsenv/core/tests/snapshots_directory.js";
 import { startFileServer } from "@jsenv/core/tests/start_file_server.js";
 import { executeInBrowser } from "@jsenv/core/tests/execute_in_browser.js";
 
-const test = async ({ expectedUrl, ...rest }) => {
+const test = async ({ name, expectedUrl, ...rest }) => {
   await build({
     logLevel: "warn",
     sourceDirectoryUrl: new URL("./client/", import.meta.url),
+    buildDirectoryUrl: new URL("./dist/", import.meta.url),
     entryPoints: {
       "./main.html": "main.html",
     },
-    buildDirectoryUrl: new URL("./dist/", import.meta.url),
     outDirectoryUrl: new URL("./.jsenv/", import.meta.url),
     versioning: false,
     ...rest,
   });
+  takeDirectorySnapshot(
+    new URL("./dist/", import.meta.url),
+    new URL(`./snapshots/${name}/`, import.meta.url),
+  );
 
   const server = await startFileServer({
     rootDirectoryUrl: new URL("./dist/", import.meta.url),
@@ -35,26 +40,28 @@ const test = async ({ expectedUrl, ...rest }) => {
   assert({ actual, expected });
 };
 
-// can use <script type="module"> + bundling
+// can use <script type="module">
 await test({
+  name: "0_js_module",
   runtimeCompat: { chrome: "89" },
   plugins: [jsenvPluginBundling()],
   expectedUrl: "/main.html",
 });
-// cannot use <script type="module"> + bundling
-await test({
-  runtimeCompat: { chrome: "60" },
-  plugins: [jsenvPluginBundling()],
-  expectedUrl: "/main.html__inline_script__1",
-});
-// cannot use <script type="module"> + no bundling
-await test({
-  runtimeCompat: { chrome: "60" },
-  expectedUrl: "/main.html__inline_script__1",
-});
-// cannot use <script type="module"> + no bundling + versioning
-await test({
-  runtimeCompat: { chrome: "60" },
-  versioning: true,
-  expectedUrl: "/main.html__inline_script__1",
-});
+// cannot use <script type="module">
+// await test({
+//   name: "1_js_module_fallback",
+//   runtimeCompat: { chrome: "60" },
+//   plugins: [jsenvPluginBundling()],
+//   expectedUrl: "/main.html__inline_script__1",
+// });
+// // cannot use <script type="module"> + no bundling
+// await test({
+//   runtimeCompat: { chrome: "60" },
+//   expectedUrl: "/main.html__inline_script__1",
+// });
+// // cannot use <script type="module"> + no bundling + versioning
+// await test({
+//   runtimeCompat: { chrome: "60" },
+//   versioning: true,
+//   expectedUrl: "/main.html__inline_script__1",
+// });
