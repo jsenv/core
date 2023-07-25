cafe58190b36ebd04b7a4e45c9797b695b9e6e05
dmail <dmaillard06@gmail.com>
2023-07-25 11:54:27 +0200

    update snapshot

--- tests/build/script_classic_and_sourcemap/script_classic_and_sourcemap.test.mjs
+++ tests/build/script_classic_and_sourcemap/script_classic_and_sourcemap.test.mjs
@@ -1,6 +1,7 @@
 import { assert } from "@jsenv/assert";
 
 import { build } from "@jsenv/core";
+import { takeDirectorySnapshot } from "@jsenv/core/tests/snapshots_directory.js";
 import { startFileServer } from "@jsenv/core/tests/start_file_server.js";
 import { executeInBrowser } from "@jsenv/core/tests/execute_in_browser.js";
 
@@ -8,12 +9,17 @@ const test = async (params) => {
   await build({
     logLevel: "warn",
     sourceDirectoryUrl: new URL("./client/", import.meta.url),
+    buildDirectoryUrl: new URL("./dist/", import.meta.url),
     entryPoints: {
       "./main.html": "main.html",
     },
-    buildDirectoryUrl: new URL("./dist/", import.meta.url),
     ...params,
   });
+  takeDirectorySnapshot(
+    new URL("./dist/", import.meta.url),
+    new URL("./snapshots/", import.meta.url),
+    false,
+  );
   const server = await startFileServer({
     rootDirectoryUrl: new URL("./dist/", import.meta.url),
   });
