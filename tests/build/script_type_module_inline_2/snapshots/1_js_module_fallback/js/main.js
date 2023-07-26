f3c1ad3f1ff4bc623be59d612d11c27b2a0462be
dmail <dmaillard06@gmail.com>
2023-07-26 17:41:11 +0200

    update snapshots

--- tests/build/script_type_module_inline_2/snapshots/1_js_module_fallback/js/main.js
+++
@@ -1,4 +0,0 @@
-import { util } from "/js/util.js?v=b75b52b8";
-util();
-console.log("Hello world");
-export const answer = 42;
--- tests/build/script_type_module_inline_2/snapshots/1_js_module_fallback/js/util.js
+++
@@ -1,3 +0,0 @@
-export const util = () => {
-  console.log("util");
-};
