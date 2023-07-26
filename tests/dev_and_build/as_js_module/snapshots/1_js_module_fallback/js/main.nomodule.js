7005bd77755fc9c3c757be87decb9cb7b96ad234
dmail <dmaillard06@gmail.com>
2023-07-26 17:38:28 +0200

    update a test

---
+++ tests/dev_and_build/as_js_module/snapshots/1_js_module_fallback/js/main.nomodule.js
@@ -0,0 +1,10 @@
+System.register([__v__("/js/file.nomodule.js")], function (_export, _context) {
+  "use strict";
+
+  return {
+    setters: [function (_) {}],
+    execute: function () {
+      window.resolveResultPromise(window.__answer__);
+    }
+  };
+});
--- tests/dev_and_build/as_js_module/snapshots/js/main.js
+++
@@ -1,2 +0,0 @@
-import "/js/file.mjs?v=cf9d4ba5";
-window.resolveResultPromise(window.__answer__);
