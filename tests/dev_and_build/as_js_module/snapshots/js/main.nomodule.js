dcf6a7564293969f5b8ab89438f0c25760c75c8a
dmail <dmaillard06@gmail.com>
2023-07-26 09:43:29 +0200

    update some snapshots

--- tests/dev_and_build/as_js_module/snapshots/js/main.nomodule.js
+++ tests/dev_and_build/as_js_module/snapshots/js/main.nomodule.js
@@ -2,7 +2,7 @@ System.register([__v__("/js/file.nomodule.js")], function (_export, _context) {
   "use strict";
 
   return {
-    setters: [function (_clientFileJsAs_js_module) {}],
+    setters: [function (_) {}],
     execute: function () {
       window.resolveResultPromise(window.__answer__);
     }
