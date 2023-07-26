e6dad462d70612892ea8bfb65e245621d739734b
dmail <dmaillard06@gmail.com>
2023-07-26 11:05:51 +0200

    remove a test

--- tests/dev_and_build/js_module_fallback_query_param/js_module_fallback_query_param_dev.test_manual.mjs
+++
@@ -1,8 +0,0 @@
-import { startDevServer } from "@jsenv/core";
-
-await startDevServer({
-  logLevel: "debug",
-  sourceDirectoryUrl: new URL("./client/", import.meta.url),
-  sourceMainFilePath: "main.html",
-  outDirectoryUrl: new URL("./.jsenv/", import.meta.url),
-});
