e351939269996cf8c98d428de490584feb5d7561
dmail <dmaillard06@gmail.com>
2023-07-27 15:54:12 +0200

    update test

--- tests/build/new_url_directory_2/new_url_directory_2.test.mjs
+++ tests/build/new_url_directory_2/new_url_directory_2.test.mjs
@@ -1,13 +1,10 @@
-import { readFileSync } from "node:fs";
 import { assert } from "@jsenv/assert";
 
 import { build } from "@jsenv/core";
 import { takeDirectorySnapshot } from "@jsenv/core/tests/snapshots_directory.js";
-import { startFileServer } from "@jsenv/core/tests/start_file_server.js";
-import { executeInBrowser } from "@jsenv/core/tests/execute_in_browser.js";
 
 const test = async (params) => {
-  const { buildManifest } = await build({
+  await build({
     logLevel: "warn",
     sourceDirectoryUrl: new URL("./client/", import.meta.url),
     buildDirectoryUrl: new URL("./dist/", import.meta.url),
@@ -21,27 +18,11 @@ const test = async (params) => {
     new URL("./dist/", import.meta.url),
     new URL("./snapshots/", import.meta.url),
   );
-  const server = await startFileServer({
-    rootDirectoryUrl: new URL("./dist/", import.meta.url),
-  });
-  const { returnValue } = await executeInBrowser({
-    url: `${server.origin}/main.html`,
-    /* eslint-disable no-undef */
-    pageFunction: () => window.resultPromise,
-    /* eslint-enable no-undef */
-  });
-  const actual = {
-    returnValue,
-    jsFileContent: String(
-      readFileSync(new URL("./dist/src/sub/file.js", import.meta.url)),
-    ),
-  };
-  const expected = {
-    returnValue: {
-      directoryUrl: `${server.origin}/${buildManifest["src/"]}`,
-    },
-    jsFileContent: `console.log("Hello");\n`,
-  };
+
+  // eslint-disable-next-line import/no-unresolved
+  const { directoryUrl } = await import("./dist/main.js");
+  const actual = directoryUrl;
+  const expected = new URL("./dist/src/", import.meta.url).href;
   assert({ actual, expected });
 };
 
