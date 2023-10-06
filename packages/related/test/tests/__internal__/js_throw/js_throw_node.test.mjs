fe4d4f0ed5abeff6a7f94ce592ebb64ad461e5f0
dmail <dmaillard06@gmail.com>
2023-10-06 11:25:40 +0200

    update a test

--- packages/related/test/tests/__internal__/js_throw/js_throw_node.test.mjs
+++ packages/related/test/tests/__internal__/js_throw/js_throw_node.test.mjs
@@ -1,5 +1,5 @@
 import { assert } from "@jsenv/assert";
-import { execute, nodeChildProcess } from "@jsenv/test";
+import { execute, nodeChildProcess, nodeWorkerThread } from "@jsenv/test";
 
 const test = async (params) => {
   const { errors } = await execute({
@@ -12,32 +12,34 @@ const test = async (params) => {
     ignoreError: true,
     ...params,
   });
-  return errors;
-};
-
-const [error] = await test({
-  runtime: nodeChildProcess(),
-});
-const clientDirectoryUrl = new URL("./client", import.meta.url).href;
-
-const actual = {
-  isException: error.isException,
-  name: error.name,
-  message: error.message,
-  stack: error.stack,
-  site: error.site,
-};
-const expected = {
-  isException: true,
-  name: "Error",
-  message: "SPECIAL_STRING_UNLIKELY_TO_COLLIDE",
-  stack: assert.startsWith(`Error: SPECIAL_STRING_UNLIKELY_TO_COLLIDE
+  const [error] = errors;
+  const clientDirectoryUrl = new URL("./client", import.meta.url).href;
+  const actual = {
+    isException: error.isException,
+    name: error.name,
+    message: error.message,
+    stack: error.stack,
+    site: error.site,
+  };
+  const expected = {
+    isException: true,
+    name: "Error",
+    message: "SPECIAL_STRING_UNLIKELY_TO_COLLIDE",
+    stack: assert.startsWith(`Error: SPECIAL_STRING_UNLIKELY_TO_COLLIDE
   at triggerError (${clientDirectoryUrl}/trigger_error.js:2:9)
   at ${clientDirectoryUrl}/main.js:3:1`),
-  site: {
-    url: `${clientDirectoryUrl}/trigger_error.js`,
-    line: 2,
-    column: 8,
-  },
+    site: {
+      url: `${clientDirectoryUrl}/trigger_error.js`,
+      line: 2,
+      column: 8,
+    },
+  };
+  assert({ actual, expected });
 };
-assert({ actual, expected });
+
+await test({
+  runtime: nodeChildProcess(),
+});
+await test({
+  runtime: nodeWorkerThread(),
+});
