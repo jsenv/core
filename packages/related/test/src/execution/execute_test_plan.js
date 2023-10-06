2061f5fe3ec83539c5059386ec5338aa7faaf0aa
dmail <dmaillard06@gmail.com>
2023-10-06 14:40:33 +0200

    update logs for github check

--- packages/related/test/src/execution/execute_test_plan.js
+++ packages/related/test/src/execution/execute_test_plan.js
@@ -375,7 +375,6 @@ export const executeTestPlan = async ({
           }),
         );
       }
-
       githubCheckRun.progress({
         summary: formatExecutionLabel(afterExecutionInfo, {
           logTimeUsage,
