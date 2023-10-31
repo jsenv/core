08d6851c2dd39b81848d143d6182f22b9c3e3fb8
dmail <dmaillard06@gmail.com>
2023-10-31 14:32:21 +0100

    add static variable test + enableRerenderOnCustomElementUpdate

--- packages/independent/custom-elements-redefine/src/main.js
+++ packages/independent/custom-elements-redefine/src/main.js
@@ -6,15 +6,17 @@
 import { customElementClassMap } from "./internal/custom_element_class_map.js";
 import { createCustomElementFacade } from "./internal/custom_element_facade.js";
 
+let rerenderOnUpdate = false;
+export const enableRerenderOnCustomElementUpdate = () => {
+  rerenderOnUpdate = true;
+};
+
 const defineOriginal = customElements.define;
 customElements.define = (customElementName, customElementClass, options) => {
   const registeredCustomElement = customElements.get(customElementName);
   customElementClassMap.set(customElementName, customElementClass);
   if (registeredCustomElement) {
-    // const onCustomElementChange = globalThis.hmrCache.onCustomElementChange;
-    // if (onCustomElementChange && typeof onCustomElementChange === "function") {
-    //   onCustomElementChange(customElementName, customElementClass, options);
-    // }
+    onCustomElementChange(customElementName, customElementClass, options);
     return;
   }
   const CustomElementFacade = createCustomElementFacade(
@@ -27,4 +29,30 @@ customElements.define = (customElementName, customElementClass, options) => {
     CustomElementFacade,
     options,
   );
+};
+
+const elementsChanged = [];
+let timeoutId;
+const onCustomElementChange = (
+  customElementName,
+  // customElementClass,
+  // options,
+) => {
+  if (!rerenderOnUpdate) {
+    return;
+  }
+  elementsChanged.push(customElementName);
+  clearTimeout(timeoutId);
+  timeoutId = setTimeout(() => {
+    if (document.body) {
+      requestAnimationFrame(() => {
+        // re-render the whole DOM
+        // this will make less calls to connectedCallback/disconnectedCallback on replaced child node when created.
+        const oldBodyHtml = document.body.innerHTML;
+        document.body.innerHTML = "";
+        document.body.innerHTML = oldBodyHtml;
+      });
+    }
+    elementsChanged.length = 0;
+  }, 250);
 };
