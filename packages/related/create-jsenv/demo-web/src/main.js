30f9cc0268ea285efb849193038a2fd1a8cd134c
dmail <dmaillard06@gmail.com>
2023-10-31 16:22:39 +0100

    change how define gets allowed on web demo

--- packages/related/create-jsenv/demo-web/src/main.js
+++ packages/related/create-jsenv/demo-web/src/main.js
@@ -1,9 +1,9 @@
-import "./app/custom_element_allow_override.js";
 import "./app/app_custom_element.js";
 
 document.querySelector("#root").innerHTML = `<my-app></my-app>`;
 
 if (import.meta.hot) {
+  import("@jsenv/custom-elements-redefine");
   import.meta.hot.accept(() => {
     document.querySelector("#root").innerHTML = "";
   });
