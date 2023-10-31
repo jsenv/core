d32c4fed45cdd18e8526d02e14c689d0bc044629
dmail <dmaillard06@gmail.com>
2023-10-31 17:28:49 +0100

    add number of clicks to web demo

--- packages/related/create-jsenv/demo-web/src/app/app_custom_element.js
+++ packages/related/create-jsenv/demo-web/src/app/app_custom_element.js
@@ -10,13 +10,26 @@ class AppCustomElement extends HTMLElement {
       ...shadow.adoptedStyleSheets,
       AppCustomElementStyleSheet,
     ];
+    let clicks = 0;
     shadow.innerHTML = `
 <h1>Hello world!</h1>
 <img class="logo" src=${jsenvLogoUrl} alt="logo" />
 <p>
+  <button id="click_button">Click me!</button>
+  <p>
+    Number of clicks: <span id="click_output">${clicks}</span>
+  </p>
+</p>
+<p>
   Edit <a href="javascript:window.fetch('/__open_in_editor__/jsenv_logo.svg')">jsenv_logo.svg</a> and save to test HMR updates.
 </p>
 <a href="https://github.com/jsenv/core" target="_blank">Documentation</a>`;
+    const button = shadow.querySelector("#click_button");
+    const clickOutput = shadow.querySelector("#click_output");
+    button.onclick = () => {
+      clicks++;
+      clickOutput.innerHTML = clicks;
+    };
   }
 }
 
