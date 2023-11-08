95bf906f00d9c044a28dea635545a48fd50dc237
dmail <dmaillard06@gmail.com>
2023-11-08 11:15:45 +0100

    update babel helpers

--- packages/internal/plugin-transpilation/src/babel/babel_helper_directory/babel_helpers/isNativeFunction/isNativeFunction.js
+++ packages/internal/plugin-transpilation/src/babel/babel_helper_directory/babel_helpers/isNativeFunction/isNativeFunction.js
@@ -1,4 +1,9 @@
-// Note: This function returns "true" for core-js functions.
-export default function (fn) {
-  return Function.toString.call(fn).indexOf("[native code]") !== -1
+export default function _isNativeFunction(fn) {
+  // Note: This function returns "true" for core-js functions.
+  try {
+    return Function.toString.call(fn).indexOf("[native code]") !== -1;
+  } catch (e) {
+    // Firefox 31 throws when "toString" is applied to an HTMLElement
+    return typeof fn === "function";
+  }
 }
