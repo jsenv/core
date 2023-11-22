5c9f3c25b680b0e2cc2272e7ad3d516b67430e53
dmail <dmaillard06@gmail.com>
2023-11-22 09:48:37 +0100

    update commands related to snapshots

--- tests/dev_server/errors/dev_errors_snapshots.test.mjs
+++ tests/dev_server/errors/dev_errors_snapshots.test.mjs
@@ -1,25 +1,12 @@
-import { readFileSync, readdirSync } from "node:fs";
-import { assert } from "@jsenv/assert";
-
-const readHtmlFiles = () => {
-  const snapshotDirectoryUrl = new URL("./snapshots/", import.meta.url);
-  const snapshotFilenames = readdirSync(snapshotDirectoryUrl);
-  const htmlFiles = {};
-  snapshotFilenames.forEach((snapshotFilename) => {
-    if (!snapshotFilename.endsWith(".html")) {
-      return;
-    }
-    const htmlFileUrl = new URL(snapshotFilename, snapshotDirectoryUrl);
-    htmlFiles[snapshotFilename] = String(readFileSync(htmlFileUrl));
-  });
-  return htmlFiles;
-};
+import { assertSnapshotDirectoryTakenByFunction } from "@jsenv/snapshot";
 
 // disable on windows because it would fails due to line endings (CRLF)
 if (process.platform !== "win32") {
-  const expected = readHtmlFiles();
-  process.env.FROM_TESTS = "true";
-  await import("./generate_snapshot_files.mjs");
-  const actual = readHtmlFiles();
-  assert({ actual, expected });
+  assertSnapshotDirectoryTakenByFunction(
+    new URL("./snapshots/", import.meta.url),
+    async () => {
+      process.env.FROM_TESTS = "true";
+      await import("./generate_snapshot_files.mjs");
+    },
+  );
 }
