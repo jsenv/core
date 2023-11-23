5fec6bb207e037dc51a4d2a95ac73c8f037f43a9
dmail <dmaillard06@gmail.com>
2023-11-23 16:32:02 +0100

    update remaining test

--- tests/build/new_url_directory_2/new_url_directory_2.test.mjs
+++ tests/build/new_url_directory_2/new_url_directory_2.test.mjs
@@ -1,28 +1,28 @@
-import { takeDirectorySnapshotAndCompare } from "@jsenv/snapshot";
+import { takeDirectorySnapshot, compareSnapshots } from "@jsenv/snapshot";
 import { assert } from "@jsenv/assert";
 
 import { build } from "@jsenv/core";
 
 const test = async (params) => {
+  const snapshotDirectoryUrl = new URL(`./snapshots/`, import.meta.url);
+  const expectedBuildSnapshot = takeDirectorySnapshot(snapshotDirectoryUrl);
   await build({
     logLevel: "warn",
     sourceDirectoryUrl: new URL("./client/", import.meta.url),
-    buildDirectoryUrl: new URL("./dist/", import.meta.url),
+    buildDirectoryUrl: snapshotDirectoryUrl,
     entryPoints: {
       "./main.js": "main.js",
     },
     outDirectoryUrl: new URL("./.jsenv/", import.meta.url),
     ...params,
   });
-  takeDirectorySnapshotAndCompare(
-    new URL("./dist/", import.meta.url),
-    new URL("./snapshots/", import.meta.url),
-  );
+  const actualBuildSnapshot = takeDirectorySnapshot(snapshotDirectoryUrl);
+  compareSnapshots(actualBuildSnapshot, expectedBuildSnapshot);
 
   // eslint-disable-next-line import/no-unresolved
-  const { directoryUrl } = await import("./dist/main.js");
+  const { directoryUrl } = await import("./snapshots/main.js");
   const actual = directoryUrl;
-  const expected = new URL("./dist/src/", import.meta.url).href;
+  const expected = new URL("./snapshots/src/", import.meta.url).href;
   assert({ actual, expected });
 };
 
--- tests/build/script_classic_and_sourcemap/script_classic_and_sourcemap.test.mjs
+++ tests/build/script_classic_and_sourcemap/script_classic_and_sourcemap.test.mjs
@@ -1,4 +1,3 @@
-import { takeDirectorySnapshotAndCompare } from "@jsenv/snapshot";
 import { assert } from "@jsenv/assert";
 
 import { build } from "@jsenv/core";
@@ -6,22 +5,18 @@ import { startFileServer } from "@jsenv/core/tests/start_file_server.js";
 import { executeInBrowser } from "@jsenv/core/tests/execute_in_browser.js";
 
 const test = async (params) => {
+  const snapshotDirectoryUrl = new URL(`./snapshots/`, import.meta.url);
   await build({
     logLevel: "warn",
     sourceDirectoryUrl: new URL("./client/", import.meta.url),
-    buildDirectoryUrl: new URL("./dist/", import.meta.url),
+    buildDirectoryUrl: snapshotDirectoryUrl,
     entryPoints: {
       "./main.html": "main.html",
     },
     ...params,
   });
-  takeDirectorySnapshotAndCompare(
-    new URL("./dist/", import.meta.url),
-    new URL("./snapshots/", import.meta.url),
-    false,
-  );
   const server = await startFileServer({
-    rootDirectoryUrl: new URL("./dist/", import.meta.url),
+    rootDirectoryUrl: snapshotDirectoryUrl,
   });
   const { returnValue } = await executeInBrowser({
     url: `${server.origin}/main.html`,
