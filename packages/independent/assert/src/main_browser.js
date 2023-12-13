ca4e5976fff8893c85aae1914816985777b8680b
dmail <dmaillard06@gmail.com>
2023-12-13 10:20:51 +0100

    prepare adding color to assert messages

---
+++ packages/independent/assert/src/main_browser.js
@@ -0,0 +1,4 @@
+export { assert } from "./assert.js";
+export { isAssertionError, createAssertionError } from "./assertion_error.js";
+
+export { formatStringAssertionErrorMessage } from "./internal/error_message/strings.js";
