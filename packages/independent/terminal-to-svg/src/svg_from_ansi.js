4fd1cf05bfcc6d1d73c63305c705920001c5f6c6
dmail <dmaillard06@gmail.com>
2023-12-14 16:24:54 +0100

    working on svg generation

--- packages/independent/terminal-to-svg/src/svg_from_ansi.js
+++ packages/independent/terminal-to-svg/src/svg_from_ansi.js
@@ -1,4 +1,40 @@
 import { parseAnsi } from "./parse_ansi.js";
+import { startGeneratingSvg } from "./svg_generator.js";
+
+const colorsDefault = {
+  black: "#000000",
+  red: "#B22222",
+  green: "#32CD32",
+  yellow: "#DAA520",
+  blue: "#4169E1",
+  magenta: "#9932CC",
+  cyan: "#008B8B",
+  white: "#D3D3D3",
+  gray: "#A9A9A9",
+  redBright: "#FF4500",
+  greenBright: "#ADFF2F",
+  yellowBright: "#FFFF00",
+  blueBright: "#87CEEB",
+  magentaBright: "#FF00FF",
+  cyanBright: "#00FFFF",
+  whiteBright: "#FFFFFF",
+  bgBlack: "#000000",
+  bgRed: "#B22222",
+  bgGreen: "#32CD32",
+  bgYellow: "#DAA520",
+  bgBlue: "#4169E1",
+  bgMagenta: "#9932CC",
+  bgCyan: "#008B8B",
+  bgWhite: "#D3D3D3",
+  bgGray: "#A9A9A9",
+  bgRedBright: "#FF0000",
+  bgGreenBright: "#ADFF2F",
+  bgYellowBright: "#FFFF00",
+  bgBlueBright: "#87CEEB",
+  bgMagentaBright: "#FF00FF",
+  bgCyanBright: "#00FFFF",
+  bgWhiteBright: "#FFFFFF",
+};
 
 export const svgFromAnsi = (
   ansi,
@@ -13,17 +49,15 @@ export const svgFromAnsi = (
     paddingLeft = 0,
     paddingBottom = 0,
     paddingRight = 0,
-    // Assume we would like a Retina-ready image
-    scale = 1,
 
-    foregroundColor,
-    backgroundColor,
+    globalBackgroundColor = "#000000",
+    globalForegroundColor = "#D3D3D3",
+    colors = colorsDefault,
   } = {},
 ) => {
-  let svg = ``;
   const { rows, columns, chunks } = parseAnsi(ansi);
-
   const font = {
+    size: fontSize,
     width: 8.4013671875,
     height: 14,
     face: fontFace,
@@ -32,31 +66,36 @@ export const svgFromAnsi = (
     emHeightAscent: 10.5546875,
     emHeightDescent: 3.4453125,
   };
-
   const textAreaWidth = columns * font.width;
   const textAreaHeight = rows * (font.lineHeight + 1) + font.emHeightDescent;
-  const width = paddingLeft + textAreaWidth + paddingRight;
-  const height = paddingTop + textAreaHeight + paddingBottom;
+  const globalWidth = paddingLeft + textAreaWidth + paddingRight;
+  const globalHeight = paddingTop + textAreaHeight + paddingBottom;
   const offsetTop = paddingTop + font.lineHeight - font.emHeightDescent;
   const offsetLeft = paddingLeft;
 
-  svg += openSvgTag("svg", {
+  const svg = startGeneratingSvg();
+  svg.setAttributes({
     "xmlns": "http://www.w3.org/2000/svg",
     "font-family": font.family,
     "font-size": font.size,
-    "viewBox": `0, 0, ${round(width)}, ${round(height)}`,
-    backgroundColor,
+    "viewBox": `0, 0, ${globalWidth}, ${globalHeight}`,
+    "backgroundColor": globalBackgroundColor,
   });
-  svg += "\n  ";
-  svg += openSvgTag("g", { fill: foregroundColor });
-  svg += "\n    ";
-  svg += selfClosingSvgTag("rect", {
+  const g = svg.createElement("g");
+  g.setAttributes({
+    fill: globalForegroundColor,
+  });
+  svg.appendChild(g);
+
+  const rect = svg.createElement("rect");
+  rect.setAttributes({
     x: 0,
     y: 0,
-    width,
-    height,
-    color: backgroundColor,
+    width: globalWidth,
+    height: globalHeight,
+    color: globalBackgroundColor,
   });
+  g.appendChild(rect);
 
   for (const chunk of chunks) {
     const { type, value, position, style } = chunk;
@@ -70,7 +109,6 @@ export const svgFromAnsi = (
     const fontStyle = {};
     const attrs = {};
     let opacity = 1;
-
     if (style.bold) {
       attrs["font-weight"] = "bold";
     }
@@ -80,10 +118,10 @@ export const svgFromAnsi = (
     if (style.dim) {
       opacity = 0.5;
     }
-
     if (style.backgroundColor) {
-      const backgroundColor = opts.colors[style.backgroundColor];
-      svg += selfClosingSvgTag("rect", {
+      const backgroundColor = colors[style.backgroundColor];
+      const rect = svg.createElement("rect");
+      rect.setAttributes({
         x,
         y: y - font.lineHeight + font.emHeightDescent,
         width: w,
@@ -91,36 +129,39 @@ export const svgFromAnsi = (
         color: backgroundColor,
         ...(opacity ? { opacity } : {}),
       });
+      g.appendChild(rect);
     }
 
-    let foregroundColorScoped;
+    let foregroundColor;
     if (style.foregroundColor) {
-      foregroundColorScoped = opts.colors[style.foregroundColor];
+      foregroundColor = colors[style.foregroundColor];
       attrs["fill"] = foregroundColor;
     }
 
     // Underline & Strikethrough:
     // Some SVG implmentations do not support underline and
     // strikethrough for <text> elements (see Sketch 49.2)
-
     if (style.underline) {
       const yOffset = font.height * 0.14;
       const ys = y - -yOffset;
       const xw = x + w;
-      svg += selfClosingSvgTag("path", {
+      const path = svg.createElement("path");
+      path.setAttributes({
         d: `M${x},${ys} L${xw},${ys} Z`,
-        color: foregroundColorScoped || foregroundColor,
+        color: foregroundColor || globalForegroundColor,
       });
+      g.appendChild(path);
     }
-
     if (style.strikethrough) {
       const yOffset = font.height * 0.3;
       const ys = y - yOffset;
       const xw = x + w;
-      svg += selfClosingSvgTag("path", {
+      const path = svg.createElement("path");
+      path.setAttributes({
         d: `M${x},${ys} L${xw},${ys} Z`,
-        color: foregroundColorScoped || foregroundColor,
+        color: foregroundColor || globalForegroundColor,
       });
+      g.appendChild(path);
     }
 
     // Do not output elements containing whitespace with no style
@@ -131,49 +172,17 @@ export const svgFromAnsi = (
       continue;
     }
 
-    svg += openSvgTag("text", {
+    const text = svg.createElement("text");
+    text.setAttributes({
       x,
       y,
       fontStyle,
     });
-    const entified = he.encode(value, { decimal: false });
-    svg += entified;
-    svg += "\n</text>";
+    text.setContent(value);
+    g.appendChild(text);
   }
 
-  svg += `</g></svg>`;
-
-  return svg;
-};
-
-const openSvgTag = (name, attrs = {}) => {
-  let openTagString = "";
-
-  openTagString += `<${name}`;
-  write_attributes: {
-    const attributeNames = Object.keys(attrs);
-    if (attributeNames.length) {
-      openTagString += " ";
-      for (const attributeName of attributeNames) {
-        let attributeValue = attrs[attributeName];
-        if (
-          attributeName === "width" ||
-          attributeName === "height" ||
-          attributeName === "x" ||
-          attributeName === "y"
-        ) {
-          attributeValue = round(attributeValue);
-        }
-        openTagString += `${attributeName}=${attributeValue}`;
-      }
-    }
-  }
-  openTagString += ">";
-  return openTagString;
-};
-
-const selfClosingSvgTag = (name, attrs) => {
-  return `${openSvgTag(name, attrs).slice(1)}/>`;
+  return svg.renderAsString();
 };
 
 // Some SVG Implementations drop whitespaces
@@ -181,12 +190,4 @@ const selfClosingSvgTag = (name, attrs) => {
 const adjustXforWhitespace = (text, x) => {
   const leadingSpace = text.match(/^\s*/g);
   return x + leadingSpace[0].length;
-};
-
-// Round: Make number values smaller in output
-// Eg: 14.23734 becomes 14.24
-// Credit @Chris Martin: https://stackoverflow.com/a/43012696/2816869
-const round = (x) => {
-  const rounded = Number(`${Math.round(`${x}e2`)}e-2`);
-  return rounded;
 };
---
+++ packages/independent/terminal-to-svg/src/svg_generator.js
@@ -0,0 +1,128 @@
+import he from "he";
+
+export const startGeneratingSvg = () => {
+  const createElement = (name) => {
+    const isSelfClosing = selfClosingTags.includes(name);
+    const canReceiveChild = name !== "text" && !isSelfClosing;
+    const canReceiveContent = name === "text" || name === "style";
+
+    const attributes = {};
+    const children = [];
+    const setAttributes = (namedValues) => {
+      Object.assign(attributes, namedValues);
+    };
+
+    const node = {
+      name,
+      content: "",
+      children,
+      attributes,
+      isSelfClosing,
+      setAttributes,
+      createElement,
+      appendChild: (childNode) => {
+        if (!canReceiveChild) {
+          throw new Error(`cannot appendChild into ${name}`);
+        }
+        children.push(childNode);
+      },
+      setContent: (value) => {
+        if (!canReceiveContent) {
+          throw new Error(`cannot setContent on ${name}`);
+        }
+        node.content = value;
+      },
+      renderAsString: () => {
+        const renderNode = (node, { depth }) => {
+          let nodeString = "";
+          nodeString += `<${node.name}`;
+
+          write_attributes: {
+            const attributeNames = Object.keys(node.attributes);
+            if (attributeNames.length) {
+              nodeString += " ";
+              for (const attributeName of attributeNames) {
+                let attributeValue = node.attributes[attributeName];
+                if (
+                  attributeName === "width" ||
+                  attributeName === "height" ||
+                  attributeName === "x" ||
+                  attributeName === "y"
+                ) {
+                  attributeValue = round(attributeValue);
+                }
+                nodeString += `${attributeName}="${attributeValue}"`;
+              }
+            }
+          }
+
+          if (node.isSelfClosing) {
+            nodeString += `/>`;
+            return nodeString;
+          }
+
+          nodeString += `>`;
+          if (node.content) {
+            if (node.name !== "text") {
+              nodeString += "\n  ";
+              nodeString += "  ".repeat(depth);
+            }
+            const contentEncoded = he.encode(node.content, { decimal: false });
+            nodeString += contentEncoded;
+            if (node.name !== "text") {
+              nodeString += "\n";
+              nodeString += "  ".repeat(depth);
+            }
+          }
+          write_children: {
+            if (node.children.length > 0) {
+              nodeString += "\n  ";
+              nodeString += "  ".repeat(depth);
+              for (const child of node.children) {
+                nodeString += renderNode(child, {
+                  depth: depth + 1,
+                });
+              }
+              nodeString += "\n";
+              nodeString += "  ".repeat(depth);
+            }
+          }
+          nodeString += `</${name}>`;
+          return nodeString;
+        };
+
+        return renderNode(node, {
+          depth: 0,
+        });
+      },
+    };
+
+    return node;
+  };
+
+  return createElement("svg");
+};
+
+const selfClosingTags = ["path", "rect"];
+
+// Round: Make number values smaller in output
+// Eg: 14.23734 becomes 14.24
+// Credit @Chris Martin: https://stackoverflow.com/a/43012696/2816869
+const round = (x) => {
+  const rounded = Number(`${Math.round(`${x}e2`)}e-2`);
+  return rounded;
+};
+
+// const svg = startGeneratingSvg();
+
+// svg.setAttributes({
+//   width: 200,
+// });
+// const g = svg.createElement("g");
+// g.setAttributes({
+//   fill: "red",
+// });
+// svg.appendChild(g);
+
+// const string = svg.renderAsString();
+// console.log(string);
