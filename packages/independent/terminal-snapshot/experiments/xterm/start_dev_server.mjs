845829580c894b85a69fb75e5a6d1448e20d66fb
dmail <dmaillard06@gmail.com>
2024-01-03 15:05:51 +0100

    prepare xterm.html

--- packages/independent/terminal-snapshot/experiments/xterm/start_dev_server.mjs
+++ packages/independent/terminal-snapshot/experiments/xterm/start_dev_server.mjs
@@ -1,5 +1,9 @@
 import { startDevServer } from "@jsenv/core";
+import { requestCertificate } from "@jsenv/https-local";
+
+const { certificate, privateKey } = requestCertificate();
 
 await startDevServer({
   sourceDirectoryUrl: new URL("./", import.meta.url),
+  https: { certificate, privateKey },
 });
