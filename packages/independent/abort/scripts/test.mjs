bba5d87da586ed74d325e3143f12866651b2abcd
dmail <dmaillard06@gmail.com>
2024-01-05 09:30:21 +0100

    update tests

--- packages/independent/abort/scripts/test.mjs
+++ packages/independent/abort/scripts/test.mjs
@@ -22,8 +22,5 @@ await executeTestPlan({
       },
     },
   },
-  failFast: process.argv.includes("--workspace"),
-  logShortForCompletedExecutions: true,
-  logMergeForCompletedExecutions: process.argv.includes("--workspace"),
-  githubCheckEnabled: false,
+  githubCheck: false,
 });
