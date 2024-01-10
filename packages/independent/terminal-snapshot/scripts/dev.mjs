5c45b7c2033855dfacbf4c7cd32f884816e3d1cf
dmail <dmaillard06@gmail.com>
2024-01-10 10:44:42 +0100

    move xterm experiment

---
+++ packages/independent/terminal-snapshot/scripts/dev.mjs
@@ -0,0 +1,13 @@
+import { startDevServer } from "@jsenv/core";
+import { requestCertificate } from "@jsenv/https-local";
+
+const { certificate, privateKey } = requestCertificate();
+
+const server = await startDevServer({
+  logLevel: "info",
+  port: 3467,
+  https: { certificate, privateKey },
+  sourceDirectoryUrl: new URL("../src/", import.meta.url),
+});
+
+console.log(`open ${server.origin}/video_recording/xterm_experiment.html`);
