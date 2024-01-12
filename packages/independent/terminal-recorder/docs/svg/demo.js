d0f89bfa738c262a5a5c2b319e45351055e8f11b
dmail <dmaillard06@gmail.com>
2024-01-12 11:23:23 +0100

    update svg demo

--- packages/independent/terminal-recorder/docs/svg/demo.js
+++ packages/independent/terminal-recorder/docs/svg/demo.js
@@ -1,10 +1,14 @@
 import { writeFileSync } from "node:fs";
-import { renderTerminalSvg } from "@jsenv/terminal-recorder";
+import { startTerminalRecording } from "@jsenv/terminal-recorder";
 
-const terminalSvg = await renderTerminalSvg(
-  `[31mred [39m[33myellow [39m[32mgreen [39m[36mcyan [39m[34mblue [39m[35mmagenta[39m`,
-  {
+const terminalRecorder = await startTerminalRecording({
+  svg: {
     title: "Terminal",
   },
+});
+await terminalRecorder.write(
+  `[31mred [39m[33myellow [39m[32mgreen [39m[36mcyan [39m[34mblue [39m[35mmagenta[39m`,
 );
-writeFileSync(new URL("./terminal.svg", import.meta.url), terminalSvg);
+const result = await terminalRecorder.stop();
+const svg = await result.svg();
+writeFileSync(new URL("./terminal.svg", import.meta.url), svg);
