1ef33dccea4599f13a9c8cb6febc5dea0b4dadc7
dmail <dmaillard06@gmail.com>
2024-01-19 10:38:33 +0100

    update log snapshots

--- packages/related/test/src/execution/reporters/reporter_list.js
+++ packages/related/test/src/execution/reporters/reporter_list.js
@@ -242,6 +242,8 @@ ${testPlanResult.patterns.join("\n")}`;
     fileFoundLine += `${numberOfFiles} files matching "testPlan" in ${directory}`;
   }
 
+  // TODO: an option to log how many cpu, memory etc we'll use?
+
   lines.push(fileFoundLine);
 
   return `${renderBigSection({
@@ -629,7 +631,10 @@ export const renderOutro = (testPlanResult, logOptions = {}) => {
     lines.push(memoryUsageLine);
   }
 
-  return `\n${renderBigSection({ title: "summary", content: lines.join("\n") })}\n`;
+  return `\n${renderBigSection({
+    title: "execution end",
+    content: lines.join("\n"),
+  })}\n`;
 };
 
 const humanizeTiming = (value) => {
