3b889a79a6dd847b269785fa8710f0a547a29fd8
dmail <dmaillard06@gmail.com>
2024-03-13 11:44:52 +0100

    restore array tests

--- packages/independent/assert/tests/array.test.js
+++ packages/independent/assert/tests/array.test.js
@@ -32,58 +32,58 @@ await startSnapshotTesting("array", {
       expected: [,],
     });
   },
-  // ["object expected, array received"]: () => {
-  //   assert({
-  //     actual: [],
-  //     expected: {},
-  //   });
-  // },
-  // ["false should be an array"]: () => {
-  //   assert({
-  //     actual: false,
-  //     expected: [],
-  //   });
-  // },
-  // ["associative array expected, object received"]: () => {
-  //   const array = [];
-  //   array.foo = true;
-  //   assert({
-  //     actual: array,
-  //     expected: {
-  //       foo: true,
-  //     },
-  //   });
-  // },
-  // ["diff on associate array.foo and object.foo"]: () => {
-  //   const array = [];
-  //   array.foo = true;
-  //   assert({
-  //     actual: array,
-  //     expected: {
-  //       foo: false,
-  //     },
-  //   });
-  // },
-  // ["diff on associate array deep property and object deep property"]: () => {
-  //   const array = [];
-  //   array.user = {
-  //     name: "bob",
-  //   };
-  //   assert({
-  //     actual: array,
-  //     expected: {
-  //       user: {
-  //         name: "alice",
-  //       },
-  //     },
-  //   });
-  // },
-  // ["array expected, object received"]: () => {
-  //   assert({
-  //     actual: {},
-  //     expected: [],
-  //   });
-  // },
+  ["object expected, array received"]: () => {
+    assert({
+      actual: [],
+      expected: {},
+    });
+  },
+  ["false should be an array"]: () => {
+    assert({
+      actual: false,
+      expected: [],
+    });
+  },
+  ["associative array expected, object received"]: () => {
+    const array = [];
+    array.foo = true;
+    assert({
+      actual: array,
+      expected: {
+        foo: true,
+      },
+    });
+  },
+  ["diff on associate array.foo and object.foo"]: () => {
+    const array = [];
+    array.foo = true;
+    assert({
+      actual: array,
+      expected: {
+        foo: false,
+      },
+    });
+  },
+  ["diff on associate array deep property and object deep property"]: () => {
+    const array = [];
+    array.user = {
+      name: "bob",
+    };
+    assert({
+      actual: array,
+      expected: {
+        user: {
+          name: "alice",
+        },
+      },
+    });
+  },
+  ["array expected, object received"]: () => {
+    assert({
+      actual: {},
+      expected: [],
+    });
+  },
   // TODO:
   // - diff on associate when array contains indexed values (now it's just on empty array)
   // and ensure the array length does not prevent the property diff to be displayed
--- packages/independent/assert/tests/snapshots/array.md
+++ packages/independent/assert/tests/snapshots/array.md
@@ -45,3 +45,85 @@ assert({
 
 ![img](<./array/empty removed.svg>)
 
+# object expected, array received
+
+```js
+assert({
+  actual: [],
+  expected: {},
+});
+```
+
+![img](<./array/object expected, array received.svg>)
+
+# false should be an array
+
+```js
+assert({
+  actual: false,
+  expected: [],
+});
+```
+
+![img](<./array/false should be an array.svg>)
+
+# associative array expected, object received
+
+```js
+const array = [];
+array.foo = true;
+assert({
+  actual: array,
+  expected: {
+    foo: true,
+  },
+});
+```
+
+![img](<./array/associative array expected, object received.svg>)
+
+# diff on associate array.foo and object.foo
+
+```js
+const array = [];
+array.foo = true;
+assert({
+  actual: array,
+  expected: {
+    foo: false,
+  },
+});
+```
+
+![img](<./array/diff on associate array.foo and object.foo.svg>)
+
+# diff on associate array deep property and object deep property
+
+```js
+const array = [];
+array.user = {
+  name: "bob",
+};
+assert({
+  actual: array,
+  expected: {
+    user: {
+      name: "alice",
+    },
+  },
+});
+```
+
+![img](<./array/diff on associate array deep property and object deep property.svg>)
+
+# array expected, object received
+
+```js
+assert({
+  actual: {},
+  expected: [],
+});
+```
+
+![img](<./array/array expected, object received.svg>)
+
