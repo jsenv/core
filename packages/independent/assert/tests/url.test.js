4a73b693ab449ad93f72d5ed8afe071f08d7fd35
dmail <dmaillard06@gmail.com>
2024-03-15 16:03:16 +0100

    stuff

--- packages/independent/assert/tests/url.test.js
+++ packages/independent/assert/tests/url.test.js
@@ -5,46 +5,46 @@ import { createAssert } from "../src/assert.js";
 const assert = createAssert();
 
 await startSnapshotTesting("url", {
-  // ["url port"]: () => {
-  //   assert({
-  //     actual: new URL("http://example.com"),
-  //     expected: new URL("http://example.com:8000"),
-  //   });
-  // },
-  // ["url and url string"]: () => {
-  //   assert({
-  //     actual: new URL("http://example.com"),
-  //     expected: "http://example.com:8000",
-  //   });
-  // },
+  ["url port"]: () => {
+    assert({
+      actual: new URL("http://example.com"),
+      expected: new URL("http://example.com:8000"),
+    });
+  },
+  ["url and url string"]: () => {
+    assert({
+      actual: new URL("http://example.com"),
+      expected: "http://example.com:8000",
+    });
+  },
   ["url string and url"]: () => {
     assert({
       actual: "http://example.com",
       expected: new URL("http://example.com:8000"),
     });
   },
-  // ["url string and url string"]: () => {
-  //   assert({
-  //     actual: "http://example.com",
-  //     expected: "http://example.com:8000",
-  //   });
-  // },
-  // ["url and non url string"]: () => {
-  //   assert({
-  //     actual: new URL("http://example.com"),
-  //     expected: "totoabcexample.com",
-  //   });
-  // },
-  // ["non url string and url"]: () => {
-  //   assert({
-  //     actual: "totoabcexample.com",
-  //     expected: new URL("http://example.com"),
-  //   });
-  // },
-  // ["url and boolean"]: () => {
-  //   assert({
-  //     actual: new URL("http://example.com"),
-  //     expected: true,
-  //   });
-  // },
+  ["url string and url string"]: () => {
+    assert({
+      actual: "http://example.com",
+      expected: "http://example.com:8000",
+    });
+  },
+  ["url and non url string"]: () => {
+    assert({
+      actual: new URL("http://example.com"),
+      expected: "totoabcexample.com",
+    });
+  },
+  ["non url string and url"]: () => {
+    assert({
+      actual: "totoabcexample.com",
+      expected: new URL("http://example.com"),
+    });
+  },
+  ["url and boolean"]: () => {
+    assert({
+      actual: new URL("http://example.com"),
+      expected: true,
+    });
+  },
 });
