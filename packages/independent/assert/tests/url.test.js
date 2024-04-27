ca129c8ca28dfce65a961636369b6c1a855a7286
dmail <dmaillard06@gmail.com>
2024-04-27 12:19:54 +0200

    working on url

---
+++ packages/independent/assert/tests/url.test.js
@@ -0,0 +1,49 @@
+import { startSnapshotTesting } from "./start_snapshot_testing.js";
+
+import { createAssert } from "../src/assert.js";
+
+const assert = createAssert();
+await startSnapshotTesting("url", {
+  ["url port"]: () => {
+    assert({
+      actual: new URL("http://example.com"),
+      expect: new URL("http://example.com:8000"),
+    });
+  },
+  // ["url and url string"]: () => {
+  //   assert({
+  //     actual: new URL("http://example.com"),
+  //     expected: "http://example.com:8000",
+  //   });
+  // },
+  // ["url string and url"]: () => {
+  //   assert({
+  //     actual: "http://example.com",
+  //     expected: new URL("http://example.com:8000"),
+  //   });
+  // },
+  // ["url string and url string"]: () => {
+  //   assert({
+  //     actual: "http://example.com",
+  //     expected: "http://example.com:8000",
+  //   });
+  // },
+  // ["url and non url string"]: () => {
+  //   assert({
+  //     actual: new URL("http://example.com"),
+  //     expected: "totoabcexample.com",
+  //   });
+  // },
+  // ["non url string and url"]: () => {
+  //   assert({
+  //     actual: "totoabcexample.com",
+  //     expected: new URL("http://example.com"),
+  //   });
+  // },
+  // ["url and boolean"]: () => {
+  //   assert({
+  //     actual: new URL("http://example.com"),
+  //     expected: true,
+  //   });
+  // },
+});
