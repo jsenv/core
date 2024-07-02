754027f43baf3a193198ca417872452c1cd4cd5e
dmail <dmaillard06@gmail.com>
2024-07-02 21:31:37 +0200

    try to debug ubuntu failure

--- packages/related/test/tests/test_plan_execution/logs/node_client/error_in_test_jsenv_assert.spec.js
+++ packages/related/test/tests/test_plan_execution/logs/node_client/error_in_test_jsenv_assert.spec.js
@@ -1,4 +1,10 @@
 import { assert } from "@jsenv/assert";
+import { ANSI } from "@jsenv/humanize";
+
+console.log({
+  env: process.env.GITHUB_WORKFLOW,
+  supported: ANSI.supported,
+});
 
 assert({
   actual: true,
