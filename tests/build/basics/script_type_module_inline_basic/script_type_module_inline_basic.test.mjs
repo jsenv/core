4cd11353a6d072aaa11d45eea7c12f9c4489e20f
dmail <dmaillard06@gmail.com>
2024-07-31 11:13:39 +0200

    test migrate

--- tests/build/basics/script_type_module_inline_basic/script_type_module_inline_basic.test.mjs
+++ tests/build/basics/script_type_module_inline_basic/script_type_module_inline_basic.test.mjs
@@ -1,28 +1,24 @@
-import { takeDirectorySnapshot } from "@jsenv/snapshot";
-
 import { build } from "@jsenv/core";
+import { snapshotBuildTests } from "@jsenv/core/tests/snapshot_build_side_effects.js";
 
-const test = async ({ name, ...params }) => {
-  const snapshotDirectoryUrl = new URL(`./snapshots/${name}/`, import.meta.url);
-  const buildDirectorySnapshot = takeDirectorySnapshot(snapshotDirectoryUrl);
-  await build({
-    logLevel: "warn",
-    sourceDirectoryUrl: new URL("./client/", import.meta.url),
-    buildDirectoryUrl: snapshotDirectoryUrl,
-    entryPoints: {
-      "./main.html": "main.html",
-    },
-    outDirectoryUrl: new URL("./.jsenv/", import.meta.url),
-    ...params,
-  });
-  buildDirectorySnapshot.compare();
-};
+await snapshotBuildTests(
+  ({ test }) => {
+    const testParams = {
+      sourceDirectoryUrl: new URL("./client/", import.meta.url),
+      buildDirectoryUrl: new URL("./build/", import.meta.url),
+      entryPoints: { "./main.html": "main.html" },
+      outDirectoryUrl: new URL("./.jsenv/", import.meta.url),
+      bundling: false,
+      minification: false,
+      versioning: false,
+    };
 
-// cannot use <script type="module">
-await test({
-  name: "0_js_module_fallback",
-  runtimeCompat: { chrome: "60" },
-  bundling: false,
-  minification: false,
-  versioning: false,
-});
+    // cannot use <script type="module">
+    test("0_js_module_fallback", () =>
+      build({
+        ...testParams,
+        runtimeCompat: { chrome: "60" },
+      }));
+  },
+  new URL("./output/script_type_module_inline_basic.md", import.meta.url),
+);
