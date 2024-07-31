1d45becb7ad3b29f7c55f3a46645898e4334840d
dmail <dmaillard06@gmail.com>
2024-07-31 14:31:28 +0200

    test

--- tests/build/new_url_directory_2/new_url_directory_2.test.mjs
+++ tests/build/new_url_directory_2/new_url_directory_2.test.mjs
@@ -1,33 +1,23 @@
 import { assert } from "@jsenv/assert";
-import { takeDirectorySnapshot } from "@jsenv/snapshot";
-
 import { build } from "@jsenv/core";
+import { snapshotBuildTests } from "@jsenv/core/tests/snapshot_build_side_effects.js";
 
-const test = async (params) => {
-  const snapshotDirectoryUrl = new URL(`./snapshots/`, import.meta.url);
-  const buildDirectorySnapshot = takeDirectorySnapshot(snapshotDirectoryUrl);
-  await build({
-    logLevel: "warn",
-    sourceDirectoryUrl: new URL("./client/", import.meta.url),
-    buildDirectoryUrl: snapshotDirectoryUrl,
-    entryPoints: {
-      "./main.js": "main.js",
-    },
-    outDirectoryUrl: new URL("./.jsenv/", import.meta.url),
-    ...params,
-  });
-  buildDirectorySnapshot.compare();
+await snapshotBuildTests(
+  ({ test }) => {
+    test("0_copy", () =>
+      build({
+        sourceDirectoryUrl: new URL("./client/", import.meta.url),
+        buildDirectoryUrl: new URL("./build/", import.meta.url),
+        entryPoints: { "./main.js": "main.js" },
+        runtimeCompat: { node: "19" },
+        directoryReferenceEffect: "copy",
+      }));
+  },
+  new URL("./output/new_url_directory_2.md", import.meta.url),
+);
 
-  // eslint-disable-next-line import/no-unresolved
-  const { directoryUrl } = await import("./snapshots/main.js");
-  const actual = directoryUrl;
-  const expect = new URL("./snapshots/src/", import.meta.url).href;
-  assert({ actual, expect });
-};
-
-await test({
-  directoryReferenceEffect: "copy",
-  runtimeCompat: { node: "19" },
-  bundling: false,
-  minification: false,
-});
+// eslint-disable-next-line import/no-unresolved
+const { directoryUrl } = await import("./output/0_copy/build/main.js");
+const actual = directoryUrl;
+const expect = new URL("./output/0_copy/build/src/", import.meta.url).href;
+assert({ actual, expect });
---
+++ tests/build/new_url_directory_2/output/0_copy/build/main.js
@@ -0,0 +1,3 @@
+const directoryUrl = new URL("./src/", import.meta.url).href;
+
+export { directoryUrl };
