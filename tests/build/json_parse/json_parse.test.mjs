01b2c9a118c9124ff4ba3562e9218d8dfcb46061
dmail <dmaillard06@gmail.com>
2024-07-31 12:51:52 +0200

    migrate json parse

--- tests/build/json_parse/json_parse.test.mjs
+++ tests/build/json_parse/json_parse.test.mjs
@@ -1,54 +1,36 @@
 import { assert } from "@jsenv/assert";
-import { takeDirectorySnapshot } from "@jsenv/snapshot";
-
 import { build } from "@jsenv/core";
-import { executeInBrowser } from "@jsenv/core/tests/execute_in_browser.js";
-import { startFileServer } from "@jsenv/core/tests/start_file_server.js";
+import { executeBuildHtmlInBrowser } from "@jsenv/core/tests/execute_build_html_in_browser.js";
+import { snapshotBuildTests } from "@jsenv/core/tests/snapshot_build_side_effects.js";
 
-const test = async () => {
-  const snapshotDirectoryUrl = new URL(`./snapshots/`, import.meta.url);
-  const buildDirectorySnapshot = takeDirectorySnapshot(snapshotDirectoryUrl);
-  const { buildInlineContents } = await build({
-    logLevel: "warn",
-    sourceDirectoryUrl: new URL("./client/", import.meta.url),
-    buildDirectoryUrl: snapshotDirectoryUrl,
-    entryPoints: {
-      "./main.html": "main.html",
-    },
-    bundling: false,
-    minification: true,
-    versioning: false,
-    runtimeCompat: { chrome: "89" },
-  });
-  buildDirectorySnapshot.compare();
+await snapshotBuildTests(
+  ({ test }) => {
+    test("0_basic", () =>
+      build({
+        sourceDirectoryUrl: new URL("./client/", import.meta.url),
+        buildDirectoryUrl: new URL("./build/", import.meta.url),
+        entryPoints: { "./main.html": "main.html" },
+        bundling: false,
+        minification: true,
+        versioning: false,
+        runtimeCompat: { chrome: "89" },
+      }));
+  },
+  new URL("./output/json_parse.md", import.meta.url),
+);
 
-  const server = await startFileServer({
-    rootDirectoryUrl: snapshotDirectoryUrl,
-  });
-  const { returnValue } = await executeInBrowser({
-    url: `${server.origin}/main.html`,
-    /* eslint-disable no-undef */
+const actual = await executeBuildHtmlInBrowser(
+  new URL("./output/0_basic/build/", import.meta.url),
+  "main.html",
+  {
     pageFunction: async (jsRelativeUrl) => {
       const namespace = await import(jsRelativeUrl);
       return { ...namespace };
     },
-    /* eslint-enable no-undef */
-    pageArguments: ["./js/main.js"],
-  });
-  const actual = {
-    returnValue,
-    buildInlineContents,
-  };
-  const expect = {
-    returnValue: {
-      data: { answer: 42 },
-    },
-    buildInlineContents: {
-      // this is to assert JSON string does not contain whitespaces
-      "js/main.js@L1C31-L3C2.json": '{"answer":42}',
-    },
-  };
-  assert({ actual, expect });
+    pageFunctionArg: "./js/main.js",
+  },
+);
+const expect = {
+  data: { answer: 42 },
 };
-
-await test();
+assert({ actual, expect });
--- tests/execute_build_html_in_browser.js
+++ tests/execute_build_html_in_browser.js
@@ -4,15 +4,20 @@ import { startFileServer } from "@jsenv/core/tests/start_file_server.js";
 export const executeBuildHtmlInBrowser = async (
   rootDirectoryUrl,
   htmlFileRelativeUrl = "main.html",
+  {
+    /* eslint-disable no-undef */
+    pageFunction = () => window.resultPromise,
+    /* eslint-enable no-undef */
+    pageFunctionArg = undefined,
+  } = {},
 ) => {
   const server = await startFileServer({
     rootDirectoryUrl,
   });
   const { returnValue } = await executeInBrowser({
     url: `${server.origin}/${htmlFileRelativeUrl}`,
-    /* eslint-disable no-undef */
-    pageFunction: () => window.resultPromise,
-    /* eslint-enable no-undef */
+    pageFunction,
+    pageArguments: [pageFunctionArg],
   });
   return returnValue;
 };
