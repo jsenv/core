c08c17d1553c3c61e39a9279463acb8bc339dc82
dmail <dmaillard06@gmail.com>
2024-08-01 17:11:18 +0200

    update test

--- packages/related/test/tests/test_plan_execution/coverage_istanbul/coverage_istanbul.test.mjs
+++ packages/related/test/tests/test_plan_execution/coverage_istanbul/coverage_istanbul.test.mjs
@@ -1,7 +1,12 @@
 import { startDevServer } from "@jsenv/core";
-import { takeDirectorySnapshot } from "@jsenv/snapshot";
-
-import { chromium, executeTestPlan, firefox, webkit } from "@jsenv/test";
+import {
+  chromium,
+  executeTestPlan,
+  firefox,
+  reportCoverageAsHtml,
+  webkit,
+} from "@jsenv/test";
+import { snapshotTestPlanSideEffects } from "@jsenv/test/tests/snapshot_execution_side_effects.js";
 import { takeCoverageSnapshots } from "../take_coverage_snapshots.js";
 
 if (process.platform === "win32") {
@@ -9,52 +14,59 @@ if (process.platform === "win32") {
   process.exit();
 }
 
-const devServer = await startDevServer({
-  logLevel: "warn",
-  sourceDirectoryUrl: new URL("./", import.meta.url),
-  keepProcessAlive: false,
-  port: 0,
-  outDirectoryUrl: new URL("./.jsenv/", import.meta.url),
-  clientAutoreload: false,
-  ribbon: false,
-});
-const testPlanResult = await executeTestPlan({
-  logs: {
-    level: "warn",
-  },
-  rootDirectoryUrl: new URL("./", import.meta.url),
-  webServer: {
-    origin: devServer.origin,
-  },
-  testPlan: {
-    "./client/main.test.html": {
-      chromium: {
-        runtime: chromium(),
-      },
-      firefox: {
-        runtime: firefox({
-          disableOnWindowsBecauseFlaky: true,
-        }),
-      },
-      webkit: {
-        runtime: webkit(),
-      },
-    },
-  },
-  // keepRunning: true,
-  coverage: {
-    include: {
-      "./client/file.js": true,
-    },
-    methodForBrowsers: "istanbul",
-  },
-  githubCheck: false,
-});
+await snapshotTestPlanSideEffects(import.meta.url, ({ test }) => {
+  test("0_basic", async () => {
+    const devServer = await startDevServer({
+      logLevel: "warn",
+      sourceDirectoryUrl: new URL("./", import.meta.url),
+      keepProcessAlive: false,
+      port: 0,
+      outDirectoryUrl: new URL("./.jsenv/", import.meta.url),
+      clientAutoreload: false,
+      ribbon: false,
+    });
 
-const snapshotDirectoryUrl = new URL(`./output/snapshots/`, import.meta.url);
-const directorySnapshot = takeDirectorySnapshot(snapshotDirectoryUrl);
-await takeCoverageSnapshots(testPlanResult, {
-  testOutputDirectoryUrl: new URL("./output/", import.meta.url),
-  fileRelativeUrls: ["file.js"],
+    const testPlanResult = await executeTestPlan({
+      logs: {
+        level: "warn",
+      },
+      rootDirectoryUrl: new URL("./", import.meta.url),
+      webServer: {
+        origin: devServer.origin,
+      },
+      testPlan: {
+        "./client/main.test.html": {
+          chromium: {
+            runtime: chromium(),
+          },
+          firefox: {
+            runtime: firefox({
+              disableOnWindowsBecauseFlaky: true,
+            }),
+          },
+          webkit: {
+            runtime: webkit(),
+          },
+        },
+      },
+      coverage: {
+        include: {
+          "./client/file.js": true,
+        },
+        methodForBrowsers: "istanbul",
+      },
+    });
+
+    reportCoverageAsHtml(
+      testPlanResult,
+      new URL("./.coverage/", import.meta.url),
+    );
+    await takeCoverageSnapshots(
+      new URL("./.coverage/", import.meta.url),
+      ["file.js"],
+      {
+        screenshotDirectoryUrl: new URL("./", import.meta.url),
+      },
+    );
+  });
 });
-directorySnapshot.compare();
