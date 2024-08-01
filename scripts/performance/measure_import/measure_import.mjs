7f076d9fcb3a69a83e10b1cc47634bf2f34744f3
dmail <dmaillard06@gmail.com>
2024-08-01 09:37:01 +0200

    add open/stat operations to perf measures

--- scripts/performance/measure_import/measure_import.mjs
+++ scripts/performance/measure_import/measure_import.mjs
@@ -6,11 +6,13 @@ const measures = startMeasures({
   filesystem: true,
 });
 await import("../../../dist/jsenv_core.js?v=1");
-const { duration, memoryHeapTotal, memoryHeapUsed, fsRead } = measures.stop();
+const { duration, memoryHeapTotal, memoryHeapUsed, fsOpenCall, fsStatCall } =
+  measures.stop();
 
 export const importMetrics = {
   "import duration": { value: duration, unit: "ms" },
   "import memory heap used": { value: memoryHeapUsed, unit: "byte" },
   "import memory heap total": { value: memoryHeapTotal, unit: "byte" },
-  "import fs read operations": { value: fsRead },
+  "import fs open operations": { value: fsOpenCall },
+  "import fs stat operations": { value: fsStatCall },
 };
