60c2b59840dd9c81c1541c91cccba250c607b1c9
dmail <dmaillard06@gmail.com>
2024-08-01 10:51:49 +0200

    migrate test

--- tests/dev_and_build/import_type_json/import_type_json_build.test.mjs
+++ tests/dev_and_build/import_type_json/import_type_json_build.test.mjs
@@ -1,64 +1,66 @@
 import { assert } from "@jsenv/assert";
-import { takeDirectorySnapshot } from "@jsenv/snapshot";
-
 import { build } from "@jsenv/core";
-import { executeInBrowser } from "@jsenv/core/tests/execute_in_browser.js";
-import { startFileServer } from "@jsenv/core/tests/start_file_server.js";
+import { executeBuildHtmlInBrowser } from "@jsenv/core/tests/execute_build_html_in_browser.js";
+import { snapshotBuildTests } from "@jsenv/core/tests/snapshot_build_side_effects.js";
 
-const test = async ({ name, ...params }) => {
-  const snapshotDirectoryUrl = new URL(`./snapshots/${name}/`, import.meta.url);
-  const buildDirectorySnapshot = takeDirectorySnapshot(snapshotDirectoryUrl);
-  await build({
-    logLevel: "warn",
+const { dirUrlMap } = await snapshotBuildTests(import.meta.url, ({ test }) => {
+  const testParams = {
     sourceDirectoryUrl: new URL("./client/", import.meta.url),
-    buildDirectoryUrl: snapshotDirectoryUrl,
-    entryPoints: {
-      "./main.html": "main.html",
-    },
-    outDirectoryUrl: new URL("./.jsenv/", import.meta.url),
-    ...params,
-  });
-  buildDirectorySnapshot.compare();
-
-  const server = await startFileServer({
-    rootDirectoryUrl: snapshotDirectoryUrl,
-  });
-  const { returnValue } = await executeInBrowser({
-    url: `${server.origin}/main.html`,
-    /* eslint-disable no-undef */
-    pageFunction: () => window.resultPromise,
-    /* eslint-enable no-undef */
-  });
-  const actual = returnValue;
-  const expect = {
-    data: { answer: 42 },
+    buildDirectoryUrl: new URL("./build/", import.meta.url),
+    entryPoints: { "./main.html": "main.html" },
+    minification: false,
   };
-  assert({ actual, expect });
-};
 
-// support for <script type="module">
-await test({
-  name: "0_js_module",
-  runtimeCompat: { chrome: "89" },
-  minification: false,
+  test("0_js_module", () =>
+    build({
+      ...testParams,
+      runtimeCompat: { chrome: "89" },
+    }));
+  test("1_js_module_no_bundling", () =>
+    build({
+      ...testParams,
+      runtimeCompat: { chrome: "89" },
+      bundling: false,
+    }));
+  test("2_js_module_fallback", () =>
+    build({
+      ...testParams,
+      runtimeCompat: { chrome: "60" },
+    }));
+  test("3_js_module_fallback_no_bundling", () =>
+    build({
+      ...testParams,
+      runtimeCompat: { chrome: "60" },
+      bundling: false,
+    }));
 });
-// support for <script type="module"> + no bundling
-await test({
-  name: "1_js_module_no_bundling",
-  runtimeCompat: { chrome: "89" },
-  bundling: false,
-  minification: false,
-});
-// no support <script type="module">
-await test({
-  name: "2_js_module_fallback",
-  runtimeCompat: { chrome: "60" },
-  minification: false,
-});
-// no support <script type="module"> and no bundling
-await test({
-  name: "3_js_module_fallback_no_bundling",
-  runtimeCompat: { chrome: "60" },
-  bundling: false,
-  minification: false,
-});
+
+const actual = {
+  jsModule: await executeBuildHtmlInBrowser(
+    `${dirUrlMap.get("0_js_module")}build/`,
+  ),
+  jsModuleNoBundling: await executeBuildHtmlInBrowser(
+    `${dirUrlMap.get("1_js_module_no_bundling")}build/`,
+  ),
+  jsModuleFallback: await executeBuildHtmlInBrowser(
+    `${dirUrlMap.get("2_js_module_fallback")}build/`,
+  ),
+  jsModuleFallbackNoBundling: await executeBuildHtmlInBrowser(
+    `${dirUrlMap.get("3_js_module_fallback_no_bundling")}build/`,
+  ),
+};
+const expect = {
+  jsModule: {
+    data: { answer: 42 },
+  },
+  jsModuleNoBundling: {
+    data: { answer: 42 },
+  },
+  jsModuleFallback: {
+    data: { answer: 42 },
+  },
+  jsModuleFallbackNoBundling: {
+    data: { answer: 42 },
+  },
+};
+assert({ actual, expect });
