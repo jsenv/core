23532dd6c2ca50e11096009abf2911244d96aab8
dmail <dmaillard06@gmail.com>
2024-08-01 12:42:32 +0200

    an other test update

---
+++ packages/related/test/tests/execution/performance/performance_browsers.test.mjs
@@ -0,0 +1,43 @@
+import { startDevServer } from "@jsenv/core";
+import { chromium, execute, firefox, webkit } from "@jsenv/test";
+import { snapshotFileExecutionSideEffects } from "@jsenv/test/tests/snapshot_execution_side_effects.js";
+
+if (process.platform === "win32") {
+  process.exit(0);
+}
+
+await snapshotFileExecutionSideEffects(import.meta.url, async ({ test }) => {
+  const run = async ({ runtime }) => {
+    const devServer = await startDevServer({
+      logLevel: "off",
+      sourceDirectoryUrl: new URL("./client/", import.meta.url),
+      keepProcessAlive: false,
+      port: 0,
+    });
+    const { performance } = await execute({
+      runtime,
+      rootDirectoryUrl: new URL("./client/", import.meta.url),
+      webServer: {
+        origin: devServer.origin,
+        rootDirectoryUrl: new URL("./client/", import.meta.url),
+      },
+      fileRelativeUrl: `./main.html`,
+      collectPerformance: true,
+    });
+    devServer.stop();
+    return { performance };
+  };
+
+  test("0_chromium", () =>
+    run({
+      runtime: chromium(),
+    }));
+  test("1_firefox", () =>
+    run({
+      runtime: firefox({ disableOnWindowsBecauseFlaky: false }),
+    }));
+  test("2_webkit", () =>
+    run({
+      runtime: webkit(),
+    }));
+});
