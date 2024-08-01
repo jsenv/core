9535327256214018b16e9441622361a1fffe3e65
dmail <dmaillard06@gmail.com>
2024-08-01 11:04:54 +0200

    migrate a test

--- tests/dev_and_build/top_level_await/top_level_await_build.test.mjs
+++ tests/dev_and_build/top_level_await/top_level_await_build.test.mjs
@@ -1,51 +1,38 @@
 import { assert } from "@jsenv/assert";
-import { takeDirectorySnapshot } from "@jsenv/snapshot";
-
 import { build } from "@jsenv/core";
-import { executeInBrowser } from "@jsenv/core/tests/execute_in_browser.js";
-import { startFileServer } from "@jsenv/core/tests/start_file_server.js";
+import { executeBuildHtmlInBrowser } from "@jsenv/core/tests/execute_build_html_in_browser.js";
+import { snapshotBuildTests } from "@jsenv/core/tests/snapshot_build_side_effects.js";
 
-const test = async ({ name, ...params }) => {
-  const snapshotDirectoryUrl = new URL(`./snapshots/${name}/`, import.meta.url);
-  const buildDirectorySnapshot = takeDirectorySnapshot(snapshotDirectoryUrl);
-  await build({
-    logLevel: "warn",
+const { dirUrlMap } = await snapshotBuildTests(import.meta.url, ({ test }) => {
+  const testParams = {
     sourceDirectoryUrl: new URL("./client/", import.meta.url),
-    buildDirectoryUrl: snapshotDirectoryUrl,
-    entryPoints: {
-      "./main.html": "main.html",
-    },
-    outDirectoryUrl: new URL("./.jsenv/", import.meta.url),
-    ...params,
-  });
-  buildDirectorySnapshot.compare();
+    buildDirectoryUrl: new URL("./build/", import.meta.url),
+    entryPoints: { "./main.html": "main.html" },
+    bundling: false,
+    minification: false,
+  };
+  test("0_top_level_await", () =>
+    build({
+      ...testParams,
+      runtimeCompat: { chrome: "89" },
+    }));
+  test("1_top_level_await_fallback", () =>
+    build({
+      ...testParams,
+      runtimeCompat: { chrome: "55" },
+    }));
+});
 
-  const server = await startFileServer({
-    rootDirectoryUrl: snapshotDirectoryUrl,
-  });
-  const { returnValue } = await executeInBrowser({
-    url: `${server.origin}/main.html`,
-    /* eslint-disable no-undef */
-    pageFunction: () => window.resultPromise,
-    /* eslint-enable no-undef */
-  });
-
-  const actual = returnValue;
-  const expect = { answer: 42 };
-  assert({ actual, expect });
+const actual = {
+  topLevelAwait: await executeBuildHtmlInBrowser(
+    `${dirUrlMap.get("0_top_level_await")}build/`,
+  ),
+  topLevelAwaitFallback: await executeBuildHtmlInBrowser(
+    `${dirUrlMap.get("1_top_level_await_fallback")}build/`,
+  ),
 };
-
-// support
-await test({
-  name: "0_supported",
-  runtimeCompat: { chrome: "89" },
-  bundling: false,
-  minification: false,
-});
-// no support for top level await
-await test({
-  name: "1_not_supported",
-  runtimeCompat: { chrome: "55" },
-  bundling: false,
-  minification: false,
-});
+const expect = {
+  topLevelAwait: { answer: 42 },
+  topLevelAwaitFallback: { answer: 42 },
+};
+assert({ actual, expect });
--- tests/dev_and_build/top_level_await_2/top_level_await_2_build.test.mjs
+++ tests/dev_and_build/top_level_await_2/top_level_await_2_build.test.mjs
@@ -1,77 +1,60 @@
 import { assert } from "@jsenv/assert";
-import { takeDirectorySnapshot } from "@jsenv/snapshot";
-
 import { build } from "@jsenv/core";
-import { executeInBrowser } from "@jsenv/core/tests/execute_in_browser.js";
-import { startFileServer } from "@jsenv/core/tests/start_file_server.js";
+import { executeBuildHtmlInBrowser } from "@jsenv/core/tests/execute_build_html_in_browser.js";
+import { snapshotBuildTests } from "@jsenv/core/tests/snapshot_build_side_effects.js";
 
-const test = async ({ name, ...params }) => {
-  const snapshotDirectoryUrl = new URL(`./snapshots/${name}/`, import.meta.url);
-  const buildDirectorySnapshot = takeDirectorySnapshot(snapshotDirectoryUrl);
-  await build({
-    logLevel: "warn",
+const { dirUrlMap } = await snapshotBuildTests(import.meta.url, ({ test }) => {
+  const testParams = {
     sourceDirectoryUrl: new URL("./client/", import.meta.url),
-    buildDirectoryUrl: snapshotDirectoryUrl,
-    entryPoints: {
-      "./main.html": "main.html",
-    },
-    outDirectoryUrl: new URL("./.jsenv/", import.meta.url),
-    ...params,
-  });
-  buildDirectorySnapshot.compare();
+    buildDirectoryUrl: new URL("./build/", import.meta.url),
+    entryPoints: { "./main.html": "main.html" },
+    bundling: false,
+    minification: false,
+  };
+  test("0_top_level_await", () =>
+    build({
+      ...testParams,
+      runtimeCompat: { chrome: "89" },
+    }));
+  test("1_top_level_await_fallback", () =>
+    build({
+      ...testParams,
+      runtimeCompat: { chrome: "55" },
+    }));
+  // support for <script type="module"> but not TLA
+  // Considering that TLA + export on old runtimes is not recommended:
+  // - TLA should be reserved to entry points where exports are not needed)
+  // - It would be overkill to use systemjs only because code uses TLA + export
+  // -> Jsenv throw an error when TLA + exports is used and systemjs is not
+  // (ideally jsenv would throw a custom error explaining all this)
+  test("2_top_level_await_throw", () =>
+    build({
+      ...testParams,
+      runtimeCompat: { chrome: "65" },
+      versioning: false,
+    }));
+});
 
-  const server = await startFileServer({
-    rootDirectoryUrl: snapshotDirectoryUrl,
-  });
-  const { returnValue } = await executeInBrowser({
-    url: `${server.origin}/main.html`,
-    /* eslint-disable no-undef */
-    pageFunction: () => window.resultPromise,
-    /* eslint-enable no-undef */
-  });
-  const actual = returnValue;
-  const expect = [
+const actual = {
+  topLevelAwait: await executeBuildHtmlInBrowser(
+    `${dirUrlMap.get("0_top_level_await")}build/`,
+  ),
+  topLevelAwaitFallback: await executeBuildHtmlInBrowser(
+    `${dirUrlMap.get("1_top_level_await_fallback")}build/`,
+  ),
+};
+const expect = {
+  topLevelAwait: [
     "a_before_timeout",
     "a_after_timeout",
     "before_import_a",
     "after_import_a",
-  ];
-  assert({ actual, expect });
+  ],
+  topLevelAwaitFallback: [
+    "a_before_timeout",
+    "a_after_timeout",
+    "before_import_a",
+    "after_import_a",
+  ],
 };
-
-// support for top level await and <script type="module">
-await test({
-  name: "0_supported",
-  runtimeCompat: { chrome: "89" },
-  bundling: false,
-  minification: false,
-});
-// no support for <script type="module">
-await test({
-  name: "1_not_supported",
-  runtimeCompat: { chrome: "55" },
-  bundling: false,
-  minification: false,
-});
-
-// support for <script type="module"> but not TLA
-// Considering that TLA + export on old runtimes is not recommended:
-// - TLA should be reserved to entry points where exports are not needed)
-// - It would be overkill to use systemjs only because code uses TLA + export
-// -> Jsenv throw an error when TLA + exports is used and systemjs is not
-// (ideally jsenv would throw a custom error explaining all this)
-try {
-  await test({
-    runtimeCompat: { chrome: "65" },
-    bundling: false,
-    minification: false,
-    versioning: false,
-  });
-  throw new Error("should throw");
-} catch (e) {
-  const actual = e.message.includes(
-    'Cannot export after a top-level await when using topLevelAwait: "simple"!',
-  );
-  const expect = true;
-  assert({ actual, expect });
-}
+assert({ actual, expect });
