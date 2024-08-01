e5cb0b2efa0234fe558a82a91e2921193f351ebe
dmail <dmaillard06@gmail.com>
2024-08-01 12:26:50 +0200

    update a test

--- packages/related/test/tests/execution/timings/timings_node.test.mjs
+++ packages/related/test/tests/execution/timings/timings_node.test.mjs
@@ -2,60 +2,52 @@ import { assert } from "@jsenv/assert";
 
 import { execute, nodeChildProcess, nodeWorkerThread } from "@jsenv/test";
 
-const test = async (params) => {
-  const { status, timings } = await execute({
+const run = async ({ runtime }) => {
+  const { timings } = await execute({
     // logLevel: "debug",
     rootDirectoryUrl: new URL("./node_client/", import.meta.url),
     fileRelativeUrl: `./main.js`,
-    ...params,
+    runtime,
   });
-  {
-    const actual = {
-      status,
-      timings,
-    };
-    const expect = {
-      status: "completed",
-      timings: {
-        origin: assert.any(Number),
-        start: assert.any(Number),
-        runtimeStart: assert.any(Number),
-        executionStart: assert.any(Number),
-        executionEnd: assert.any(Number),
-        runtimeEnd: assert.any(Number),
-        end: assert.any(Number),
-      },
-    };
-    assert({ actual, expect });
-  }
-  {
-    const runDuration = timings.end - timings.start;
-    const executionDuration = timings.executionEnd - timings.executionStart;
-    const runtimeDuration = timings.runtimeEnd - timings.runtimeStart;
-    const timeBetweenRuntimeStartAndExecutionStart =
-      timings.executionStart - timings.runtimeStart;
-    const actual = {
-      executionDuration,
-      runDuration,
-      runtimeDuration,
-      timeBetweenRuntimeStartAndExecutionStart,
-    };
-    const expect = {
-      // execution must take around 2s (due to the timeout)
-      executionDuration: assert.between(2_000, 3_000),
-      // the overall run duration and runtime alive duration is between 2/5s
-      runDuration: assert.between(2_000, 5_000),
-      runtimeDuration: assert.between(2_000, 5_000),
-      // it does not take more than 500ms to start the file
-      timeBetweenRuntimeStartAndExecutionStart: assert.between(0, 500),
-    };
-    assert({ actual, expect });
-  }
+  const runDuration = timings.end - timings.start;
+  const executionDuration = timings.executionEnd - timings.executionStart;
+  const runtimeDuration = timings.runtimeEnd - timings.runtimeStart;
+  const timeBetweenRuntimeStartAndExecutionStart =
+    timings.executionStart - timings.runtimeStart;
+  return {
+    runDuration,
+    executionDuration,
+    runtimeDuration,
+    timeBetweenRuntimeStartAndExecutionStart,
+  };
 };
 
-await test({
-  runtime: nodeChildProcess(),
-});
-await test({
-  runtime: nodeWorkerThread(),
-});
+const actual = {
+  nodeWorkerThread: await run({
+    runtime: nodeChildProcess(),
+  }),
+  nodeChildProcess: await run({
+    runtime: nodeWorkerThread(),
+  }),
+};
+const expect = {
+  nodeWorkerThread: {
+    // execution must take around 2s (due to the timeout)
+    executionDuration: assert.between(2_000, 3_000),
+    // the overall run duration and runtime alive duration is between 2/5s
+    runDuration: assert.between(2_000, 5_000),
+    runtimeDuration: assert.between(2_000, 5_000),
+    // it does not take more than 500ms to start the file
+    timeBetweenRuntimeStartAndExecutionStart: assert.between(0, 500),
+  },
+  nodeChildProcess: {
+    // execution must take around 2s (due to the timeout)
+    executionDuration: assert.between(2_000, 3_000),
+    // the overall run duration and runtime alive duration is between 2/5s
+    runDuration: assert.between(2_000, 5_000),
+    runtimeDuration: assert.between(2_000, 5_000),
+    // it does not take more than 500ms to start the file
+    timeBetweenRuntimeStartAndExecutionStart: assert.between(0, 500),
+  },
+};
+assert({ actual, expect });
