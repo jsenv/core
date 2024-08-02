139d4d041638c2954fb25eaefc0e4d9c3f3cfbc6
dmail <dmaillard06@gmail.com>
2024-08-02 14:05:42 +0200

    ensure side effect files properly cleaned and compare

--- packages/independent/snapshot/tests/snapshot_tests_out_clear/snapshot_tests_out_clear.test.mjs
+++ packages/independent/snapshot/tests/snapshot_tests_out_clear/snapshot_tests_out_clear.test.mjs
@@ -5,35 +5,52 @@ import { snapshotTests } from "@jsenv/snapshot";
 await snapshotTests(
   import.meta.url,
   ({ test }) => {
-    test("0_basic", () => {
-      writeFileSync(new URL("./toto.txt", import.meta.url));
+    test("0_first", () => {
+      writeFileSync(new URL("./first.txt", import.meta.url));
+    });
+    test("1_second", () => {
+      writeFileSync(new URL("./second.txt", import.meta.url));
     });
   },
   {
+    throwWhenDiff: false,
     filesystemEffects: {
       textualFilesIntoDirectory: true,
     },
   },
 );
+{
+  const actual = readFileStructureSync(
+    new URL("./side_effects/snapshot_tests_out_clear/", import.meta.url),
+  );
+  const expect = {
+    "0_first/first.txt": "",
+    "1_second/second.txt": "",
+  };
+  assert({ actual, expect });
+}
+
 // now re-run it but it will write tata.txt
-const { dirUrlMap } = await snapshotTests(
+await snapshotTests(
   import.meta.url,
   ({ test }) => {
-    test("0_basic", () => {
+    test("0_first", () => {
       writeFileSync(new URL("./tata.txt", import.meta.url));
     });
   },
   {
+    throwWhenDiff: false,
     filesystemEffects: {
       textualFilesIntoDirectory: true,
     },
   },
 );
-
-// - it should not have toto.txt
-const testDirUrl = dirUrlMap.get("0_basic");
-const actual = readFileStructureSync(`${testDirUrl}`);
-const expect = {
-  "tata.txt": "",
-};
-assert({ actual, expect });
+{
+  const actual = readFileStructureSync(
+    new URL("./side_effects/snapshot_tests_out_clear/", import.meta.url),
+  );
+  const expect = {
+    "0_first/tata.txt": "",
+  };
+  assert({ actual, expect });
+}
