e032fed48dfe42856445e0cfbf0df0a52e73cf0b
dmail <dmaillard06@gmail.com>
2024-08-05 14:07:56 +0200

    update a test

--- tests/dev_and_build/resource_hints/modulepreload/modulepreload_build.test.mjs
+++ tests/dev_and_build/resource_hints/modulepreload/modulepreload_build.test.mjs
@@ -1,63 +1,33 @@
-import { assert } from "@jsenv/assert";
-import { takeDirectorySnapshot } from "@jsenv/snapshot";
-
 import { build, startBuildServer } from "@jsenv/core";
-import { executeInBrowser } from "@jsenv/core/tests/execute_in_browser.js";
+import { executeHtml } from "@jsenv/core/tests/execute_html.js";
+import { snapshotBuildTests } from "@jsenv/core/tests/snapshot_build_side_effects.js";
 
-const test = async ({ name, ...params }) => {
-  const snapshotDirectoryUrl = new URL(`./snapshots/${name}/`, import.meta.url);
-  const buildDirectorySnapshot = takeDirectorySnapshot(snapshotDirectoryUrl);
+const run = async ({ runtimeCompat, versioningMethod }) => {
   await build({
-    logLevel: "warn",
     sourceDirectoryUrl: new URL("./client/", import.meta.url),
-    buildDirectoryUrl: snapshotDirectoryUrl,
-    entryPoints: {
-      "./main.html": "main.html",
-    },
-    outDirectoryUrl: new URL("./.jsenv/", import.meta.url),
-    ...params,
+    buildDirectoryUrl: new URL("./build/", import.meta.url),
+    entryPoints: { "./main.html": "main.html" },
+    bundling: false,
+    minification: false,
+    runtimeCompat,
+    versioningMethod,
   });
-  buildDirectorySnapshot.compare();
-
-  const server = await startBuildServer({
-    logLevel: "warn",
+  const buildServer = await startBuildServer({
+    buildDirectoryUrl: new URL("./build/", import.meta.url),
     keepProcessAlive: false,
-    buildDirectoryUrl: snapshotDirectoryUrl,
     port: 0,
   });
-  const { returnValue, consoleOutput } = await executeInBrowser(
-    `${server.origin}/main.html`,
-    {
-      collectConsole: true,
-    },
-  );
-  const actual = {
-    returnValue,
-    consoleLogs: consoleOutput.logs,
-    consoleWarnings: consoleOutput.warnings,
-  };
-  const expect = {
-    returnValue: 42,
-    consoleLogs: [],
-    consoleWarnings: [],
-  };
-  assert({ actual, expect });
+  return executeHtml(`${buildServer.origin}/main.html`);
 };
 
-// support for <script type="module">
-await test({
-  name: "0_js_module",
-  runtimeCompat: { chrome: "89" },
-  bundling: false,
-  minification: false,
-  // versioningMethod: "filename",
-});
-
-// no support for <script type="module">
-await test({
-  name: "1_js_module_fallback",
-  runtimeCompat: { chrome: "60" },
-  bundling: false,
-  minification: false,
-  versioningMethod: "filename",
+await snapshotBuildTests(import.meta.url, ({ test }) => {
+  test("0_js_module", () =>
+    run({
+      runtimeCompat: { chrome: "89" },
+    }));
+  test("1_js_module_fallback", () =>
+    run({
+      runtimeCompat: { chrome: "60" },
+      versioningMethod: "filename",
+    }));
 });
