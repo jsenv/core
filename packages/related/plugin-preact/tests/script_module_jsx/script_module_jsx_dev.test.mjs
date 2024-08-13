f890c8f21efa5ef06991951f7e5bf109f06fb2bc
dmail <dmaillard06@gmail.com>
2024-08-13 08:54:33 +0200

    update a test

--- packages/related/plugin-preact/tests/script_module_jsx/script_module_jsx_dev.test.mjs
+++ packages/related/plugin-preact/tests/script_module_jsx/script_module_jsx_dev.test.mjs
@@ -1,17 +1,21 @@
-import { assert } from "@jsenv/assert";
 import { startDevServer } from "@jsenv/core";
-import { executeInBrowser } from "@jsenv/core/tests/execute_in_browser.js";
-
+import { executeHtml } from "@jsenv/core/tests/execute_html.js";
+import { snapshotDevSideEffects } from "@jsenv/core/tests/snapshot_dev_side_effects.js";
 import { jsenvPluginPreact } from "@jsenv/plugin-preact";
+import { chromium } from "playwright";
 
-const devServer = await startDevServer({
-  logLevel: "warn",
-  sourceDirectoryUrl: new URL("./client/", import.meta.url),
-  keepProcessAlive: false,
-  plugins: [jsenvPluginPreact()],
-  port: 0,
+const run = async ({ browserLauncher }) => {
+  const devServer = await startDevServer({
+    sourceDirectoryUrl: new URL("./client/", import.meta.url),
+    keepProcessAlive: false,
+    port: 0,
+    plugins: [jsenvPluginPreact()],
+  });
+  return executeHtml(`${devServer.origin}/main.html`, {
+    browserLauncher,
+  });
+};
+
+await snapshotDevSideEffects(import.meta.url, ({ test }) => {
+  test("0_chromium", () => run({ browserLauncher: chromium }));
 });
-const { returnValue } = await executeInBrowser(`${devServer.origin}/main.html`);
-const actual = returnValue;
-const expect = "Hello world";
-assert({ actual, expect });
