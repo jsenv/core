feec9905bd6d01e25673a194d997f69137e61525
dmail <dmaillard06@gmail.com>
2024-08-19 18:23:01 +0200

    update a test

--- tests/build/new_url_directory_2/new_url_directory_2.test.mjs
+++ tests/build/new_url_directory_2/new_url_directory_2.test.mjs
@@ -1,24 +1,20 @@
-import { assert } from "@jsenv/assert";
 import { build } from "@jsenv/core";
 import { snapshotBuildTests } from "@jsenv/core/tests/snapshot_build_side_effects.js";
 
-const run = () => {
-  return build({
+const run = async () => {
+  await build({
     sourceDirectoryUrl: new URL("./client/", import.meta.url),
     buildDirectoryUrl: new URL("./build/", import.meta.url),
     entryPoints: { "./main.js": "main.js" },
     runtimeCompat: { node: "19" },
     directoryReferenceEffect: "copy",
   });
+  const { directoryUrl } = await import(
+    new URL("./build/main.js", import.meta.url)
+  );
+  return directoryUrl;
 };
 
-const { dirUrlMap } = await snapshotBuildTests(import.meta.url, ({ test }) => {
+await snapshotBuildTests(import.meta.url, ({ test }) => {
   test("0_copy", () => run());
 });
-
-const { directoryUrl } = await import(
-  `${dirUrlMap.get("0_copy")}build/main.js`
-);
-const actual = directoryUrl;
-const expect = `${dirUrlMap.get("0_copy")}build/src/`;
-assert({ actual, expect });
