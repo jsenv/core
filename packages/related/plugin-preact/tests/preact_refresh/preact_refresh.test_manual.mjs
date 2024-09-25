196394476f640ab6b6a0f4543670abcc93dfc4f3
dmail <dmaillard06@gmail.com>
2024-09-25 10:14:20 +0200

    fix a manual test

--- packages/related/plugin-preact/tests/preact_refresh/preact_refresh.test_manual.mjs
+++ packages/related/plugin-preact/tests/preact_refresh/preact_refresh.test_manual.mjs
@@ -4,7 +4,7 @@ import { jsenvPluginPreact } from "@jsenv/plugin-preact";
 
 startDevServer({
   port: 5678,
-  rootDirectoryUrl: new URL("./git_ignored/", import.meta.url),
+  sourceDirectoryUrl: new URL("./git_ignored/", import.meta.url),
   plugins: [
     jsenvPluginExplorer({
       groups: {
@@ -18,7 +18,7 @@ startDevServer({
     }),
   ],
   sourcemaps: "file",
-  clientFiles: {
+  sourceFilesConfig: {
     "./**": true,
   },
 });
