8c43653efaa336da8ae552a2102e1528566abb50
Damien Maillard <dmaillard06@gmail.com>
2024-11-07 09:32:12 +0100

    remove useless files

--- tests/dev_server/jsenv_scripts/output/main.js
+++
@@ -1,9 +0,0 @@
-import { createImportMetaHot } from "/@fs/mock/src/plugins/import_meta_hot/client/import_meta_hot.js";
-import.meta.hot = createImportMetaHot(import.meta.url);
-import { value } from "/a.js";
-
-document.querySelector("#app").innerHTML = value;
-
-if (import.meta.hot) {
-  import.meta.hot.accept();
-}
