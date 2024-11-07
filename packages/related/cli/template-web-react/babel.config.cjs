b331b75c1ad3349ab307c9d6a68aac247af041a5
Damien Maillard <dmaillard06@gmail.com>
2024-11-07 15:36:46 +0100

    update babel dep

--- packages/related/cli/template-web-react/babel.config.cjs
+++ packages/related/cli/template-web-react/babel.config.cjs
@@ -1,6 +1,6 @@
 module.exports = {
   plugins: [
-    "@babel/plugin-syntax-import-assertions",
+    "@babel/plugin-syntax-import-attributes",
     [
       "@babel/plugin-transform-react-jsx",
       {
