b4739850a3c4cfbf896744e0792ff88973a326bb
dmail <dmaillard06@gmail.com>
2024-11-15 10:47:26 +0100

    regen snapshots

--- tests/dev_server/jsenv_scripts/_jsenv_scripts.test.mjs/0_basic/.jsenv/chrome@130.00/main.js
+++ tests/dev_server/jsenv_scripts/_jsenv_scripts.test.mjs/0_basic/.jsenv/chrome@130.00/main.js
@@ -1,6 +1,4 @@
-import { createImportMetaHot } from "/@fs@jsenv/core/src/plugins/import_meta_hot/client/import_meta_hot.js";
-import.meta.hot = createImportMetaHot(import.meta.url);
-import { value } from "/a.js";
+import { createImportMetaHot } from "/@fs@jsenv/core/src/plugins/import_meta_hot/client/import_meta_hot.js";import.meta.hot = createImportMetaHot(import.meta.url);import { value } from "/a.js";
 
 document.querySelector("#app").innerHTML = value;
 
