807ff7494c63c7db4a440998a78d0647d11606b4
dmail <dmaillard06@gmail.com>
2024-11-24 22:38:53 +0100

    update urlToFilepath

--- packages/independent/urls/src/url_to_filesystem_path.js
+++ packages/independent/urls/src/url_to_filesystem_path.js
@@ -2,17 +2,19 @@ import { fileURLToPath } from "node:url";
 
 export const urlToFileSystemPath = (url) => {
   const urlObject = new URL(url);
-  let urlString = String(url);
-  if (urlString[urlString.length - 1] === "/") {
-    // remove trailing / so that nodejs path becomes predictable otherwise it logs
-    // the trailing slash on linux but does not on windows
-    urlString = urlString.slice(0, -1);
-  }
+  let urlString;
   if (urlObject.hash) {
     const origin =
       urlObject.protocol === "file:" ? "file://" : urlObject.origin;
     urlString = `${origin}${urlObject.pathname}${urlObject.search}%23${urlObject.hash.slice(1)}`;
+  } else {
+    urlString = urlObject.href;
   }
   const fileSystemPath = fileURLToPath(urlString);
+  if (fileSystemPath[fileSystemPath.length - 1] === "/") {
+    // remove trailing / so that nodejs path becomes predictable otherwise it logs
+    // the trailing slash on linux but does not on windows
+    return fileSystemPath.slice(0, -1);
+  }
   return fileSystemPath;
 };
