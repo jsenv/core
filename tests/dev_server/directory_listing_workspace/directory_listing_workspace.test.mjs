cd2e2362cf01e756c8d91a16a84c3adb6f65f6e6
dmail <dmaillard06@gmail.com>
2024-12-30 10:33:07 +0100

    prepare testing directory listing on workspace

---
+++ tests/dev_server/directory_listing_workspace/directory_listing_workspace.test.mjs
@@ -0,0 +1,44 @@
+import { startDevServer } from "@jsenv/core";
+import { replaceFileStructureSync, writeFileSync } from "@jsenv/filesystem";
+import { chromium } from "playwright";
+
+let debug = false;
+const sourceDirectoryUrl = new URL("./git_ignored/src/", import.meta.url);
+const snapshotsDirectoryUrl = new URL("./snapshots/", import.meta.url);
+replaceFileStructureSync({
+  from: new URL("./fixtures/0_at_start/", import.meta.url),
+  to: sourceDirectoryUrl,
+});
+
+const devServer = await startDevServer({
+  logLevel: "off",
+  serverLogLevel: "off",
+  sourceDirectoryUrl,
+  keepProcessAlive: !debug,
+  directoryListingUrlMocks: true,
+  clientAutoreload: false,
+  port: 0,
+});
+const browser = await chromium.launch({
+  headless: !debug,
+  devtools: debug,
+});
+const page = await browser.newPage({ ignoreHTTPSErrors: true });
+await page.setViewportSize({ width: 800, height: 500 }); // set a relatively small and predicatble size
+const takeScreenshot = async (scenario) => {
+  const sceenshotBuffer = await page.screenshot();
+  writeFileSync(
+    new URL(`./${scenario}.png`, snapshotsDirectoryUrl),
+    sceenshotBuffer,
+  );
+};
+
+try {
+  await page.goto(`${devServer.origin}`);
+  await takeScreenshot("0_at_start");
+} finally {
+  if (!debug) {
+    browser.close();
+    devServer.stop();
+  }
+}
