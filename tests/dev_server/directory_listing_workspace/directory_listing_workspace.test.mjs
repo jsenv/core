8c168d865d7871dbce5cadc41c4fdc83d99985c2
dmail <dmaillard06@gmail.com>
2025-01-29 07:38:02 +0100

    directory listing workspace works

--- tests/dev_server/directory_listing_workspace/directory_listing_workspace.test.mjs
+++ tests/dev_server/directory_listing_workspace/directory_listing_workspace.test.mjs
@@ -39,7 +39,9 @@ const takeScreenshot = async (scenario) => {
 try {
   await page.goto(`${devServer.origin}`);
   await takeScreenshot("0_at_start");
-  await page.locator(`a:text-matches("../packages/")`).click();
+  await page.locator(`a:text-matches("git_ignored")`).click();
+  await takeScreenshot("1_after_click_git_ignored");
+  await page.locator(`a:text-matches("packages/")`).click();
   await takeScreenshot("1_after_click_packages");
 } finally {
   if (!debug) {
