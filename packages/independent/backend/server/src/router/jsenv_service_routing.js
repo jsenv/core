19146728b3bf9640e17ed84e6d12a10c8a7bf647
dmail <dmaillard06@gmail.com>
2025-02-25 11:33:22 +0100

    implement injectResponseHeader

--- packages/independent/backend/server/src/router/jsenv_service_routing.js
+++ packages/independent/backend/server/src/router/jsenv_service_routing.js
@@ -6,15 +6,30 @@ export const jsenvServiceRouting = (routes) => {
     router.add(route);
   }
 
+  const headersToInjectMap = new Map();
+
   return {
     name: "jsenv:routing",
     handleRequest: async (request) => {
       const response = await router.match(request, {
-        injectResponseHeader: () => {
-          // TODO: how to do this?
+        injectResponseHeader: (name, value) => {
+          const headers = headersToInjectMap.get(request);
+          if (headers) {
+            headers[name] = value;
+          } else {
+            headersToInjectMap.set(request, { [name]: value });
+          }
         },
       });
+      request.signal.addEventListener("abort", () => {
+        headersToInjectMap.delete(request);
+      });
       return response;
+    },
+    injectResponseHeaders: (response, { request }) => {
+      const headers = headersToInjectMap.get(request);
+      headersToInjectMap.delete(request);
+      return headers;
     },
   };
 };
