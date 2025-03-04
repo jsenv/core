2e8bb10bdb0e0ff8c4428e660638c5ba112b56f2
dmail <dmaillard06@gmail.com>
2025-03-04 06:40:02 +0100

    update test

--- packages/independent/backend/server/tests/read_request_body/read_request_body_2.test.mjs
+++ packages/independent/backend/server/tests/read_request_body/read_request_body_2.test.mjs
@@ -1,29 +1,20 @@
 import { assert } from "@jsenv/assert";
 import { startServer } from "@jsenv/server";
-import { readRequestBody } from "@jsenv/server/src/request_body_handling.js";
-import { fetchUsingNodeBuiltin } from "@jsenv/server/tests/test_helpers.mjs";
 
-const { origin } = await startServer({
+const server = await startServer({
   keepProcessAlive: false,
   logLevel: "warn",
-  services: [
+  routes: [
     {
-      handleRequest: async (request) => {
-        const requestBody = await readRequestBody(request, { as: "string" });
-        const responseBody = `${requestBody} world`;
-        return {
-          status: 200,
-          headers: {
-            "content-length": Buffer.byteLength(responseBody),
-          },
-          body: responseBody,
-        };
+      endpoint: "POST *",
+      response: async (request) => {
+        const requestBody = await request.text();
+        return new Response(`${requestBody} world`);
       },
     },
   ],
 });
-const response = await fetchUsingNodeBuiltin(origin, { body: "Hello" });
-
-const actual = await new Response();
+const response = await fetch(server.origin, { method: "POST", body: "Hello" });
+const actual = await response.text();
 const expect = "Hello world";
 assert({ actual, expect });
