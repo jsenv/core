2cfb9e9ab5599ffcaa6603e6dfbfa0fae2d9b728
dmail <dmaillard06@gmail.com>
2025-03-04 06:11:46 +0100

    update a test

--- packages/independent/backend/server/tests/http2_push/http2_push_prevent.test.mjs
+++ packages/independent/backend/server/tests/http2_push/http2_push_prevent.test.mjs
@@ -1,76 +1,92 @@
-import { assert } from "@jsenv/assert";
-import { readFile } from "@jsenv/filesystem";
 import { requestCertificate } from "@jsenv/https-local";
+import { fetchFileSystem, startServer } from "@jsenv/server";
+import { snapshotTests } from "@jsenv/snapshot";
 import { connect } from "node:http2";
 
-import { fetchFileSystem, startServer } from "@jsenv/server";
-import { applyDnsResolution } from "@jsenv/server/src/internal/dns_resolution.js";
-
-if (process.platform !== "linux") {
+if (process.env.CI && process.platform !== "linux") {
   // certificates only generated on linux
   process.exit();
 }
 
-const localhostDns = await applyDnsResolution("localhost");
-const expectedOrigin =
-  localhostDns.address === "127.0.0.1" ? "localhost" : "127.0.0.1";
-
-const { certificate, privateKey } = requestCertificate();
-const server = await startServer({
-  logLevel: "warn",
-  https: { certificate, privateKey },
-  http2: true,
-  keepProcessAlive: false,
-  services: [
-    {
-      onResponsePush: ({ path }, { prevent }) => {
-        if (path === "/preventme") {
-          prevent();
-        }
+const run = async () => {
+  const { certificate, privateKey } = requestCertificate();
+  const server = await startServer({
+    https: { certificate, privateKey },
+    http2: true,
+    keepProcessAlive: false,
+    services: [
+      {
+        onResponsePush: ({ path }, { prevent }) => {
+          if (path === "/preventme") {
+            prevent();
+          }
+        },
       },
-      handleRequest: (request, { pushResponse }) => {
-        if (request.pathname === "/main.html") {
-          pushResponse({ path: "/preventme" });
-          pushResponse({ path: "/style.css" });
-        }
-
-        return fetchFileSystem(
-          new URL(request.resource.slice(1), import.meta.url),
-          {
-            headers: request.headers,
-            canReadDirectory: true,
-          },
-        );
+    ],
+    routes: [
+      {
+        endpoint: "GET *",
+        response: (request, { pushResponse }) => {
+          if (request.pathname === "/main.html") {
+            pushResponse({ path: "/preventme" });
+            pushResponse({ path: "/style.css" });
+          }
+          return fetchFileSystem(
+            new URL(request.resource.slice(1), import.meta.url),
+            {
+              headers: request.headers,
+              canReadDirectory: true,
+            },
+          );
+        },
       },
-    },
-  ],
-});
-
-const request = async (http2Client, path) => {
-  let responseBodyAsString = "";
-  const pushedHeaders = [];
-
-  await new Promise((resolve, reject) => {
-    http2Client.on("error", reject);
-    http2Client.on("socketError", reject);
-    http2Client.on("stream", (pushedStream, headers) => {
-      headers = { ...headers };
-      // ignore node internal symbols
-      Object.getOwnPropertySymbols(headers).forEach((symbol) => {
-        delete headers[symbol];
-      });
-      pushedHeaders.push(headers);
-    });
-    const clientStream = http2Client.request({ ":path": path });
-    clientStream.setEncoding("utf8");
-    clientStream.on("data", (chunk) => {
-      responseBodyAsString += chunk;
-    });
-    clientStream.on("end", () => {
-      resolve();
-    });
-    clientStream.end();
+    ],
   });
+
+  const request = async (http2Client, path) => {
+    let responseBodyAsString = "";
+    const pushedHeaders = [];
+
+    await new Promise((resolve, reject) => {
+      http2Client.on("error", reject);
+      http2Client.on("socketError", reject);
+      http2Client.on("stream", (pushedStream, headers) => {
+        headers = { ...headers };
+        // ignore node internal symbols
+        Object.getOwnPropertySymbols(headers).forEach((symbol) => {
+          delete headers[symbol];
+        });
+        pushedHeaders.push(headers);
+      });
+      const clientStream = http2Client.request({ ":path": path });
+      clientStream.setEncoding("utf8");
+      clientStream.on("data", (chunk) => {
+        responseBodyAsString += chunk;
+      });
+      clientStream.on("end", () => {
+        resolve();
+      });
+      clientStream.end();
+    });
+
+    return {
+      responseBodyAsString,
+      pushedHeaders,
+    };
+  };
+
+  const client1 = connect(server.origin, {
+    ca: certificate,
+    // Node.js won't trust my custom certificate
+    // We could also do this: https://github.com/nodejs/node/issues/27079
+    rejectUnauthorized: false,
+  });
+  const { responseBodyAsString, pushedHeaders } = await request(
+    client1,
+    "/main.html",
+  );
+  client1.close();
+  client1.destroy();
 
   return {
     responseBodyAsString,
@@ -78,34 +94,14 @@ const request = async (http2Client, path) => {
   };
 };
 
-const client1 = connect(server.origin, {
-  ca: certificate,
-  // Node.js won't trust my custom certificate
-  // We could also do this: https://github.com/nodejs/node/issues/27079
-  rejectUnauthorized: false,
-});
-const { responseBodyAsString, pushedHeaders } = await request(
-  client1,
-  "/main.html",
+await snapshotTests(
+  import.meta.url,
+  ({ test }) => {
+    test("0_basic", () => {
+      return run();
+    });
+  },
+  {
+    filesystemEffects: false,
+  },
 );
-client1.close();
-
-const actual = {
-  responseBodyAsString,
-  pushedHeaders,
-};
-const expect = {
-  responseBodyAsString: await readFile(
-    new URL("./main.html", import.meta.url),
-    { as: "string" },
-  ),
-  pushedHeaders: [
-    {
-      ":path": "/style.css",
-      ":method": "GET",
-      ":authority": `${expectedOrigin}:${server.port}`,
-      ":scheme": "https",
-    },
-  ],
-};
-assert({ actual, expect });
