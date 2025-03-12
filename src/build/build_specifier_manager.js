e5fe1a6e1d406cd8e0503f1b69df3cd79d72b8d5
dmail <dmaillard06@gmail.com>
2025-03-12 15:27:14 +0100

    throw on error on the case we dont' support for now

--- src/build/build_specifier_manager.js
+++ src/build/build_specifier_manager.js
@@ -96,7 +96,12 @@ export const createBuildSpecifierManager = ({
               : ownerUrlInfo,
           );
       const urlRelativeToParent = urlToRelativeUrl(buildUrl, parentBuildUrl);
-      if (urlRelativeToParent[0] === ".") {
+      if (urlRelativeToParent === "") {
+        // TODO : understand what's going on and how to support this
+        throw new Error(
+          "A file is referencing itself, this is not supported at this stage of the build",
+        );
+      } else if (urlRelativeToParent[0] === ".") {
         buildSpecifier = urlRelativeToParent;
       } else {
         // ensure "./" on relative url (otherwise it could be a "bare specifier")
@@ -749,7 +754,8 @@ export const createBuildSpecifierManager = ({
               contentBeforeReplace,
               (placeholder) => {
                 const reference = placeholderToReferenceMap.get(placeholder);
-                return generateReplacement(reference);
+                const value = generateReplacement(reference);
+                return value;
               },
             );
             urlInfo.mutateContent({ content, sourcemap });
