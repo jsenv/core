31e576fb2f676b568329ff4aa05dd9b397e3bea2
dmail <dmaillard06@gmail.com>
2025-03-13 14:28:42 +0100

    change how s.js is built

--- packages/internal/js-module-fallback/scripts/build.mjs
+++ packages/internal/js-module-fallback/scripts/build.mjs
@@ -2,8 +2,8 @@ import { build } from "@jsenv/core";
 import { jsenvPluginAsJsClassic } from "@jsenv/plugin-as-js-classic";
 
 await build({
-  sourceDirectoryUrl: new URL("../src/", import.meta.url),
-  buildDirectoryUrl: new URL("../dist/", import.meta.url),
+  sourceDirectoryUrl: import.meta.resolve("../src/"),
+  buildDirectoryUrl: import.meta.resolve("../dist/"),
   entryPoints: {
     "./main.js": "jsenv_js_module_fallback.js",
   },
@@ -14,26 +14,25 @@ await build({
     node: "16.14",
   },
   scenarioPlaceholders: false,
-});
-
-// "s.js" is used in the build files, it must be compatible as much as possible
-// so we convert async/await, arrow function, ... to be compatible with
-// old browsers
-await build({
-  sourceDirectoryUrl: new URL("../src/", import.meta.url),
-  buildDirectoryUrl: new URL("../dist/js/", import.meta.url),
-  entryPoints: {
-    "./client/s.js?as_js_classic": "s.js",
-  },
-  plugins: [jsenvPluginAsJsClassic()],
-  directoryToClean: false,
-  runtimeCompat: {
-    chrome: "0",
-    firefox: "0",
-  },
-  sourcemaps: "file",
-  sourcemapsSourcesContent: true,
-  bundling: false,
-  minification: false,
-  versioning: false,
+  subbuilds: [
+    // "s.js" is used in the build files, it must be compatible as much as possible
+    // so we convert async/await, arrow function, ... to be compatible with
+    // old browsers
+    {
+      buildDirectoryUrl: import.meta.resolve("../dist/client/"),
+      entryPoints: {
+        "./client/s.js?as_js_classic": "s.js",
+      },
+      plugins: [jsenvPluginAsJsClassic()],
+      sourcemaps: "file",
+      sourcemapsSourcesContent: true,
+      bundling: false,
+      minification: false,
+      versioning: false,
+      runtimeCompat: {
+        chrome: "0",
+        firefox: "0",
+      },
+    },
+  ],
 });
