9c87aebbc1c8d52ee0bcc280d8adfe7da7d10f7f
dmail <dmaillard06@gmail.com>
2025-03-17 14:49:16 +0100

    update a test

--- tests/build/for_node/vendor_and_dynamic_import/vendor_and_dynamic_import.test.mjs
+++ tests/build/for_node/vendor_and_dynamic_import/vendor_and_dynamic_import.test.mjs
@@ -5,11 +5,12 @@ const run = async () => {
   await build({
     sourceDirectoryUrl: new URL("./source/", import.meta.url),
     buildDirectoryUrl: new URL("./build/", import.meta.url),
-    entryPoints: { "./main.js": "main_build.js" },
-    runtimeCompat: {
-      node: "20",
+    entryPoints: {
+      "./main.js": {
+        buildRelativeUrl: "./main_build.js",
+        runtimeCompat: { node: "20" },
+      },
     },
-    // bundling: false,
   });
 };
 
