305926763053d6743d8761d211674e80f252bac8
dmail <dmaillard06@gmail.com>
2025-03-18 12:50:11 +0100

    fix chunks when there is an asset directory, ensure we can remap when rollup generated files are not in the same dir

--- packages/internal/plugin-bundling/src/js_module/bundle_js_modules.js
+++ packages/internal/plugin-bundling/src/js_module/bundle_js_modules.js
@@ -7,6 +7,7 @@ import {
   injectQueryParams,
   isFileSystemPath,
   urlToBasename,
+  urlToRelativeUrl,
 } from "@jsenv/urls";
 import { readFileSync } from "node:fs";
 import { fileUrlConverter } from "../file_url_converter.js";
@@ -36,6 +37,7 @@ export const bundleJsModules = async (
     isSupportedOnCurrentClients,
     getPluginMeta,
     kitchen,
+    assetsDirectory,
   } = jsModuleUrlInfos[0].context;
   const graph = jsModuleUrlInfos[0].graph;
   if (buildDirectoryUrl === undefined) {
@@ -79,8 +81,8 @@ export const bundleJsModules = async (
           workspaces = packageJSON.workspaces;
         }
       }
-      let nodeModuleChunkName = "node_modules";
-      let packagesChunkName = "packages";
+      let nodeModuleChunkName = `node_modules`;
+      let packagesChunkName = `packages`;
 
       if (packageName) {
         let packageNameAsFilename = packageName
@@ -90,6 +92,11 @@ export const bundleJsModules = async (
         nodeModuleChunkName = `${packageNameAsFilename}_node_modules`;
         packagesChunkName = `${packageNameAsFilename}_packages`;
       }
+      if (assetsDirectory) {
+        nodeModuleChunkName = `${assetsDirectory}${nodeModuleChunkName}`;
+        packagesChunkName = `${assetsDirectory}${packagesChunkName}`;
+      }
+
       chunks[nodeModuleChunkName] = {
         "file:///**/node_modules/": true,
         ...chunks.vendors,
@@ -487,7 +494,22 @@ const rollupPluginJsenv = ({
               continue;
             }
             const importUrl = getOriginalUrl(importRollupFileInfo);
-            const rollupSpecifier = `./${importRollupFileInfo.fileName}`;
+            const importerBuildUrl = new URL(
+              rollupFileInfo.fileName,
+              buildDirectoryUrl,
+            ).href;
+            const urlToImport = new URL(
+              importRollupFileInfo.fileName,
+              buildDirectoryUrl,
+            ).href;
+            const specifierRelative = urlToRelativeUrl(
+              urlToImport,
+              importerBuildUrl,
+            );
+            const rollupSpecifier =
+              specifierRelative[0] === "."
+                ? specifierRelative
+                : `./${specifierRelative}`;
             specifierToUrlMap.set(rollupSpecifier, importUrl);
           }
         }
