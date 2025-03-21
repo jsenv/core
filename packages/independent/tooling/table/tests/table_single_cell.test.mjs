4a1ad734dd5acf069955aa6c822bf45d2b279e64
dmail <dmaillard06@gmail.com>
2025-03-21 16:36:07 +0100

    single cell properly rendered again

--- packages/independent/tooling/table/tests/table_single_cell.test.mjs
+++ packages/independent/tooling/table/tests/table_single_cell.test.mjs
@@ -7,102 +7,102 @@ const run = (lines, options) => {
 
 await snapshotTableTests(import.meta.url, ({ test }) => {
   const scenarios = {
-    // none: {
-    //   borderTop: null,
-    //   borderLeft: null,
-    //   borderRight: null,
-    //   borderBottom: null,
-    // },
+    none: {
+      borderTop: null,
+      borderLeft: null,
+      borderRight: null,
+      borderBottom: null,
+    },
     top: {
       borderTop: {},
       borderLeft: null,
       borderRight: null,
       borderBottom: null,
     },
-    // left: {
-    //   borderTop: null,
-    //   borderLeft: {},
-    //   borderRight: null,
-    //   borderBottom: null,
-    // },
-    // right: {
-    //   borderTop: null,
-    //   borderLeft: null,
-    //   borderRight: {},
-    //   borderBottom: null,
-    // },
-    // bottom: {
-    //   borderTop: null,
-    //   borderLeft: null,
-    //   borderRight: null,
-    //   borderBottom: {},
-    // },
-    // topLeft: {
-    //   borderTop: {},
-    //   borderLeft: {},
-    //   borderRight: null,
-    //   borderBottom: null,
-    // },
-    // topRight: {
-    //   borderTop: {},
-    //   borderLeft: null,
-    //   borderRight: {},
-    //   borderBottom: null,
-    // },
-    // bottomRight: {
-    //   borderTop: null,
-    //   borderLeft: null,
-    //   borderRight: {},
-    //   borderBottom: {},
-    // },
-    // bottomLeft: {
-    //   borderTop: null,
-    //   borderLeft: {},
-    //   borderRight: null,
-    //   borderBottom: {},
-    // },
-    // leftAndRight: {
-    //   borderTop: null,
-    //   borderLeft: {},
-    //   borderRight: {},
-    //   borderBottom: null,
-    // },
-    // topAndBottom: {
-    //   borderTop: {},
-    //   borderLeft: null,
-    //   borderRight: null,
-    //   borderBottom: {},
-    // },
-    // allButTop: {
-    //   borderTop: null,
-    //   borderLeft: {},
-    //   borderRight: {},
-    //   borderBottom: {},
-    // },
-    // allButRight: {
-    //   borderTop: {},
-    //   borderLeft: {},
-    //   borderRight: null,
-    //   borderBottom: {},
-    // },
-    // allButLeft: {
-    //   borderTop: {},
-    //   borderLeft: null,
-    //   borderRight: {},
-    //   borderBottom: {},
-    // },
-    // allButBottom: {
-    //   borderTop: {},
-    //   borderLeft: {},
-    //   borderRight: {},
-    //   borderBottom: null,
-    // },
-    // all: {
-    //   borderLeft: {},
-    //   borderTop: {},
-    //   borderRight: {},
-    //   borderBottom: {},
-    // },
+    left: {
+      borderTop: null,
+      borderLeft: {},
+      borderRight: null,
+      borderBottom: null,
+    },
+    right: {
+      borderTop: null,
+      borderLeft: null,
+      borderRight: {},
+      borderBottom: null,
+    },
+    bottom: {
+      borderTop: null,
+      borderLeft: null,
+      borderRight: null,
+      borderBottom: {},
+    },
+    topLeft: {
+      borderTop: {},
+      borderLeft: {},
+      borderRight: null,
+      borderBottom: null,
+    },
+    topRight: {
+      borderTop: {},
+      borderLeft: null,
+      borderRight: {},
+      borderBottom: null,
+    },
+    bottomRight: {
+      borderTop: null,
+      borderLeft: null,
+      borderRight: {},
+      borderBottom: {},
+    },
+    bottomLeft: {
+      borderTop: null,
+      borderLeft: {},
+      borderRight: null,
+      borderBottom: {},
+    },
+    leftAndRight: {
+      borderTop: null,
+      borderLeft: {},
+      borderRight: {},
+      borderBottom: null,
+    },
+    topAndBottom: {
+      borderTop: {},
+      borderLeft: null,
+      borderRight: null,
+      borderBottom: {},
+    },
+    allButTop: {
+      borderTop: null,
+      borderLeft: {},
+      borderRight: {},
+      borderBottom: {},
+    },
+    allButRight: {
+      borderTop: {},
+      borderLeft: {},
+      borderRight: null,
+      borderBottom: {},
+    },
+    allButLeft: {
+      borderTop: {},
+      borderLeft: null,
+      borderRight: {},
+      borderBottom: {},
+    },
+    allButBottom: {
+      borderTop: {},
+      borderLeft: {},
+      borderRight: {},
+      borderBottom: null,
+    },
+    all: {
+      borderLeft: {},
+      borderTop: {},
+      borderRight: {},
+      borderBottom: {},
+    },
   };
 
   test(`0_single_cell_borders`, () => {
