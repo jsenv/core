a20eed106ae03046743aca3a9e3a27939e8bcbb6
dmail <dmaillard06@gmail.com>
2025-04-16 14:00:58 +0200

    workon eslint

--- packages/independent/shared/importmap/tests/composeTwoImportMaps.test.js
+++ packages/independent/shared/importmap/tests/composeTwoImportMaps.test.js
@@ -1,6 +1,4 @@
 import { assert } from "@jsenv/assert";
-// https://github.com/un-ts/eslint-plugin-import-x/issues/305
-// eslint-disable-next-line import-x/no-extraneous-dependencies
 import { composeTwoImportMaps, sortImportMap } from "@jsenv/importmap";
 
 {
