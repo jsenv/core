659edbc004822c492e684a2a693885fa93e2c03f
dmail <dmaillard06@gmail.com>
2025-04-28 11:10:48 +0200

    publish latest

--- dist/build/build.js
+++ dist/build/build.js
@@ -1,4 +1,4 @@
-import { parseHtml, injectHtmlNodeAsEarlyAsPossible, createHtmlNode, stringifyHtmlAst, applyBabelPlugins, generateUrlForInlineContent, parseJsWithAcorn, injectJsenvScript, visitHtmlNodes, analyzeScriptNode, getHtmlNodeText, getHtmlNodeAttribute, getHtmlNodePosition, getUrlForContentInsideHtml, setHtmlNodeAttributes, setHtmlNodeText, parseCssUrls, getHtmlNodeAttributePosition, parseSrcSet, removeHtmlNodeText, parseJsUrls, getUrlForContentInsideJs, analyzeLinkNode, findHtmlNode, removeHtmlNode, insertHtmlNodeAfter } from "@jsenv/ast";
+import { parseHtml, injectHtmlNodeAsEarlyAsPossible, createHtmlNode, stringifyHtmlAst, applyBabelPlugins, generateUrlForInlineContent, injectJsenvScript, parseJsWithAcorn, visitHtmlNodes, analyzeScriptNode, getHtmlNodeText, getHtmlNodeAttribute, getHtmlNodePosition, getUrlForContentInsideHtml, setHtmlNodeAttributes, setHtmlNodeText, parseCssUrls, getHtmlNodeAttributePosition, parseSrcSet, removeHtmlNodeText, parseJsUrls, getUrlForContentInsideJs, analyzeLinkNode, findHtmlNode, removeHtmlNode, insertHtmlNodeAfter } from "@jsenv/ast";
 import { jsenvPluginBundling } from "@jsenv/plugin-bundling";
 import { jsenvPluginMinification } from "@jsenv/plugin-minification";
 import { jsenvPluginTranspilation, jsenvPluginJsModuleFallback } from "@jsenv/plugin-transpilation";
@@ -2123,6 +2123,176 @@ ${urlInfo.url}`,
   return urlInfo;
 };
 
+const injectionSymbol = Symbol.for("jsenv_injection");
+const INJECTIONS = {
+  global: (value) => {
+    return { [injectionSymbol]: "global", value };
+  },
+  optional: (value) => {
+    return { [injectionSymbol]: "optional", value };
+  },
+};
+
+const isPlaceholderInjection = (value) => {
+  return (
+    !value || !value[injectionSymbol] || value[injectionSymbol] !== "global"
+  );
+};
+
+const applyContentInjections = (content, contentInjections, urlInfo) => {
+  const keys = Object.keys(contentInjections);
+  const globals = {};
+  const placeholderReplacements = [];
+  for (const key of keys) {
+    const contentInjection = contentInjections[key];
+    if (contentInjection && contentInjection[injectionSymbol]) {
+      const valueBehindSymbol = contentInjection[injectionSymbol];
+      if (valueBehindSymbol === "global") {
+        globals[key] = contentInjection.value;
+      } else if (valueBehindSymbol === "optional") {
+        placeholderReplacements.push({
+          key,
+          isOptional: true,
+          value: contentInjection.value,
+        });
+      } else {
+        throw new Error(`unknown injection type "${valueBehindSymbol}"`);
+      }
+    } else {
+      placeholderReplacements.push({
+        key,
+        value: contentInjection,
+      });
+    }
+  }
+
+  const needGlobalsInjection = Object.keys(globals).length > 0;
+  const needPlaceholderReplacements = placeholderReplacements.length > 0;
+
+  if (needGlobalsInjection && needPlaceholderReplacements) {
+    const globalInjectionResult = injectGlobals(content, globals, urlInfo);
+    const replaceInjectionResult = injectPlaceholderReplacements(
+      globalInjectionResult.content,
+      placeholderReplacements,
+      urlInfo,
+    );
+    return {
+      content: replaceInjectionResult.content,
+      sourcemap: composeTwoSourcemaps(
+        globalInjectionResult.sourcemap,
+        replaceInjectionResult.sourcemap,
+      ),
+    };
+  }
+  if (needGlobalsInjection) {
+    return injectGlobals(content, globals, urlInfo);
+  }
+  if (needPlaceholderReplacements) {
+    return injectPlaceholderReplacements(
+      content,
+      placeholderReplacements,
+      urlInfo,
+    );
+  }
+  return null;
+};
+
+const injectPlaceholderReplacements = (
+  content,
+  placeholderReplacements,
+  urlInfo,
+) => {
+  const magicSource = createMagicSource(content);
+  for (const { key, isOptional, value } of placeholderReplacements) {
+    let index = content.indexOf(key);
+    if (index === -1) {
+      if (!isOptional) {
+        urlInfo.context.logger.warn(
+          `placeholder "${key}" not found in ${urlInfo.url}.
+--- suggestion a ---
+Add "${key}" in that file.
+--- suggestion b ---
+Fix eventual typo in "${key}"?
+--- suggestion c ---
+Mark injection as optional using INJECTIONS.optional():
+import { INJECTIONS } from "@jsenv/core";
+
+return {
+  "${key}": INJECTIONS.optional(${JSON.stringify(value)}),
+};`,
+        );
+      }
+      continue;
+    }
+
+    while (index !== -1) {
+      const start = index;
+      const end = index + key.length;
+      magicSource.replace({
+        start,
+        end,
+        replacement:
+          urlInfo.type === "js_classic" ||
+          urlInfo.type === "js_module" ||
+          urlInfo.type === "html"
+            ? JSON.stringify(value, null, "  ")
+            : value,
+      });
+      index = content.indexOf(key, end);
+    }
+  }
+  return magicSource.toContentAndSourcemap();
+};
+
+const injectGlobals = (content, globals, urlInfo) => {
+  if (urlInfo.type === "html") {
+    return globalInjectorOnHtml(content, globals, urlInfo);
+  }
+  if (urlInfo.type === "js_classic" || urlInfo.type === "js_module") {
+    return globalsInjectorOnJs(content, globals, urlInfo);
+  }
+  throw new Error(`cannot inject globals into "${urlInfo.type}"`);
+};
+const globalInjectorOnHtml = (content, globals, urlInfo) => {
+  // ideally we would inject an importmap but browser support is too low
+  // (even worse for worker/service worker)
+  // so for now we inject code into entry points
+  const htmlAst = parseHtml({
+    html: content,
+    url: urlInfo.url,
+    storeOriginalPositions: false,
+  });
+  const clientCode = generateClientCodeForGlobals(globals, {
+    isWebWorker: false,
+  });
+  injectJsenvScript(htmlAst, {
+    content: clientCode,
+    pluginName: "jsenv:inject_globals",
+  });
+  return {
+    content: stringifyHtmlAst(htmlAst),
+  };
+};
+const globalsInjectorOnJs = (content, globals, urlInfo) => {
+  const clientCode = generateClientCodeForGlobals(globals, {
+    isWebWorker:
+      urlInfo.subtype === "worker" ||
+      urlInfo.subtype === "service_worker" ||
+      urlInfo.subtype === "shared_worker",
+  });
+  const magicSource = createMagicSource(content);
+  magicSource.prepend(clientCode);
+  return magicSource.toContentAndSourcemap();
+};
+const generateClientCodeForGlobals = (globals, { isWebWorker = false }) => {
+  const globalName = isWebWorker ? "self" : "window";
+  return `Object.assign(${globalName}, ${JSON.stringify(
+    globals,
+    null,
+    "  ",
+  )});`;
+};
+
 const defineGettersOnPropertiesDerivedFromOriginalContent = (
   urlInfo,
 ) => {
@@ -2224,170 +2394,6 @@ const getContentAst = (content, type, url) => {
 
 const getContentEtag = (content) => {
   return bufferToEtag(Buffer.from(content));
-};
-
-const injectionSymbol = Symbol.for("jsenv_injection");
-const INJECTIONS = {
-  global: (value) => {
-    return { [injectionSymbol]: "global", value };
-  },
-  optional: (value) => {
-    return { [injectionSymbol]: "optional", value };
-  },
-};
-
-const applyContentInjections = (content, contentInjections, urlInfo) => {
-  const keys = Object.keys(contentInjections);
-  const globals = {};
-  const placeholderReplacements = [];
-  for (const key of keys) {
-    const contentInjection = contentInjections[key];
-    if (contentInjection && contentInjection[injectionSymbol]) {
-      const valueBehindSymbol = contentInjection[injectionSymbol];
-      if (valueBehindSymbol === "global") {
-        globals[key] = contentInjection.value;
-      } else if (valueBehindSymbol === "optional") {
-        placeholderReplacements.push({
-          key,
-          isOptional: true,
-          value: contentInjection.value,
-        });
-      } else {
-        throw new Error(`unknown injection type "${valueBehindSymbol}"`);
-      }
-    } else {
-      placeholderReplacements.push({
-        key,
-        value: contentInjection,
-      });
-    }
-  }
-
-  const needGlobalsInjection = Object.keys(globals).length > 0;
-  const needPlaceholderReplacements = placeholderReplacements.length > 0;
-
-  if (needGlobalsInjection && needPlaceholderReplacements) {
-    const globalInjectionResult = injectGlobals(content, globals, urlInfo);
-    const replaceInjectionResult = injectPlaceholderReplacements(
-      globalInjectionResult.content,
-      placeholderReplacements,
-      urlInfo,
-    );
-    return {
-      content: replaceInjectionResult.content,
-      sourcemap: composeTwoSourcemaps(
-        globalInjectionResult.sourcemap,
-        replaceInjectionResult.sourcemap,
-      ),
-    };
-  }
-  if (needGlobalsInjection) {
-    return injectGlobals(content, globals, urlInfo);
-  }
-  if (needPlaceholderReplacements) {
-    return injectPlaceholderReplacements(
-      content,
-      placeholderReplacements,
-      urlInfo,
-    );
-  }
-  return null;
-};
-
-const injectPlaceholderReplacements = (
-  content,
-  placeholderReplacements,
-  urlInfo,
-) => {
-  const magicSource = createMagicSource(content);
-  for (const { key, isOptional, value } of placeholderReplacements) {
-    let index = content.indexOf(key);
-    if (index === -1) {
-      if (!isOptional) {
-        urlInfo.context.logger.warn(
-          `placeholder "${key}" not found in ${urlInfo.url}.
---- suggestion a ---
-Add "${key}" in that file.
---- suggestion b ---
-Fix eventual typo in "${key}"?
---- suggestion c ---
-Mark injection as optional using INJECTIONS.optional():
-import { INJECTIONS } from "@jsenv/core";
-
-return {
-"${key}": INJECTIONS.optional(${JSON.stringify(value)}),
-};`,
-        );
-      }
-      continue;
-    }
-
-    while (index !== -1) {
-      const start = index;
-      const end = index + key.length;
-      magicSource.replace({
-        start,
-        end,
-        replacement:
-          urlInfo.type === "js_classic" ||
-          urlInfo.type === "js_module" ||
-          urlInfo.type === "html"
-            ? JSON.stringify(value, null, "  ")
-            : value,
-      });
-      index = content.indexOf(key, end);
-    }
-  }
-  return magicSource.toContentAndSourcemap();
-};
-
-const injectGlobals = (content, globals, urlInfo) => {
-  if (urlInfo.type === "html") {
-    return globalInjectorOnHtml(content, globals, urlInfo);
-  }
-  if (urlInfo.type === "js_classic" || urlInfo.type === "js_module") {
-    return globalsInjectorOnJs(content, globals, urlInfo);
-  }
-  throw new Error(`cannot inject globals into "${urlInfo.type}"`);
-};
-const globalInjectorOnHtml = (content, globals, urlInfo) => {
-  // ideally we would inject an importmap but browser support is too low
-  // (even worse for worker/service worker)
-  // so for now we inject code into entry points
-  const htmlAst = parseHtml({
-    html: content,
-    url: urlInfo.url,
-    storeOriginalPositions: false,
-  });
-  const clientCode = generateClientCodeForGlobals(globals, {
-    isWebWorker: false,
-  });
-  injectJsenvScript(htmlAst, {
-    content: clientCode,
-    pluginName: "jsenv:inject_globals",
-  });
-  return {
-    content: stringifyHtmlAst(htmlAst),
-  };
-};
-const globalsInjectorOnJs = (content, globals, urlInfo) => {
-  const clientCode = generateClientCodeForGlobals(globals, {
-    isWebWorker:
-      urlInfo.subtype === "worker" ||
-      urlInfo.subtype === "service_worker" ||
-      urlInfo.subtype === "shared_worker",
-  });
-  const magicSource = createMagicSource(content);
-  magicSource.prepend(clientCode);
-  return magicSource.toContentAndSourcemap();
-};
-const generateClientCodeForGlobals = (globals, { isWebWorker = false }) => {
-  const globalName = isWebWorker ? "self" : "window";
-  return `Object.assign(${globalName}, ${JSON.stringify(
-    globals,
-    null,
-    "  ",
-  )});`;
 };
 
 const createUrlInfoTransformer = ({
@@ -2937,6 +2943,7 @@ const createKitchen = ({
       inlineContentClientFileUrl,
       isSupportedOnCurrentClients: memoizeIsSupported(clientRuntimeCompat),
       isSupportedOnFutureClients: memoizeIsSupported(runtimeCompat),
+      isPlaceholderInjection,
       getPluginMeta: null,
       sourcemaps,
       outDirectoryUrl,
