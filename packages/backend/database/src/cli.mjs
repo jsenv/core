3d8a0cc5efdfb675aca6f6cbca3bf8e181d86c2e
dmail <dmaillard06@gmail.com>
2025-04-28 15:56:52 +0200

    database setup command works

--- packages/backend/database/src/cli.mjs
+++ packages/backend/database/src/cli.mjs
@@ -1,5 +1,6 @@
 #!/usr/bin/env node
 
+import { UNICODE } from "@jsenv/humanize";
 import { execSync } from "node:child_process";
 import { platform } from "node:os";
 import { parseArgs } from "node:util";
@@ -86,7 +87,114 @@ Command:
     }
     throw new Error(`Unsupported operating system: ${platformName}`);
   },
+  setup: async () => {
+    const { default: postgres } = await import("postgres");
+
+    const defaultUsername = execSync("whoami").toString().trim();
+    const host = "localhost";
+    const port = 5432;
+    const databaseUrl = `postgresql://${defaultUsername}@${host}:${port}`;
+    console.log(`${UNICODE.INFO} Connecting to ${databaseUrl}`);
+    const sql = postgres({
+      host: "localhost",
+      port: 5432,
+      database: "postgres",
+      username: defaultUsername,
+      password: "",
+    });
+    console.log(`${UNICODE.OK} Connected to database`);
+
+    // TODO: read this from secrets.json?
+    const databaseName = "jsenv";
+    const adminRole = "admin";
+    const username = "me";
+    const password = "password";
+
+    admin_role_setup: {
+      console.log(`${UNICODE.INFO} Check role "${adminRole}":`);
+      const roles = await sql`
+SELECT
+  rolname AS role_name,
+  rolcreatedb as can_create_db,
+  rolcanlogin as can_login
+FROM pg_roles
+WHERE rolname=${adminRole};`;
+      if (roles.length === 0) {
+        console.log(
+          `  ${UNICODE.INFO} Role "${adminRole}" not found, creating it...`,
+        );
+        await sql`CREATE ROLE ${sql(adminRole)} LOGIN CREATEDB;`;
+        console.log(`${UNICODE.OK} Role "${adminRole}" created`);
+      } else {
+        const [{ role_name, can_create_db, can_login }] = roles;
+        if (can_create_db && can_login) {
+          console.log(
+            `  ${UNICODE.OK} "${role_name}" role exists with the right attributes.`,
+          );
+        } else {
+          console.log(
+            `  ${UNICODE.INFO} LOGIN and CREATEDB attributes are missing on role "${adminRole}".`,
+          );
+          await sql`ALTER ROLE ${sql(adminRole)} LOGIN CREATEDB;`;
+          console.log(
+            `  ${UNICODE.OK} LOGIN and CREATEDB attributes added to role "${adminRole}".`,
+          );
+        }
+      }
+    }
+    user_setup: {
+      console.log(`${UNICODE.INFO} Check user "${username}":`);
+      const users = await sql`
+SELECT
+  usename AS user_name,
+  usecreatedb AS can_create_db
+FROM pg_user
+WHERE usename=${username};`;
+      if (users.length === 0) {
+        console.log(`  ${UNICODE.INFO} not found, creating it...`);
+        await sql`CREATE USER ${sql(username)} WITH PASSWORD '${sql(password)}';`;
+        console.log(`  ${UNICODE.OK} "${username}" created`);
+        await sql`GRANT ${sql(adminRole)} TO ${sql(username)};`;
+        console.log(
+          `  ${UNICODE.OK} Role "${adminRole}" granted to user "${username}"`,
+        );
+      } else {
+        const roleGrantResults = await sql`
+SELECT 1
+FROM pg_user u
+JOIN pg_auth_members m ON u.usesysid = m.member
+JOIN pg_roles r ON m.roleid = r.oid
+WHERE u.usename = ${username}`;
+        if (roleGrantResults.length === 0) {
+          console.log(
+            `  ${UNICODE.INFO} Role "${adminRole}" is missing on user.`,
+          );
+          await sql`GRANT ${sql(adminRole)} TO ${sql(username)};`;
+          console.log(`  ${UNICODE.OK} Role "${adminRole}" assigned to user.`);
+        } else {
+          console.log(`  ${UNICODE.OK} User found with role "${adminRole}".`);
+        }
+      }
+    }
+    database_setup: {
+      console.log(`${UNICODE.INFO} Check database "${databaseName}":`);
+      const databases = await sql`
+SELECT 1
+FROM pg_database
+WHERE datname = ${databaseName};`;
+      if (databases.length === 0) {
+        console.log(`  ${UNICODE.INFO} not found, creating it...`);
+        await sql`CREATE DATABASE ${sql(databaseName)} OWNER ${sql(username)}`;
+        console.log(`  ${UNICODE.OK} Database "${databaseName}" created.`);
+      } else {
+        console.log(`  ${UNICODE.OK} Database "${databaseName}" found.`);
+      }
+    }
+    await sql.end();
+  },
 };
+
+await commands.setup();
 
 const { values, positionals } = parseArgs({ options, allowPositionals: true });
 if (values.help || positionals.length === 0) {
