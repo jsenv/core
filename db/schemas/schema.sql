ef4ae7f2d7890ed7a7f7a01614b5adbb61d91caf
dmail <dmaillard06@gmail.com>
2025-05-02 11:42:23 +0200

    workon schema

--- db/schemas/schema.sql
+++ db/schemas/schema.sql
@@ -2,11 +2,3 @@ CREATE TABLE IF NOT EXISTS users (
   id serial NOT NULL PRIMARY KEY,
   name text NOT NULL,
 );
-
-CREATE TABLE IF NOT EXISTS sessions (id serial NOT NULL PRIMARY KEY,);
-
-CREATE TABLE IF NOT EXISTS users_session (
-  user_id int NOT NULL REFERENCES users (id) ON DELETE CASCADE,
-  session_id int NOT NULL REFERENCES sessions (id) ON DELETE CASCADE,
-  UNIQUE (user_id, session_id)
-);
