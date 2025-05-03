887e1014b6d1d6fa62eb90afedcd27f24035a21c
dmail <dmaillard06@gmail.com>
2025-05-03 23:22:09 +0200

    work on ability to move columns

--- packages/related/plugin-database-manager/src/jsenv_plugin_database_manager.js
+++ packages/related/plugin-database-manager/src/jsenv_plugin_database_manager.js
@@ -22,10 +22,8 @@ export const jsenvPluginDatabaseManager = () => {
   return {
     name: "jsenv:database_manager",
     init: async ({ rootDirectoryUrl }) => {
-      // surement mieux que je me logue comme superuser
-      // sinon je peux pas changer les owner etc
-      const { username, password, database } = readParamsFromContext();
-      sql = connectAs({ username, password, database });
+      const { defaultUsername, database } = readParamsFromContext();
+      sql = connectAs({ username: defaultUsername, password: "", database });
       databaseManagerRootDirectoryUrl = new URL(
         "./.internal/database/",
         rootDirectoryUrl,
@@ -116,6 +114,90 @@ export const jsenvPluginDatabaseManager = () => {
             `;
           }
           return Response.json({ rowsecurity: value === "on" });
+        },
+      },
+      // https://wiki.postgresql.org/wiki/Alter_column_position#Add_columns_and_move_data
+      {
+        endpoint:
+          "PUT /.internal/database/api/tables/:name/columns/:columnName/ordinal_position",
+        declarationSource: import.meta.url,
+        acceptedMediaTypes: ["application/json"],
+        fetch: async (request) => {
+          const tableName = request.params.name;
+          const columnName = request.params.columnName;
+          const value = await request.json();
+          // first I need to know the current column position and type
+          const columns = await sql`
+            SELECT
+              ordinal_position,
+              data_type
+            FROM
+              information_schema.columns
+            WHERE
+              table_name = ${sql(tableName)}
+              AND column_name = ${sql(columnName)}
+          `;
+          const columnIndex = columns.findIndex(
+            (column) => column.column_name === columnName,
+          );
+          const column = columns[columnIndex];
+          if (column.ordinal_position === value) {
+            return Response.json({ ordinal_position: value });
+          }
+          const columnAfterThisOneArray = columns.slice(columnIndex + 1);
+          add_new_columns: {
+            let query = `ALTER TABLE ${sql(tableName)}`;
+            const addInstructions = [];
+            for (const columnAfterThisOne of columnAfterThisOneArray) {
+              addInstructions.push(
+                `ADD COLUMN ${sql(columnAfterThisOne.column_name)}_temp ${sql(
+                  columnAfterThisOne.data_type,
+                )}`,
+              );
+            }
+            query += ` `;
+            query += addInstructions.join(", ");
+            query += `;`;
+            await sql(query);
+          }
+          update_new_columns: {
+            let query = `ALTER TABLE ${sql(tableName)}`;
+            const copyInstructions = [];
+            for (const columnAfterThisOne of columnAfterThisOneArray) {
+              copyInstructions.push(
+                `${sql(columnAfterThisOne.column_name)}_temp = ${sql(
+                  columnAfterThisOne.column_name,
+                )}`,
+              );
+            }
+            query += ` SET`;
+            query += copyInstructions.join(", ");
+            query += `;`;
+            await sql(query);
+          }
+          remove_old_columns: {
+            let query = `ALTER TABLE ${sql(tableName)}`;
+            const removeInstructions = [];
+            for (const columnAfterThisOne of columnAfterThisOneArray) {
+              removeInstructions.push(
+                `DROP COLUMN ${sql(columnAfterThisOne.column_name)} cascade`,
+              );
+            }
+            query += ` `;
+            query += removeInstructions.join(", ");
+            query += `;`;
+            await sql(query);
+          }
+          rename_new_columns: {
+            for (const columnAfterThisOne of columnAfterThisOneArray) {
+              await sql(
+                `ALTER TABLE ${sql(tableName)} RENAME COLUMN ${sql(columnAfterThisOne.column_name)}_temp TO ${sql(
+                  columnAfterThisOne.column_name,
+                )}`,
+              );
+            }
+          }
+          return Response.json({ ordinal_position: value });
         },
       },
     ],
