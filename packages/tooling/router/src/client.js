b4923900f3351829f95608f84e3bf8d707a26b4a
dmail <dmaillard06@gmail.com>
2025-05-05 10:27:27 +0200

    reorg files

---
+++ packages/tooling/router/src/client.js
@@ -0,0 +1,30 @@
+export { useCanGoBack, useCanGoForward } from "./client/can_go_back_forward.js";
+export { useDocumentUrl } from "./client/document_url.js";
+export {
+  registerRoutes,
+  registerRoute,
+  setBaseUrl,
+  useRouteData,
+  useRouteError,
+  useRouteIsLoading,
+  useRouteIsMatching,
+  useRouteLoadIsAborted,
+  useRouteLoadingState,
+  useRouteUrl,
+} from "./client/route.js";
+export { Route } from "./client/route.jsx";
+export { goBack, goForward, goTo, reload, stopLoad } from "./client/router.js";
+export {
+  useCanStopLoad,
+  useRouterIsBusy,
+  useRouterReadyState,
+} from "./client/router_ready_state.js";
+export { useDetails } from "./client/hooks/use_details.js";
+export {
+  SPAForm,
+  SPAButton,
+  useSPAFormStatus,
+} from "./client/components/spa_form.jsx";
+export { SPADeleteButton } from "./client/components/spa_delete_button.jsx";
+export { SPACheckBox } from "./client/components/spa_checkbox.jsx";
+export { useUrlBooleanParam, withUrlStringParam } from "./client/url.js";
