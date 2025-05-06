4711fc4ac96ef3744c11d83c9c524e06caa0bd14
dmail <dmaillard06@gmail.com>
2025-05-06 10:55:28 +0200

    workon actions

--- packages/tooling/router/src/client/action/action.js
+++ packages/tooling/router/src/client/action/action.js
@@ -3,26 +3,131 @@ import { IDLE, EXECUTING, DONE, FAILED } from "./action_status.js";
 import { routingWhile } from "../document_routing.js";
 
 const actionSet = new Set();
-
+const actionWithParamsSetMap = new Map();
 export const registerAction = (fn) => {
   const action = {
     fn,
+    match: ({ formAction }) => formAction.action === action,
+    toString: () => fn.name,
+    withParams: (params) => {
+      let actionWithParamsSet = actionWithParamsSetMap.get(action);
+      if (!actionWithParamsSet) {
+        actionWithParamsSet = new Set();
+        actionWithParamsSetMap.add(action, actionWithParamsSet);
+        const actionWithParams = createActionWithParams(action, params);
+        actionWithParamsSet.add(actionWithParams);
+        return actionWithParams;
+      }
+      for (const actionWithParamsCandidate of actionWithParamsSet) {
+        if (compareTwoJsValues(actionWithParamsCandidate.params, params)) {
+          return actionWithParamsCandidate;
+        }
+      }
+      const actionWithParams = createActionWithParams(action, params);
+      actionWithParamsSet.add(actionWithParams);
+      return actionWithParams;
+    },
+  };
+  actionSet.add(action);
+  return action;
+};
+
+const createActionWithParams = (action, params) => {
+  let removeDataSignalEffect;
+  let removeErrorSignalEffect;
+  const actionWithParams = {
+    params,
+    action,
+    fn: (navParams) => {
+      return action.fn({
+        ...navParams,
+        ...params,
+      });
+    },
     executionStateSignal: signal(IDLE),
     errorSignal: signal(null),
     dataSignal: signal(null),
     error: null,
     data: undefined,
-    match: ({ formAction }) => formAction.action === action,
-    toString: () => fn.name,
+    subscribeCount: 0,
+    subscribe: () => {
+      actionWithParams.subscribeCount++;
+    },
+    unsubscribe: () => {
+      actionWithParams.subscribeCount--;
+      if (actionWithParams.subscribeCount === 0) {
+        removeDataSignalEffect();
+        removeDataSignalEffect = null;
+        removeErrorSignalEffect();
+        removeErrorSignalEffect = null;
+      }
+    },
   };
-  effect(() => {
+  removeDataSignalEffect = effect(() => {
     action.data = action.dataSignal.value;
   });
-  effect(() => {
+  removeErrorSignalEffect = effect(() => {
     action.error = action.errorSignal.value;
   });
-  actionSet.add(action);
-  return action;
+  return actionWithParams;
+};
+
+const compareTwoJsValues = (a, b) => {
+  if (a === b) {
+    return true;
+  }
+  const aIsIsTruthy = Boolean(a);
+  const bIsTruthy = Boolean(b);
+  if (aIsIsTruthy && !bIsTruthy) {
+    return false;
+  }
+  if (!aIsIsTruthy && !bIsTruthy) {
+    // null, undefined, 0, false, NaN
+    if (isNaN(a) && isNaN(b)) {
+      return true;
+    }
+    return a === b;
+  }
+  const aType = typeof a;
+  const bType = typeof b;
+  if (aType !== bType) {
+    return false;
+  }
+  const aIsArray = Array.isArray(a);
+  const bIsArray = Array.isArray(b);
+  if (aIsArray !== bIsArray) {
+    return false;
+  }
+  if (aIsArray) {
+    // compare arrays
+    if (a.length !== b.length) {
+      return false;
+    }
+    let i = 0;
+    while (i < a.length) {
+      const aValue = a[i];
+      const bValue = b[i];
+      if (!compareTwoJsValues(aValue, bValue)) {
+        return false;
+      }
+      i++;
+    }
+    return true;
+  }
+  // compare objects
+  const aKeys = Object.keys(a);
+  const bKeys = Object.keys(b);
+  if (aKeys.length !== bKeys.length) {
+    return false;
+  }
+  for (const key of aKeys) {
+    const aValue = a[key];
+    const bValue = b[key];
+    if (!compareTwoJsValues(aValue, bValue)) {
+      return false;
+    }
+  }
+  return true;
 };
 
 export const applyAction = async (action, { signal }) => {
--- packages/tooling/router/src/client/action/action_hooks.js
+++ packages/tooling/router/src/client/action/action_hooks.js
@@ -1,65 +1,19 @@
-import { useMemo, useEffect } from "preact/hooks";
-import { signal } from "@preact/signals";
+import { useEffect } from "preact/hooks";
 import { EXECUTING, ABORTED } from "./action_status.js";
 
-const routeActionParamsSet = new Map();
-export const useAction = (route, params = {}) => {
-  let routeParamsSet = routeActionParamsSet.get(route);
-  if (!routeParamsSet) {
-    routeParamsSet = new Map();
-    routeActionParamsSet.set(route, routeParamsSet);
-  }
-  if (!routeParamsSet.has(params)) {
-    let existingParams;
-    for (const paramsCandidate of routeParamsSet) {
-      if (compareParams(paramsCandidate, params)) {
-        existingParams = paramsCandidate;
-        break;
-      }
-    }
-    if (existingParams) {
-      params = existingParams;
-    } else {
-      routeParamsSet.add(params);
-    }
-  }
-  const action = useMemo(() => {
-    const fn = (navParams) => {
-      return route.handler({
-        ...navParams,
-        ...params,
-      });
-    };
-    fn.route = route;
-    fn.pendingSignal = signal(false);
-    fn.errorSignal = signal(null);
-    action.subscribeCount = 0;
-    return fn;
-  }, [params]);
+export const useAction = (action, params = {}) => {
+  const actionWithParams = action.withParams(params);
 
-  action.subscribeCount++;
   // when no one is interested by this action anymore
   // we can delete the usage of this param and eventually the route
   useEffect(() => {
+    actionWithParams.subscribe();
     return () => {
-      action.subscribeCount--;
-      if (action.subscribeCount === 0) {
-        routeParamsSet.delete(params);
-        if (routeParamsSet.size === 0) {
-          routeActionParamsSet.delete(route);
-        }
-      }
+      actionWithParams.unsubscribe();
     };
   }, []);
 
-  return action;
-};
-const compareParams = (a, b) => {
-  if (a === b) {
-    return true;
-  }
-  // TODO: real comparison
-  return false;
+  return actionWithParams;
 };
 
 export const useActionStatus = (action) => {
