a799d90148ac64a3a501efd52f75fc3ad4cd5390
dmail <dmaillard06@gmail.com>
2025-05-08 17:01:05 +0200

    simplify optimistic ui state

--- packages/tooling/router/src/client/hooks/use_optimistic_ui_state.js
+++ packages/tooling/router/src/client/hooks/use_optimistic_ui_state.js
@@ -1,33 +1,12 @@
-import { useRef, useLayoutEffect, useCallback, useState } from "preact/hooks";
+import { useRef, useCallback } from "preact/hooks";
 import { useSPAFormStatus } from "../components/use_spa_form_status.js";
 
 export const useOptimisticUIState = (frontendMemoryState) => {
-  const { pending, aborted, error } = useSPAFormStatus();
+  const { pending } = useSPAFormStatus();
   const optimisticStateRef = useRef(frontendMemoryState);
-  const [, setAborted] = useState(false);
-  const [, setError] = useState(false);
-
-  if (!pending || aborted || error) {
+  if (!pending) {
     optimisticStateRef.current = frontendMemoryState;
   }
-
-  useLayoutEffect(() => {
-    if (pending) {
-      setAborted(false);
-      setError(false);
-    }
-  }, [pending]);
-  useLayoutEffect(() => {
-    if (aborted) {
-      setAborted(true);
-    }
-  }, [aborted]);
-  useLayoutEffect(() => {
-    if (error) {
-      setError(true);
-    }
-  }, [error]);
-
   const setOptimisticState = useCallback((value) => {
     optimisticStateRef.current = value;
   }, []);
