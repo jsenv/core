8c511bda4f81ecdc84a3f8067d6bac91682c1c5a
dmail <dmaillard06@gmail.com>
2025-05-18 17:33:03 +0200

    prepare assert to become resizable

--- packages/related/plugin-database-manager/src/client/database_manager.jsx
+++ packages/related/plugin-database-manager/src/client/database_manager.jsx
@@ -3,6 +3,7 @@ import "./router.js"; // must be the first import (so that setBaseUrl is called 
 import { render } from "preact";
 import { DatabaseRoutes } from "./database/database_page.jsx";
 import "./database_manager.css" with { type: "css" };
+import { Aside } from "./layout/aside.jsx";
 import { Explorer } from "./explorer/explorer.jsx";
 import { RoleRoutes } from "./role/role_page.jsx";
 import { TableRoutes } from "./table/table_page.jsx";
@@ -10,9 +11,9 @@ import { TableRoutes } from "./table/table_page.jsx";
 const App = () => {
   return (
     <div>
-      <aside>
+      <Aside>
         <Explorer />
-      </aside>
+      </Aside>
       <main>
         {/* <h1 title="Explore and manager your database">Database Manager</h1> */}
 
---
+++ packages/related/plugin-database-manager/src/client/layout/aside.jsx
@@ -0,0 +1,5 @@
+import "./aside.css" with { type: "css" };
+
+export const Aside = ({ children }) => {
+  return <aside>{children}</aside>;
+};
