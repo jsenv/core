2b02338a48acd60a303c04b48866d217dcd0670e
dmail <dmaillard06@gmail.com>
2025-05-20 10:46:52 +0200

    improve local storage stuff

--- packages/frontend/router/src/value_in_local_storage.js
+++ packages/frontend/router/src/value_in_local_storage.js
@@ -7,27 +7,46 @@ export const valueInLocalStorage = (key, { type = "string" } = {}) => {
       ).join(", ")}`,
     );
   }
+  const getValidityMessage = (value) => {
+    if (!converter) {
+      return "";
+    }
+    if (!converter.checkValidity) {
+      return "";
+    }
+    const checkValidityResult = converter.checkValidity(value);
+    if (checkValidityResult === false) {
+      return value;
+    }
+    if (!checkValidityResult) {
+      return "";
+    }
+    return checkValidityResult;
+  };
 
   const get = () => {
     const valueInLocalStorage = window.localStorage.getItem(key);
     if (valueInLocalStorage === null) {
       return undefined;
     }
-    if (valueInLocalStorage === "undefined") {
+    const validityMessage = getValidityMessage(valueInLocalStorage);
+    if (validityMessage) {
       console.warn(
-        `Invalid value for ${key} in local storage, found ${valueInLocalStorage}`,
+        `The value found in localStorage "${key}" is invalid: ${validityMessage}`,
       );
       return undefined;
     }
     if (converter) {
-      const result = converter.decode(valueInLocalStorage);
-      if (result === undefined) {
-        console.warn(
-          `Invalid value for ${key} in local storage, found ${valueInLocalStorage}`,
-        );
-        return undefined;
+      if (converter.checkValidity) {
+        const validity = converter.checkValidity(valueInLocalStorage);
+        if (!validity) {
+        }
       }
-      return result;
+      if (converter.decode) {
+        const valueDecoded = converter.decode(valueInLocalStorage);
+        return valueDecoded;
+      }
+      return valueInLocalStorage;
     }
     return valueInLocalStorage;
   };
@@ -36,8 +55,20 @@ export const valueInLocalStorage = (key, { type = "string" } = {}) => {
       window.localStorage.removeItem(key);
       return;
     }
-    const valueToStore = converter ? converter.encode(value) : value;
-    window.localStorage.setItem("aside_width", valueToStore);
+    const validityMessage = getValidityMessage(value);
+    if (validityMessage) {
+      console.warn(
+        `The value to set in localStorage "${key}" is invalid: ${validityMessage}`,
+      );
+    }
+    if (converter) {
+      if (converter.encode) {
+        const valueEncoded = converter.encode(value);
+        window.localStorage.setItem(key, valueEncoded);
+        return;
+      }
+    }
+    window.localStorage.setItem(key, value);
   };
 
   return [get, set];
@@ -45,7 +76,12 @@ export const valueInLocalStorage = (key, { type = "string" } = {}) => {
 
 const typeConverters = {
   number: {
-    encode: (value) => value,
+    checkValidity: (value) => {
+      if (typeof value !== "number") {
+        return `must be a number, got ${value}`;
+      }
+      return "";
+    },
     decode: (value) => {
       const valueParsed = parseFloat(value);
       if (!isFinite(valueParsed)) {
@@ -55,23 +91,31 @@ const typeConverters = {
     },
   },
   percentage: {
-    encode: (value) => value,
-    decode: (value) => {
+    checkValidity: (value) => {
+      if (typeof value !== "string") {
+        return `must be a percentage, got ${value}`;
+      }
       if (!value.endsWith("%")) {
-        return undefined;
+        return `must end with %, got ${value}`;
       }
       const percentageString = value.slice(0, -1);
       const percentageFloat = parseFloat(percentageString);
       if (typeof percentageFloat !== "number") {
-        return undefined;
+        return `must be a percentage, got ${value}`;
       }
       if (percentageFloat < 0 || percentageFloat > 100) {
-        return undefined;
+        return `must be between 0 and 100, got ${value}`;
       }
-      return value;
+      return "";
     },
   },
   string: {
+    checkValidity: (value) => {
+      if (typeof value !== "string") {
+        return `must be a string, got ${value}`;
+      }
+      return "";
+    },
     encode: (value) => value,
     decode: (value) => value,
   },
