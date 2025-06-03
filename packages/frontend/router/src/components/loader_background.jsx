60cbcf8d49300e10afbad5a0653cc9906b4676d4
dmail <dmaillard06@gmail.com>
2025-06-03 21:35:13 +0200

    loader background can work without wrapper

--- packages/frontend/router/src/components/loader_background.jsx
+++ packages/frontend/router/src/components/loader_background.jsx
@@ -1,17 +1,51 @@
+import { createPortal } from "preact/compat";
 import { useDebounceTrue } from "../hooks/use_debounce_true.js";
 import { RectangleLoading } from "./rectangle_loading.jsx";
 
-export const LoaderBackground = ({ pending, children }) => {
-  const shouldShowSpinner = useDebounceTrue(pending, 300);
-
-  // useLayoutEffect(() => {
-  //   if (pending) {
-  //     // show the loading stuff, ensure we match checkbox size and color somehow
-  //   }
-  // }, [pending, aborted]);
+export const LoaderBackground = ({ pending, containerRef, children }) => {
+  if (containerRef) {
+    const container = containerRef.current;
+    if (!container) {
+      return null;
+    }
+    container.style.position = "relative";
+    return createPortal(
+      <LoaderBackgroundWithPortal pending={pending}>
+        {children}
+      </LoaderBackgroundWithPortal>,
+      container,
+    );
+  }
 
   return (
-    <div style="display:inline-flex;position: relative;">
+    <LoaderBackgroundWithWrapper pending={pending}>
+      {children}
+    </LoaderBackgroundWithWrapper>
+  );
+};
+
+const LoaderBackgroundWithPortal = ({ pending, children }) => {
+  const shouldShowSpinner = useDebounceTrue(pending, 300);
+
+  if (!shouldShowSpinner) {
+    return children;
+  }
+
+  return (
+    <>
+      <div style="position: absolute; inset: 0">
+        {shouldShowSpinner && <RectangleLoading />}
+      </div>
+      {children}
+    </>
+  );
+};
+
+const LoaderBackgroundWithWrapper = ({ pending, children }) => {
+  const shouldShowSpinner = useDebounceTrue(pending, 300);
+
+  return (
+    <div style="display:inline-flex; position: relative;">
       {shouldShowSpinner && (
         <div style="position: absolute; inset: 0">
           <RectangleLoading />
