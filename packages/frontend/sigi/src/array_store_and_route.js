c059e243adca9896eb9744ff0400032a0dcc6487
dmail <dmaillard06@gmail.com>
2025-06-05 12:15:58 +0200

    write a comment

--- packages/frontend/sigi/src/array_store_and_route.js
+++ packages/frontend/sigi/src/array_store_and_route.js
@@ -21,6 +21,15 @@ export const connectStoreAndRoute = (store, route, key) => {
       route.reload();
     },
     reinserted: () => {
+      // this will reload all routes which works but
+      // - most of the time only "route" is impacted, any other route could stay as is
+      // - we already have the data, reloading the route will refetch the backend which is unnecessary
+      // we could just remove routing error (which is cause by 404 likely)
+      // to actually let the data be displayed
+      // because they are available, but in reality the route has no data
+      // because the fetch failed
+      // so conceptually reloading is fine,
+      // the only thing that bothers me a little is that it reloads all routes
       route.reload();
     },
   });
