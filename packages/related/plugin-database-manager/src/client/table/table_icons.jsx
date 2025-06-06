54acc7c1e748e0b3c4646305f8026577fb9024f7
dmail <dmaillard06@gmail.com>
2025-06-06 14:55:06 +0200

    add table icon

--- packages/related/plugin-database-manager/src/client/table/table_icons.jsx
+++ packages/related/plugin-database-manager/src/client/table/table_icons.jsx
@@ -11,14 +11,21 @@ export const TableSvg = ({ color = "currentColor" }) => {
       width="100%"
       height="100%"
       fill="none"
+      stroke={color}
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
       xmlns="http://www.w3.org/2000/svg"
     >
-      <path
-        fill-rule="evenodd"
-        clip-rule="evenodd"
-        d="M8 9C8 6.79086 9.79086 5 12 5C14.2091 5 16 6.79086 16 9C16 11.2091 14.2091 13 12 13C9.79086 13 8 11.2091 8 9ZM15.8243 13.6235C17.1533 12.523 18 10.8604 18 9C18 5.68629 15.3137 3 12 3C8.68629 3 6 5.68629 6 9C6 10.8604 6.84668 12.523 8.17572 13.6235C4.98421 14.7459 3 17.2474 3 20C3 20.5523 3.44772 21 4 21C4.55228 21 5 20.5523 5 20C5 17.7306 7.3553 15 12 15C16.6447 15 19 17.7306 19 20C19 20.5523 19.4477 21 20 21C20.5523 21 21 20.5523 21 20C21 17.2474 19.0158 14.7459 15.8243 13.6235Z"
-        fill={color}
-      />
+      {/* Table outer border */}
+      <rect x="3" y="4" width="18" height="16" rx="1" />
+
+      {/* Horizontal grid line */}
+      <line x1="3" y1="10" x2="21" y2="10" />
+
+      {/* Vertical grid lines */}
+      <line x1="9" y1="4" x2="9" y2="20" />
+      <line x1="15" y1="4" x2="15" y2="20" />
     </svg>
   );
 };
