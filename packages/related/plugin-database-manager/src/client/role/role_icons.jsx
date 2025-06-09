607e7fb66a4c851407d5bce6e3e95a83c9369994
dmail <dmaillard06@gmail.com>
2025-06-09 20:32:02 +0200

    fill user

--- packages/related/plugin-database-manager/src/client/role/role_icons.jsx
+++ packages/related/plugin-database-manager/src/client/role/role_icons.jsx
@@ -32,7 +32,7 @@ export const UserGroupSvg = ({ color = "currentColor" }) => {
   );
 };
 
-export const UserSvg = ({ color = "currentColor" }) => {
+export const UserSvg = ({ color = "currentColor", fillColor = "red" }) => {
   return (
     <svg
       viewBox="0 0 24 24"
@@ -41,11 +41,26 @@ export const UserSvg = ({ color = "currentColor" }) => {
       fill="none"
       xmlns="http://www.w3.org/2000/svg"
     >
+      {/* Remplissage complet (uniquement quand fillColor est fourni) */}
+      {fillColor && (
+        <>
+          {/* Cercle de la tête complètement rempli */}
+          <circle cx="12" cy="9" r="4" fill={fillColor} />
+
+          <path
+            d="M4.5 19.8C4.5 17.5 6.8 15 12 15C17.2 15 19.5 17.5 19.5 19.8C19.5 20.4 19.2 20.8 18.8 20.8H5.2C4.8 20.8 4.5 20.4 4.5 19.8Z"
+            fill={fillColor}
+          />
+        </>
+      )}
+
+      {/* Chemin original conservé tel quel */}
       <path
         fill-rule="evenodd"
         clip-rule="evenodd"
         d="M8 9C8 6.79086 9.79086 5 12 5C14.2091 5 16 6.79086 16 9C16 11.2091 14.2091 13 12 13C9.79086 13 8 11.2091 8 9ZM15.8243 13.6235C17.1533 12.523 18 10.8604 18 9C18 5.68629 15.3137 3 12 3C8.68629 3 6 5.68629 6 9C6 10.8604 6.84668 12.523 8.17572 13.6235C4.98421 14.7459 3 17.2474 3 20C3 20.5523 3.44772 21 4 21C4.55228 21 5 20.5523 5 20C5 17.7306 7.3553 15 12 15C16.6447 15 19 17.7306 19 20C19 20.5523 19.4477 21 20 21C20.5523 21 21 20.5523 21 20C21 17.2474 19.0158 14.7459 15.8243 13.6235Z"
         fill={color}
+        fill-opacity="0.5"
       />
     </svg>
   );
