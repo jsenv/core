460cb0978bd5f3260bff521097299629e643f0ee
dmail <dmaillard06@gmail.com>
2025-07-01 16:25:40 +0200

    log warning when nav state id already used

--- packages/frontend/navi/src/components/use_nav_state.js
+++ packages/frontend/navi/src/components/use_nav_state.js
@@ -1,9 +1,41 @@
-import { useRef } from "preact/hooks";
+import { useEffect, useRef } from "preact/hooks";
 
-const none = {};
-export const useNavState = (id, initialValue) => {
-  const navStateRef = useRef(none);
-  if (navStateRef.current === none) {
+const idUsageMap = new Map();
+const useNavStateWithWarnings = (id, initialValue) => {
+  const idRef = useRef(undefined);
+  if (import.meta.dev && idRef.current !== id) {
+    const oldId = idRef.current;
+    idUsageMap.delete(oldId);
+    idRef.current = id;
+
+    const usage = idUsageMap.get(id);
+    if (!usage) {
+      idUsageMap.set(id, {
+        stackTrace: new Error().stack,
+      });
+    } else {
+      console.warn(
+        `useNavState ID conflict detected!
+ID "${id}" is already in use by another component.
+This can cause UI state conflicts and unexpected behavior.
+Consider using unique IDs for each component instance.`,
+      );
+    }
+  }
+
+  useEffect(() => {
+    return () => {
+      idUsageMap.delete(id);
+    };
+  }, [id]);
+
+  return useNavStateWithoutWarnings(id, initialValue);
+};
+
+const NONE = {};
+const useNavStateWithoutWarnings = (id, initialValue) => {
+  const navStateRef = useRef(NONE);
+  if (navStateRef.current === NONE) {
     const navEntryState = navigation.currentEntry.getState();
     navStateRef.current =
       navEntryState && id ? navEntryState[id] : initialValue;
@@ -31,3 +63,7 @@ export const useNavState = (id, initialValue) => {
     },
   ];
 };
+
+export const useNavState = import.meta.dev
+  ? useNavStateWithWarnings
+  : useNavStateWithoutWarnings;
