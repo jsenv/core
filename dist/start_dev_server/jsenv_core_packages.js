968ad8bd73404c25c87607dca30cd5464e8a0cf8
dmail <dmaillard06@gmail.com>
2025-07-02 14:54:04 +0200

    rebuild

--- dist/start_dev_server/jsenv_core_packages.js
+++ dist/start_dev_server/jsenv_core_packages.js
@@ -4776,7 +4776,7 @@ const mainLegacyResolvers = {
     };
   },
   node: ({ packageJson, conditions }) => {
-    if (conditions.includes("import")) {
+    if (conditions.includes("import") && !conditions.includes("require")) {
       if (typeof packageJson.module === "string") {
         return { type: "field:module", isMain: true, path: packageJson.module };
       }
