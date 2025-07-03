42266b02878cc8e3ef1e2d4748d06fc03f7449db
dmail <dmaillard06@gmail.com>
2025-07-03 07:45:32 +0200

    workon button

--- packages/frontend/navi/src/components/input/button.jsx
+++ packages/frontend/navi/src/components/input/button.jsx
@@ -3,107 +3,45 @@ import { forwardRef } from "preact/compat";
 import { useImperativeHandle, useRef } from "preact/hooks";
 import { useActionStatus } from "../../use_action_status.js";
 import { useParentAction } from "../action_execution/action_context.js";
+import { useAction } from "../action_execution/use_action.js";
 import { useAutoFocus } from "../use_auto_focus.js";
 
-export const Button = forwardRef(
-  (
-    {
-      action,
-      children,
-      // confirmMessage, // TODO
-      ...rest
-    },
-    ref,
-  ) => {
-    if (action) {
-      return (
-        <ButtonWithOwnAction action={action} ref={ref} {...rest}>
-          {children}
-        </ButtonWithOwnAction>
-      );
-    }
+export const Button = forwardRef((props, ref) => {
+  const {
+    action,
+    autoFocus,
+    constraints = [],
+    disabled,
+    children,
+    onClick,
+    ...rest
+  } = props;
 
-    const parentAction = useParentAction();
-    if (parentAction) {
-      return (
-        <ButtonWithParentAction parentAction={parentAction} ref={ref} {...rest}>
-          {children}
-        </ButtonWithParentAction>
-      );
-    }
+  const innerRef = useRef();
+  useImperativeHandle(ref, () => innerRef.current);
+  useAutoFocus(innerRef, autoFocus);
+  useConstraints(innerRef, constraints);
 
-    return (
-      <ButtonWithoutAction ref={ref} {...rest}>
-        {children}
-      </ButtonWithoutAction>
-    );
-  },
-);
+  const parentAction = useParentAction();
+  const effectiveAction = action ? useAction(action) : parentAction;
+  const { pending } = effectiveAction
+    ? useActionStatus(effectiveAction)
+    : { pending: false };
 
-const ButtonWithOwnAction = forwardRef(
-  (
-    { action, autoFocus, constraints = [], disabled, children, ...rest },
-    ref,
-  ) => {
-    const innerRef = useRef();
-    useImperativeHandle(ref, () => innerRef.current);
-    useAutoFocus(innerRef, autoFocus);
-    useConstraints(innerRef, constraints);
-
-    const { pending } = useActionStatus(innerRef, action);
-
-    return (
-      <button
-        ref={innerRef}
-        data-validation-message-arrow-x="center"
-        {...rest}
-        disabled={disabled || pending}
-        onClick={(event) => {
+  return (
+    <button
+      ref={innerRef}
+      data-validation-message-arrow-x="center"
+      {...rest}
+      disabled={disabled || pending}
+      onClick={(event) => {
+        if (action) {
           event.target.requestAction();
-        }}
-      >
-        {children}
-      </button>
-    );
-  },
-);
-
-const ButtonWithParentAction = forwardRef(
-  (
-    { parentAction, autoFocus, constraints = [], disabled, children, ...rest },
-    ref,
-  ) => {
-    const innerRef = useRef();
-    useImperativeHandle(ref, () => innerRef.current);
-    useAutoFocus(innerRef, autoFocus);
-    useConstraints(innerRef, constraints);
-
-    const { pending } = useActionStatus(innerRef, parentAction);
-
-    return (
-      <button
-        ref={innerRef}
-        data-validation-message-arrow-x="center"
-        {...rest}
-        disabled={disabled || pending}
-      >
-        {children}
-      </button>
-    );
-  },
-);
-
-const ButtonWithoutAction = forwardRef(
-  ({ children, autoFocus, constraints = [], ...rest }, ref) => {
-    const innerRef = useRef();
-    useImperativeHandle(ref, () => innerRef.current);
-    useAutoFocus(innerRef, autoFocus);
-    useConstraints(innerRef, constraints);
-
-    return (
-      <button ref={innerRef} data-validation-message-arrow-x="center" {...rest}>
-        {children}
-      </button>
-    );
-  },
-);
+        }
+        onClick?.(event);
+      }}
+    >
+      {children}
+    </button>
+  );
+});
