c822fabcb412adc949b4b9dc1e9fe5959c8e1a01
dmail <dmaillard06@gmail.com>
2025-07-08 14:50:43 +0200

    workon key shortcut

--- packages/frontend/navi/src/components/link/link.jsx
+++ packages/frontend/navi/src/components/link/link.jsx
@@ -3,9 +3,26 @@ import.meta.css = /* css */ `
   }
 `;
 
-export const Link = ({ children, className = "", ...rest }) => {
+export const Link = ({
+  children,
+  onEnterKeyDown,
+  onKeyDown,
+  className = "",
+  ...rest
+}) => {
   return (
-    <a {...rest} className={["navi_link", ...className.split(" ")].join(" ")}>
+    <a
+      {...rest}
+      className={["navi_link", ...className.split(" ")].join(" ")}
+      onKeyDown={(e) => {
+        if (e.key === "Enter" && onEnterKeyDown) {
+          e.preventDefault();
+          e.stopPropagation();
+          onEnterKeyDown(e);
+        }
+        onKeyDown?.(e);
+      }}
+    >
       {children}
     </a>
   );
