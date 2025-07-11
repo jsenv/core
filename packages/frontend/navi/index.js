bd83a0c3a2071c25d25457aee1640e64884ccf3c
dmail <dmaillard06@gmail.com>
2025-07-11 14:51:36 +0200

    reorg files

--- packages/frontend/navi/index.js
+++ packages/frontend/navi/index.js
@@ -1,18 +1,20 @@
-export {
-  getOptionsForActionConnectedToLocalStorageBoolean,
-  getOptionsForActionConnectedToLocalStorageString,
-} from "./src/action_connections.js";
+// actions
 export { createAction, reloadActions, updateActions } from "./src/actions.js";
-export { resource } from "./src/resource_graph.js";
 export { useActionData } from "./src/use_action_data.js";
 export { useActionStatus } from "./src/use_action_status.js";
 
+// state management (store)
+export { resource } from "./src/store/resource_graph.js";
+export { valueInLocalStorage } from "./src/store/value_in_local_storage.js";
+
+// UI
 export { ActionRenderer } from "./src/components/action_renderer.jsx";
 export { Details } from "./src/components/details/details.jsx";
 export {
   EditableText,
   useEditableController,
 } from "./src/components/editable_text/editable_text.jsx";
+export { ErrorBoundaryContext } from "./src/components/error_boundary_context.js";
 export { Form } from "./src/components/form.jsx";
 export { Button } from "./src/components/input/button.jsx";
 export { CheckboxList } from "./src/components/input/checkbox_list.jsx";
@@ -20,7 +22,3 @@ export { Field } from "./src/components/input/field.jsx";
 export { Input } from "./src/components/input/input.jsx";
 export { RadioList } from "./src/components/input/radio_list.jsx";
 export { Link } from "./src/components/link/link.jsx";
-
-export { ErrorBoundaryContext } from "./src/components/error_boundary_context.js";
-
-export { valueInLocalStorage } from "./src/value_in_local_storage.js";
