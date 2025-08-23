fdaab4f0848ef468a60fd774d76e9d71db2bc225
dmail <dmaillard06@gmail.com>
2025-08-23 14:51:56 +0200

    workon tablist

--- packages/frontend/navi/src/components/tablist/tablist.jsx
+++ packages/frontend/navi/src/components/tablist/tablist.jsx
@@ -18,11 +18,9 @@ import.meta.css = /* css */ `
     position: relative;
   }
 
-  .active_marker {
+  .navi_tab .active_marker {
     display: none;
-  }
 
-  li[aria-selected="true"] .active_marker {
     background: rgb(205, 52, 37);
     border-radius: 0.1px;
     bottom: 2px;
@@ -30,6 +28,9 @@ import.meta.css = /* css */ `
     position: absolute;
     width: 100%;
     z-index: 1;
+  }
+
+  .navi_tab[aria-selected="true"] .active_marker {
     display: block;
   }
 `;
@@ -39,12 +40,7 @@ export const TabList = ({ children, ...props }) => {
     <nav className="navi_tablist" role="tablist" {...props}>
       <ul>
         {children.map((child) => {
-          return (
-            <li key={child.props.key} data-active="">
-              {child}
-              <span className="active_marker"></span>
-            </li>
-          );
+          return <li key={child.props.key}>{child}</li>;
         })}
       </ul>
     </nav>
@@ -53,8 +49,14 @@ export const TabList = ({ children, ...props }) => {
 
 export const Tab = ({ children, selected, ...props }) => {
   return (
-    <div role="tab" aria-selected={selected ? "true" : "false"} {...props}>
+    <div
+      className="navi_tab"
+      role="tab"
+      aria-selected={selected ? "true" : "false"}
+      {...props}
+    >
       {children}
+      <span className="active_marker"></span>
     </div>
   );
 };
