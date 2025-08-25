d221911e950a0c8f82ac9aca08b7bdb00f768d5c
dmail <dmaillard06@gmail.com>
2025-08-25 08:55:06 +0200

    work

--- packages/related/plugin-database-manager/src/client/table/table_data.jsx
+++ packages/related/plugin-database-manager/src/client/table/table_data.jsx
@@ -60,6 +60,7 @@ export const TableData = ({ table, rows }) => {
         if (rowData.isEditing) {
           return "editing...";
         }
+
         return <DatabaseField column={column} action={null} value={value} />;
       },
       footer: (info) => info.column.id,
