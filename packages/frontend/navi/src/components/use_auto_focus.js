5c054e8afef0ac08f4313fb9c46fb50071450bfc
dmail <dmaillard06@gmail.com>
2025-08-26 17:23:18 +0200

    work

--- packages/frontend/navi/src/components/use_auto_focus.js
+++ packages/frontend/navi/src/components/use_auto_focus.js
@@ -33,38 +33,46 @@ export const useAutoFocus = (
       focusableElement.scrollLeft = 0;
     }
     return () => {
-      // if the input is focused when the component is unmounted,
-      // we restore focus to the element that was focused before
-      // the input was focused
-      if (
+      const focusIsOnSelfOrInsideSelf =
         document.activeElement === focusableElement ||
-        document.activeElement === document.body
+        focusableElement.contains(document.activeElement);
+      if (
+        !focusIsOnSelfOrInsideSelf &&
+        document.activeElement !== document.body
       ) {
-        if (!document.body.contains(activeElement)) {
-          // previously active element is no longer in the document
+        // focus is not on our element (or body) anymore
+        // keep it where it is
+        return;
+      }
+
+      // We have focus but we are unmounted
+      // -> try to move focus back to something more meaningful that what browser would do
+      // (browser would put it to document.body)
+      // -> We'll try to move focus back to the element that had focus before we moved it to this element
+
+      if (!document.body.contains(activeElement)) {
+        // previously active element is no longer in the document
+        return;
+      }
+
+      if (blurEvent) {
+        // But if this element is unmounted during a blur, the element that is about to receive focus should prevail
+        const elementAboutToReceiveFocus = blurEvent.relatedTarget;
+        const isSelfOrInsideSelf =
+          elementAboutToReceiveFocus === focusableElement ||
+          focusableElement.contains(elementAboutToReceiveFocus);
+        const isPreviouslyActiveElementOrInsideIt =
+          elementAboutToReceiveFocus === activeElement ||
+          (activeElement && activeElement.contains(elementAboutToReceiveFocus));
+        if (!isSelfOrInsideSelf && !isPreviouslyActiveElementOrInsideIt) {
+          // the element about to receive focus is not the input itself or inside it
+          // and is not the previously active element or inside it
+          // -> the element about to receive focus should prevail
           return;
         }
-        console.log({ blurEvent });
-        if (blurEvent) {
-          // But if this element is unmounted during a blur, the element that is about to receive focus should prevail
-          const elementAboutToReceiveFocus = blurEvent.relatedTarget;
-          const isSelfOrInsideSelf =
-            elementAboutToReceiveFocus === focusableElement ||
-            focusableElement.contains(elementAboutToReceiveFocus);
-          const isPreviouslyActiveElementOrInsideIt =
-            elementAboutToReceiveFocus === activeElement ||
-            (activeElement &&
-              activeElement.contains(elementAboutToReceiveFocus));
-          if (!isSelfOrInsideSelf && !isPreviouslyActiveElementOrInsideIt) {
-            // the element about to receive focus is not the input itself or inside it
-            // and is not the previously active element or inside it
-            // -> the element about to receive focus should prevail
-            return;
-          }
-        }
-
-        activeElement.focus();
       }
+
+      activeElement.focus();
     };
   }, []);
 
