9d861394c8f85466970e99f89e19c7930ef2c4c8
dmail <dmaillard06@gmail.com>
2025-09-05 09:28:29 +0200

    work

--- packages/frontend/navi/src/components/keyboard_shortcuts/active_keyboard_shortcuts.jsx
+++ packages/frontend/navi/src/components/keyboard_shortcuts/active_keyboard_shortcuts.jsx
@@ -2,7 +2,7 @@ import { generateAriaKeyShortcuts } from "./aria_key_shortcuts.js";
 import { activeShortcutsSignal } from "./keyboard_shortcuts.js";
 
 import.meta.css = /* css */ `
-  .navi_shortcut_container {
+  .navi_shortcut_container[data-visually-hidden] {
     /* Visually hidden container - doesn't affect layout */
     position: absolute;
     width: 1px;
@@ -19,7 +19,7 @@ import.meta.css = /* css */ `
     pointer-events: none;
   }
 
-  .navi_shortcut_button {
+  .navi_shortcut_button[data-visually-hidden] {
     /* Visually hidden but accessible to screen readers */
     position: absolute;
     width: 1px;
@@ -37,15 +37,19 @@ import.meta.css = /* css */ `
   }
 `;
 
-export const ActiveKeyboardShortcuts = () => {
+export const ActiveKeyboardShortcuts = ({ visible }) => {
   const activeShortcuts = activeShortcutsSignal.value;
 
   return (
-    <div className="navi_shortcut_container">
+    <div
+      className="navi_shortcut_container"
+      data-visually-hidden={visible ? undefined : ""}
+    >
       {activeShortcuts.map((shortcut) => {
         return (
           <KeyboardShortcutAriaElement
             key={shortcut.key}
+            visible={visible}
             keyCombination={shortcut.key}
             description={shortcut.description}
             enabled={shortcut.enabled}
@@ -58,6 +62,7 @@ export const ActiveKeyboardShortcuts = () => {
   );
 };
 const KeyboardShortcutAriaElement = ({
+  visible,
   keyCombination,
   description,
   enabled,
@@ -70,6 +75,7 @@ const KeyboardShortcutAriaElement = ({
   return (
     <button
       className="navi_shortcut_button"
+      data-visually-hidden={visible ? undefined : ""}
       aria-keyshortcuts={ariaKeyshortcuts}
       tabIndex="-1"
       disabled={!enabled}
