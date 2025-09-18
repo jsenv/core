991fdab57cd259d9e0b0c636455f9a05eed17199
dmail <dmaillard06@gmail.com>
2025-09-18 16:50:28 +0200

    work

---
+++ packages/tooling/eslint-plugin-jsenv/tests/06_function_chaining/function_chaining.test.js
@@ -0,0 +1,38 @@
+import { RuleTester } from "eslint";
+import { readFileSync } from "fs";
+import { join } from "path";
+import rule from "../../lib/rules/no-extra-params.js";
+
+const ruleTester = new RuleTester({
+  languageOptions: {
+    ecmaVersion: 2022,
+    sourceType: "module",
+  },
+});
+
+const fixturesDir = join(import.meta.dirname, "fixtures");
+
+const validCode = readFileSync(join(fixturesDir, "input_valid.js"), "utf8");
+const invalidCode = readFileSync(join(fixturesDir, "input_invalid.js"), "utf8");
+
+ruleTester.run("no-extra-params - function chaining", rule, {
+  valid: [
+    {
+      name: "parameter used through chaining with spread operator",
+      code: validCode,
+    },
+  ],
+  invalid: [
+    {
+      name: "parameter not used in chaining",
+      code: invalidCode,
+      errors: [
+        {
+          messageId: "extraParam",
+          data: { param: "c", func: "toto" },
+          type: "Property",
+        },
+      ],
+    },
+  ],
+});
