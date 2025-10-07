9dcdd3f43a97aeb5c74ca707f1dc018690fd206f
dmail <dmaillard06@gmail.com>
2025-10-07 16:31:08 +0200

    work

--- packages/frontend/navi/src/components/action_execution/use_execute_action.js
+++ packages/frontend/navi/src/components/action_execution/use_execute_action.js
@@ -57,120 +57,125 @@ export const useExecuteAction = (
     };
   });
 
-  const executeAction = useCallback((actionEvent) => {
-    const { action, requester, event, method } = actionEvent.detail;
+  // const errorEffectRef = useRef();
+  // errorEffectRef.current = errorEffect;
+  const executeAction = useCallback(
+    (actionEvent) => {
+      const { action, requester, event, method } = actionEvent.detail;
 
-    if (debug) {
-      console.debug(
-        "executing action, requested by",
-        requester,
-        `(event: ${event?.type})`,
-      );
-    }
+      if (debug) {
+        console.debug(
+          "executing action, requested by",
+          requester,
+          `(event: ${event?.type})`,
+        );
+      }
 
-    const dispatchCustomEvent = (type, options) => {
-      const element = elementRef.current;
-      const customEvent = new CustomEvent(type, options);
-      element.dispatchEvent(customEvent);
-    };
-    if (resetErrorBoundary) {
-      resetErrorBoundary();
-    }
-    removeErrorMessage();
-    setError(null);
+      const dispatchCustomEvent = (type, options) => {
+        const element = elementRef.current;
+        const customEvent = new CustomEvent(type, options);
+        element.dispatchEvent(customEvent);
+      };
+      if (resetErrorBoundary) {
+        resetErrorBoundary();
+      }
+      removeErrorMessage();
+      setError(null);
 
-    const validationMessageTarget = requester || elementRef.current;
-    validationMessageTargetRef.current = validationMessageTarget;
+      const validationMessageTarget = requester || elementRef.current;
+      validationMessageTargetRef.current = validationMessageTarget;
 
-    dispatchCustomEvent("actionstart", {
-      detail: {
-        action,
-        requester,
-        event,
-        method,
-      },
-    });
+      dispatchCustomEvent("actionstart", {
+        detail: {
+          action,
+          requester,
+          event,
+          method,
+        },
+      });
 
-    return action[method]({
-      reason: `"${event.type}" event on ${(() => {
-        const target = event.target;
-        const tagName = target.tagName.toLowerCase();
+      return action[method]({
+        reason: `"${event.type}" event on ${(() => {
+          const target = event.target;
+          const tagName = target.tagName.toLowerCase();
 
-        if (target.id) {
-          return `${tagName}#${target.id}`;
-        }
+          if (target.id) {
+            return `${tagName}#${target.id}`;
+          }
 
-        const uiName = target.getAttribute("data-ui-name");
-        if (uiName) {
-          return `${tagName}[data-ui-name="${uiName}"]`;
-        }
+          const uiName = target.getAttribute("data-ui-name");
+          if (uiName) {
+            return `${tagName}[data-ui-name="${uiName}"]`;
+          }
 
-        return `<${tagName}>`;
-      })()}`,
-      onAbort: (reason) => {
-        if (
-          // at this stage the action side effect might have removed the <element> from the DOM
-          // (in theory no because action side effect are batched to happen after)
-          // but other side effects might do this
-          elementRef.current
-        ) {
-          dispatchCustomEvent("actionabort", {
-            detail: {
-              reason,
+          return `<${tagName}>`;
+        })()}`,
+        onAbort: (reason) => {
+          if (
+            // at this stage the action side effect might have removed the <element> from the DOM
+            // (in theory no because action side effect are batched to happen after)
+            // but other side effects might do this
+            elementRef.current
+          ) {
+            dispatchCustomEvent("actionabort", {
+              detail: {
+                reason,
 
-              action,
-              requester,
-              event,
-              method,
-            },
-          });
-        }
-      },
-      onError: (error) => {
-        if (
-          // at this stage the action side effect might have removed the <element> from the DOM
-          // (in theory no because action side effect are batched to happen after)
-          // but other side effects might do this
-          elementRef.current
-        ) {
-          dispatchCustomEvent("actionerror", {
-            detail: {
-              error,
+                action,
+                requester,
+                event,
+                method,
+              },
+            });
+          }
+        },
+        onError: (error) => {
+          if (
+            // at this stage the action side effect might have removed the <element> from the DOM
+            // (in theory no because action side effect are batched to happen after)
+            // but other side effects might do this
+            elementRef.current
+          ) {
+            dispatchCustomEvent("actionerror", {
+              detail: {
+                error,
 
-              action,
-              requester,
-              event,
-              method,
-            },
-          });
-        }
-        if (errorEffect === "show_validation_message") {
-          addErrorMessage(error);
-        } else if (errorEffect === "throw") {
-          setError(error);
-        }
-      },
-      onComplete: (data) => {
-        if (
-          // at this stage the action side effect might have removed the <element> from the DOM
-          // (in theory no because action side effect are batched to happen after)
-          // but other side effects might do this
-          elementRef.current
-        ) {
-          dispatchCustomEvent("actionend", {
-            detail: {
-              data,
+                action,
+                requester,
+                event,
+                method,
+              },
+            });
+          }
+          if (errorEffect === "show_validation_message") {
+            addErrorMessage(error);
+          } else if (errorEffect === "throw") {
+            setError(error);
+          }
+        },
+        onComplete: (data) => {
+          if (
+            // at this stage the action side effect might have removed the <element> from the DOM
+            // (in theory no because action side effect are batched to happen after)
+            // but other side effects might do this
+            elementRef.current
+          ) {
+            dispatchCustomEvent("actionend", {
+              detail: {
+                data,
 
-              action,
-              requester,
-              event,
-              method,
-            },
-          });
-        }
-      },
-    });
-  }, []);
+                action,
+                requester,
+                event,
+                method,
+              },
+            });
+          }
+        },
+      });
+    },
+    [errorEffect],
+  );
 
   return executeAction;
 };
