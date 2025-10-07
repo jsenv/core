944805641fddfb72d3dc2f2f3000eb062b40e2b5
dmail <dmaillard06@gmail.com>
2025-10-07 20:17:07 +0200

    work

---
+++ packages/frontend/dom/index.js
@@ -0,0 +1,87 @@
+// style and attributes getters/setters
+export {
+  addWillChange,
+  getStyle,
+  setAttribute,
+  setAttributes,
+  setStyles,
+} from "./src/style_and_attributes.js";
+
+// traversal
+export {
+  findAfter,
+  findAncestor,
+  findBefore,
+  findDescendant,
+} from "./src/traversal.js";
+
+export { elementIsVisible } from "./src/element_is_visible.js";
+export {
+  activeElementSignal,
+  addActiveElementEffect,
+  useActiveElement,
+} from "./src/focus/active_element.js";
+export { elementIsFocusable } from "./src/focus/element_is_focusable.js";
+export { initFocusGroup } from "./src/focus/focus_group.js";
+export { preventFocusNav } from "./src/focus/focus_nav_event_marker.js";
+export { trapFocusInside } from "./src/focus/focus_trap.js";
+export { canInterceptKeys } from "./src/keyboard.js";
+export {
+  getAncestorScrolls,
+  getScrollableParent,
+  getScrollableParentSet,
+  trapScrollInside,
+} from "./src/scroll.js";
+export { getVisualRect } from "./src/visual_rect.js";
+
+export { addAttributeEffect } from "./src/add_attribute_effect.js";
+
+// size
+export { initFlexDetailsSet } from "./src/size/flex_details_set.js";
+export { getAvailableHeight } from "./src/size/get_available_height.js";
+export { getAvailableWidth } from "./src/size/get_available_width.js";
+export { getBorderSizes } from "./src/size/get_border_sizes.js";
+export { getHeight } from "./src/size/get_height.js";
+export { getInnerHeight } from "./src/size/get_inner_height.js";
+export { getInnerWidth } from "./src/size/get_inner_width.js";
+export { getMarginSizes } from "./src/size/get_margin_sizes.js";
+export { getMaxHeight } from "./src/size/get_max_height.js";
+export { getMaxWidth } from "./src/size/get_max_width.js";
+export { getMinHeight } from "./src/size/get_min_height.js";
+export { getMinWidth } from "./src/size/get_min_width.js";
+export { getPaddingSizes } from "./src/size/get_padding_sizes.js";
+export { getWidth } from "./src/size/get_width.js";
+export { resolveCSSSize } from "./src/size/resolve_css_size.js";
+// size hooks
+export { useAvailableHeight } from "./src/size/hooks/use_available_height.js";
+export { useAvailableWidth } from "./src/size/hooks/use_available_width.js";
+export { useMaxHeight } from "./src/size/hooks/use_max_height.js";
+export { useMaxWidth } from "./src/size/hooks/use_max_width.js";
+export { useResizeStatus } from "./src/size/hooks/use_resize_status.js";
+
+// sticky helper
+export { initPositionSticky } from "./src/position_sticky.js";
+
+export { followPosition } from "./src/position/follow_position.js";
+
+// transition
+export {
+  createHeightTransition,
+  createOpacityTransition,
+  createTranslateXTransition,
+  createWidthTransition,
+} from "./src/transition/dom_transition.js";
+export { EASING, cubicBezier } from "./src/transition/easing.js";
+export {
+  createTimelineTransition,
+  createTransition,
+} from "./src/transition/transition_playback.js";
+export { initUITransition } from "./src/ui_transition/ui_transition.js";
+
+// interaction
+export {
+  createDragGesture,
+  createDragToMoveGesture,
+} from "./src/interaction/drag_gesture.js";
+export { startDragToResizeGesture } from "./src/interaction/drag_to_resize_gesture.js";
+export { getDropTargetInfo } from "./src/interaction/drop_target_detection.js";
