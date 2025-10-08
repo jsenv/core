2d876bf74b1d8708fcd4697425e4c7c40b410a80
dmail <dmaillard06@gmail.com>
2025-10-08 10:42:54 +0200

    work

--- packages/frontend/navi/src/components/table/sticky/table_sticky.jsx
+++ packages/frontend/navi/src/components/table/sticky/table_sticky.jsx
@@ -132,25 +132,6 @@ import.meta.css = /* css */ `
     top: var(--sticky-frontier-preview-position, 0px);
   }
 
-  /* Avoid overlaping between sticky frontiers and resize handles */
-  [data-after-sticky-left-frontier] .navi_table_cell_resize_handle[data-top],
-  [data-after-sticky-left-frontier]
-    .navi_table_cell_resize_handle[data-bottom] {
-    left: var(--sticky-left-frontier-width);
-  }
-  [data-sticky-left-frontier] .navi_table_cell_resize_handle[data-top],
-  [data-sticky-left-frontier] .navi_table_cell_resize_handle[data-bottom] {
-    right: var(--sticky-left-frontier-width);
-  }
-  [data-after-sticky-top-frontier] .navi_table_cell_resize_handle[data-left],
-  [data-after-sticky-top-frontier] .navi_table_cell_resize_handle[data-right] {
-    top: var(--sticky-top-frontier-height);
-  }
-  [data-sticky-top-frontier] .navi_table_cell_resize_handle[data-left],
-  [data-sticky-top-frontier] .navi_table_cell_resize_handle[data-right] {
-    bottom: var(--sticky-top-frontier-height);
-  }
-
   /* Positioning adjustments for ::after pseudo-elements on cells adjacent to sticky frontiers */
   /* These ensure selection and focus borders align with the ::before borders */
   .navi_table[data-border-collapse] th[data-after-sticky-left-frontier]::after,
