6f4c4c893801be6ee3fce02e4d5c64da7192f277
dmail <dmaillard06@gmail.com>
2025-10-10 10:42:49 +0200

    work

--- packages/frontend/navi/src/components/table/drag/drag_table_column.js
+++ packages/frontend/navi/src/components/table/drag/drag_table_column.js
@@ -10,7 +10,7 @@ import {
 import { createPubSub } from "../../pub_sub.js";
 import { Z_INDEX_DROP_PREVIEW } from "../z_indexes.js";
 
-const DEBUG_VISUAL = false;
+const DEBUG_VISUAL = true;
 
 import.meta.css = /* css */ `
   .navi_table_column_drop_preview {
@@ -21,6 +21,7 @@ import.meta.css = /* css */ `
     height: var(--table-height);
     pointer-events: none;
     z-index: ${Z_INDEX_DROP_PREVIEW};
+    color: rgba(0, 0, 255, 0.5);
   }
 
   .navi_table_column_drop_preview_line {
@@ -31,31 +32,32 @@ import.meta.css = /* css */ `
     background: rgba(0, 0, 255, 0.5);
     opacity: 0;
     left: var(--table-column-drop-target-left);
+    transform: translateX(-50%);
   }
   .navi_table_column_drop_preview[data-visible]
     .navi_table_column_drop_preview_line {
     opacity: 1;
   }
 
-  .navi_table_column_drop_preview_arrow {
+  .navi_table_column_drop_preview .arrow_positioner {
     position: absolute;
-    top: -10px;
     left: var(--table-column-drop-target-left);
-    /* background: yellow; */
     display: flex;
     opacity: 0;
+    transform: translateX(-50%);
   }
-  .navi_table_column_drop_preview_arrow svg {
-    width: 10px;
-    height: 10px;
-  }
-  .navi_table_column_drop_preview[data-visible]
-    .navi_table_column_drop_preview_arrow {
+  .navi_table_column_drop_preview[data-visible] .arrow_positioner {
     opacity: 1;
   }
-  .navi_table_column_drop_preview[data-after]
-    .navi_table_column_drop_preview_arrow {
-    transform: translateX(-100%);
+  .navi_table_column_drop_preview .arrow_positioner[data-top] {
+    top: -10px;
+  }
+  .navi_table_column_drop_preview .arrow_positioner[data-bottom] {
+    bottom: -10px;
+  }
+  .arrow_positioner svg {
+    width: 10px;
+    height: 10px;
   }
 `;
 
@@ -63,7 +65,7 @@ const dropPreviewTemplate = /* html */ `
   <div
     class="navi_table_column_drop_preview"
   >
-    <div class="navi_table_column_drop_preview_arrow">
+    <div class="arrow_positioner" data-top="">
       <svg fill="currentColor" viewBox="0 0 30.727 30.727" xml:space="preserve">
         <path
           d="M29.994,10.183L15.363,24.812L0.733,10.184c-0.977-0.978-0.977-2.561,0-3.536c0.977-0.977,2.559-0.976,3.536,0
@@ -72,6 +74,14 @@ const dropPreviewTemplate = /* html */ `
       </svg>
     </div>
     <div class="navi_table_column_drop_preview_line"></div>
+    <div class="arrow_positioner" data-bottom="">
+      <svg fill="currentColor" viewBox="0 0 30.727 30.727" xml:space="preserve">
+        <path
+          d="M29.994,10.183L15.363,24.812L0.733,10.184c-0.977-0.978-0.977-2.561,0-3.536c0.977-0.977,2.559-0.976,3.536,0
+		l11.095,11.093L26.461,6.647c0.977-0.976,2.559-0.976,3.535,0C30.971,7.624,30.971,9.206,29.994,10.183z"
+        />
+      </svg>
+    </div>
   </div>
 `;
 const createDropPreview = () => {
