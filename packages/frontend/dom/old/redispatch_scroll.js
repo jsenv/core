76eb7a56e866b7e26031deae8c35eb3bf0c41789
dmail <dmaillard06@gmail.com>
2025-10-12 13:37:40 +0200

    work

---
+++ packages/frontend/dom/old/redispatch_scroll.js
@@ -0,0 +1,15 @@
+// redispatch "scroll" events from document to documentElement
+// This way getScrollableParent(el).addEventListener("scroll")
+// can be used even if scrollable parent is documentElement
+document.addEventListener(
+  "scroll",
+  (scrollEvent) => {
+    const scrollEventCopy = new scrollEvent.constructor(scrollEvent.type, {
+      bubbles: false,
+      cancelable: scrollEvent.cancelable,
+      composed: scrollEvent.composed,
+    });
+    document.documentElement.dispatchEvent(scrollEventCopy);
+  },
+  { passive: true },
+);
--- packages/frontend/dom/src/position/visible_rect.js
+++ packages/frontend/dom/src/position/visible_rect.js
@@ -285,19 +285,3 @@ export const visibleRectEffect = (element, update) => {
     },
   };
 };
-
-// redispatch "scroll" events from document to documentElement
-// This way getScrollableParent(el).addEventListener("scroll")
-// can be used even if scrollable parent is documentElement
-document.addEventListener(
-  "scroll",
-  (scrollEvent) => {
-    const scrollEventCopy = new scrollEvent.constructor(scrollEvent.type, {
-      bubbles: false,
-      cancelable: scrollEvent.cancelable,
-      composed: scrollEvent.composed,
-    });
-    document.documentElement.dispatchEvent(scrollEventCopy);
-  },
-  { passive: true },
-);
