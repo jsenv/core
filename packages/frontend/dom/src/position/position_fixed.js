aa1facc52e9b21eee560255b6c9be388676bc3aa
dmail <dmaillard06@gmail.com>
2025-10-20 17:22:47 +0200

    work

--- packages/frontend/dom/src/position/position_fixed.js
+++ packages/frontend/dom/src/position/position_fixed.js
@@ -1,12 +1,15 @@
-export const findAncestorFixedPosition = (element) => {
-  let current = element.parentElement;
-  while (current && current !== document.documentElement) {
+export const findSelfOrAncestorFixedPosition = (element) => {
+  let current = element;
+  while (true) {
     const computedStyle = window.getComputedStyle(current);
     if (computedStyle.position === "fixed") {
       const { left, top } = current.getBoundingClientRect();
       return [left, top];
     }
     current = current.parentElement;
+    if (!current || current === document.documentElement) {
+      break;
+    }
   }
   return null;
 };
