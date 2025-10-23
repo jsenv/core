24551675dd6dadb7bb4fbdc2b9dc9822670c5675
dmail <dmaillard06@gmail.com>
2025-10-23 15:11:43 +0200

    prepare navi build

---
+++ packages/frontend/navi/scripts/build.mjs
@@ -0,0 +1,23 @@
+import { build } from "@jsenv/core";
+
+await build({
+  sourceDirectoryUrl: import.meta.resolve("../"),
+  buildDirectoryUrl: import.meta.resolve("../dist/"),
+  entryPoints: {
+    "./index.js": {
+      buildRelativeUrl: "./jsenv_dom.js",
+      runtimeCompat: {
+        chrome: "64",
+        edge: "79",
+        firefox: "67",
+        safari: "11.3",
+      },
+      minification: false,
+      preserveComments: true, // JSDoc comments are useful when people use the built file as reference
+      versioning: false,
+      ignore: {
+        "file://**/node_modules/": true,
+      },
+    },
+  },
+});
