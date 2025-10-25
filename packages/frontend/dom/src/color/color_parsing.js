1d391c6fe117a74b41e88cbecd122576fea1ab1f
dmail <dmaillard06@gmail.com>
2025-10-25 06:15:40 +0200

    work

--- packages/frontend/dom/src/color/color_parsing.js
+++ packages/frontend/dom/src/color/color_parsing.js
@@ -115,6 +115,7 @@ export const stringifyCSSColor = (rgba) => {
 };
 
 const namedColors = {
+  // Basic colors
   black: [0, 0, 0],
   white: [255, 255, 255],
   red: [255, 0, 0],
@@ -123,9 +124,21 @@ const namedColors = {
   yellow: [255, 255, 0],
   cyan: [0, 255, 255],
   magenta: [255, 0, 255],
+
+  // Gray variations
   silver: [192, 192, 192],
   gray: [128, 128, 128],
   grey: [128, 128, 128],
+  darkgray: [169, 169, 169],
+  darkgrey: [169, 169, 169],
+  lightgray: [211, 211, 211],
+  lightgrey: [211, 211, 211],
+  dimgray: [105, 105, 105],
+  dimgrey: [105, 105, 105],
+  gainsboro: [220, 220, 220],
+  whitesmoke: [245, 245, 245],
+
+  // Extended basic colors
   maroon: [128, 0, 0],
   olive: [128, 128, 0],
   lime: [0, 255, 0],
@@ -134,6 +147,133 @@ const namedColors = {
   navy: [0, 0, 128],
   fuchsia: [255, 0, 255],
   purple: [128, 0, 128],
+
+  // Red variations
+  darkred: [139, 0, 0],
+  firebrick: [178, 34, 34],
+  crimson: [220, 20, 60],
+  indianred: [205, 92, 92],
+  lightcoral: [240, 128, 128],
+  salmon: [250, 128, 114],
+  darksalmon: [233, 150, 122],
+  lightsalmon: [255, 160, 122],
+
+  // Pink variations
+  pink: [255, 192, 203],
+  lightpink: [255, 182, 193],
+  hotpink: [255, 105, 180],
+  deeppink: [255, 20, 147],
+  mediumvioletred: [199, 21, 133],
+  palevioletred: [219, 112, 147],
+
+  // Orange variations
+  orange: [255, 165, 0],
+  darkorange: [255, 140, 0],
+  orangered: [255, 69, 0],
+  tomato: [255, 99, 71],
+  coral: [255, 127, 80],
+
+  // Yellow variations
+  gold: [255, 215, 0],
+  lightyellow: [255, 255, 224],
+  lemonchiffon: [255, 250, 205],
+  lightgoldenrodyellow: [250, 250, 210],
+  papayawhip: [255, 239, 213],
+  moccasin: [255, 228, 181],
+  peachpuff: [255, 218, 185],
+  palegoldenrod: [238, 232, 170],
+  khaki: [240, 230, 140],
+  darkkhaki: [189, 183, 107],
+
+  // Green variations
+  darkgreen: [0, 100, 0],
+  forestgreen: [34, 139, 34],
+  seagreen: [46, 139, 87],
+  mediumseagreen: [60, 179, 113],
+  springgreen: [0, 255, 127],
+  mediumspringgreen: [0, 250, 154],
+  lawngreen: [124, 252, 0],
+  chartreuse: [127, 255, 0],
+  greenyellow: [173, 255, 47],
+  limegreen: [50, 205, 50],
+  palegreen: [152, 251, 152],
+  lightgreen: [144, 238, 144],
+  mediumaquamarine: [102, 205, 170],
+  aquamarine: [127, 255, 212],
+  darkolivegreen: [85, 107, 47],
+  olivedrab: [107, 142, 35],
+  yellowgreen: [154, 205, 50],
+
+  // Blue variations
+  darkblue: [0, 0, 139],
+  mediumblue: [0, 0, 205],
+  royalblue: [65, 105, 225],
+  steelblue: [70, 130, 180],
+  dodgerblue: [30, 144, 255],
+  deepskyblue: [0, 191, 255],
+  skyblue: [135, 206, 235],
+  lightskyblue: [135, 206, 250],
+  lightblue: [173, 216, 230],
+  powderblue: [176, 224, 230],
+  lightcyan: [224, 255, 255],
+  paleturquoise: [175, 238, 238],
+  darkturquoise: [0, 206, 209],
+  mediumturquoise: [72, 209, 204],
+  turquoise: [64, 224, 208],
+  cadetblue: [95, 158, 160],
+  darkcyan: [0, 139, 139],
+  lightseagreen: [32, 178, 170],
+
+  // Purple variations
+  indigo: [75, 0, 130],
+  darkviolet: [148, 0, 211],
+  blueviolet: [138, 43, 226],
+  mediumpurple: [147, 112, 219],
+  mediumslateblue: [123, 104, 238],
+  slateblue: [106, 90, 205],
+  darkslateblue: [72, 61, 139],
+  lavender: [230, 230, 250],
+  thistle: [216, 191, 216],
+  plum: [221, 160, 221],
+  violet: [238, 130, 238],
+  orchid: [218, 112, 214],
+  mediumorchid: [186, 85, 211],
+  darkorchid: [153, 50, 204],
+  darkmagenta: [139, 0, 139],
+
+  // Brown variations
+  brown: [165, 42, 42],
+  saddlebrown: [139, 69, 19],
+  sienna: [160, 82, 45],
+  chocolate: [210, 105, 30],
+  darkgoldenrod: [184, 134, 11],
+  peru: [205, 133, 63],
+  rosybrown: [188, 143, 143],
+  goldenrod: [218, 165, 32],
+  sandybrown: [244, 164, 96],
+  tan: [210, 180, 140],
+  burlywood: [222, 184, 135],
+  wheat: [245, 222, 179],
+  navajowhite: [255, 222, 173],
+  bisque: [255, 228, 196],
+  blanchedalmond: [255, 235, 205],
+  cornsilk: [255, 248, 220],
+
+  // Special colors
+  transparent: [0, 0, 0], // Note: alpha will be 0 for transparent
+  aliceblue: [240, 248, 255],
+  antiquewhite: [250, 235, 215],
+  azure: [240, 255, 255],
+  beige: [245, 245, 220],
+  honeydew: [240, 255, 240],
+  ivory: [255, 255, 240],
+  lavenderblush: [255, 240, 245],
+  linen: [250, 240, 230],
+  mintcream: [245, 255, 250],
+  mistyrose: [255, 228, 225],
+  oldlace: [253, 245, 230],
+  seashell: [255, 245, 238],
+  snow: [255, 250, 250],
 };
 
 /**
