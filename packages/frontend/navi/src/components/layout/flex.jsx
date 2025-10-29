cfdaf160feb3121a4819748fe74c12f7a9fb6f7f
dmail <dmaillard06@gmail.com>
2025-10-29 15:17:05 +0100

    work

--- packages/frontend/navi/src/components/layout/flex.jsx
+++ packages/frontend/navi/src/components/layout/flex.jsx
@@ -1,3 +1,6 @@
+import { createContext } from "preact";
+import { useContext } from "preact/hooks";
+
 import { withPropsStyle } from "../props_composition/with_props_style.js";
 import { consumeSpacingProps } from "./spacing.jsx";
 
@@ -15,7 +18,13 @@ import.meta.css = /* css */ `
     align-items: center;
     gap: 0;
   }
+
+  .navi_flex_item {
+    flex-shrink: 0;
+  }
 `;
+
+const FlexDirectionContext = createContext();
 
 export const FlexRow = ({ alignY, gap, style, children, ...rest }) => {
   const innerStyle = withPropsStyle(
@@ -28,12 +37,13 @@ export const FlexRow = ({ alignY, gap, style, children, ...rest }) => {
   );
 
   return (
-    <div className="navi_flex_row" style={innerStyle}>
-      {children}
+    <div {...rest} className="navi_flex_row" style={innerStyle}>
+      <FlexDirectionContext.Provider value="row">
+        {children}
+      </FlexDirectionContext.Provider>
     </div>
   );
 };
-
 export const FlexColumn = ({ alignX, gap, style, children, ...rest }) => {
   const innerStyle = withPropsStyle(
     {
@@ -45,7 +55,56 @@ export const FlexColumn = ({ alignX, gap, style, children, ...rest }) => {
   );
 
   return (
-    <div className="navi_flex_column" style={innerStyle}>
+    <div {...rest} className="navi_flex_column" style={innerStyle}>
+      <FlexDirectionContext.Provider value="column">
+        {children}
+      </FlexDirectionContext.Provider>
+    </div>
+  );
+};
+export const useConsumAlignProps = (props) => {
+  const flexDirection = useContext(FlexDirectionContext);
+
+  const alignX = props.alignX;
+  const alignY = props.alignY;
+  delete props.alignX;
+  delete props.alignY;
+
+  const style = {};
+  if (alignX !== "start") {
+  }
+
+  return style;
+};
+export const FlexItem = ({
+  alignX,
+  alignY,
+  grow,
+  shrink,
+  style,
+  children,
+  ...rest
+}) => {
+  const flexDirection = useContext(FlexDirectionContext);
+  if (!flexDirection) {
+    console.warn(
+      "FlexItem must be used within a FlexRow or FlexColumn component.",
+    );
+  }
+
+  const alignStyle = useConsumAlignProps({ alignX, alignY });
+  const innerStyle = withPropsStyle(
+    {
+      flexGrow: grow ? 1 : undefined,
+      flexShrink: shrink ? 1 : undefined,
+      ...consumeSpacingProps(rest),
+      ...alignStyle,
+    },
+    style,
+  );
+
+  return (
+    <div {...rest} className="navi_flex_item" style={innerStyle}>
       {children}
     </div>
   );
--- packages/frontend/navi/src/components/layout/spacing.jsx
+++ packages/frontend/navi/src/components/layout/spacing.jsx
@@ -1,77 +1,88 @@
 import { withPropsStyle } from "../props_composition/with_props_style.js";
 
 export const consumeSpacingProps = (props) => {
-  const consume = (name) => {
-    if (Object.hasOwn(props, name)) {
-      const value = props[name];
-      delete props[name];
-      return value;
-    }
-    return undefined;
-  };
-  const margin = consume("margin");
-  const marginX = consume("marginX");
-  const marginY = consume("marginY");
-  const marginLeft = consume("marginLeft");
-  const marginRight = consume("marginRight");
-  const marginTop = consume("marginTop");
-  const marginBottom = consume("marginBottom");
-
   const style = {};
-  if (margin !== undefined) {
-    style.margin = margin;
-  }
-  if (marginLeft !== undefined) {
-    style.marginLeft = marginLeft;
-  } else if (marginX !== undefined) {
-    style.marginLeft = marginX;
-  }
-  if (marginRight !== undefined) {
-    style.marginRight = marginRight;
-  } else if (marginX !== undefined) {
-    style.marginRight = marginX;
-  }
-  if (marginTop !== undefined) {
-    style.marginTop = marginTop;
-  } else if (marginY !== undefined) {
-    style.marginTop = marginY;
-  }
-  if (marginBottom !== undefined) {
-    style.marginBottom = marginBottom;
-  } else if (marginY !== undefined) {
-    style.marginBottom = marginY;
+
+  outer_spacing: {
+    const margin = props.margin;
+    const marginX = props.marginX;
+    const marginY = props.marginY;
+    const marginLeft = props.marginLeft;
+    const marginRight = props.marginRight;
+    const marginTop = props.marginTop;
+    const marginBottom = props.marginBottom;
+    delete props.margin;
+    delete props.marginX;
+    delete props.marginY;
+    delete props.marginLeft;
+    delete props.marginRight;
+    delete props.marginTop;
+    delete props.marginBottom;
+
+    if (margin !== undefined) {
+      style.margin = margin;
+    }
+    if (marginLeft !== undefined) {
+      style.marginLeft = marginLeft;
+    } else if (marginX !== undefined) {
+      style.marginLeft = marginX;
+    }
+    if (marginRight !== undefined) {
+      style.marginRight = marginRight;
+    } else if (marginX !== undefined) {
+      style.marginRight = marginX;
+    }
+    if (marginTop !== undefined) {
+      style.marginTop = marginTop;
+    } else if (marginY !== undefined) {
+      style.marginTop = marginY;
+    }
+    if (marginBottom !== undefined) {
+      style.marginBottom = marginBottom;
+    } else if (marginY !== undefined) {
+      style.marginBottom = marginY;
+    }
   }
 
-  const padding = consume("padding");
-  const paddingX = consume("paddingX");
-  const paddingY = consume("paddingY");
-  const paddingLeft = consume("paddingLeft");
-  const paddingRight = consume("paddingRight");
-  const paddingTop = consume("paddingTop");
-  const paddingBottom = consume("paddingBottom");
+  inner_spacing: {
+    const padding = props.padding;
+    const paddingX = props.paddingX;
+    const paddingY = props.paddingY;
+    const paddingLeft = props.paddingLeft;
+    const paddingRight = props.paddingRight;
+    const paddingTop = props.paddingTop;
+    const paddingBottom = props.paddingBottom;
+    delete props.padding;
+    delete props.paddingX;
+    delete props.paddingY;
+    delete props.paddingLeft;
+    delete props.paddingRight;
+    delete props.paddingTop;
+    delete props.paddingBottom;
 
-  if (padding !== undefined) {
-    style.padding = padding;
-  }
-  if (paddingLeft !== undefined) {
-    style.paddingLeft = paddingLeft;
-  } else if (paddingX !== undefined) {
-    style.paddingLeft = paddingX;
-  }
-  if (paddingRight !== undefined) {
-    style.paddingRight = paddingRight;
-  } else if (paddingX !== undefined) {
-    style.paddingRight = paddingX;
-  }
-  if (paddingTop !== undefined) {
-    style.paddingTop = paddingTop;
-  } else if (paddingY !== undefined) {
-    style.paddingTop = paddingY;
-  }
-  if (paddingBottom !== undefined) {
-    style.paddingBottom = paddingBottom;
-  } else if (paddingY !== undefined) {
-    style.paddingBottom = paddingY;
+    if (padding !== undefined) {
+      style.padding = padding;
+    }
+    if (paddingLeft !== undefined) {
+      style.paddingLeft = paddingLeft;
+    } else if (paddingX !== undefined) {
+      style.paddingLeft = paddingX;
+    }
+    if (paddingRight !== undefined) {
+      style.paddingRight = paddingRight;
+    } else if (paddingX !== undefined) {
+      style.paddingRight = paddingX;
+    }
+    if (paddingTop !== undefined) {
+      style.paddingTop = paddingTop;
+    } else if (paddingY !== undefined) {
+      style.paddingTop = paddingY;
+    }
+    if (paddingBottom !== undefined) {
+      style.paddingBottom = paddingBottom;
+    } else if (paddingY !== undefined) {
+      style.paddingBottom = paddingY;
+    }
   }
 
   return style;
