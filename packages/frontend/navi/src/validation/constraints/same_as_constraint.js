b11f73a70a4f1c406a9de76f42ded7a878dbe104
dmail <dmaillard06@gmail.com>
2025-10-30 15:16:33 +0100

    work

---
+++ packages/frontend/navi/src/validation/constraints/same_as_constraint.js
@@ -0,0 +1,42 @@
+export const SAME_AS_CONSTRAINT = {
+  name: "same_as",
+  check: (element) => {
+    const sameAs = element.getAttribute("data-same-as");
+    if (!sameAs) {
+      return null;
+    }
+
+    const otherElement = document.querySelector(sameAs);
+    if (!otherElement) {
+      console.warn(
+        `Same as constraint: could not find element for selector ${sameAs}`,
+      );
+      return null;
+    }
+
+    const value = element.value;
+    const otherValue = otherElement.value;
+    if (value === "" || otherValue === "") {
+      // don't validate if one of the two values is empty
+      return null;
+    }
+
+    if (value === otherValue) {
+      return null;
+    }
+
+    const message = element.getAttribute("data-same-as-message");
+    if (message) {
+      return message;
+    }
+
+    const type = element.type;
+    if (type === "password") {
+      return `Ce mot de passe doit être identique au précédent.`;
+    }
+    if (type === "email") {
+      return `Cette adresse e-mail doit être identique a la précédente.`;
+    }
+    return `Ce champ doit être identique au champ précédent.`;
+  },
+};
