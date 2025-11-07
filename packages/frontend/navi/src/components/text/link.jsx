970016180322ca83aa5a969d9fabe7048e3adf19
dmail <dmaillard06@gmail.com>
2025-11-07 10:25:58 +0100

    work

--- packages/frontend/navi/src/components/text/link.jsx
+++ packages/frontend/navi/src/components/text/link.jsx
@@ -344,8 +344,18 @@ const EmailSvg = () => {
       xmlns="http://www.w3.org/2000/svg"
     >
       <path
-        d="M4 4h16c1.1 0 2 .9 2 2v12c0 1.1-.9 2-2 2H4c-1.1 0-2-.9-2-2V6c0-1.1.9-2 2-2zm16 4l-8 5-8-5V6l8 5 8-5v2z"
-        fill="currentColor"
+        d="M4 4h16c1.1 0 2 .9 2 2v12c0 1.1-.9 2-2 2H4c-1.1 0-2-.9-2-2V6c0-1.1.9-2 2-2z"
+        fill="none"
+        stroke="currentColor"
+        stroke-width="2"
+      />
+      <path
+        d="m2 6 8 5 2 1.5 2-1.5 8-5"
+        fill="none"
+        stroke="currentColor"
+        stroke-width="2"
+        stroke-linecap="round"
+        stroke-linejoin="round"
       />
     </svg>
   );
