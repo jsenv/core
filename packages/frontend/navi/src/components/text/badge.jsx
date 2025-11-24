26e43eaf7fdecb3e3132d121dfeb1061193751c1
dmail <dmaillard06@gmail.com>
2025-11-24 18:50:20 +0100

    work

--- packages/frontend/navi/src/components/text/badge.jsx
+++ packages/frontend/navi/src/components/text/badge.jsx
@@ -7,7 +7,9 @@ import.meta.css = /* css */ `
   @layer navi {
   }
   .navi_badge_count {
+    --x-size: 1.5em;
     --x-border-radius: var(--border-radius, 1em);
+    --x-font-size: var(--font-size, inherit);
 
     display: inline-flex;
     box-sizing: border-box;
@@ -17,9 +19,12 @@ import.meta.css = /* css */ `
     border-radius: var(--x-border-radius);
   }
   .navi_badge_count_visual {
-    width: 1lh;
-    height: 1lh;
-    font-size: var(--font-size, inherit);
+    display: inline-flex;
+    width: var(--x-size);
+    height: var(--x-size);
+    align-items: inherit;
+    justify-content: inherit;
+    font-size: var(--x-font-size);
     text-align: center;
     background: var(--background);
     background-color: var(--background-color, var(--background));
@@ -32,12 +37,15 @@ import.meta.css = /* css */ `
   }
 
   .navi_badge_count[data-single-digit] {
-    --x-size: 1lh;
     --x-border-radius: 100%;
   }
-  .navi_badge_count[data-two-digits] {
-    --x-size: 1lh;
+  .navi_badge_count[data-two-digits] .navi_badge_count_text {
+    font-size: 0.8em;
     --x-border-radius: 100%;
+  }
+  .navi_badge_count[data-two-digits][data-value-overflow]
+    .navi_badge_count_text {
+    font-size: 0.65em;
   }
   .navi_badge_count[data-indeterminate-digits] .navi_badge_count_visual {
     width: auto;
@@ -72,41 +80,13 @@ export const BadgeCount = ({
 }) => {
   const defaultRef = useRef();
   const ref = props.ref || defaultRef;
+  useContrastingColor(ref, ".navi_badge_count_visual");
 
-  const numericValue =
+  const valueRequested =
     typeof children === "string" ? parseInt(children, 10) : children;
-  // Calculer la valeur à afficher en fonction du paramètre max
-  const getDisplayValue = () => {
-    if (
-      max === undefined ||
-      max === Infinity ||
-      max === false ||
-      max === "false" ||
-      max === "Infinity" ||
-      max === "none"
-    ) {
-      return children;
-    }
-
-    const numericMax = typeof max === "string" ? parseInt(max, 10) : max;
-    if (isNaN(numericValue) || isNaN(numericMax)) {
-      return children;
-    }
-    if (numericValue > numericMax) {
-      return (
-        <>
-          {numericMax}
-          {maxElement}
-        </>
-      );
-    }
-    return children;
-  };
-
-  const displayValue = getDisplayValue();
-  useContrastingColor(ref);
-
-  const digitCount = String(numericValue).length;
+  const valueDisplayed = applyMaxToValue(max, valueRequested);
+  const digitCount = String(valueDisplayed).length;
+  const hasOverflow = valueDisplayed !== valueRequested;
 
   return (
     <Text
@@ -115,6 +95,7 @@ export const BadgeCount = ({
       bold
       data-single-digit={digitCount === 1 ? "" : undefined}
       data-two-digits={digitCount === 2 ? "" : undefined}
+      data-value-overflow={hasOverflow ? "" : undefined}
       data-indeterminate-digits={digitCount > 2 ? "" : undefined}
       {...props}
       styleCSSVars={BadgeStyleCSSVars}
@@ -125,8 +106,36 @@ export const BadgeCount = ({
       {/* This also forces .navi_badge_count to take 1 line-height even if .navi_badge_count_visual font-size differs  */}
       <span style="user-select: none">&#8203;</span>
       {/* Force element to take 1 line-height */}
-      <span className="navi_badge_count_visual">{displayValue}</span>
+      <span className="navi_badge_count_visual">
+        <span className="navi_badge_count_text">
+          {valueDisplayed}
+          {hasOverflow && maxElement}
+        </span>
+      </span>
       <span style="user-select: none">&#8203;</span>
     </Text>
   );
+};
+const applyMaxToValue = (max, value) => {
+  if (isNaN(value)) {
+    return value;
+  }
+  if (
+    max === undefined ||
+    max === Infinity ||
+    max === false ||
+    max === "false" ||
+    max === "Infinity" ||
+    max === "none"
+  ) {
+    return value;
+  }
+  const numericMax = typeof max === "string" ? parseInt(max, 10) : max;
+  if (isNaN(numericMax)) {
+    return value;
+  }
+  if (value > numericMax) {
+    return numericMax;
+  }
+  return value;
 };
--- packages/frontend/navi/src/components/text/use_contrasting_color.js
+++ packages/frontend/navi/src/components/text/use_contrasting_color.js
@@ -3,15 +3,22 @@ import { useLayoutEffect } from "preact/hooks";
 
 const CSS_VAR_NAME = "--x-color-contrasting";
 
-export const useContrastingColor = (ref) => {
+export const useContrastingColor = (ref, backgroundElementSelector) => {
   useLayoutEffect(() => {
     const el = ref.current;
     if (!el) {
       return;
     }
+    let elementToCheck = el;
+    if (backgroundElementSelector) {
+      elementToCheck = el.querySelector(backgroundElementSelector);
+      if (!elementToCheck) {
+        return;
+      }
+    }
     const lightColor = "var(--navi-color-light)";
     const darkColor = "var(--navi-color-dark)";
-    const backgroundColor = getComputedStyle(el).backgroundColor;
+    const backgroundColor = getComputedStyle(elementToCheck).backgroundColor;
     if (!backgroundColor) {
       el.style.removeProperty(CSS_VAR_NAME);
       return;
