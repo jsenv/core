29a70b163a4ca047ede21b5305e163b1b594eb3f
dmail <dmaillard06@gmail.com>
2025-11-25 18:28:49 +0100

    Make link anchor more agnostic to where it's inserted

--- packages/frontend/navi/src/components/text/link_anchor.jsx
+++ packages/frontend/navi/src/components/text/link_anchor.jsx
@@ -1,32 +1,42 @@
 import { Link } from "./link.jsx";
 
 import.meta.css = /* css */ `
-  .navi_title > .navi_link[data-link-anchor] {
+  .navi_link_anchor {
     position: absolute !important;
-    top: 50%;
+    top: 1em;
     left: -1em;
     width: 1em;
     height: 1em;
-    font-size: 0.7em;
+    font-size: 1em;
     opacity: 0;
-    transform: translateY(-50%);
+    transform: translateY(-25%);
   }
 
-  .navi_title > .navi_link[data-link-anchor]:focus,
-  .navi_title > .navi_link[data-link-anchor]:focus-visible,
-  .navi_title:hover > .navi_link[data-link-anchor] {
+  .navi_title .navi_link_anchor {
+    font-size: 0.7em;
+  }
+
+  .navi_link_anchor:focus,
+  .navi_link_anchor:focus-visible,
+  *:hover > .navi_link_anchor {
     opacity: 1;
   }
-
   /* The anchor link is displayed only on :hover */
   /* So we "need" a visual indicator when it's shown by focus */
   /* (even if it's focused by mouse aka not :focus-visible) */
   /* otherwise we might wonder why we see this UI element */
-  .navi_title > .navi_link[data-link-anchor][data-focus] {
+  .navi_link_anchor[data-focus] {
     outline-width: 2px;
   }
 `;
 
 export const LinkAnchor = (props) => {
-  return <Link color="inherit" id={props.href.slice(1)} {...props} />;
+  return (
+    <Link
+      className="navi_link_anchor"
+      color="inherit"
+      id={props.href.slice(1)}
+      {...props}
+    />
+  );
 };
