9062323146fa65de210474e7addb40e276c24205
dmail <dmaillard06@gmail.com>
2025-12-02 15:14:27 +0100

    work

--- packages/frontend/navi/src/nav/build_route_relative_url.test.js
+++ packages/frontend/navi/src/nav/build_route_relative_url.test.js
@@ -1,133 +1,104 @@
-import { assert } from "@jsenv/assert";
+import { snapshotTests } from "@jsenv/snapshot";
 import {
   buildRouteRelativeUrl,
   rawUrlPart,
 } from "./build_route_relative_url.js";
 
-// Test basic optional wildcard removal
-{
-  const { relativeUrl } = buildRouteRelativeUrl("/api/users/*?");
-  assert({
-    actual: relativeUrl,
-    expect: "/api/users",
-  });
-}
+const run = (urlPattern, params) => {
+  return buildRouteRelativeUrl(urlPattern, params).relativeUrl;
+};
 
-// Test complex optional group removal
-{
-  const { relativeUrl } = buildRouteRelativeUrl("/map/isochrone{/time/*}?");
-  assert({
-    actual: relativeUrl,
-    expect: "/map/isochrone",
+await snapshotTests(import.meta.url, ({ test }) => {
+  test("optional parts removal", () => {
+    return {
+      optional_wildcard: run("/api/users/*?"),
+      complex_optional_group: run("/map/isochrone{/time/*}?"),
+      optional_parameter: run("/users/:id?"),
+      optional_curly_parameter: run("/posts/{id}?"),
+      multiple_optional_parts: run("/api/users/:id?/posts/*?"),
+      optional_group_no_params: run("/map/isochrone{/time/:duration}?"),
+    };
   });
-}
 
-// Test optional parameter removal
-{
-  const { relativeUrl } = buildRouteRelativeUrl("/users/:id?");
-  assert({
-    actual: relativeUrl,
-    expect: "/users",
+  test("parameter replacement", () => {
+    return {
+      basic_parameters: run("/users/:id/posts/:postId?", {
+        id: "123",
+        postId: "456",
+      }),
+      optional_with_value: run("/users/:id?", {
+        id: "123",
+      }),
+      wildcard_parameter: run("/files/*", {
+        0: "documents/readme.txt",
+      }),
+      optional_group_with_params: run("/map/isochrone{/time/:duration}?", {
+        time: "15",
+        duration: "minutes",
+      }),
+    };
   });
-}
 
-// Test optional curly brace parameter removal
-{
-  const { relativeUrl } = buildRouteRelativeUrl("/posts/{id}?");
-  assert({
-    actual: relativeUrl,
-    expect: "/posts",
+  test("raw url parts", () => {
+    return {
+      raw_wildcard: run("/files/*", {
+        0: rawUrlPart("documents/readme.txt"),
+      }),
+      raw_with_special_chars: run("/path/*", {
+        0: rawUrlPart("special chars & symbols"),
+      }),
+    };
   });
-}
 
-// Test multiple optional parts
-{
-  const { relativeUrl } = buildRouteRelativeUrl("/api/users/:id?/posts/*?");
-  assert({
-    actual: relativeUrl,
-    expect: "/api/users",
+  test("extra parameters", () => {
+    return {
+      as_search_params: run("/api/users", {
+        page: "2",
+        limit: "10",
+      }),
+      mixed_with_path_params: run("/users/:id", {
+        id: "123",
+        sort: "name",
+        order: "asc",
+      }),
+      empty_params: run("/api/endpoint", {}),
+      undefined_values: run("/api/test", {
+        defined: "value",
+        undefined,
+        null: null,
+      }),
+    };
   });
-}
 
-// Test with parameters that should be replaced
-{
-  const { relativeUrl } = buildRouteRelativeUrl("/users/:id/posts/:postId?", {
-    id: "123",
-    postId: "456",
+  test("edge cases", () => {
+    return {
+      empty_pattern: run(""),
+      root_pattern: run("/"),
+      no_params: run("/api/users"),
+      trailing_wildcard: run("/files/*"),
+      multiple_wildcards: run("/*/files/*", {
+        0: "folder1",
+        1: "file.txt",
+      }),
+      special_characters: run("/users/:id", {
+        id: "user@domain.com",
+      }),
+    };
   });
-  assert({
-    actual: relativeUrl,
-    expect: "/users/123/posts/456",
-  });
-}
 
-// Test optional part not removed when parameter is provided
-{
-  const { relativeUrl } = buildRouteRelativeUrl("/users/:id?", {
-    id: "123",
+  test("complex patterns", () => {
+    return {
+      nested_optional_groups: run("/api{/v1{/users/:id}?}?", {
+        id: "123",
+      }),
+      mixed_parameter_types: run("/api/:version/{resource}/*?", {
+        version: "v1",
+        resource: "users",
+        0: "search",
+      }),
+      url_encoding: run("/search/*", {
+        0: "query with spaces & symbols",
+      }),
+    };
   });
-  assert({
-    actual: relativeUrl,
-    expect: "/users/123",
-  });
-}
-
-// Test wildcard with parameter
-{
-  const { relativeUrl } = buildRouteRelativeUrl("/files/*", {
-    0: "documents/readme.txt",
-  });
-  assert({
-    actual: relativeUrl,
-    expect: "/files/documents%2Freadme.txt",
-  });
-}
-
-// Test rawUrlPart bypass encoding
-{
-  const { relativeUrl } = buildRouteRelativeUrl("/files/*", {
-    0: rawUrlPart("documents/readme.txt"),
-  });
-  assert({
-    actual: relativeUrl,
-    expect: "/files/documents/readme.txt",
-  });
-}
-
-// Test extra parameters as search params
-{
-  const { relativeUrl } = buildRouteRelativeUrl("/api/users", {
-    page: "2",
-    limit: "10",
-  });
-  assert({
-    actual: relativeUrl,
-    expect: "/api/users?page=2&limit=10",
-  });
-}
-
-// Test complex case: optional group with parameters
-{
-  const { relativeUrl } = buildRouteRelativeUrl(
-    "/map/isochrone{/time/:duration}?",
-    {
-      time: "15",
-      duration: "minutes",
-    },
-  );
-  assert({
-    actual: relativeUrl,
-    expect: "/map/isochrone/15/minutes",
-  });
-}
-
-// Test optional group removal when no matching params
-{
-  const { relativeUrl } = buildRouteRelativeUrl(
-    "/map/isochrone{/time/:duration}?",
-  );
-  assert({
-    actual: relativeUrl,
-    expect: "/map/isochrone",
-  });
-}
+});
