30546f2b039b26d58dfdb88ca9e45fd76e296d0f
dmail <dmaillard06@gmail.com>
2025-12-03 18:19:55 +0100

    work

--- packages/frontend/navi/src/utils/compare_two_js_values.js
+++ packages/frontend/navi/src/utils/compare_two_js_values.js
@@ -90,83 +90,93 @@
  */
 export const SYMBOL_IDENTITY = Symbol.for("navi_object_identity");
 
-export const compareTwoJsValues = (a, b, seenSet = new Set()) => {
-  if (a === b) {
-    return true;
-  }
-  const aIsIsTruthy = Boolean(a);
-  const bIsTruthy = Boolean(b);
-  if (aIsIsTruthy && !bIsTruthy) {
-    return false;
-  }
-  if (!aIsIsTruthy && !bIsTruthy) {
-    // null, undefined, 0, false, NaN
-    if (isNaN(a) && isNaN(b)) {
+export const compareTwoJsValues = (rootA, rootB, { keyComparator } = {}) => {
+  const seenSet = new Set();
+  const compare = (a, b) => {
+    if (a === b) {
       return true;
     }
-    return a === b;
-  }
-  const aType = typeof a;
-  const bType = typeof b;
-  if (aType !== bType) {
-    return false;
-  }
-  const aIsPrimitive =
-    a === null || (aType !== "object" && aType !== "function");
-  const bIsPrimitive =
-    b === null || (bType !== "object" && bType !== "function");
-  if (aIsPrimitive !== bIsPrimitive) {
-    return false;
-  }
-  if (aIsPrimitive && bIsPrimitive) {
-    return a === b;
-  }
-  if (seenSet.has(a)) {
-    return false;
-  }
-  if (seenSet.has(b)) {
-    return false;
-  }
-  seenSet.add(a);
-  seenSet.add(b);
-  const aIsArray = Array.isArray(a);
-  const bIsArray = Array.isArray(b);
-  if (aIsArray !== bIsArray) {
-    return false;
-  }
-  if (aIsArray) {
-    // compare arrays
-    if (a.length !== b.length) {
+    const aIsIsTruthy = Boolean(a);
+    const bIsTruthy = Boolean(b);
+    if (aIsIsTruthy && !bIsTruthy) {
       return false;
     }
-    let i = 0;
-    while (i < a.length) {
-      const aValue = a[i];
-      const bValue = b[i];
-      if (!compareTwoJsValues(aValue, bValue, seenSet)) {
+    if (!aIsIsTruthy && !bIsTruthy) {
+      // null, undefined, 0, false, NaN
+      if (isNaN(a) && isNaN(b)) {
+        return true;
+      }
+      return a === b;
+    }
+    const aType = typeof a;
+    const bType = typeof b;
+    if (aType !== bType) {
+      return false;
+    }
+    const aIsPrimitive =
+      a === null || (aType !== "object" && aType !== "function");
+    const bIsPrimitive =
+      b === null || (bType !== "object" && bType !== "function");
+    if (aIsPrimitive !== bIsPrimitive) {
+      return false;
+    }
+    if (aIsPrimitive && bIsPrimitive) {
+      return a === b;
+    }
+    if (seenSet.has(a)) {
+      return false;
+    }
+    if (seenSet.has(b)) {
+      return false;
+    }
+    seenSet.add(a);
+    seenSet.add(b);
+    const aIsArray = Array.isArray(a);
+    const bIsArray = Array.isArray(b);
+    if (aIsArray !== bIsArray) {
+      return false;
+    }
+    if (aIsArray) {
+      // compare arrays
+      if (a.length !== b.length) {
         return false;
       }
-      i++;
+      let i = 0;
+      while (i < a.length) {
+        const aValue = a[i];
+        const bValue = b[i];
+        const comparator = keyComparator || compare;
+        if (!comparator(aValue, bValue, i, compare)) {
+          return false;
+        }
+        i++;
+      }
+      return true;
     }
-    return true;
-  }
-  // compare objects
-  const aIdentity = a[SYMBOL_IDENTITY];
-  const bIdentity = b[SYMBOL_IDENTITY];
-  if (aIdentity === bIdentity && SYMBOL_IDENTITY in a && SYMBOL_IDENTITY in b) {
-    return true;
-  }
-  const aKeys = Object.keys(a);
-  const bKeys = Object.keys(b);
-  if (aKeys.length !== bKeys.length) {
-    return false;
-  }
-  for (const key of aKeys) {
-    const aValue = a[key];
-    const bValue = b[key];
-    if (!compareTwoJsValues(aValue, bValue, seenSet)) {
+    // compare objects
+    const aIdentity = a[SYMBOL_IDENTITY];
+    const bIdentity = b[SYMBOL_IDENTITY];
+    if (
+      aIdentity === bIdentity &&
+      SYMBOL_IDENTITY in a &&
+      SYMBOL_IDENTITY in b
+    ) {
+      return true;
+    }
+    const aKeys = Object.keys(a);
+    const bKeys = Object.keys(b);
+    if (aKeys.length !== bKeys.length) {
       return false;
     }
-  }
-  return true;
+    for (const key of aKeys) {
+      const aValue = a[key];
+      const bValue = b[key];
+      const comparator = keyComparator || compare;
+      if (!comparator(aValue, bValue, key, compare)) {
+        return false;
+      }
+    }
+    return true;
+  };
+  return compare(rootA, rootB);
 };
