1a84e6c18b67e9314426932218d1e32157e344e1
dmail <dmaillard06@gmail.com>
2025-12-03 15:25:19 +0100

    work

---
+++ packages/frontend/navi/src/nav/demos/root_route/root_route_demo.jsx
@@ -0,0 +1,42 @@
+import { render } from "preact";
+
+import { Route, Routes, setupRoutes, TabList } from "@jsenv/navi";
+
+const { HOME_ROUTE, OTHER_ROUTE } = setupRoutes({
+  HOME_ROUTE: "/",
+  OTHER_ROUTE: "/other",
+});
+
+const App = () => {
+  return (
+    <div
+      style={{
+        fontFamily: "Arial, sans-serif",
+        maxWidth: "800px",
+        margin: "0 auto",
+        padding: "20px",
+      }}
+    >
+      <div
+        style={{
+          border: "1px solid #ddd",
+          borderRadius: "8px",
+          padding: "20px",
+          marginTop: "20px",
+        }}
+      >
+        <TabList>
+          <TabList.Tab route={HOME_ROUTE}>Home</TabList.Tab>
+          <TabList.Tab route={OTHER_ROUTE}>Other</TabList.Tab>
+        </TabList>
+
+        <Routes>
+          <Route route={HOME_ROUTE} element={"Home"} />
+          <Route route={OTHER_ROUTE} element={"Other"} />
+        </Routes>
+      </div>
+    </div>
+  );
+};
+
+render(<App />, document.getElementById("app"));
