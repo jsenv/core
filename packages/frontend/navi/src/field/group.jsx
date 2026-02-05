cfb70c7561bb3f2d314c9ff0d5ebfc9ed54d77bc
dmail <dmaillard06@gmail.com>
2026-02-05 09:57:05 +0100

    work

--- packages/frontend/navi/src/field/group.jsx
+++ packages/frontend/navi/src/field/group.jsx
@@ -2,27 +2,38 @@ import { Box } from "../box/box.jsx";
 
 import.meta.css = /* css */ `
   .navi_group {
-    /* First child: no bottom border radius */
-    > *:first-child:not(:only-child) {
-      border-top-right-radius: 0 !important;
-      border-bottom-right-radius: 0 !important;
+    /* Horizontal (default): First child loses right radius, last child loses left radius */
+    &:not([data-vertical]) {
+      > *:first-child:not(:only-child) {
+        border-top-right-radius: 0 !important;
+        border-bottom-right-radius: 0 !important;
+      }
+
+      > *:last-child:not(:only-child) {
+        border-top-left-radius: 0 !important;
+        border-bottom-left-radius: 0 !important;
+      }
+
+      > *:not(:first-child):not(:last-child) {
+        border-radius: 0 !important;
+      }
     }
 
-    /* Last child: no top border radius */
-    > *:last-child:not(:only-child) {
-      border-top-left-radius: 0 !important;
-      border-bottom-left-radius: 0 !important;
-    }
-
-    /* Middle children: no border radius */
-    > *:not(:first-child):not(:last-child) {
-      border-right: none !important;
-      border-left: none !important;
-      border-radius: 0 !important;
-    }
-
+    /* Vertical: First child loses bottom radius, last child loses top radius */
     &[data-vertical] {
-      /* TODO: vertical stuff */
+      > *:first-child:not(:only-child) {
+        border-top-left-radius: revert !important;
+        border-top-right-radius: revert !important;
+        border-bottom-right-radius: 0 !important;
+        border-bottom-left-radius: 0 !important;
+      }
+
+      > *:last-child:not(:only-child) {
+        border-top-left-radius: 0 !important;
+        border-top-right-radius: 0 !important;
+        border-bottom-right-radius: revert !important;
+        border-bottom-left-radius: revert !important;
+      }
     }
   }
 `;
