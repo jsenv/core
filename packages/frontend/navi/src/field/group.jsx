a61105c0bb1f9fe9a53600ae6b610e188e51b3c5
dmail <dmaillard06@gmail.com>
2026-02-05 09:49:42 +0100

    work

--- packages/frontend/navi/src/field/group.jsx
+++ packages/frontend/navi/src/field/group.jsx
@@ -3,32 +3,38 @@ import { Box } from "../box/box.jsx";
 import.meta.css = /* css */ `
   .navi_group {
     /* First child: no bottom border radius */
-    > *:first-child {
+    > *:first-child:not(:only-child) {
+      border-top-right-radius: 0 !important;
       border-bottom-right-radius: 0 !important;
-      border-bottom-left-radius: 0 !important;
     }
 
     /* Last child: no top border radius */
-    > *:last-child {
+    > *:last-child:not(:only-child) {
       border-top-left-radius: 0 !important;
-      border-top-right-radius: 0 !important;
+      border-bottom-left-radius: 0 !important;
     }
 
     /* Middle children: no border radius */
-    > * {
+    > *:not(:first-child):not(:last-child) {
+      border-right: none !important;
+      border-left: none !important;
       border-radius: 0 !important;
     }
 
-    /* Single child: restore original border radius */
-    > *:only-child {
-      border-radius: revert !important;
+    &[data-vertical] {
+      /* TODO: vertical stuff */
     }
   }
 `;
 
-export const Group = ({ children, ...props }) => {
+export const Group = ({ children, row, vertical = row, ...props }) => {
   return (
-    <Box baseClassName="navi_group" {...props}>
+    <Box
+      baseClassName="navi_group"
+      data-vertical={vertical ? "" : undefined}
+      row={row}
+      {...props}
+    >
       {children}
     </Box>
   );
