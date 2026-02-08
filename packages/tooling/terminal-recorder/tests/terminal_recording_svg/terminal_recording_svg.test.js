0132166890e8ad9c06696f469d8bd8dcc8701453
dmail <dmaillard06@gmail.com>
2026-02-08 09:45:39 +0100

    revert palywright for now

--- packages/tooling/terminal-recorder/tests/terminal_recording_svg/terminal_recording_svg.test.js
+++ packages/tooling/terminal-recorder/tests/terminal_recording_svg/terminal_recording_svg.test.js
@@ -3,9 +3,10 @@ import { startTerminalRecording } from "@jsenv/terminal-recorder";
 import { readFileSync, writeFileSync } from "node:fs";
 
 if (
-  process.platform === "win32" ||
-  // fail since update of macos, see https://github.com/microsoft/playwright/issues/30585
-  process.platform === "darwin"
+  process.env.CI &&
+  (process.platform === "win32" ||
+    // fail since update of macos, see https://github.com/microsoft/playwright/issues/30585
+    process.platform === "darwin")
 ) {
   process.exit(0);
 }
