2bfea0ee86f678ff80ebfe356364d3f807b4a9d6
dmail <dmaillard06@gmail.com>
2026-02-24 16:58:25 +0100

    Work

--- packages/frontend/navi/src/field/input_textual.jsx
+++ packages/frontend/navi/src/field/input_textual.jsx
@@ -27,6 +27,7 @@ import { PSEUDO_CLASSES } from "../box/pseudo_styles.js";
 import { Icon } from "../graphic/icon.jsx";
 import { CloseSvg } from "../graphic/icons/close_svg.jsx";
 import { EmailSvg } from "../graphic/icons/email_svg.jsx";
+import { PhoneSvg } from "../graphic/icons/phone_svg.jsx";
 import { SearchSvg } from "../graphic/icons/search_svg.jsx";
 import { useStableCallback } from "../utils/use_stable_callback.js";
 import { ReportReadOnlyOnLabelContext } from "./label.jsx";
@@ -407,6 +408,8 @@ const InputTextualBasic = (props) => {
       innerIcon = <SearchSvg />;
     } else if (type === "email") {
       innerIcon = <EmailSvg />;
+    } else if (type === "tel") {
+      innerIcon = <PhoneSvg />;
     }
   } else {
     innerIcon = icon;
--- packages/frontend/navi/src/nav/link.jsx
+++ packages/frontend/navi/src/nav/link.jsx
@@ -18,6 +18,7 @@ import {
   LinkAnchorSvg,
   LinkBlankTargetSvg,
 } from "../graphic/icons/link_svgs.jsx";
+import { PhoneSvg } from "../graphic/icons/phone_svg.jsx";
 import { useKeyboardShortcuts } from "../keyboard/keyboard_shortcuts.js";
 import { applySpacingOnTextChildren } from "../text/text.jsx";
 import { TitleLevelContext } from "../text/title.jsx";
@@ -367,17 +368,6 @@ const LinkPlain = (props) => {
         <Icon marginLeft={innerChildren ? "xxs" : undefined}>{innerIcon}</Icon>
       )}
     </Box>
-  );
-};
-
-const PhoneSvg = () => {
-  return (
-    <svg viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-      <path
-        d="M6.62 10.79c1.44 2.83 3.76 5.14 6.59 6.59l2.2-2.2c.27-.27.67-.36 1.02-.24 1.12.37 2.33.57 3.57.57.55 0 1 .45 1 1V20c0 .55-.45 1-1 1-9.39 0-17-7.61-17-17 0-.55.45-1 1-1h3.5c.55 0 1 .45 1 1 0 1.25.2 2.45.57 3.57.11.35.03.74-.25 1.02l-2.2 2.2z"
-        fill="currentColor"
-      />
-    </svg>
   );
 };
 
