724f7f3a9879cf319168e38bd721fb2be7736b0f
dmail <dmaillard06@gmail.com>
2026-03-07 14:48:25 +0100

    work

--- packages/related/plugin-database-manager/src/client/routes.js
+++ packages/related/plugin-database-manager/src/client/routes.js
@@ -19,6 +19,18 @@ export const rolnameSignal = stateSignal(null);
 export const datnameSignal = stateSignal(null);
 export const tablenameSignal = stateSignal(null);
 
+// TODO: roleCanLoginOpenSignal controls ROLE_CAN_LOGIN.GET_MANY
+// so we need this logic somehow
+// we likely can groupe that with the search param debounce concept where a search param
+// controls an action but with a debouncing logic
+// as a result it means the details element should actually only controls the roleCanLoginOpenSignal
+// and not run the action itself
+// however we still want to connect the details with the state of the action
+// so we likely have a missing concept here
+// where a UI element can be bound to an action but would actuall not trigger the action per se
+// because in the end actions are core things that can often/always be triggered outside UI
+// so UI is just an other way of triggering an action but should be able to catch if anything else does
+
 export const {
   ROLE_ROUTE,
   DATABASE_ROUTE,
