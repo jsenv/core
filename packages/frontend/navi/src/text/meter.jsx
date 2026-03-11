f08a1176a7dcd47a8fb3407fa27c3cc1b799fba5
dmail <dmaillard06@gmail.com>
2026-03-11 09:50:48 +0100

    work

--- packages/frontend/navi/src/text/meter.jsx
+++ packages/frontend/navi/src/text/meter.jsx
@@ -49,18 +49,11 @@ import.meta.css = /* css */ `
 
     &[data-loading] {
       .navi_meter_track {
-        background-image: linear-gradient(
-          90deg,
-          var(--track-color) 25%,
-          color-mix(in srgb, var(--track-color) 55%, white) 50%,
-          var(--track-color) 75%
-        );
-        background-size: 200% 100%;
-        background-color: var(--track-color);
-        animation: navi_meter_loading 1.5s linear infinite;
-
         .navi_meter_fill {
-          display: none;
+          width: 35%;
+          background-color: var(--fill-color-optimum);
+          background-image: none;
+          animation: navi_meter_loading 1.5s ease-in-out infinite;
         }
       }
     }
@@ -68,10 +61,13 @@ import.meta.css = /* css */ `
 
   @keyframes navi_meter_loading {
     0% {
-      background-position: 200% 0;
+      transform: translateX(-150%);
+    }
+    65% {
+      transform: translateX(320%);
     }
     100% {
-      background-position: -200% 0;
+      transform: translateX(320%);
     }
   }
 `;
