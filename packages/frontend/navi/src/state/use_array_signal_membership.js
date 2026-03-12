d22eaeb0377de00c29cc7438be9c030dfc7d66be
dmail <dmaillard06@gmail.com>
2026-03-12 16:35:45 +0100

    work

--- packages/frontend/navi/src/state/use_array_signal_membership.js
+++ packages/frontend/navi/src/state/use_array_signal_membership.js
@@ -2,7 +2,13 @@ import { useCallback } from "preact/hooks";
 
 import { addIntoArray, removeFromArray } from "../utils/array_add_remove.js";
 
-export const useArraySignalMembership = (arraySignal, id) => {
+export const useArraySignalMembership = (...args) => {
+  if (args.length < 2) {
+    throw new Error(
+      "useArraySignalMembership requires at least 2 arguments: [arraySignal, id]",
+    );
+  }
+  const [arraySignal, id] = args;
   const array = arraySignal.value;
   const isMember = array.includes(id);
 
@@ -10,13 +16,13 @@ export const useArraySignalMembership = (arraySignal, id) => {
     const arrayWithId = addIntoArray(arraySignal.peek(), id);
     arraySignal.value = arrayWithId;
     return arrayWithId;
-  }, []);
+  }, [id]);
 
   const remove = useCallback(() => {
     const arrayWithoutId = removeFromArray(arraySignal.peek(), id);
     arraySignal.value = arrayWithoutId;
     return arrayWithoutId;
-  }, []);
+  }, [id]);
 
   return [isMember, add, remove];
 };
