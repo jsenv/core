f9fa5bef3309bceeae850aade44e774afb0b51a8
dmail <dmaillard06@gmail.com>
2026-04-11 17:46:29 +0200

    work

--- src/plugins/import_meta_css/client/import_meta_css_dev.js
+++ src/plugins/import_meta_css/client/import_meta_css_dev.js
@@ -1,58 +1,51 @@
 export const installImportMetaCssDev = (importMeta) => {
-  let currentCssSource;
-  let cssText = "";
-  let stylesheet;
-  let adopted = false;
-
-  const css = {
-    toString: () => cssText,
-    update: (value) => {
-      if (currentCssSource === value) {
-        return;
-      }
-      currentCssSource = value;
-      cssText = value;
-      cssText += `
+  // useless today but browser might catch up to display it in devtools
+  const addUrlInfo = (cssText) => {
+    let cssTextWithUrlInfo = cssText;
+    cssTextWithUrlInfo += `
 /* sourceURL=${importMeta.url} */
 /* inlined from ${importMeta.url} */`;
-      if (currentCssSource === undefined) {
-        stylesheet = new CSSStyleSheet({ baseUrl: importMeta.url });
-        stylesheet.replaceSync(cssText);
-        css.inject();
-      } else {
-        stylesheet.replaceSync(cssText);
-      }
-    },
-    inject: () => {
-      if (adopted) {
-        return;
-      }
-      document.adoptedStyleSheets = [
-        ...document.adoptedStyleSheets,
-        stylesheet,
-      ];
-      adopted = true;
-    },
-    remove: () => {
-      if (!adopted) {
-        return;
-      }
-      document.adoptedStyleSheets = document.adoptedStyleSheets.filter(
-        (s) => s !== stylesheet,
-      );
-      adopted = false;
-    },
+    return cssTextWithUrlInfo;
   };
 
+  let stylesheet;
+  const adopt = (value) => {
+    stylesheet = new CSSStyleSheet({ baseUrl: importMeta.url });
+    stylesheet.replaceSync(addUrlInfo(value));
+    document.adoptedStyleSheets = [...document.adoptedStyleSheets, stylesheet];
+  };
+  const update = (value) => {
+    stylesheet.replaceSync(addUrlInfo(value));
+  };
+  const remove = () => {
+    document.adoptedStyleSheets = document.adoptedStyleSheets.filter(
+      (s) => s !== stylesheet,
+    );
+  };
+
+  let currentCssSource;
+  let adopted = false;
   Object.defineProperty(importMeta, "css", {
     configurable: true,
     get() {
-      return css;
+      return undefined;
     },
     set(value) {
-      css.update(value);
+      if (value === undefined) {
+        if (adopted) {
+          remove();
+          adopted = false;
+          currentCssSource = undefined;
+        }
+        return;
+      }
+      if (!adopted) {
+        adopt(value);
+        currentCssSource = value;
+      } else if (currentCssSource !== value) {
+        update(value);
+        currentCssSource = value;
+      }
     },
   });
-
-  return css.remove;
 };
