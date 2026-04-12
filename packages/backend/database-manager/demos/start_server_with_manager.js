4d8c124a2048df731e6228229d6a8fb24ec20023
dmail <dmaillard06@gmail.com>
2026-04-12 15:54:57 +0200

    work

--- packages/backend/database-manager/demos/start_server_with_manager.js
+++ packages/backend/database-manager/demos/start_server_with_manager.js
@@ -1,6 +1,13 @@
 import { serverPluginDatabaseManager } from "@jsenv/database-manager";
-import { startServer } from "@jsenv/server";
+import { fetchFileSystem, startServer } from "@jsenv/server";
 
 await startServer({
+  port: 8976,
   plugins: [serverPluginDatabaseManager()],
+  routes: [
+    {
+      endpoint: "GET /",
+      fetch: fetchFileSystem(import.meta.resolve("./")),
+    },
+  ],
 });
