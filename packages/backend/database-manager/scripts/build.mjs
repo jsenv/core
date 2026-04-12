cee291b1a8d6b9ebe629c19f08d159c8aede92e0
dmail <dmaillard06@gmail.com>
2026-04-12 15:57:26 +0200

    work

--- packages/backend/database-manager/scripts/build.mjs
+++ packages/backend/database-manager/scripts/build.mjs
@@ -5,16 +5,13 @@ await build({
   sourceDirectoryUrl: import.meta.resolve("../src/"),
   buildDirectoryUrl: import.meta.resolve("../dist/"),
   entryPoints: {
-    "./jsenv_plugin_database_manager.js": {
-      runtimeCompat: {
-        node: "16.2.0",
-        chrome: "64",
-        edge: "79",
-        firefox: "67",
-        safari: "11.3",
-      },
-      minification: false,
-      versioning: false,
+    "./index.js": {
+      buildRelativeUrl: "./jsenv_database_manager.js",
+      runtimeCompat: { node: "20.0" },
+    },
+    "./client/database_manager.html": {
+      buildRelativeUrl: "./client/database_manager.html",
+      runtimeCompat: { chrome: "89" },
       plugins: [jsenvPluginPreact()],
     },
   },
