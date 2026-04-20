3e30498e1e725b64978ff4a07fe06c66e285f364
dmail <dmaillard06@gmail.com>
2026-04-20 14:33:06 +0200

    work

--- packages/backend/server/src/plugins/cookies/server_plugin_cookies.js
+++ packages/backend/server/src/plugins/cookies/server_plugin_cookies.js
@@ -4,27 +4,14 @@ export const serverPluginCookies = () => {
   return {
     name: "jsenv:cookies",
     augmentRouteFetchSecondArg: (request, fetchSecondArg) => {
-      const requestCookieMap = parseRequestCookieHeader(
-        request.headers["cookie"],
-      );
-      const responseCookieMap = new Map();
-
-      const cookies = {
-        get: (name) => {
-          if (responseCookieMap.has(name)) {
-            return responseCookieMap.get(name);
-          }
-          return requestCookieMap.get(name);
-        },
+      const responseCookies = {
         set: (name, value, options = {}) => {
-          responseCookieMap.set(name, value);
           fetchSecondArg.injectResponseHeader(
             "set-cookie",
             COOKIE.stringify(COOKIE.from(name, value, options)),
           );
         },
         delete: (name, options = {}) => {
-          responseCookieMap.delete(name);
           fetchSecondArg.injectResponseHeader(
             "set-cookie",
             COOKIE.stringify(
@@ -37,24 +24,7 @@ export const serverPluginCookies = () => {
         },
       };
 
-      return { cookies };
+      return { responseCookies };
     },
   };
-};
-
-const parseRequestCookieHeader = (cookieHeader) => {
-  const map = new Map();
-  if (!cookieHeader) {
-    return map;
-  }
-  for (const pair of cookieHeader.split("; ")) {
-    const eqIndex = pair.indexOf("=");
-    if (eqIndex === -1) {
-      continue;
-    }
-    const name = pair.slice(0, eqIndex).trim();
-    const value = decodeURIComponent(pair.slice(eqIndex + 1));
-    map.set(name, value);
-  }
-  return map;
 };
