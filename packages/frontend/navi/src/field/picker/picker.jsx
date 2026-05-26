cfce625524430dfb9f97bacad33e3b98fe7ae3cd
dmail <dmaillard06@gmail.com>
2026-05-26 19:04:02 +0200

    work

--- packages/frontend/navi/src/field/picker/picker.jsx
+++ packages/frontend/navi/src/field/picker/picker.jsx
@@ -350,11 +350,8 @@ const PickerButton = (props) => {
       icon={undefined}
       ui={undefined}
       onnavi_get_managed_fields={(e) => {
-        // we must check for the pickerEl content to search for a valid input because we might be a button used to validate for instance
-        // no necessarily the field itself
-        const pickerEl = e.currentTarget;
-        const managedField = getPickerManagedField(pickerEl);
-        e.detail.respondWith(managedField);
+        const pickerInput = e.currentTarget;
+        e.detail.respondWith(pickerInput);
       }}
     >
       <LoadingOutline
@@ -370,6 +367,22 @@ const PickerButton = (props) => {
         // eslint-disable-next-line react/no-children-prop
         children={undefined} // we will render children into the button
         id={undefined}
+        onnavi_get_managed_fields={(e) => {
+          // we must check for the pickerEl content to search for a valid input because we might be a button used to validate for instance
+          // no necessarily the field itself
+          const pickerInput = e.currentTarget;
+          let firstField;
+          let sibling = pickerInput.nextElementSibling;
+          while (sibling) {
+            const candidate = findFieldWithName(sibling);
+            if (candidate) {
+              firstField = candidate;
+              return firstField;
+            }
+            sibling = sibling.nextElementSibling;
+          }
+          e.detail.respondWith(sibling);
+        }}
       />
 
       <span className="navi_picker_right_slot">
@@ -421,20 +434,6 @@ const PickerInputPseudoClasses = [
   ":-navi-expanded",
 ];
 
-const getPickerManagedField = (pickerEl) => {
-  let pickerInput = pickerEl.querySelector(".navi_picker_input");
-  let firstField;
-  let sibling = pickerInput.nextElementSibling;
-  while (sibling) {
-    const candidate = findFieldWithName(sibling);
-    if (candidate) {
-      firstField = candidate;
-      return firstField;
-    }
-    sibling = sibling.nextElementSibling;
-  }
-  return null;
-};
 const findFieldWithName = (el) => {
   const tag = el.tagName.toLowerCase();
   if (
