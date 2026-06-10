b1bbda9b29d0f51f893b13331036af8cea383516
dmail <dmaillard06@gmail.com>
2026-06-10 06:20:23 +0200

    work

--- packages/tooling/humanize/src/_tests/time/duration.test.js
+++ packages/tooling/humanize/src/_tests/time/duration.test.js
@@ -12,8 +12,12 @@ await snapshotTests(import.meta.url, ({ test }) => {
       0.1, 1.02, 1.52, 52, 55, 99, 999, 1_421, 61_421, 3_601_200, 7_651_200,
     ];
     const grid = [
-      [cell("input (ms)"), cell("output")],
-      ...cases.map((ms) => [cell(String(ms)), cell(humanizeDuration(ms))]),
+      [cell("input (ms)"), cell("output"), cell("output (short)")],
+      ...cases.map((ms) => [
+        cell(String(ms)),
+        cell(humanizeDuration(ms)),
+        cell(humanizeDuration(ms, { short: true })),
+      ]),
     ];
     return renderTable(grid, { borderCollapse: true });
   });
