b5fc88b1b72cd04417eb27f17dfb70793f1b684a
dmail <dmaillard06@gmail.com>
2026-06-10 07:13:44 +0200

    work

--- packages/tooling/validity/src/rules.js
+++ packages/tooling/validity/src/rules.js
@@ -1,5 +1,26 @@
 import { TYPES } from "./types.js";
 
+export const TYPE_RULE = {
+  id: "type",
+  applyOn: (type, value) => {
+    const actualType = typeof value;
+    let message;
+    const typeDef = TYPES[type];
+    if (typeDef?.validate) {
+      message = typeDef.validate(value);
+    } else if (actualType !== type) {
+      message = `must be a ${type}, got ${actualType}`;
+    }
+    if (!message) {
+      return null;
+    }
+    const convertValue = typeDef?.convert?.[actualType];
+    return {
+      message,
+      autoFix: convertValue ? () => convertValue(value) : null,
+    };
+  },
+};
 export const MIN_RULE = {
   id: "min",
   applyOn: (min, value, ruleConfig) => {
@@ -224,26 +245,4 @@ const formatTemporalBound = (value, type) => {
     return d.toLocaleString();
   }
   return String(value);
-};
-
-export const TYPE_RULE = {
-  id: "type",
-  applyOn: (type, value) => {
-    const actualType = typeof value;
-    let message;
-    const typeDef = TYPES[type];
-    if (typeDef?.validate) {
-      message = typeDef.validate(value);
-    } else if (actualType !== type) {
-      message = `must be a ${type}, got ${actualType}`;
-    }
-    if (!message) {
-      return null;
-    }
-    const convertValue = typeDef?.convert?.[actualType];
-    return {
-      message,
-      autoFix: convertValue ? () => convertValue(value) : null,
-    };
-  },
 };
