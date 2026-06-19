51ff861d7d3869ad7ee962e1009eac6f9e29829b
dmail <dmaillard06@gmail.com>
2026-06-19 12:41:26 +0200

    work

--- packages/frontend/navi/src/control/validation/constraints/single_space_constraint.js
+++ packages/frontend/navi/src/control/validation/constraints/single_space_constraint.js
@@ -15,16 +15,16 @@ export const SINGLE_SPACE_CONSTRAINT = {
     const hasLeadingSpace = valueAsString.startsWith(" ");
     const hasTrailingSpace = valueAsString.endsWith(" ");
     const hasDoubleSpace = valueAsString.includes("  ");
-    if (hasLeadingSpace || hasDoubleSpace || hasTrailingSpace) {
-      if (hasLeadingSpace) {
-        return naviI18n("constraint.single_space.start.default");
-      }
-      if (hasTrailingSpace) {
-        return naviI18n("constraint.single_space.end.default");
-      }
-      return naviI18n("constraint.single_space.consecutive.default");
+    if (!hasLeadingSpace && !hasTrailingSpace && !hasDoubleSpace) {
+      return null;
     }
-    return "";
+    if (hasLeadingSpace) {
+      return naviI18n("constraint.single_space.start.default");
+    }
+    if (hasTrailingSpace) {
+      return naviI18n("constraint.single_space.end.default");
+    }
+    return naviI18n("constraint.single_space.consecutive.default");
   },
 };
 CONSTRAINT_ATTRIBUTE_SET.add("data-single-space");
