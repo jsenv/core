e659e5f2f36eca506a6068ec27e0b11ac111282f
dmail <dmaillard06@gmail.com>
2026-06-20 16:13:23 +0200

    work

--- packages/frontend/navi/src/control/input/input_range.jsx
+++ packages/frontend/navi/src/control/input/input_range.jsx
@@ -116,7 +116,7 @@ const css = /* css */ `
     width: fit-content;
     height: var(--height);
     margin: 2px;
-    flex-direction: inherit;
+    flex-direction: row;
     align-items: center;
     /* Just for the outline, the real border radius of the range is fixed */
     font-size: var(--font-size);
