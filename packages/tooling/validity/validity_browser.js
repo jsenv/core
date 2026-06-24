a801161548bb16bb6b6175a00ab834b7a5ee14aa
dmail <dmaillard06@gmail.com>
2026-06-24 09:59:54 +0200

    work

--- packages/tooling/validity/validity_browser.js
+++ packages/tooling/validity/validity_browser.js
@@ -1,5 +1,6 @@
 export {
-  parseDurationComponents,
-  parseDurationToSeconds,
-} from "./src/types.js";
+  durationToSeconds,
+  durationToString,
+  parseDuration,
+} from "./src/duration.js";
 export { createValidity } from "./src/validity.js";
