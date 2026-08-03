da8643fc29aa05f3b32f087b71a1f5d6aa0811d0
dmail <dmaillard06@gmail.com>
2026-08-03 18:16:32 +0200

    work

---
+++ packages/frontend/navi/src/graphic/icons/pencil_svg.jsx
@@ -0,0 +1,20 @@
+export const PencilSvg = () => {
+  return (
+    <svg viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <path
+        d="M4 20H8L18.5 9.5C19.6046 8.39543 19.6046 6.60457 18.5 5.5C17.3954 4.39543 15.6046 4.39543 14.5 5.5L4 16V20Z"
+        stroke="currentColor"
+        stroke-width="2"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+      <path
+        d="M13.5 6.5L17.5 10.5"
+        stroke="currentColor"
+        stroke-width="2"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+    </svg>
+  );
+};
